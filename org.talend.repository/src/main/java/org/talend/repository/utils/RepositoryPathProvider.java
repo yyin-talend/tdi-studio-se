4087d67c1e2d619499f97d6efd345798e8f17546
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-10-03 08:18:33 +0000

    PathProvider
    git-svn-id: http://talendforge.org/svn/tos/trunk@51 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/utils/RepositoryPathProvider.java
@@ -0,0 +1,76 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.repository.utils;
+
+import org.eclipse.core.resources.IFolder;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.core.runtime.Path;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.utils.workbench.resources.ResourceUtils;
+import org.talend.core.CorePlugin;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.general.Project;
+import org.talend.repository.model.RepositoryConstants;
+import org.talend.repository.model.ResourceModelUtils;
+
+
+/**
+ * DOC amaumont  class global comment. Detailled comment
+ * <br/>
+ *
+ * $Id$
+ *
+ */
+public class RepositoryPathProvider {
+
+    public static IPath getPathRootProject() {
+        RepositoryContext repositoryContext = (RepositoryContext) CorePlugin.getContext().getProperty(Context.REPOSITORY_CONTEXT_KEY);
+        Project project = repositoryContext.getProject();
+        try {
+            IProject iProject = ResourceModelUtils.getProject(project);
+            return iProject.getFullPath();
+        } catch (PersistenceException e) {
+            return null;
+        }
+    }
+    
+    public static IPath getPathProjectFolder(String folderName) {
+        RepositoryContext repositoryContext = (RepositoryContext) CorePlugin.getContext().getProperty(Context.REPOSITORY_CONTEXT_KEY);
+        Project project = repositoryContext.getProject();
+        try {
+            IProject iProject = ResourceModelUtils.getProject(project);
+            IFolder folder = ResourceUtils.getFolder(iProject, folderName, true);
+            return folder.getLocation();
+        } catch (PersistenceException e) {
+            return null;
+        }
+
+    }
+    
+    public static IPath getPathFileName(String folderName, String fileName) {
+        IPath pathProjectFolder = getPathProjectFolder(folderName);
+        return pathProjectFolder.append(fileName);
+    }
+    
+}
