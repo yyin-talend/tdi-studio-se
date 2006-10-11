e2299bf81466f6d1a0bd1a8aa290ae06a0f4b608
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-10-11 14:44:46 +0000

    removed interface Container from properties.ecore
    git-svn-id: http://talendforge.org/svn/tos/trunk@168 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/FolderHelper.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/FolderHelper.java
@@ -31,7 +31,6 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.emf.common.util.EList;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.util.EcoreUtil;
-import org.talend.core.model.properties.Container;
 import org.talend.core.model.properties.FolderItem;
 import org.talend.core.model.properties.FolderType;
 import org.talend.core.model.properties.Item;
@@ -39,64 +38,63 @@ import org.talend.core.model.properties.Project;
 import org.talend.core.model.properties.PropertiesFactory;
 import org.talend.core.model.properties.Property;
 
-
 /**
  * This helper class contains a set of methods to perform basic operations on FolderItem objects.
  * 
- * DOC tguiu  class global comment. Detailled comment
- * <br/>
- *
+ * DOC tguiu class global comment. Detailled comment <br/>
+ * 
  * $Id$
- *
+ * 
  */
 public class FolderHelper {
-    
+
     private Project project;
-    
-    
+
     /**
      * DOC tguiu FolderHelper constructor comment.
+     * 
      * @param project
      */
     private FolderHelper(Project project) {
         this.project = project;
     }
-    
+
     public static FolderHelper createInstance(org.talend.core.model.general.Project project) {
         return createInstance(project.getEmfProject());
     }
 
-    public static FolderHelper createInstance(Project project)
-    {
+    public static FolderHelper createInstance(Project project) {
         return new FolderHelper(project);
     }
-    
-    public void createSystemFolder (IPath path)
-    {
+
+    public void createSystemFolder(IPath path) {
         doCreateFolder(path, FolderType.SYSTEM_FOLDER_LITERAL);
     }
-    public void createFolder (String path)
-    {
+
+    public void createFolder(String path) {
         createFolder(new Path(path));
     }
-    public void createFolder (IPath path)
-    {
+
+    public void createFolder(IPath path) {
         doCreateFolder(path, FolderType.FOLDER_LITERAL);
     }
-    private void doCreateFolder (IPath path, FolderType type)
-    {
+
+    private void doCreateFolder(IPath path, FolderType type) {
         int segmentCount = path.segmentCount();
-        if (segmentCount < 1)
-            return ;
-        Container folder = project;
-        for (int i = 0; i < segmentCount; i++)
-        {
+        if (segmentCount < 1) {
+            return;
+        }
+        FolderItem folder = null;
+        for (int i = 0; i < segmentCount; i++) {
             String segment = path.segment(i);
-            FolderItem child = folder.findChildFolder(segment);
-            if (child == null)
-            {
+            FolderItem child = findChildFolder(folder, segment);
+            if (child == null) {
                 child = createFolder(project.eResource(), segment, type);
-                folder.getContent().add(child);
+                if (folder == null) {
+                    project.getFolders().add(child);
+                } else {
+                    folder.getChildren().add(child);
+                }
             }
             folder = child;
         }
@@ -104,9 +102,10 @@ public class FolderHelper {
 
     /**
      * DOC tguiu Comment method "createFolder".
-     * @param resource 
+     * 
+     * @param resource
      * @param name
-     * @param type 
+     * @param type
      * @return
      */
     private FolderItem createFolder(Resource resource, String name, FolderType type) {
@@ -121,124 +120,134 @@ public class FolderHelper {
         resource.setModified(true);
         return folder;
     }
-    
+
     public void deleteFolder(String completePath) {
         deleteFolder(new Path(completePath));
     }
-    
-    public void deleteFolder (IPath path)
-    {
+
+    public void deleteFolder(IPath path) {
         int segmentCount = path.segmentCount();
-        if (segmentCount < 1)
-            return ;
-        Container folder = project;
-        Container previousFolder = null;
-        for (int i = 1; i < segmentCount; i++)
-        {
+        if (segmentCount < 1) {
+            return;
+        }
+        FolderItem folder = null;
+        FolderItem previousFolder = null;
+        for (int i = 0; i < segmentCount; i++) {
             String segment = path.segment(i);
-            FolderItem child = folder.findChildFolder(segment);
-            if (child == null)
-            {
-                return ;
+            FolderItem child = findChildFolder(folder, segment);
+            if (child == null) {
+                return;
             }
             previousFolder = folder;
             folder = child;
         }
-        previousFolder.getContent().remove(folder);
+        if (previousFolder == null) {
+            project.getFolders().remove(folder);
+        } else {
+            previousFolder.getChildren().remove(folder);
+        }
         project.eResource().setModified(true);
         cleanResource(project.eResource(), folder);
     }
-    
+
     /**
      * DOC tguiu Comment method "cleanResource".
+     * 
      * @param resource
      * @param folder
      */
-    private void cleanResource(Resource resource, Container folder) {
-        resource.getContents().remove(((Item)folder).getProperty());
-        for (Object o : folder.getContent())
-            cleanResource(resource, (Container)o);
+    private void cleanResource(Resource resource, FolderItem folder) {
+        resource.getContents().remove(folder.getProperty());
+        for (Object o : folder.getChildren()) {
+            cleanResource(resource, (FolderItem) o);
+        }
     }
 
-    private static FolderItem find(List folders, String name)
-    {
-        for (Iterator it = folders.iterator(); it.hasNext();)
-        {
-            FolderItem folder = (FolderItem)it.next();
-            if (folder.getProperty().getLabel().equals(name))
+    private static FolderItem find(List folders, String name) {
+        for (Iterator it = folders.iterator(); it.hasNext();) {
+            FolderItem folder = (FolderItem) it.next();
+            if (folder.getProperty().getLabel().equals(name)) {
                 return folder;
+            }
         }
         return null;
     }
-    
-    public Set<IPath> listFolders ()
-    {
+
+    public Set<IPath> listFolders() {
         HashSet<IPath> folders = new HashSet<IPath>();
-        list(folders, project.getContent(), new Path(""));
+        list(folders, project.getFolders(), new Path(""));
         return folders;
     }
 
     /**
      * DOC tguiu Comment method "list".
+     * 
      * @param content
      * @param folders2
      */
     private void list(HashSet<IPath> collector, EList content, IPath currentPath) {
-        for (Object o : content)
-        {
+        for (Object o : content) {
             if (o instanceof FolderItem) {
-                FolderItem folder = (FolderItem)o;
+                FolderItem folder = (FolderItem) o;
                 IPath folderPath = currentPath.append(folder.getProperty().getLabel());
                 collector.add(folderPath);
                 list(collector, folder.getChildren(), folderPath);
-            }            
+            }
         }
     }
 
-    public Container getFolder(String pathStr) {
-         return getFolder(new Path(pathStr));
+    public FolderItem getFolder(String pathStr) {
+        return getFolder(new Path(pathStr));
     }
-    
-    public Container getFolder(IPath path) {
+
+    public FolderItem getFolder(IPath path) {
         int segmentCount = path.segmentCount();
-        Container folder = project;
-        for (int i = 0; i < segmentCount; i++)
-        {
+        FolderItem folder = null;
+        for (int i = 0; i < segmentCount; i++) {
             String segment = path.segment(i);
-            FolderItem child = folder.findChildFolder(segment);
-            if (child == null)
-            {
+            FolderItem child = findChildFolder(folder, segment);
+            if (child == null) {
                 return null;
             }
             folder = child;
         }
         return folder;
     }
-    
+
     public void moveFolder(String completeOldPath, String completeNewPath) {
-        Container folder = getFolder(completeOldPath);
-        if (folder == null)
-            return ;
-        Container destination = getFolder(new Path(completeNewPath).removeLastSegments(1));
-        if (destination == null)
-            return ;
-        destination.getContent().add(folder);
-        
-    }
-
-    /**
-     * DOC tguiu Comment method "renameFolder".
-     * @param completePath
-     * @param label
-     */
-    public void renameFolder(String completePath, String label) {
-        Container folder = getFolder(completePath);
-        if (folder != null && folder instanceof FolderItem)
-        {
-            ((FolderItem)folder).getProperty().setLabel(label);
+        FolderItem folder = getFolder(completeOldPath);
+        if (folder == null) {
+            return;
         }
-        
+        FolderItem destination = getFolder(new Path(completeNewPath).removeLastSegments(1));
+        if (destination == null) {
+            return;
+        }
+        destination.getChildren().add(folder);
+
     }
 
- 
+    public void renameFolder(String completePath, String label) {
+        FolderItem folder = getFolder(completePath);
+        if (folder != null) {
+            folder.getProperty().setLabel(label);
+        }
+    }
+
+    public FolderItem findChildFolder(FolderItem parent, String name) {
+        EList children;
+        if (parent != null) {
+            children = parent.getChildren();
+        } else {
+            children = project.getFolders();
+        }
+
+        for (Iterator it = children.iterator(); it.hasNext();) {
+            Item item = (Item) it.next();
+            if (item instanceof FolderItem && item.getProperty().getLabel().equals(name)) {
+                return (FolderItem) item;
+            }
+        }
+        return null;
+    }
 }
