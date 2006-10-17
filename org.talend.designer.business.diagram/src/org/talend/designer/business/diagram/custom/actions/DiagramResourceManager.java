b5b7bea1e54d3142e644f2929330e9bf702e0f4c
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-10-17 13:59:18 +0000

    *** empty log message ***
    git-svn-id: http://talendforge.org/svn/tos/trunk@187 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.business.diagram/src/org/talend/designer/business/diagram/custom/actions/DiagramResourceManager.java
+++ org.talend.designer.business.diagram/src/org/talend/designer/business/diagram/custom/actions/DiagramResourceManager.java
@@ -29,7 +29,6 @@ import org.eclipse.core.resources.IFile;
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.emf.common.util.URI;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.resource.impl.ResourceSetImpl;
@@ -96,6 +95,8 @@ public class DiagramResourceManager {
 
     public void updateResource(BusinessProcessItem businessProcessItem, IFile file) {
         Resource resource = createResource(file);
+
+        EcoreUtil.resolveAll(businessProcessItem.eResource());
 
         resource.getContents().clear();
         resource.getContents().add(businessProcessItem.getSemantic());
