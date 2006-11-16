6d2cea7d3b4db95c2ce8afbe7f33ed8b82821a72
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-11-16 15:38:38 +0000

    Modification: EImage move out of ImageProvider
    git-svn-id: http://talendforge.org/svn/tos/trunk@534 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/actions/CompareAction.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/actions/CompareAction.java
@@ -21,9 +21,25 @@
 // ============================================================================
 package org.talend.repository.ui.actions;
 
+import java.util.Collections;
+import java.util.Comparator;
+
+import org.eclipse.emf.common.util.EList;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.InputDialog;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Shell;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.CorePlugin;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.properties.FolderItem;
+import org.talend.core.ui.EImage;
 import org.talend.core.ui.ImageProvider;
-import org.talend.core.ui.ImageProvider.EImage;
+import org.talend.repository.model.IRepositoryFactory;
+import org.talend.repository.model.RepositoryFactoryProvider;
 
 /**
  * Action used to refresh a repository view.<br/>
@@ -41,5 +57,76 @@ public class CompareAction extends Action {
     }
 
     public void run() {
+        InputDialog id = new InputDialog(new Shell(), "Other project", "Enter other project name", "tagada", null);
+        if (id.open() == Window.OK) {
+            String projectName = id.getValue();
+            IRepositoryFactory rep = RepositoryFactoryProvider.getInstance();
+            Project toCompare = null;
+            try {
+                Project[] list = rep.readProject();
+                for (Project current : list) {
+                    if (current.getLabel().equals(projectName)) {
+                        toCompare = current;
+                    }
+                }
+            } catch (PersistenceException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            if (toCompare == null) {
+                MessageDialog.openError(new Shell(), "Project not found", "Project " + projectName + " cannot be found");
+                return;
+            }
+            RepositoryContext repositoryContext = (RepositoryContext) CorePlugin.getContext().getProperty(
+                    Context.REPOSITORY_CONTEXT_KEY);
+            Project currentProject = repositoryContext.getProject();
+            MessageDialog.openInformation(new Shell(), "Result", ""
+                    + new MyComparator().compare(toCompare.getEmfProject().getFolders(), currentProject.getEmfProject()
+                            .getFolders()));
+        }
+    }
+
+    /**
+     * 
+     * DOC smallet CompareAction class global comment. Detailled comment <br/>
+     * 
+     * $Id$
+     * 
+     */
+    private class MyComparator implements Comparator<EList> {
+
+        public int compare(EList a, EList b) {
+            Collections.sort(a, new FolderIdComparator());
+            Collections.sort(b, new FolderIdComparator());
+
+            for (Object c : a) {
+
+            }
+
+            return 0;
+        }
+
+    }
+
+    /**
+     * 
+     * DOC smallet CompareAction class global comment. Detailled comment <br/>
+     * 
+     * $Id$
+     * 
+     */
+    private class FolderIdComparator implements Comparator<Object> {
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.util.Comparator#compare(java.lang.Object, java.lang.Object)
+         */
+        public int compare(Object o1, Object o2) {
+            FolderItem a = (FolderItem) o1;
+            FolderItem b = (FolderItem) o2;
+            return a.getProperty().getId().compareTo(b.getProperty().getId());
+        }
+
     }
 }
