38c5ca471328b04eede5c2686991163219723724
phou <phou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-11-17 07:35:52 +0000

    Add SqlBuilderRepositoryObject for TreeNode color, image, isBuildIn...
    git-svn-id: http://talendforge.org/svn/tos/trunk@554 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/dbstructure/SqlBuilderRepositoryObject.java
@@ -0,0 +1,65 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License, or (at your option) any later version.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.sqlbuilder.dbstructure;
+
+import org.talend.core.model.properties.Property;
+import org.talend.core.model.repository.RepositoryObject;
+
+/**
+ * SqlBuilderRepositoryObject.
+ *
+ */
+public class SqlBuilderRepositoryObject extends RepositoryObject {
+	private String sourceName;
+	private String image;
+	private String color;
+	private boolean isBuildIn;
+	public SqlBuilderRepositoryObject(Property property) {
+		super(property);
+	}
+	
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public String getImage() {
+		return image;
+	}
+	public void setImage(String image) {
+		this.image = image;
+	}
+	public String getSourceName() {
+		return sourceName;
+	}
+	public void setSourceName(String sourceName) {
+		this.sourceName = sourceName;
+	}
+	public boolean isBuildIn() {
+		return isBuildIn;
+	}
+	public void setBuildIn(boolean isBuildIn) {
+		this.isBuildIn = isBuildIn;
+	}
+	
+}
--- org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/ui/DBStructureComposite.java
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/ui/DBStructureComposite.java
@@ -157,10 +157,10 @@ public class DBStructureComposite extends Composite {
     }
 
     public void openNewEditor() {
-        treeViewer.getTree().setSelection(treeViewer.getTree().getItem(0));
-        Action tempOpenNewEditorAction = new OpenNewEditorAction(treeViewer, builderDialog, builderDialog
-                .getConnParameters(), true);
-        tempOpenNewEditorAction.run();
+//        treeViewer.getTree().setSelection(treeViewer.getTree().getItem(0));
+//        Action tempOpenNewEditorAction = new OpenNewEditorAction(treeViewer, builderDialog, builderDialog
+//                .getConnParameters(), true);
+//        tempOpenNewEditorAction.run();
     }
 
     /**
