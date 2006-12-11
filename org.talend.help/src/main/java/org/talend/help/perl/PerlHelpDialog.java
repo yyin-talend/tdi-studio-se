904d26ffd3423c4a6b051c70c9a4f88c414bb630
tguiu <tguiu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2006-12-11 15:32:43 +0000

    git-svn-id: http://talendforge.org/svn/tos/trunk@950 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.help/src/main/java/org/talend/help/perl/PerlHelpDialog.java
+++ org.talend.help/src/main/java/org/talend/help/perl/PerlHelpDialog.java
@@ -23,6 +23,7 @@ package org.talend.help.perl;
 
 import org.eclipse.jface.dialogs.TitleAreaDialog;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -35,11 +36,24 @@ import org.talend.help.perl.ui.PerlHelpComposite;
  */
 public class PerlHelpDialog extends TitleAreaDialog {
 
+	private static Point size ;
+	private static Point location ;
+
 	public PerlHelpDialog(Shell parentShell) {
 		super(parentShell);
         setShellStyle(SWT.DIALOG_TRIM  | getDefaultOrientation() | SWT.RESIZE);
 	}
+	protected Point getInitialSize() {
+		if ( size != null)
+			return size;
+		return super.getInitialSize();
+	}
 
+	protected Point getInitialLocation(Point initialSize) {
+		if (location != null)
+			return location;
+		return super.getInitialLocation(initialSize);
+	}
 	protected Control createDialogArea(Composite parent) {
 		Composite composite = (Composite) super.createDialogArea(parent);
 		createDialogAreaContent(composite);
@@ -49,5 +63,11 @@ public class PerlHelpDialog extends TitleAreaDialog {
 	private void createDialogAreaContent(Composite parent) {
 		PerlHelpComposite perlHelpComposite = new PerlHelpComposite(parent, SWT.BORDER);
 		perlHelpComposite.setLayoutData(new GridData(GridData.FILL_BOTH));
+	}
+	
+	public boolean close() {
+		size = getShell().getSize();
+		location = getShell().getLocation();
+		return super.close();
 	}
 }
