677524ec1a07749b85fd3718d9e0436441279372
smallet <smallet@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-02-05 11:30:40 +0000

    Modification: switch log level in ComponentsFactory from DEBUG to TRACE  + add timer trace in CodeGeneratorEmittersPoolFactory
    git-svn-id: http://talendforge.org/svn/tos/trunk@1835 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/model/ComponentsFactory.java
+++ org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/model/ComponentsFactory.java
@@ -72,7 +72,7 @@ public class ComponentsFactory implements IComponentsFactory {
         // 3. Load user components:
         loadComponentsFromFolder(userPath);
 
-        log.debug(componentList.size() + " components loaded in " + (System.currentTimeMillis() - startTime) + " ms"); //$NON-NLS-1$ //$NON-NLS-2$
+        log.trace(componentList.size() + " components loaded in " + (System.currentTimeMillis() - startTime) + " ms");  //$NON-NLS-1$ //$NON-NLS-2$
     }
 
     private void loadComponentsFromFolder(String pathSource) {
