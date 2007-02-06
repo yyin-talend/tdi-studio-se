790825b735572dd8bb8e22a6361d97970980a887
bqian <bqian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-02-06 02:44:08 +0000

    Modification: Add the method of "getString(final String key, final Object... args)" to this class in order to internationalization.
    git-svn-id: http://talendforge.org/svn/tos/trunk@1856 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/Messages.java
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/Messages.java
@@ -21,22 +21,38 @@
 // ============================================================================
 package org.talend.sqlbuilder;
 
-import java.util.MissingResourceException;
 import java.util.ResourceBundle;
+import org.talend.commons.i18n.MessagesCore;
 
 /**
  * This class manages the string bundle.
  */
-public class Messages {
+public class Messages extends MessagesCore {
 
-    private static ResourceBundle resourceBundle = ResourceBundle.getBundle("messages"); //$NON-NLS-1$
+    private static final String BUNDLE_NAME = "messages"; //$NON-NLS-1$
 
-    public static String getString(String key) {
-        try {
-            return resourceBundle.getString(key);
-        } catch (MissingResourceException e) {
-            return '!' + key + '!';
-        }
+    private static ResourceBundle resourceBundle = ResourceBundle.getBundle(BUNDLE_NAME);
+
+    /**
+     * Returns the i18n formatted message for <i>key</i> in the class bundle.
+     * 
+     * @param key - the key for the desired string
+     * @return the string for the given key in the class resource bundle
+     * @see MessagesCore#getString(String, ResourceBundle)
+     */
+    public static String getString(final String key) {
+        return getString(key, resourceBundle);
     }
 
+    /**
+     * Returns the i18n formatted message for <i>key</i> and <i>args</i> in the specified bundle.
+     * 
+     * @param key - the key for the desired string
+     * @param args - arg to include in the string
+     * @return the string for the given key in the given resource bundle
+     * @see MessagesCore#getString(String, ResourceBundle, Object[])
+     */
+    public static String getString(final String key, final Object... args) {
+        return getString(key, resourceBundle, args);
+    }
 }
