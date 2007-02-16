d9d3f42df33efa34d42aec6535dc8bfa8b9fa7b2
plegall <plegall@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-02-16 16:00:09 +0000

    Feature 580 added: components tPostgreSQLInput, tPostgreSQLOutput, tPostgreSQLRow replace tDBInput, tDBOutput and tDBSQLRow for PostgreSQL. We don't use anymore the XML mapping between talend types and database types. Warning: the usual "update if insert fails" wasn't used because of transactions. In a transaction, no query must fail, so I had to implement a cache with all table keys.
    Feature 402 added for PostgreSQL: new table action "create table if not
    exists".
    
    Feature 210 added for PostgreSQL: transaction size configuration is
    possible.
    
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@2088 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.components.localprovider/components/tPostgresqlOutput/Postgresql.pm
@@ -0,0 +1,113 @@
+package tPostgresqlOutput::Postgresql;
+
+use Carp;
+
+sub getTableCreationQuery {
+    my %param = @_;
+
+    my %talendtype_to_dbtype = (
+        char    => 'CHAR',
+        Day     => 'DATE',
+        double  => 'DOUBLE PRECISION',
+        float   => 'REAL',
+        int     => 'INTEGER',
+        long    => 'BIGINT',
+        String  => 'VARCHAR',
+    );
+
+    # In $param{schema}, each column looks like this:
+    #
+    # {
+    #     name    => 'shop_code',
+    #     key     => true,
+    #     type    => 'int',
+    #     len     => null,
+    #     precision => null,
+    #     null    => false,
+    #     default => '',
+    #     comment => '',
+    # }
+
+    my $query;
+    my $column_num = 1;
+    my @key_names = map { $_->{name} } grep { $_->{key} } @{ $param{schema} };
+
+
+    # PostgreSQL creation table statement example
+    #
+    # CREATE TABLE sales_copy (
+    #   shop_code integer NOT NULL,
+    #   ean char(13) NOT NULL,
+    #   sales integer default NULL,
+    #   quantity integer default NULL
+    #   primary key(shop_code, ean)
+    # );
+    $query = 'CREATE TABLE '.$param{dbschema}.'.'.$param{tablename}.' ('."\n";
+
+    foreach my $column_href (@{ $param{schema} }) {
+        $column_href->{dbtype} = $talendtype_to_dbtype{$column_href->{type}};
+
+        if (lc $column_href->{type} eq 'string') {
+            if (not defined $column_href->{len}
+                or $column_href->{len} == -1) {
+                $column_href->{len} = 255;
+            }
+        }
+
+        if ($column_num++ > 1) {
+            $query.= ', ';
+        }
+
+        $query.= $column_href->{name};
+        $query.= ' '.$column_href->{dbtype};
+
+        if (grep /^$column_href->{type}$/, qw/String char float double/
+            and defined $column_href->{len}
+            and $column_href->{len} != -1
+        ) {
+            $query.= '(';
+            $query.= $column_href->{len};
+
+            if (grep /^$column_href->{type}$/, qw/float double/
+                and defined $column_href->{precision}
+                and $column_href->{precision} != -1
+            ) {
+                $query.= ','.$column_href->{precision};
+            }
+
+            $query.= ')';
+        }
+
+        if (not $column_href->{null}) {
+            $query.= ' NOT NULL';
+        }
+
+        if ($column_href->{default} ne '') {
+            $query.= " default '".$column_href->{default}."'";
+        }
+
+        $query.= "\n";
+
+        $column_num++;
+    }
+
+    if (@key_names) {
+        $query.= sprintf(
+            ", PRIMARY KEY(%s)\n",
+            join(
+                ',',
+                @key_names
+            )
+        );
+    }
+
+    $query.= ')';
+
+#     use Data::Dumper;
+#     print Dumper($param{schema});
+#    print $query; exit();
+
+    return $query;
+}
+
+1;
