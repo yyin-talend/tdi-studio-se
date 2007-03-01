1b995f6b0d6c01aeffd1388485d3a1ec5ae3896b
cantoine <cantoine@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-03-01 10:40:29 +0000

    Modified : modification about Shadow process to work in Java mode.
    git-svn-id: http://talendforge.org/svn/tos/trunk@2240 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/utils/ShadowProcessHelper.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/utils/ShadowProcessHelper.java
@@ -32,6 +32,7 @@ import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IConfigurationElement;
 import org.eclipse.core.runtime.IExtensionRegistry;
 import org.eclipse.core.runtime.Platform;
+import org.talend.commons.ui.utils.PathUtils;
 import org.talend.core.model.metadata.IMetadataColumn;
 import org.talend.core.model.metadata.IMetadataTable;
 import org.talend.core.model.metadata.MetadataColumn;
@@ -41,10 +42,11 @@ import org.talend.core.model.metadata.builder.connection.LdifFileConnection;
 import org.talend.core.model.metadata.builder.connection.SchemaTarget;
 import org.talend.core.model.metadata.builder.connection.XmlFileConnection;
 import org.talend.core.model.metadata.builder.connection.XmlXPathLoopDescriptor;
+import org.talend.core.model.utils.TalendTextUtils;
 import org.talend.core.utils.XmlArray;
 import org.talend.repository.i18n.Messages;
-import org.talend.repository.preview.IPreview;
 import org.talend.repository.preview.AsynchronousPreviewHandler;
+import org.talend.repository.preview.IPreview;
 import org.talend.repository.preview.ProcessDescription;
 
 /**
@@ -70,31 +72,37 @@ public class ShadowProcessHelper {
     public static ProcessDescription getProcessDescription(final FileConnection connection) {
         ProcessDescription processDescription = new ProcessDescription();
 
-        processDescription.setServer(connection.getServer());
-        processDescription.setFilepath(connection.getFilePath());
-        processDescription.setFieldSeparator("\"" + connection.getFieldSeparatorValue() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
-        processDescription.setRowSeparator("\"" + connection.getRowSeparatorValue() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
+        processDescription.setFilepath(TalendTextUtils.addQuotes(PathUtils.getPortablePath(connection.getFilePath())));
 
-        processDescription.setPattern("'" + connection.getFieldSeparatorValue() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+        processDescription.setServer(TalendTextUtils.addQuotes(connection.getServer()));
+        processDescription.setFieldSeparator(TalendTextUtils.addQuotes(connection.getFieldSeparatorValue())); //$NON-NLS-1$
+        
+        // PTODO cantoine : view with plegall for RowSeparator
+        processDescription.setRowSeparator("\"" + connection.getRowSeparatorValue() + "\""); //$NON-NLS-1$
+//        processDescription.setRowSeparator(TalendTextUtils.addQuotes(connection.getRowSeparatorValue())); //$NON-NLS-1$
+
+        processDescription.setPattern(TalendTextUtils.addQuotes(connection.getFieldSeparatorValue())); //$NON-NLS-1$
 
         processDescription.setHeaderRow(connection.getHeaderValue());
         processDescription.setFooterRow(connection.getFooterValue());
         processDescription.setLimitRows(connection.getLimitValue());
 
-        if (connection.getEscapeChar() != null && !connection.getEscapeChar().equals("") && !connection.getEscapeChar().equals("Empty")) { //$NON-NLS-1$ //$NON-NLS-2$
-            processDescription.setEscapeCharacter("'" + connection.getEscapeChar() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+        if (connection.getEscapeChar() != null
+                && !connection.getEscapeChar().equals("") && !connection.getEscapeChar().equals("Empty")) { //$NON-NLS-1$
+            processDescription.setEscapeCharacter(TalendTextUtils.addQuotes(connection.getEscapeChar())); //$NON-NLS-1$
         } else {
-            processDescription.setEscapeCharacter("''"); //$NON-NLS-1$
+            processDescription.setEscapeCharacter(TalendTextUtils.addQuotes("")); //$NON-NLS-1$
         }
-        if (connection.getTextEnclosure() != null && !connection.getTextEnclosure().equals("") //$NON-NLS-1$
-                && !connection.getTextEnclosure().equals("Empty")) { //$NON-NLS-1$
-            processDescription.setTextEnclosure("'" + connection.getTextEnclosure() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+        if (connection.getTextEnclosure() != null
+                && !connection.getTextEnclosure().equals("") && !connection.getTextEnclosure().equals("Empty")) { //$NON-NLS-1$
+            processDescription.setTextEnclosure(TalendTextUtils.addQuotes(connection.getTextEnclosure())); //$NON-NLS-1$
         } else {
-            processDescription.setTextEnclosure("''"); //$NON-NLS-1$
+            processDescription.setTextEnclosure(TalendTextUtils.addQuotes("")); //$NON-NLS-1$
         }
 
         processDescription.setRemoveEmptyRow(connection.isRemoveEmptyRow());
-        processDescription.setEncoding("\"" + connection.getEncoding() + "\""); //$NON-NLS-1$ //$NON-NLS-2$
+        processDescription.setEncoding(TalendTextUtils.addQuotes(connection.getEncoding())); //$NON-NLS-1$
+
         return processDescription;
     }
 
@@ -109,12 +117,14 @@ public class ShadowProcessHelper {
      */
     public static ProcessDescription getProcessDescription(final XmlFileConnection connection) {
         ProcessDescription processDescription = new ProcessDescription();
-        processDescription.setFilepath(connection.getXmlFilePath());
-        processDescription.setLoopQuery("'" + ((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getAbsoluteXPathQuery() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+        processDescription.setFilepath(TalendTextUtils.addQuotes(PathUtils.getPortablePath(connection.getXmlFilePath())));
+        processDescription
+                .setLoopQuery(TalendTextUtils.addQuotes(((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getAbsoluteXPathQuery())); //$NON-NLS-1$ //$NON-NLS-2$
         if (((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getLimitBoucle() != null
                 && !("").equals(((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getLimitBoucle()) //$NON-NLS-1$
                 && (((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getLimitBoucle().intValue()) != 0) {
-            processDescription.setLoopLimit("'" + ((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getLimitBoucle() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+            processDescription
+                    .setLoopLimit(TalendTextUtils.addQuotes(""+((XmlXPathLoopDescriptor) connection.getSchema().get(0)).getLimitBoucle())); //$NON-NLS-1$ //$NON-NLS-2$
         }
 
         List<Map<String, String>> mapping = new ArrayList<Map<String, String>>();
@@ -126,15 +136,15 @@ public class ShadowProcessHelper {
             while (iterate.hasNext()) {
                 SchemaTarget schemaTarget = iterate.next();
                 Map<String, String> lineMapping = new HashMap<String, String>();
-                lineMapping.put("QUERY", "'" + schemaTarget.getRelativeXPathQuery() + "'"); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                lineMapping.put("QUERY", TalendTextUtils.addQuotes(schemaTarget.getRelativeXPathQuery())); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
                 mapping.add(lineMapping);
             }
         }
         processDescription.setMapping(mapping);
         if (connection.getEncoding() != null && !("").equals(connection.getEncoding())) { //$NON-NLS-1$
-            processDescription.setEncoding("'" + connection.getEncoding() + "'"); //$NON-NLS-1$ //$NON-NLS-2$
+            processDescription.setEncoding(TalendTextUtils.addQuotes(connection.getEncoding())); //$NON-NLS-1$ //$NON-NLS-2$
         } else {
-            processDescription.setEncoding("'UTF-8'"); //$NON-NLS-1$
+            processDescription.setEncoding(TalendTextUtils.addQuotes("UTF-8")); //$NON-NLS-1$
         }
 
         return processDescription;
@@ -151,7 +161,7 @@ public class ShadowProcessHelper {
      */
     public static ProcessDescription getProcessDescription(final LdifFileConnection connection) {
         ProcessDescription processDescription = new ProcessDescription();
-        processDescription.setFilepath(connection.getFilePath());
+        processDescription.setFilepath(TalendTextUtils.addQuotes(PathUtils.getPortablePath(connection.getFilePath())));
         List<IMetadataTable> tableSchema = new ArrayList<IMetadataTable>();
 
         IMetadataTable table = new MetadataTable();
@@ -188,6 +198,10 @@ public class ShadowProcessHelper {
         table.setListColumns(schema);
         tableSchema.add(table);
         processDescription.setSchema(tableSchema);
+
+        // PTODO cantoine : create encoding field for LDIF fileConnection
+        processDescription.setEncoding(TalendTextUtils.addQuotes("UTF-8")); //$NON-NLS-1$
+        
         return processDescription;
     }
 
@@ -232,7 +246,8 @@ public class ShadowProcessHelper {
         IExtensionRegistry registry = Platform.getExtensionRegistry();
 
         // use the org.talend.repository.filepreview_provider
-        IConfigurationElement[] configurationElements = registry.getConfigurationElementsFor("org.talend.core.filepreview_provider"); //$NON-NLS-1$
+        IConfigurationElement[] configurationElements = registry
+                .getConfigurationElementsFor("org.talend.core.filepreview_provider"); //$NON-NLS-1$
 
         IPreview preview = null;
         if (configurationElements.length > 0) {
@@ -242,7 +257,7 @@ public class ShadowProcessHelper {
             log.error(Messages.getString("ShadowProcessHelper.logError.previewIsNull01") //$NON-NLS-1$
                     + Messages.getString("ShadowProcessHelper.logError.previewIsNull02")); //$NON-NLS-1$
         }
-        
+
         return preview;
     }
 
