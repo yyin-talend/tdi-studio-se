1648107fc73f2f67349488f84f5c83f41fc6122e
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-04-05 10:31:28 +0000

    Modification : change job note figure to be similar to the note figure in business diagram
    git-svn-id: http://talendforge.org/svn/tos/trunk@2941 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/notes/NoteFigure.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/notes/NoteFigure.java
@@ -28,19 +28,23 @@ import org.eclipse.draw2d.Label;
 import org.eclipse.draw2d.geometry.Point;
 import org.eclipse.draw2d.geometry.PointList;
 import org.eclipse.draw2d.geometry.Rectangle;
+import org.eclipse.swt.graphics.Color;
+import org.eclipse.swt.widgets.Display;
 
 /**
  */
 public class NoteFigure extends Figure {
 
-    private static int cornerSize = 7;
-    
+    private static final int CORNER_SIZE = 12;
+
     private Label label = new Label();
-    
+
     public NoteFigure() {
-        setBackgroundColor(ColorConstants.tooltipBackground);
-        setForegroundColor(ColorConstants.tooltipForeground);
+        setBackgroundColor(new Color(Display.getCurrent(), 255, 255, 203));
+        setForegroundColor(ColorConstants.orange);
+
         add(label);
+        label.setForegroundColor(ColorConstants.black);
     }
 
     protected void paintFigure(Graphics graphics) {
@@ -51,34 +55,34 @@ public class NoteFigure extends Figure {
         // fill the note
         if (isOpaque()) {
             PointList outline = new PointList();
-            
+
             outline.addPoint(0, 0);
-            outline.addPoint(rect.width - cornerSize, 0);
-            outline.addPoint(rect.width - 1, cornerSize);
+            outline.addPoint(rect.width - CORNER_SIZE, 0);
+            outline.addPoint(rect.width - 1, CORNER_SIZE);
             outline.addPoint(rect.width - 1, rect.height - 1);
             outline.addPoint(0, rect.height - 1);
-            
-            graphics.fillPolygon(outline); 
+
+            graphics.fillPolygon(outline);
         }
-        
+
         // draw the inner outline
         PointList innerLine = new PointList();
-        
-        innerLine.addPoint(rect.width - cornerSize - 1, 0);
-        innerLine.addPoint(rect.width - cornerSize - 1, cornerSize);
-        innerLine.addPoint(rect.width - 1, cornerSize);
-        innerLine.addPoint(rect.width - cornerSize - 1, 0);
+
+        innerLine.addPoint(rect.width - CORNER_SIZE - 1, 0);
+        innerLine.addPoint(rect.width - CORNER_SIZE - 1, CORNER_SIZE);
+        innerLine.addPoint(rect.width - 1, CORNER_SIZE);
+        innerLine.addPoint(rect.width - CORNER_SIZE - 1, 0);
         innerLine.addPoint(0, 0);
         innerLine.addPoint(0, rect.height - 1);
         innerLine.addPoint(rect.width - 1, rect.height - 1);
-        innerLine.addPoint(rect.width - 1, cornerSize);
-        
+        innerLine.addPoint(rect.width - 1, CORNER_SIZE);
+
         graphics.drawPolygon(innerLine);
-        
+
         graphics.translate(getLocation().getNegated());
-        
-        label.setLocation(new Point(getLocation().x + cornerSize, getLocation().y + cornerSize));
-        label.setSize(getSize().width - cornerSize * 2, getSize().height - cornerSize * 2);
+
+        label.setLocation(new Point(getLocation().x + CORNER_SIZE, getLocation().y + CORNER_SIZE));
+        label.setSize(getSize().width - CORNER_SIZE * 2, getSize().height - CORNER_SIZE * 2);
     }
 
     public String getText() {
