18c414e9d78e3b795d9ac30b4549f27f0a3b5b45
cantoine <cantoine@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-06-21 18:33:29 +0000

    DEACTIVE FEATURE 664 : Add capabilities to import many demo project: second step
      - Modification to keep previous revision (r3434) of ImportDemoProjectAction.java
      - dupplicate in the right folder TALEND DEMO.ZIP
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@3966 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/actions/importproject/ImportDemoProjectAction.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/actions/importproject/ImportDemoProjectAction.java
@@ -24,15 +24,14 @@ package org.talend.repository.ui.actions.importproject;
 import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.net.URL;
-import java.util.List;
 
 import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.jface.action.Action;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
-import org.eclipse.jface.wizard.WizardDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.internal.dialogs.EventLoopProgressMonitor;
 import org.eclipse.ui.internal.wizards.datatransfer.TarException;
@@ -77,61 +76,58 @@ public final class ImportDemoProjectAction extends Action {
     }
 
     public void run() {
+        final Boolean java = openJavaOrPerl(shell);
 
-        final List<DemoProjectBean> demoProjectList = ImportProjectsUtilities.getAllDemoProjects();
+        if (java == null) {
+            return;
+        }
 
-        ImportDemoProjectWizard demoProjectWizard = new ImportDemoProjectWizard(demoProjectList);
+        ProgressDialog progressDialog = new ProgressDialog(shell) {
 
-        WizardDialog dialog = new WizardDialog(shell, demoProjectWizard);
-        if (dialog.open() != 1 && demoProjectWizard.getSelectedDemoProjectIndex() != Integer.MAX_VALUE) {
-            final int selectedDemoProjectIndex = demoProjectWizard.getSelectedDemoProjectIndex();
+            private IProgressMonitor monitorWrap;
 
-            ProgressDialog progressDialog = new ProgressDialog(shell) {
+            @Override
+            public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+                monitorWrap = new EventLoopProgressMonitor(monitor);
 
-                private IProgressMonitor monitorWrap;
-
-                @Override
-                public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-                    monitorWrap = new EventLoopProgressMonitor(monitor);
-
-                    try {
-                        DemoProjectBean demoProjectBean = demoProjectList.get(selectedDemoProjectIndex);
-                        String archivePath = demoProjectBean.getArchiveFilePath();
-                        String techName = demoProjectBean.getProjectName();
-
-                        Bundle bundle = Platform.getBundle(ResourcesPlugin.PLUGIN_ID);
-
-                        URL url = FileLocator.resolve(bundle.getEntry(archivePath));
-
-                        String archiveFilePath = new Path(url.getFile()).toOSString();
-
-                        ImportProjectsUtilities.importArchiveProject(shell, techName, archiveFilePath, monitorWrap);
-
-                        lastImportedName = techName;
-
-                    } catch (IOException e) {
-                        throw new InvocationTargetException(e);
-                    } catch (TarException e) {
-                        throw new InvocationTargetException(e);
+                try {
+                    String archivePath;
+                    String techName;
+                    if (java) {
+                        archivePath = "resources/TALENDDEMOSJAVA.zip"; //$NON-NLS-1$
+                        techName = "TALENDDEMOSJAVA"; //$NON-NLS-1$
+                        lastImportedName = "TALENDDEMOSJAVA"; //$NON-NLS-1$
+                    } else {
+                        archivePath = "resources/TALENDDEMOSPERL.zip"; //$NON-NLS-1$
+                        techName = "TALENDDEMOSPERL"; //$NON-NLS-1$
+                        lastImportedName = "TALENDDEMOSPERL"; //$NON-NLS-1$
                     }
 
-                    monitorWrap.done();
-                    MessageDialog
-                            .openInformation(shell,
-                                    Messages.getString("ImportDemoProjectAction.messageDialogTitle.demoProject"), //$NON-NLS-1$
-                                    Messages
-                                            .getString("ImportDemoProjectAction.messageDialogContent.demoProjectImportedSuccessfully")); //$NON-NLS-1$
+                    Bundle bundle = Platform.getBundle(ResourcesPlugin.PLUGIN_ID);
+                    URL url = FileLocator.resolve(bundle.getEntry(archivePath));
+                    String archiveFilePath = new Path(url.getFile()).toOSString();
+
+                    ImportProjectsUtilities.importArchiveProject(shell, techName, archiveFilePath, monitorWrap);
+                } catch (IOException e) {
+                    throw new InvocationTargetException(e);
+                } catch (TarException e) {
+                    throw new InvocationTargetException(e);
                 }
 
-            };
-
-            try {
-                progressDialog.executeProcess();
-            } catch (InvocationTargetException e) {
-                MessageBoxExceptionHandler.process(e.getTargetException(), shell);
-            } catch (InterruptedException e) {
-                // Nothing to do
+                monitorWrap.done();
+                MessageDialog.openInformation(shell,
+                        Messages.getString("ImportDemoProjectAction.messageDialogTitle.demoProject"), //$NON-NLS-1$
+                        Messages.getString("ImportDemoProjectAction.messageDialogContent.demoProjectImportedSuccessfully")); //$NON-NLS-1$
             }
+
+        };
+
+        try {
+            progressDialog.executeProcess();
+        } catch (InvocationTargetException e) {
+            MessageBoxExceptionHandler.process(e.getTargetException(), shell);
+        } catch (InterruptedException e) {
+            // Nothing to do
         }
     }
 
@@ -141,5 +137,23 @@ public final class ImportDemoProjectAction extends Action {
 
     public void setShell(Shell shell) {
         this.shell = shell;
+    }
+
+    public static Boolean openJavaOrPerl(Shell parent) {
+        String title = Messages.getString("ImportDemoProjectAction.languageSelectionDialog.title"); //$NON-NLS-1$
+        String message = Messages.getString("ImportDemoProjectAction.languageSelectionDialog.message"); //$NON-NLS-1$
+        String javabutton = Messages.getString("ImportDemoProjectAction.languageSelectionDialog.java"); //$NON-NLS-1$
+        String perlbutton = Messages.getString("ImportDemoProjectAction.languageSelectionDialog.perl"); //$NON-NLS-1$
+        MessageDialog dialog = new MessageDialog(parent, title, null, message, MessageDialog.QUESTION, new String[] { javabutton,
+                perlbutton, IDialogConstants.CANCEL_LABEL }, 0);
+
+        switch (dialog.open()) {
+        case 0:
+            return true;
+        case 1:
+            return false;
+        default:
+            return null;
+        }
     }
 }
