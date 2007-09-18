3f8cb2f4e27304358a3ce1b391c0fcd36745ab95
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-18 06:41:19 +0000

    fixed bug 1980: add the option Checkbox for the add qoute when generate sql Statement.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5651 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/ui/prefs/SqlBuilderPreferencePage.java
@@ -0,0 +1,59 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.sqlbuilder.ui.prefs;
+
+import org.eclipse.jface.preference.BooleanFieldEditor;
+import org.eclipse.jface.preference.FieldEditorPreferencePage;
+import org.eclipse.ui.IWorkbench;
+import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.talend.core.CorePlugin;
+import org.talend.core.prefs.ITalendCorePrefConstants;
+
+/**
+ * DOC qiang.zhang class global comment. Detailled comment <br/>
+ * 
+ */
+public class SqlBuilderPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+    public SqlBuilderPreferencePage() {
+        super(GRID);
+        setPreferenceStore(CorePlugin.getDefault().getPreferenceStore());
+    }
+
+    private BooleanFieldEditor booleanFieldEditor;
+
+    public void init(IWorkbench workbench) {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.preference.FieldEditorPreferencePage#createFieldEditors()
+     */
+    @Override
+    protected void createFieldEditors() {
+        booleanFieldEditor = new BooleanFieldEditor(ITalendCorePrefConstants.SQL_ADD_QUOTE,
+                "add quotes, when you generate sql statement", getFieldEditorParent());
+        addField(booleanFieldEditor);
+    }
+
+}
