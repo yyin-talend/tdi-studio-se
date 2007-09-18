3694bfad2781ee25fee01d911545f0589902f390
rbillerey <rbillerey@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-18 10:28:55 +0000

    Feature 804 and 1588 fixed. Datatype mapping and array references in data management.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5671 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.localprovider/components/tMSSqlOutput/modules/tMSSqlOutput/MSSql.pm
+++ org.talend.designer.components.localprovider/components/tMSSqlOutput/modules/tMSSqlOutput/MSSql.pm
@@ -6,13 +6,12 @@ sub getTableCreationQuery {
 
 
     my %talendtype_to_dbtype = (
-        char    => 'CHAR',
-        Day     => 'DATETIME',
-        double  => 'DOUBLE',
-        float   => 'FLOAT',
+        boolean    => 'TINYINT',
+        date     => 'SMALLDATETIME',
+        datetime  => 'DATETIME',
+        decimal   => 'DECIMAL',
         int     => 'INT',
-        long    => 'NUMBER',
-        String  => 'VARCHAR',
+        string  => 'VARCHAR',
     );
 
     # In $param{schema}, each column looks like this:
@@ -62,13 +61,11 @@ sub getTableCreationQuery {
         $query.= ' '.$column_href->{dbtype};
 
         if (lc $column_href->{dbtype} eq 'varchar'
-            or lc $column_href->{dbtype} eq 'number'
-            or lc $column_href->{dbtype} eq 'char') {
+            or lc $column_href->{dbtype} eq 'decimal') {
             $query.= ' (';
             $query.= $column_href->{len};
 
-            if (lc $column_href->{type} eq 'float'
-                or lc $column_href->{type} eq 'double') {
+            if (lc $column_href->{type} eq 'decimal') {
                 $query.= ','.$column_href->{precision};
             }
 
@@ -100,7 +97,6 @@ sub getTableCreationQuery {
     }
 
     $query.= ')';
-
     return $query;
 }
 
