66484e1cf4eed93c5a48435742bc9b821ecb5260
xtan <xtan@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-19 11:48:23 +0000

    Fixed bug 1962: 1. add a migrationTask for DbColumn.
    bug description:
    1. since TOS 2.2 M1, there add the DbColumn, but the DB components still use the CommonColumn to create the SQL statement.
    2. since TOS 2.2 M2, the DB components change to use the DdColumnName to create the SQL statement.
    3. if export project in TOS 2.2 M1, there store the DbColumnName in the export file, but not check them valid, then import them to TOS 2.2 M2
    there use the invalid DbColumnName will generate the wrong SQL statement.
    
    So, keep the DbColumnName and the CommonColumn nam are the same, it is OK. 
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@5718 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/AddDdColumnMigrationTask.java
@@ -0,0 +1,111 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.List;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.migration.AbstractMigrationTask;
+import org.talend.core.model.migration.IProjectMigrationTask;
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.repository.IRepositoryObject;
+import org.talend.designer.core.model.utils.emf.talendfile.ColumnType;
+import org.talend.designer.core.model.utils.emf.talendfile.MetadataType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.repository.model.ProxyRepositoryFactory;
+
+/**
+ * <p>
+ * This Task check the originalDbColumnName="" in the *.item file, make sure the columnName and the originalDbColumnName
+ * are the same.(just remove it, it is OK.)
+ * </p>
+ * <p>
+ * Reason:
+ * <li>1. Since TOS 2.2 M1, the DB component schema add the "DbColumn" column, but in the template jet file, it use the
+ * columnName to create the SQL statement.</li>
+ * <li>2. Since TOS 2.2 M2, the DB component template file use the DbColumn to create the SQL statement.</li>
+ * <li>3. If in TOS 2.2 M1, the DbColumn and the column name are not same, the project import to TOS 2.2 M2, it will
+ * create the wrong SQL statement.</li>
+ */
+public class AddDdColumnMigrationTask extends AbstractMigrationTask implements IProjectMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.IProjectMigrationTask#execute(org.talend.core.model.general.Project)
+     */
+    public ExecutionResult execute(Project project) {
+        try {
+            removeDbColumn();
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
+
+    private void removeDbColumn() throws PersistenceException {
+        ProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+        List<IRepositoryObject> list = factory.getAll(ERepositoryObjectType.PROCESS, true);
+
+        boolean modified;
+        for (IRepositoryObject mainObject : list) {
+
+            List<IRepositoryObject> allVersion = factory.getAllVersion(mainObject.getId());
+
+            for (IRepositoryObject object : allVersion) {
+
+                ProcessItem item = (ProcessItem) object.getProperty().getItem();
+
+                modified = false;
+
+                for (Object o : item.getProcess().getNode()) {
+
+                    NodeType node = (NodeType) o;
+
+                    for (Object o2 : node.getMetadata()) {
+
+                        MetadataType metadata = (MetadataType) o2;
+
+                        for (Object o3 : metadata.getColumn()) {
+                            ColumnType column = (ColumnType) o3;
+
+                            if (column.getOriginalDbColumnName() != null) {
+                                column.setOriginalDbColumnName(null);
+                                modified = true;
+                            }
+
+                        }
+
+                    }
+                }
+
+                if (modified) {
+                    factory.save(item);
+                }
+            }
+        }
+    }
+}
