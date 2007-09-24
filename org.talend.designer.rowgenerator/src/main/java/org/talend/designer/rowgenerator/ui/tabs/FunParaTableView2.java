3d26a429205cbcf6c1cad1d984cef40769fb9333
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-24 02:45:15 +0000

    bug 2007 fixed: No auto-completion in tRowGenerator function parameter value
    git-svn-id: http://talendforge.org/svn/tos/trunk@5806 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.rowgenerator/src/main/java/org/talend/designer/rowgenerator/ui/tabs/FunParaTableView2.java
+++ org.talend.designer.rowgenerator/src/main/java/org/talend/designer/rowgenerator/ui/tabs/FunParaTableView2.java
@@ -23,6 +23,7 @@ package org.talend.designer.rowgenerator.ui.tabs;
 
 import java.util.List;
 
+import org.eclipse.jface.fieldassist.IContentProposalProvider;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.custom.CCombo;
@@ -36,15 +37,19 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Table;
 import org.eclipse.swt.widgets.TableItem;
+import org.eclipse.ui.IWorkbenchPage;
+import org.eclipse.ui.PlatformUI;
 import org.talend.commons.ui.swt.advanced.dataeditor.AbstractDataTableEditorView;
 import org.talend.commons.ui.swt.advanced.dataeditor.ExtendedToolbarView;
+import org.talend.commons.ui.swt.proposal.ExtendedTextCellEditorWithProposal;
 import org.talend.commons.ui.swt.tableviewer.TableViewerCreator;
 import org.talend.commons.ui.swt.tableviewer.TableViewerCreatorColumn;
 import org.talend.commons.ui.swt.tableviewer.celleditor.CellEditorDialogBehavior;
-import org.talend.commons.ui.swt.tableviewer.celleditor.ExtendedTextCellEditor;
 import org.talend.commons.utils.data.bean.IBeanPropertyAccessors;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.IService;
+import org.talend.core.ui.proposal.ProcessProposalProvider;
+import org.talend.designer.core.ui.MultiPageTalendEditor;
 import org.talend.designer.rowgenerator.data.Function;
 import org.talend.designer.rowgenerator.data.ListParameter;
 import org.talend.designer.rowgenerator.data.Parameter;
@@ -62,220 +67,224 @@ import org.talend.expressionbuilder.ui.IExpressionBuilderDialogController;
  */
 public class FunParaTableView2 extends AbstractDataTableEditorView<Parameter> {
 
-	private static final String VALUE_PROPERTY = "Value"; //$NON-NLS-1$
+    private static final String VALUE_PROPERTY = "Value"; //$NON-NLS-1$
 
-	private MetadataTableEditorViewExt rowGenTableEditor2;
+    private MetadataTableEditorViewExt rowGenTableEditor2;
 
-	private IExpressionBuilderDialogController dialog;
+    private IExpressionBuilderDialogController dialog;
 
-	private TableViewerCreator<Parameter> tableViewerCreator;
+    private TableViewerCreator<Parameter> tableViewerCreator;
 
-	public FunParaTableView2(Composite parentComposite, int mainCompositeStyle) {
-		super(parentComposite, mainCompositeStyle);
+    public FunParaTableView2(Composite parentComposite, int mainCompositeStyle) {
+        super(parentComposite, mainCompositeStyle);
 
-	}
+    }
 
-	public FunParaTableView2(Composite inEditorContainer, int border,
-			MetadataTableEditorViewExt genTableEditor2) {
-		this(inEditorContainer, border);
-		this.rowGenTableEditor2 = genTableEditor2;
+    public FunParaTableView2(Composite inEditorContainer, int border, MetadataTableEditorViewExt genTableEditor2) {
+        this(inEditorContainer, border);
+        this.rowGenTableEditor2 = genTableEditor2;
 
-	}
+    }
 
-	@Override
-	protected void createColumns(
-			TableViewerCreator<Parameter> tableViewerCreator, Table table) {
-		this.tableViewerCreator = tableViewerCreator;
+    @Override
+    protected void createColumns(TableViewerCreator<Parameter> tableViewerCreator, Table table) {
+        this.tableViewerCreator = tableViewerCreator;
 
-		IService expressionBuilderDialogService = GlobalServiceRegister
-				.getDefault().getService(IExpressionBuilderDialogService.class);
+        IService expressionBuilderDialogService = GlobalServiceRegister.getDefault().getService(
+                IExpressionBuilderDialogService.class);
 
-		TableViewerCreatorColumn column;
-		column = new TableViewerCreatorColumn(tableViewerCreator);
-		column.setTitle(Messages.getString("FunParaTableView2.Parameter"));
-		column
-				.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
+        TableViewerCreatorColumn column;
+        column = new TableViewerCreatorColumn(tableViewerCreator);
+        column.setTitle(Messages.getString("FunParaTableView2.Parameter"));
+        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
 
-					public String get(Parameter bean) {
-						return bean.getName();
-					}
+            public String get(Parameter bean) {
+                return bean.getName();
+            }
 
-					public void set(Parameter bean, Object value) {
-					}
+            public void set(Parameter bean, Object value) {
+            }
 
-				});
-		column.setModifiable(false);
-		column.setWidth(115);
-		// column.setCellEditor(new
-		// TextCellEditor(tableViewerCreator.getTable()));
-		// ////////////////////////////////////////////////////////
-		column = new TableViewerCreatorColumn(tableViewerCreator);
-		column.setTitle(Messages.getString("FunParaTableView2.Value"));
-		column.setId(VALUE_PROPERTY);
+        });
+        column.setModifiable(false);
+        column.setWidth(115);
+        // column.setCellEditor(new
+        // TextCellEditor(tableViewerCreator.getTable()));
+        // ////////////////////////////////////////////////////////
+        column = new TableViewerCreatorColumn(tableViewerCreator);
+        column.setTitle(Messages.getString("FunParaTableView2.Value"));
+        column.setId(VALUE_PROPERTY);
 
-		final ExtendedTextCellEditor cellEditor = new ExtendedTextCellEditor(
-				tableViewerCreator.getTable());
+        final ExtendedTextCellEditorWithProposal cellEditor = new ExtendedTextCellEditorWithProposal(tableViewerCreator
+                .getTable(), SWT.MULTI | SWT.BORDER, column);
 
-		column
-				.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
+        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
 
-					public String get(Parameter bean) {
-						return bean.getValue();
-					}
+            public String get(Parameter bean) {
+                return bean.getValue();
+            }
 
-					public void set(Parameter bean, Object value) {
+            public void set(Parameter bean, Object value) {
 
-						if (value == null) {
-							return;
-						}
+                if (value == null) {
+                    return;
+                }
 
-						bean.setValue(value.toString());
+                bean.setValue(value.toString());
 
-						if (ext != null) {
-							ext.setChanged(true);
-						}
-						rowGenTableEditor2.getTableViewerCreator()
-								.getTableViewer().refresh();
-					}
+                if (ext != null) {
+                    ext.setChanged(true);
+                }
+                rowGenTableEditor2.getTableViewerCreator().getTableViewer().refresh();
+            }
 
-				});
-		column.setModifiable(true);
-		column.setWidth(115);
+        });
+        column.setModifiable(true);
+        column.setWidth(115);
 
-		dialog = ((IExpressionBuilderDialogService) expressionBuilderDialogService)
-				.getExpressionBuilderInstance(mainComposite, cellEditor);
+        dialog = ((IExpressionBuilderDialogService) expressionBuilderDialogService).getExpressionBuilderInstance(
+                mainComposite, cellEditor);
 
-		CellEditorDialogBehavior behavior = new CellEditorDialogBehavior(
-				cellEditor);
-		behavior.setCellEditorDialog(dialog);
-		cellEditor.setCellEditorBehavior(behavior);
-		cellEditor.init();
+        CellEditorDialogBehavior behavior = new CellEditorDialogBehavior(cellEditor);
+        behavior.setCellEditorDialog(dialog);
+        cellEditor.setCellEditorBehavior(behavior);
+        cellEditor.init();
+        cellEditor.setContentProposalProvider(getProcessProposals());
+        column.setCellEditor(cellEditor);
 
-		column.setCellEditor(cellEditor);
+        // ////////////////////////////////////////////////////////
 
-		// ////////////////////////////////////////////////////////
+        column = new TableViewerCreatorColumn(tableViewerCreator);
+        column.setTitle(Messages.getString("FunParaTableView2.Comment"));
+        column.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
 
-		column = new TableViewerCreatorColumn(tableViewerCreator);
-		column.setTitle(Messages.getString("FunParaTableView2.Comment"));
-		column
-				.setBeanPropertyAccessors(new IBeanPropertyAccessors<Parameter, Object>() {
+            public String get(Parameter bean) {
+                return bean.getComment();
+            }
 
-					public String get(Parameter bean) {
-						return bean.getComment();
-					}
+            public void set(Parameter bean, Object value) {
+            }
 
-					public void set(Parameter bean, Object value) {
-					}
+        });
+        column.setModifiable(false);
+        column.setWidth(155);
+        // column.setCellEditor(new
+        // TextCellEditor(tableViewerCreator.getTable()));
+        // ////////////////////////////////////////////////////////
 
-				});
-		column.setModifiable(false);
-		column.setWidth(155);
-		// column.setCellEditor(new
-		// TextCellEditor(tableViewerCreator.getTable()));
-		// ////////////////////////////////////////////////////////
+    }
 
-	}
+    @Override
+    protected ExtendedToolbarView initToolBar() {
+        return null;
+    }
 
-	@Override
-	protected ExtendedToolbarView initToolBar() {
-		return null;
-	}
+    private MetadataColumnExt ext;
 
-	private MetadataColumnExt ext;
+    @SuppressWarnings("unchecked")//$NON-NLS-1$
+    public void update(MetadataColumnExt ext) {
+        this.ext = ext;
+        Function function = ext.getFunction();
+        setTitle(function.getDescription());
+        updateData(function.getParameters());
+    }
 
-	@SuppressWarnings("unchecked")//$NON-NLS-1$
-	public void update(MetadataColumnExt ext) {
-		this.ext = ext;
-		Function function = ext.getFunction();
-		setTitle(function.getDescription());
-		updateData(function.getParameters());
-	}
+    private void updateData(List<Parameter> params) {
 
-	private void updateData(List<Parameter> params) {
+        final Table table = this.getTable();
+        final TableViewer viewer = this.getTableViewerCreator().getTableViewer();
 
-		final Table table = this.getTable();
-		final TableViewer viewer = this.getTableViewerCreator()
-				.getTableViewer();
+        final TableEditor editor = new TableEditor(table);
+        // The editor must have the same size as the cell and must
+        // editing the Third column
+        final int eDITABLECOLUMN = 2;
+        table.addSelectionListener(new SelectionAdapter() {
 
-		final TableEditor editor = new TableEditor(table);
-		// The editor must have the same size as the cell and must
-		// editing the Third column
-		final int eDITABLECOLUMN = 2;
-		table.addSelectionListener(new SelectionAdapter() {
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                // Clean up any previous editor control
+                Control oldEditor = editor.getEditor();
+                if (oldEditor != null) {
+                    oldEditor.dispose();
+                }
+                // Identify the selected row
+                TableItem item = (TableItem) e.item;
+                if (item == null) {
+                    return;
+                }
+                Parameter param = (Parameter) item.getData();
 
-			@Override
-			public void widgetSelected(SelectionEvent e) {
-				// Clean up any previous editor control
-				Control oldEditor = editor.getEditor();
-				if (oldEditor != null) {
-					oldEditor.dispose();
-				}
-				// Identify the selected row
-				TableItem item = (TableItem) e.item;
-				if (item == null) {
-					return;
-				}
-				Parameter param = (Parameter) item.getData();
+                if (param instanceof ListParameter) {
+                    createCombo((ListParameter) param, item);
+                }
+            }
 
-				if (param instanceof ListParameter) {
-					createCombo((ListParameter) param, item);
-				}
-			}
+            private void createCombo(final ListParameter list, TableItem item) {
+                final CCombo combo = new CCombo(table, SWT.NONE);
 
-			private void createCombo(final ListParameter list, TableItem item) {
-				final CCombo combo = new CCombo(table, SWT.NONE);
+                combo.setItems(list.getValues());
 
-				combo.setItems(list.getValues());
+                combo.setFocus();
+                editor.setEditor(combo, item, eDITABLECOLUMN);
+                combo.setText(list.getValue());
+                Point size = combo.computeSize(SWT.DEFAULT, table.getItemHeight());
+                // Set attributes of the editor
+                editor.horizontalAlignment = SWT.LEFT;
+                editor.minimumHeight = size.y;
+                editor.minimumWidth = size.x;
+                combo.addSelectionListener(new SelectionAdapter() {
 
-				combo.setFocus();
-				editor.setEditor(combo, item, eDITABLECOLUMN);
-				combo.setText(list.getValue());
-				Point size = combo.computeSize(SWT.DEFAULT, table
-						.getItemHeight());
-				// Set attributes of the editor
-				editor.horizontalAlignment = SWT.LEFT;
-				editor.minimumHeight = size.y;
-				editor.minimumWidth = size.x;
-				combo.addSelectionListener(new SelectionAdapter() {
+                    @Override
+                    public void widgetSelected(SelectionEvent e) {
+                        list.setValue(combo.getText());
+                        viewer.refresh(list);
+                        ext.setChanged(true);
+                        rowGenTableEditor2.getTableViewerCreator().getTableViewer().refresh();
+                    }
 
-					@Override
-					public void widgetSelected(SelectionEvent e) {
-						list.setValue(combo.getText());
-						viewer.refresh(list);
-						ext.setChanged(true);
-						rowGenTableEditor2.getTableViewerCreator()
-								.getTableViewer().refresh();
-					}
+                });
+                combo.addFocusListener(new FocusListener() {
 
-				});
-				combo.addFocusListener(new FocusListener() {
+                    public void focusGained(FocusEvent e) {
 
-					public void focusGained(FocusEvent e) {
+                    }
 
-					}
+                    /**
+                     * Sent when a control loses focus.
+                     * 
+                     * @param e an event containing information about the focus change
+                     */
+                    public void focusLost(FocusEvent e) {
+                        combo.dispose();
+                    }
+                });
 
-					/**
-					 * Sent when a control loses focus.
-					 * 
-					 * @param e
-					 *            an event containing information about the
-					 *            focus change
-					 */
-					public void focusLost(FocusEvent e) {
-						combo.dispose();
-					}
-				});
+            }
 
-			}
+        });
+        getTableViewerCreator().setInputList(params);
+    }
 
-		});
-		getTableViewerCreator().setInputList(params);
-	}
+    public void dispose() {
+        if (getTable() != null) {
+            getTable().dispose();
+        }
+    }
 
-	public void dispose() {
-		if (getTable() != null) {
-			getTable().dispose();
-		}
-	}
+    /**
+     * 
+     * DOC ggu Comment method "getProcessProposals".
+     * 
+     * @return IContentProposalProvider
+     */
+    private IContentProposalProvider getProcessProposals() {
+
+        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
+        // create the proposal
+        IContentProposalProvider processProposalProvider = new ProcessProposalProvider(((MultiPageTalendEditor) page
+                .getActiveEditor()).getProcess());
+        return processProposalProvider;
+
+    }
 
 }
