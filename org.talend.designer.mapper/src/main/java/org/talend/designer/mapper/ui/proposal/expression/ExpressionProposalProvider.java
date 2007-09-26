e426ac2f9086d58b94a19915ba6055ff39928c32
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-26 11:08:12 +0000

    feature 1182  added: Variables persistant for EB.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5904 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/ui/proposal/expression/ExpressionProposalProvider.java
+++ org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/ui/proposal/expression/ExpressionProposalProvider.java
@@ -27,6 +27,9 @@ import java.util.List;
 
 import org.eclipse.jface.fieldassist.IContentProposal;
 import org.eclipse.jface.fieldassist.IContentProposalProvider;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.language.LanguageManager;
+import org.talend.core.model.metadata.types.JavaTypesManager;
 import org.talend.designer.abstractmap.model.table.IDataMapTable;
 import org.talend.designer.abstractmap.model.tableentry.IColumnEntry;
 import org.talend.designer.abstractmap.model.tableentry.ITableEntry;
@@ -34,8 +37,7 @@ import org.talend.designer.mapper.language.ILanguage;
 import org.talend.designer.mapper.language.LanguageProvider;
 import org.talend.designer.mapper.managers.MapperManager;
 import org.talend.designer.mapper.model.table.AbstractDataMapTable;
-import org.talend.designer.mapper.model.table.VarsTable;
-import org.talend.designer.mapper.model.tableentry.ExpressionFilterEntry;
+import org.talend.designer.mapper.model.tableentry.AbstractInOutTableEntry;
 import org.talend.designer.mapper.model.tableentry.TableEntryLocation;
 import org.talend.designer.mapper.ui.visualmap.zone.Zone;
 import org.talend.expressionbuilder.test.shadow.Variable;
@@ -48,97 +50,97 @@ import org.talend.expressionbuilder.test.shadow.Variable;
  */
 public class ExpressionProposalProvider implements IContentProposalProvider {
 
-	private MapperManager mapperManager;
+    private final MapperManager mapperManager;
 
-	private List<AbstractDataMapTable> tables;
+    private List<AbstractDataMapTable> tables;
 
-	private ILanguage currentLanguage;
+    private final ILanguage currentLanguage;
 
-	private IContentProposalProvider[] otherContentProposalProviders;
+    private final IContentProposalProvider[] otherContentProposalProviders;
 
-	private ITableEntry currentModifiedEntry;
+    private ITableEntry currentModifiedEntry;
 
-	/**
-	 * Constructs a new ProcessProposalProvider.
-	 * 
-	 * @param tables
-	 * @param control
-	 */
-	public ExpressionProposalProvider(MapperManager mapperManager,
-			IContentProposalProvider[] otherContentProposalProviders) {
-		super();
-		this.mapperManager = mapperManager;
-		this.currentLanguage = LanguageProvider.getCurrentLanguage();
-		this.otherContentProposalProviders = otherContentProposalProviders;
-	}
+    /**
+     * Constructs a new ProcessProposalProvider.
+     * 
+     * @param tables
+     * @param control
+     */
+    public ExpressionProposalProvider(MapperManager mapperManager,
+            IContentProposalProvider[] otherContentProposalProviders) {
+        super();
+        this.mapperManager = mapperManager;
+        this.currentLanguage = LanguageProvider.getCurrentLanguage();
+        this.otherContentProposalProviders = otherContentProposalProviders;
+    }
 
-	public void init(IDataMapTable currentTable, Zone[] zones,
-			ITableEntry currentEntry) {
+    public void init(IDataMapTable currentTable, Zone[] zones, ITableEntry currentEntry) {
 
-		tables = new ArrayList<AbstractDataMapTable>();
-		for (int i = 0; i < zones.length; i++) {
-			if (zones[i] == Zone.INPUTS) {
-				tables.addAll(mapperManager.getInputTables());
-			} else if (zones[i] == Zone.OUTPUTS) {
-				tables.addAll(mapperManager.getOutputTables());
-			} else if (zones[i] == Zone.VARS) {
-				tables.addAll(mapperManager.getVarsTables());
-			}
-		}
-		// if (!(currentTable instanceof VarsTable) && !(currentEntry instanceof
-		// ExpressionFilterEntry)) {
-		// tables.remove(currentTable);
-		// }
+        tables = new ArrayList<AbstractDataMapTable>();
+        for (int i = 0; i < zones.length; i++) {
+            if (zones[i] == Zone.INPUTS) {
+                tables.addAll(mapperManager.getInputTables());
+            } else if (zones[i] == Zone.OUTPUTS) {
+                tables.addAll(mapperManager.getOutputTables());
+            } else if (zones[i] == Zone.VARS) {
+                tables.addAll(mapperManager.getVarsTables());
+            }
+        }
+        // if (!(currentTable instanceof VarsTable) && !(currentEntry instanceof
+        // ExpressionFilterEntry)) {
+        // tables.remove(currentTable);
+        // }
 
-		this.currentModifiedEntry = currentEntry;
-	}
+        this.currentModifiedEntry = currentEntry;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.eclipse.jface.fieldassist.IContentProposalProvider#getProposals(java.lang.String,
-	 *      int)
-	 */
-	public IContentProposal[] getProposals(String contents, int position) {
-		List<IContentProposal> proposals = new ArrayList<IContentProposal>();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.fieldassist.IContentProposalProvider#getProposals(java.lang.String, int)
+     */
+    public IContentProposal[] getProposals(String contents, int position) {
+        List<IContentProposal> proposals = new ArrayList<IContentProposal>();
 
-		TableEntryLocation sourceEntryLocation = new TableEntryLocation();
+        TableEntryLocation sourceEntryLocation = new TableEntryLocation();
 
-		for (IDataMapTable table : this.tables) {
-			List<IColumnEntry> dataMapTableEntries = table.getColumnEntries();
-			for (IColumnEntry entrySource : dataMapTableEntries) {
-				sourceEntryLocation.tableName = entrySource.getParentName();
-				sourceEntryLocation.columnName = entrySource.getName();
-				if (mapperManager.getUiManager().checkSourceLocationIsValid(
-						entrySource, currentModifiedEntry)) {
-					proposals.add(new EntryContentProposal(entrySource,
-							this.currentLanguage));
-				}
-			}
-		}
+        for (IDataMapTable table : this.tables) {
+            List<IColumnEntry> dataMapTableEntries = table.getColumnEntries();
+            for (IColumnEntry entrySource : dataMapTableEntries) {
+                sourceEntryLocation.tableName = entrySource.getParentName();
+                sourceEntryLocation.columnName = entrySource.getName();
+                if (mapperManager.getUiManager().checkSourceLocationIsValid(entrySource, currentModifiedEntry)) {
+                    proposals.add(new EntryContentProposal(entrySource, this.currentLanguage));
+                }
+            }
+        }
 
-		for (IContentProposalProvider contentProposalProvider : otherContentProposalProviders) {
-			proposals.addAll(Arrays.asList(contentProposalProvider
-					.getProposals(contents, position)));
-		}
-		IContentProposal[] res = new IContentProposal[proposals.size()];
-		res = proposals.toArray(res);
-		return res;
-	}
+        for (IContentProposalProvider contentProposalProvider : otherContentProposalProviders) {
+            proposals.addAll(Arrays.asList(contentProposalProvider.getProposals(contents, position)));
+        }
+        IContentProposal[] res = new IContentProposal[proposals.size()];
+        res = proposals.toArray(res);
+        return res;
+    }
 
-	public List<Variable> getVariables() {
-		List<Variable> variables = new ArrayList<Variable>();
-		for (IDataMapTable table : this.tables) {
-			List<IColumnEntry> dataMapTableEntries = table.getColumnEntries();
-			for (IColumnEntry entrySource : dataMapTableEntries) {
-				String variable = LanguageProvider.getCurrentLanguage()
-						.getLocation(entrySource.getParentName(),
-								entrySource.getName());
-				variables.add(new Variable(variable, ""));
-			}
-		}
+    public List<Variable> getVariables() {
+        List<Variable> variables = new ArrayList<Variable>();
+        for (IDataMapTable table : this.tables) {
+            List<IColumnEntry> dataMapTableEntries = table.getColumnEntries();
+            for (IColumnEntry entrySource : dataMapTableEntries) {
+                String variable = LanguageProvider.getCurrentLanguage().getLocation(entrySource.getParentName(),
+                        entrySource.getName());
+                String talendType = ((AbstractInOutTableEntry) entrySource).getMetadataColumn().getTalendType();
+                boolean nullable = ((AbstractInOutTableEntry) entrySource).getMetadataColumn().isNullable();
+                if (LanguageManager.getCurrentLanguage() == ECodeLanguage.JAVA) {
+                    variables.add(new Variable(variable, JavaTypesManager.getDefaultValueFromJavaIdType(talendType,
+                            nullable).toString()));
+                } else {
+                    variables.add(new Variable(variable, ""));
+                }
+            }
+        }
 
-		return variables;
-	}
-
+        return variables;
+    }
 }
