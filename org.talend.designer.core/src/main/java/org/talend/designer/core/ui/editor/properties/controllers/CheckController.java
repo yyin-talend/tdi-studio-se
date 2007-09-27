ea2fd7ae59ab838b8aaff25d803493078ff4db63
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-09-27 09:54:33 +0000

    Modification: test if the button is disposed before refresh.
    git-svn-id: http://talendforge.org/svn/tos/trunk@5958 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/CheckController.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/CheckController.java
@@ -59,172 +59,154 @@ import org.talend.designer.core.ui.editor.properties.DynamicTabbedPropertySectio
  */
 public class CheckController extends AbstractElementPropertySectionController {
 
-	/**
-	 * DOC yzhang CheckController constructor comment.
-	 * 
-	 * @param parameterBean
-	 */
-	public CheckController(DynamicTabbedPropertySection parameterBean) {
-		super(parameterBean);
-		// TODO Auto-generated constructor stub
-	}
+    /**
+     * DOC yzhang CheckController constructor comment.
+     * 
+     * @param parameterBean
+     */
+    public CheckController(DynamicTabbedPropertySection parameterBean) {
+        super(parameterBean);
+        // TODO Auto-generated constructor stub
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.designer.core.ui.editor.properties2.editors.AbstractElementPropertySectionController#createCommand()
-	 */
-	private Command createCommand(SelectionEvent event) {
-		Set<String> elementsName;
-		Control ctrl = (Control) event.getSource();
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.ui.editor.properties2.editors.AbstractElementPropertySectionController#createCommand()
+     */
+    private Command createCommand(SelectionEvent event) {
+        Set<String> elementsName;
+        Control ctrl = (Control) event.getSource();
 
-		elementsName = hashCurControls.keySet();
-		for (String name : elementsName) {
-			Object o = hashCurControls.get(name);
-			if (o instanceof Control) {
-				ctrl = (Control) o;
-				if (ctrl == null) {
-					hashCurControls.remove(name);
-					return null;
-				}
-				if (ctrl.equals(event.getSource())) {
-					if (ctrl instanceof Button) {
-						// only for checkbox, other buttons must be checked
-						// before
-						if (!elem.getPropertyValue(name).equals(
-								new Boolean(((Button) ctrl).getSelection()))) {
-							Command cmd = null;
-							Boolean value = new Boolean(((Button) ctrl)
-									.getSelection());
-							if (name.equals(EParameterName.ACTIVATE.getName())) {
-								if (elem instanceof Node) {
-									List<Node> nodeList = new ArrayList<Node>();
-									nodeList.add((Node) elem);
-									List<Connection> connList = new ArrayList<Connection>();
-									cmd = new ChangeActivateStatusElementCommand(
-											value, nodeList, connList);
-								}
-							} else {
-								cmd = new PropertyChangeCommand(elem, name,
-										value);
-							}
-							return cmd;
-						}
-					}
-				}
-			}
-		}
-		return null;
-	}
+        elementsName = hashCurControls.keySet();
+        for (String name : elementsName) {
+            Object o = hashCurControls.get(name);
+            if (o instanceof Control) {
+                ctrl = (Control) o;
+                if (ctrl == null) {
+                    hashCurControls.remove(name);
+                    return null;
+                }
+                if (ctrl.equals(event.getSource())) {
+                    if (ctrl instanceof Button) {
+                        // only for checkbox, other buttons must be checked
+                        // before
+                        if (!elem.getPropertyValue(name).equals(new Boolean(((Button) ctrl).getSelection()))) {
+                            Command cmd = null;
+                            Boolean value = new Boolean(((Button) ctrl).getSelection());
+                            if (name.equals(EParameterName.ACTIVATE.getName())) {
+                                if (elem instanceof Node) {
+                                    List<Node> nodeList = new ArrayList<Node>();
+                                    nodeList.add((Node) elem);
+                                    List<Connection> connList = new ArrayList<Connection>();
+                                    cmd = new ChangeActivateStatusElementCommand(value, nodeList, connList);
+                                }
+                            } else {
+                                cmd = new PropertyChangeCommand(elem, name, value);
+                            }
+                            return cmd;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.designer.core.ui.editor.properties2.editors.AbstractElementPropertySectionController#createControl()
-	 */
-	@Override
-	public Control createControl(final Composite subComposite,
-			final IElementParameter param, final int numInRow,
-			final int nbInRow, final int top, final Control lastControl) {
-		final DecoratedField dField = new DecoratedField(subComposite,
-				SWT.BORDER, new IControlCreator() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.ui.editor.properties2.editors.AbstractElementPropertySectionController#createControl()
+     */
+    @Override
+    public Control createControl(final Composite subComposite, final IElementParameter param, final int numInRow,
+            final int nbInRow, final int top, final Control lastControl) {
+        final DecoratedField dField = new DecoratedField(subComposite, SWT.BORDER, new IControlCreator() {
 
-					public Control createControl(Composite parent, int style) {
-						return getWidgetFactory().createButton(parent,
-								param.getDisplayName(), SWT.CHECK);
-					}
+            public Control createControl(Composite parent, int style) {
+                return getWidgetFactory().createButton(parent, param.getDisplayName(), SWT.CHECK);
+            }
 
-				});
-		if (param.isRepositoryValueUsed()) {
-			FieldDecoration decoration = FieldDecorationRegistry.getDefault()
-					.getFieldDecoration(
-							FieldDecorationRegistry.DEC_CONTENT_PROPOSAL);
-			decoration.setDescription(Messages
-					.getString("CheckController.decoration.description")); //$NON-NLS-1$
-			dField
-					.addFieldDecoration(decoration, SWT.RIGHT | SWT.BOTTOM,
-							false);
-		}
+        });
+        if (param.isRepositoryValueUsed()) {
+            FieldDecoration decoration = FieldDecorationRegistry.getDefault().getFieldDecoration(
+                    FieldDecorationRegistry.DEC_CONTENT_PROPOSAL);
+            decoration.setDescription(Messages.getString("CheckController.decoration.description")); //$NON-NLS-1$
+            dField.addFieldDecoration(decoration, SWT.RIGHT | SWT.BOTTOM, false);
+        }
 
-		Control cLayout = dField.getLayoutControl();
-		cLayout.setBackground(subComposite.getBackground());
-		Button checkBtn = (Button) dField.getControl();
+        Control cLayout = dField.getLayoutControl();
+        cLayout.setBackground(subComposite.getBackground());
+        Button checkBtn = (Button) dField.getControl();
 
-		FormData data = new FormData();
-		data.top = new FormAttachment(0, top);
-		if (lastControl != null) {
-			data.left = new FormAttachment(lastControl, 0);
-		} else {
-			data.left = new FormAttachment(
-					(((numInRow - 1) * MAX_PERCENT) / nbInRow), 0);
-		}
-		cLayout.setLayoutData(data);
-		hashCurControls.put(param.getName(), checkBtn);
-		checkBtn.setEnabled(!param.isReadOnly());
-		checkBtn.addSelectionListener(listenerSelection);
-		if (elem instanceof Node) {
-			checkBtn.setToolTipText(VARIABLE_TOOLTIP + param.getVariableName());
-		}
+        FormData data = new FormData();
+        data.top = new FormAttachment(0, top);
+        if (lastControl != null) {
+            data.left = new FormAttachment(lastControl, 0);
+        } else {
+            data.left = new FormAttachment((((numInRow - 1) * MAX_PERCENT) / nbInRow), 0);
+        }
+        cLayout.setLayoutData(data);
+        hashCurControls.put(param.getName(), checkBtn);
+        checkBtn.setEnabled(!param.isReadOnly());
+        checkBtn.addSelectionListener(listenerSelection);
+        if (elem instanceof Node) {
+            checkBtn.setToolTipText(VARIABLE_TOOLTIP + param.getVariableName());
+        }
 
-		Point initialSize = checkBtn.computeSize(SWT.DEFAULT, SWT.DEFAULT);
-		dynamicTabbedPropertySection.setCurRowSize(initialSize.y
-				+ ITabbedPropertyConstants.VSPACE);
-		return cLayout;
-	}
+        Point initialSize = checkBtn.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+        dynamicTabbedPropertySection.setCurRowSize(initialSize.y + ITabbedPropertyConstants.VSPACE);
+        return cLayout;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController#estimateRowSize(org.eclipse.swt.widgets.Composite,
-	 *      org.talend.core.model.process.IElementParameter)
-	 */
-	@Override
-	public int estimateRowSize(Composite subComposite,
-			final IElementParameter param) {
-		final DecoratedField dField = new DecoratedField(subComposite,
-				SWT.BORDER, new IControlCreator() {
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController#estimateRowSize(org.eclipse.swt.widgets.Composite,
+     * org.talend.core.model.process.IElementParameter)
+     */
+    @Override
+    public int estimateRowSize(Composite subComposite, final IElementParameter param) {
+        final DecoratedField dField = new DecoratedField(subComposite, SWT.BORDER, new IControlCreator() {
 
-					public Control createControl(Composite parent, int style) {
-						return getWidgetFactory().createButton(parent,
-								param.getDisplayName(), SWT.CHECK);
-					}
+            public Control createControl(Composite parent, int style) {
+                return getWidgetFactory().createButton(parent, param.getDisplayName(), SWT.CHECK);
+            }
 
-				});
-		Point initialSize = dField.getLayoutControl().computeSize(SWT.DEFAULT,
-				SWT.DEFAULT);
-		dField.getLayoutControl().dispose();
+        });
+        Point initialSize = dField.getLayoutControl().computeSize(SWT.DEFAULT, SWT.DEFAULT);
+        dField.getLayoutControl().dispose();
 
-		return initialSize.y + ITabbedPropertyConstants.VSPACE;
-	}
+        return initialSize.y + ITabbedPropertyConstants.VSPACE;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
-	 */
-	public void propertyChange(PropertyChangeEvent evt) {
-		// TODO Auto-generated method stub
+    /*
+     * (non-Javadoc)
+     * 
+     * @see java.beans.PropertyChangeListener#propertyChange(java.beans.PropertyChangeEvent)
+     */
+    public void propertyChange(PropertyChangeEvent evt) {
+        // TODO Auto-generated method stub
 
-	}
+    }
 
-	SelectionListener listenerSelection = new SelectionAdapter() {
+    SelectionListener listenerSelection = new SelectionAdapter() {
 
-		public void widgetSelected(SelectionEvent event) {
-			Command cmd = createCommand(event);
-			if (cmd != null) {
-				getCommandStack().execute(cmd);
-			}
-		}
-	};
+        public void widgetSelected(SelectionEvent event) {
+            Command cmd = createCommand(event);
+            if (cmd != null) {
+                getCommandStack().execute(cmd);
+            }
+        }
+    };
 
-	@Override
-	public void refresh(IElementParameter param,
-			boolean checkErrorsWhenViewRefreshed) {
-		Button checkBtn = (Button) hashCurControls.get(param.getName());
-		Object value = param.getValue();
-		if (checkBtn == null) {
-			return;
-		}
-		checkBtn.setSelection((Boolean) value);
-	}
+    @Override
+    public void refresh(IElementParameter param, boolean checkErrorsWhenViewRefreshed) {
+        Button checkBtn = (Button) hashCurControls.get(param.getName());
+        Object value = param.getValue();
+        if (checkBtn == null || checkBtn.isDisposed()) {
+            return;
+        }
+        checkBtn.setSelection((Boolean) value);
+    }
 }
