6e34fc7d12b71cceefdaa7328572468afdb23b71
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-10-03 04:08:04 +0000

    bug 2175 fixed: avoid to have two times the encoding type.
    bug 2176 fixed: 0 length or precision will now show empty.
    
    bug 2177 fixed: show the TalendType instead of the dbTye
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@6080 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.rowgenerator/src/main/java/org/talend/designer/rowgenerator/RowGeneratorComponentDocumentation.java
+++ org.talend.designer.rowgenerator/src/main/java/org/talend/designer/rowgenerator/RowGeneratorComponentDocumentation.java
@@ -35,164 +35,166 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
 import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.language.LanguageManager;
 import org.talend.core.model.genhtml.HTMLDocUtils;
 import org.talend.core.model.genhtml.HTMLHandler;
 import org.talend.core.model.genhtml.IHTMLDocConstants;
 import org.talend.core.model.genhtml.XMLHandler;
 import org.talend.core.model.metadata.IMetadataColumn;
 import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.metadata.types.JavaTypesManager;
 import org.talend.core.model.process.IComponentDocumentation;
 import org.talend.designer.rowgenerator.ui.editor.MetadataColumnExt;
 
 /**
- * This class is used for generating HTML file for Component 'tRowGenerator'.
- * <br/> <br/>
+ * This class is used for generating HTML file for Component 'tRowGenerator'. <br/> <br/>
  * 
  */
-public class RowGeneratorComponentDocumentation implements
-		IComponentDocumentation {
+public class RowGeneratorComponentDocumentation implements IComponentDocumentation {
 
-	private String componentName;
+    private String componentName;
 
-	private String tempFolderPath;
+    private String tempFolderPath;
 
-	private List<IMetadataTable> metadataListOut;
+    private List<IMetadataTable> metadataListOut;
 
-	private Document document;
+    private Document document;
 
-	private String previewPicPath;
+    private String previewPicPath;
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.core.model.process.IComponentDocumentation#getHTMLFile()
-	 */
-	public URL getHTMLFile() {
-		String xmlFilepath = this.tempFolderPath + File.separatorChar
-				+ this.componentName + IHTMLDocConstants.XML_FILE_SUFFIX;
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.process.IComponentDocumentation#getHTMLFile()
+     */
+    public URL getHTMLFile() {
+        String xmlFilepath = this.tempFolderPath + File.separatorChar + this.componentName + IHTMLDocConstants.XML_FILE_SUFFIX;
 
-		String htmlFilePath = this.tempFolderPath + File.separatorChar
-				+ this.componentName + IHTMLDocConstants.HTML_FILE_SUFFIX;
+        String htmlFilePath = this.tempFolderPath + File.separatorChar + this.componentName + IHTMLDocConstants.HTML_FILE_SUFFIX;
 
-		final Bundle b = Platform.getBundle(RowGeneratorPlugin.PLUGIN_ID);
+        final Bundle b = Platform.getBundle(RowGeneratorPlugin.PLUGIN_ID);
 
-		URL xslFileUrl = null;
-		try {
-			xslFileUrl = FileLocator.toFileURL(FileLocator.find(b, new Path(
-					IHTMLDocConstants.TROWGENERATOR_XSL_FILE_PATH), null));
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+        URL xslFileUrl = null;
+        try {
+            xslFileUrl = FileLocator
+                    .toFileURL(FileLocator.find(b, new Path(IHTMLDocConstants.TROWGENERATOR_XSL_FILE_PATH), null));
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
 
-		String xslFilePath = xslFileUrl.getPath();
+        String xslFilePath = xslFileUrl.getPath();
 
-		generateXMLInfo();
+        generateXMLInfo();
 
-		XMLHandler.generateXMLFile(tempFolderPath, xmlFilepath, document);
-		HTMLHandler.generateHTMLFile(this.tempFolderPath, xslFilePath,
-				xmlFilepath, htmlFilePath);
+        XMLHandler.generateXMLFile(tempFolderPath, xmlFilepath, document);
+        HTMLHandler.generateHTMLFile(this.tempFolderPath, xslFilePath, xmlFilepath, htmlFilePath);
 
-		File htmlFile = new File(htmlFilePath);
-		if (htmlFile.exists()) {
-			try {
-				return htmlFile.toURL();
-			} catch (MalformedURLException e) {
-				ExceptionHandler.process(e);
-			}
-		}
-		return null;
-	}
+        File htmlFile = new File(htmlFilePath);
+        if (htmlFile.exists()) {
+            try {
+                return htmlFile.toURL();
+            } catch (MalformedURLException e) {
+                ExceptionHandler.process(e);
+            }
+        }
+        return null;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.core.model.process.IComponentDocumentation#setComponentName(java.lang.String)
-	 */
-	public void setComponentName(String componentName) {
-		this.componentName = componentName;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.process.IComponentDocumentation#setComponentName(java.lang.String)
+     */
+    public void setComponentName(String componentName) {
+        this.componentName = componentName;
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * 
-	 * @see org.talend.core.model.process.IComponentDocumentation#setTempFolderPath(java.lang.String)
-	 */
-	public void setTempFolderPath(String tempFolderpath) {
-		this.tempFolderPath = tempFolderpath;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.process.IComponentDocumentation#setTempFolderPath(java.lang.String)
+     */
+    public void setTempFolderPath(String tempFolderpath) {
+        this.tempFolderPath = tempFolderpath;
+    }
 
-	/**
-	 * Sets an instanceof <code>List<IMetadataTable></code>
-	 * 
-	 * @param metadataListOut
-	 */
-	public void setMetadataListOut(List<IMetadataTable> metadataListOut) {
-		this.metadataListOut = metadataListOut;
-	}
+    /**
+     * Sets an instanceof <code>List<IMetadataTable></code>
+     * 
+     * @param metadataListOut
+     */
+    public void setMetadataListOut(List<IMetadataTable> metadataListOut) {
+        this.metadataListOut = metadataListOut;
+    }
 
-	/**
-	 * Generates all information which for XML file.
-	 */
-	private void generateXMLInfo() {
-		document = DocumentHelper.createDocument();
-		Element externalNodeElement = document.addElement("externalNode");
-		externalNodeElement.addAttribute("name", HTMLDocUtils
-				.checkString(this.componentName));
-		externalNodeElement.addAttribute("preview", HTMLDocUtils
-				.checkString(this.previewPicPath));
+    /**
+     * Generates all information which for XML file.
+     */
+    private void generateXMLInfo() {
+        document = DocumentHelper.createDocument();
+        Element externalNodeElement = document.addElement("externalNode");
+        externalNodeElement.addAttribute("name", HTMLDocUtils.checkString(this.componentName));
+        externalNodeElement.addAttribute("preview", HTMLDocUtils.checkString(this.previewPicPath));
 
-		generateColumnInfo(externalNodeElement);
-	}
+        generateColumnInfo(externalNodeElement);
+    }
 
-	/**
-	 * DOC Administrator Comment method "generateColumnInfo".
-	 * 
-	 * @param externalNodeElement
-	 */
-	private void generateColumnInfo(Element externalNodeElement) {
+    /**
+     * DOC Administrator Comment method "generateColumnInfo".
+     * 
+     * @param externalNodeElement
+     */
+    private void generateColumnInfo(Element externalNodeElement) {
 
-		for (IMetadataTable metadataTable : this.metadataListOut) {
+        for (IMetadataTable metadataTable : this.metadataListOut) {
 
-			// List<IMetadataColumn> newMetadataTable =
-			// rowGeneratorMain.getGeneratorUI().convert(metadataTable);
-			for (IMetadataColumn tempColumn : metadataTable.getListColumns()) {
-				MetadataColumnExt column = (MetadataColumnExt) tempColumn;
-				Element columnElement = externalNodeElement
-						.addElement("column");
-				columnElement.addAttribute("name", HTMLDocUtils
-						.checkString(column.getLabel()));
-				columnElement.addAttribute("key", String
-						.valueOf(column.isKey()));
-				columnElement.addAttribute("type", HTMLDocUtils
-						.checkString(column.getTalendType()));
-				columnElement.addAttribute("nullable", String.valueOf(column
-						.isCustom()));
-				columnElement.addAttribute("length", String.valueOf(column
-						.getLength()));
-				columnElement.addAttribute("precision", String.valueOf(column
-						.getPrecision()));
-				columnElement.addAttribute("default", HTMLDocUtils
-						.checkString(column.getDefault()));
-				columnElement.addAttribute("comment", HTMLDocUtils
-						.checkString(column.getComment()));
-				columnElement.addAttribute("functions", HTMLDocUtils
-						.checkString(column.getFunction().getName()));
-				columnElement.addAttribute("parameters", HTMLDocUtils
-						.checkString(column.getParameter()));
-				// columnElement.addAttribute("preview",
-				// HTMLDocUtils.checkString(column.getPreview()));
-			}
-		}
-	}
+            // List<IMetadataColumn> newMetadataTable =
+            // rowGeneratorMain.getGeneratorUI().convert(metadataTable);
+            for (IMetadataColumn tempColumn : metadataTable.getListColumns()) {
+                MetadataColumnExt column = (MetadataColumnExt) tempColumn;
+                Element columnElement = externalNodeElement.addElement("column");
+                columnElement.addAttribute("name", HTMLDocUtils.checkString(column.getLabel()));
+                columnElement.addAttribute("key", String.valueOf(column.isKey()));
+                String type = HTMLDocUtils.checkString(column.getTalendType());
+                if (LanguageManager.getCurrentLanguage().equals(ECodeLanguage.JAVA)) {
+                    type = JavaTypesManager.getTypeToGenerate(column.getTalendType(), column.isNullable());
+                }
+                columnElement.addAttribute("type", type);
+                columnElement.addAttribute("nullable", String.valueOf(column.isNullable()));
+                String length;
+                if ((column.getLength() == null) || (column.getLength() == 0)) {
+                    length = "";
+                } else {
+                    length = String.valueOf(column.getLength());
+                }
 
-	/**
-	 * Sets the preview picture path of component.
-	 * 
-	 * @param previewPicPath
-	 */
-	public void setPreviewPicPath(String previewPicPath) {
-		this.previewPicPath = previewPicPath;
+                columnElement.addAttribute("length", length);
+                String precision;
+                if ((column.getPrecision() == null) || (column.getPrecision() == 0)) {
+                    precision = "";
+                } else {
+                    precision = String.valueOf(column.getPrecision());
+                }
+                columnElement.addAttribute("precision", precision);
+                columnElement.addAttribute("default", HTMLDocUtils.checkString(column.getDefault()));
+                columnElement.addAttribute("comment", HTMLDocUtils.checkString(column.getComment()));
+                columnElement.addAttribute("functions", HTMLDocUtils.checkString(column.getFunction().getName()));
+                columnElement.addAttribute("parameters", HTMLDocUtils.checkString(column.getParameter()));
+                // columnElement.addAttribute("preview",
+                // HTMLDocUtils.checkString(column.getPreview()));
+            }
+        }
+    }
 
-	}
+    /**
+     * Sets the preview picture path of component.
+     * 
+     * @param previewPicPath
+     */
+    public void setPreviewPicPath(String previewPicPath) {
+        this.previewPicPath = previewPicPath;
+
+    }
 }
