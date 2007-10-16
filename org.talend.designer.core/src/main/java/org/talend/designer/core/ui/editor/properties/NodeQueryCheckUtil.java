5190cc68ed08a26e5b79b50bd710a9c208dd7045
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-10-16 07:56:56 +0000

    bug 2246 fixed: Bad count of columns when you used a database function, Change the display by new rules.
    git-svn-id: http://talendforge.org/svn/tos/trunk@6331 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/NodeQueryCheckUtil.java
@@ -0,0 +1,130 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.designer.core.ui.editor.properties;
+
+import java.util.regex.Matcher;
+
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.language.LanguageManager;
+import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.utils.TalendTextUtils;
+import org.talend.designer.core.ui.editor.nodes.Node;
+
+/**
+ * DOC ggu class global comment. Detailled comment <br/>
+ * 
+ */
+public final class NodeQueryCheckUtil {
+
+    public static final String SELECT = "SELECT"; //$NON-NLS-1$   
+
+    public static final String FROM = "FROM"; //$NON-NLS-1$   
+
+    private static final int REGX_FLAG = java.util.regex.Pattern.CANON_EQ | java.util.regex.Pattern.CASE_INSENSITIVE
+            | java.util.regex.Pattern.UNICODE_CASE;
+
+    private static final String NL_REGX_ONE = "(\\s)+"; //$NON-NLS-1$   
+
+    private static final String QUOTE = LanguageManager.getCurrentLanguage() == ECodeLanguage.JAVA ? TalendTextUtils.QUOTATION_MARK
+            : TalendTextUtils.SINGLE_QUOTE;
+
+    // reg: "(\s)*(select)(\s)+(.*?)(\s)+(from)(\s)+(.*)"
+    private static final String SQL_REGX = "^" + QUOTE + "(\\s)*(" + SELECT + ")" + NL_REGX_ONE + "(.*?)" + NL_REGX_ONE
+            + "(" + FROM + ")" + NL_REGX_ONE + "(.*)" + QUOTE + "$";
+
+    private static final String NL_REGX = "(\\s)*"; //$NON-NLS-1$   
+
+    // reg:(.+?)(\s)*\((\s)*(.*)(\s)*\)
+    private static final String SQL_FUNC_REGX = "(.+?)" + NL_REGX + "\\(" + NL_REGX + "(.*)" + NL_REGX + "\\)";
+
+    /**
+     * 
+     * DOC ggu Comment method "checkQueryOK".
+     * 
+     * @param node
+     * @param sql
+     * @return
+     */
+    public static boolean checkQueryOK(Node node, String sql) {
+
+        //
+        if (sql == null) {
+            return false;
+        }
+        // replace the new line char
+        sql = sql.replaceAll("\r", " ");
+        sql = sql.replaceAll("\n", " ");
+
+        // empty
+        sql = sql.trim();
+        if ("".equals(sql)) {
+            return false;
+        }
+
+        // match sql query: select x.a, x.b from x
+        java.util.regex.Pattern sqlRegex = java.util.regex.Pattern.compile(SQL_REGX, REGX_FLAG);
+        Matcher matcher = sqlRegex.matcher(sql);
+        if (!matcher.find()) {
+            return false;
+        }
+
+        // get the columns
+        matcher.lookingAt();
+        String columns = matcher.group(4).trim();
+        if ("".equals(columns)) {
+            return false;
+        }
+        //
+        if ("*".equals(columns)) {
+            return true;
+        }
+
+        // include function
+        java.util.regex.Pattern columnRegex = java.util.regex.Pattern.compile(SQL_FUNC_REGX, REGX_FLAG);
+        Matcher columnMatcher = columnRegex.matcher(columns);
+        // no functions
+        if (!columnMatcher.find()) {
+            return compareNodeTableColumns(node, columns);
+
+        }
+
+        return true;
+    }
+
+    private static boolean compareNodeTableColumns(Node node, String columns) {
+        if (node.getMetadataList().size() == 0) {
+            return true;
+        }
+        IMetadataTable metaTable = node.getMetadataList().get(0);
+        if (metaTable == null || metaTable.getListColumns() == null) {
+            return true;
+        }
+        int originColumnSize = metaTable.getListColumns().size();
+        String[] columnArray = columns.split(",");
+        // columns not match
+        if (columnArray.length != originColumnSize) {
+            return false;
+        }
+        return true;
+    }
+
+}
