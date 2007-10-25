7f7e082967a404f437feb6d091d1e30a49d64cd6
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-10-25 01:18:52 +0000

    feature 2217 added: add an action in the repository to directly create a metadata from an xml file.
    git-svn-id: http://talendforge.org/svn/tos/trunk@6529 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/ui/actions/metadata/ImportSchemaFileAction.java
@@ -0,0 +1,139 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.repository.ui.actions.metadata;
+
+import java.io.File;
+
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.wizard.WizardDialog;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.widgets.FileDialog;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.PlatformUI;
+import org.talend.commons.ui.image.EImage;
+import org.talend.commons.ui.image.ImageProvider;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.repository.i18n.Messages;
+import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.model.RepositoryNode.EProperties;
+import org.talend.repository.ui.wizards.metadata.connection.genericshema.ImportSchemaFileWizard;
+
+/**
+ * DOC ggu class global comment. Detailled comment <br/>
+ * 
+ */
+public class ImportSchemaFileAction extends CreateGenericSchemaAction {
+
+    private static final String LABEL = Messages.getString("ImportSchemaFileAction.Label"); //$NON-NLS-1$
+
+    private File file = null;
+
+    /**
+     * DOC ggu ImportSchemaFileAction constructor comment.
+     */
+    public ImportSchemaFileAction() {
+        this.setText(LABEL);
+        this.setToolTipText(LABEL);
+        this.setImageDescriptor(ImageProvider.getImageDesc(EImage.IMPORT_ICON));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.ui.actions.metadata.CreateGenericSchemaAction#getClassForDoubleClick()
+     */
+    @Override
+    public Class getClassForDoubleClick() {
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.ui.actions.metadata.CreateGenericSchemaAction#init(org.talend.repository.model.RepositoryNode)
+     */
+    @Override
+    protected void init(RepositoryNode node) {
+
+        ERepositoryObjectType nodeType = (ERepositoryObjectType) node.getProperties(EProperties.CONTENT_TYPE);
+        if (nodeType == null) {
+            return;
+        }
+        if (nodeType != ERepositoryObjectType.METADATA_GENERIC_SCHEMA) {
+            return;
+        }
+
+        switch (node.getType()) {
+        case SIMPLE_FOLDER:
+        case SYSTEM_FOLDER:
+            collectChildNames(node);
+            setEnabled(true);
+            break;
+        default:
+            return;
+        }
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.jface.action.Action#run()
+     */
+    @Override
+    public void run() {
+        file = openImportFileDialog();
+        if (file == null) {
+            return;
+        }
+
+        openImportWizard();
+    }
+
+    private File openImportFileDialog() {
+        FileDialog dial = new FileDialog(PlatformUI.getWorkbench().getDisplay().getActiveShell(), SWT.OPEN);
+        dial.setFilterExtensions(new String[] { "*.xml" }); //$NON-NLS-1$
+        String fileName = dial.open();
+        if ((fileName != null) && (!fileName.equals(""))) { //$NON-NLS-1$
+            return new File(fileName);
+        } else {
+            return null;
+        }
+    }
+
+    private void openImportWizard() {
+        ISelection selection = getSelection();
+        ImportSchemaFileWizard wizard = new ImportSchemaFileWizard(PlatformUI.getWorkbench(), selection,
+                getExistingNames(), file);
+        if (!wizard.isInitOK()) {
+            wizard.dispose();
+            return;
+        }
+        WizardDialog wizardDialog = new WizardDialog(new Shell(), wizard);
+        wizardDialog.setPageSize(WIZARD_WIDTH, WIZARD_HEIGHT);
+        wizardDialog.create();
+        wizardDialog.open();
+        refresh(((IStructuredSelection) selection).getFirstElement());
+    }
+
+}
---
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/metadata/connection/genericshema/ImportSchemaFileWizard.java
@@ -0,0 +1,257 @@
+// ============================================================================
+//
+// Talend Community Edition
+//
+// Copyright (C) 2006-2007 Talend - www.talend.com
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the GNU Lesser General Public
+// License as published by the Free Software Foundation; either
+// version 2.1 of the License.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+//
+// ============================================================================
+package org.talend.repository.ui.wizards.metadata.connection.genericshema;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import org.apache.log4j.Logger;
+import org.eclipse.core.runtime.Path;
+import org.eclipse.jface.viewers.ISelection;
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.ui.INewWizard;
+import org.eclipse.ui.IWorkbench;
+import org.talend.commons.exception.BusinessException;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.image.ImageProvider;
+import org.talend.commons.ui.swt.dialogs.ErrorDialogWidthDetailArea;
+import org.talend.commons.utils.VersionUtils;
+import org.talend.core.CorePlugin;
+import org.talend.core.context.Context;
+import org.talend.core.context.RepositoryContext;
+import org.talend.core.model.metadata.MetadataSchema;
+import org.talend.core.model.metadata.builder.connection.ConnectionFactory;
+import org.talend.core.model.metadata.builder.connection.GenericSchemaConnection;
+import org.talend.core.model.metadata.builder.connection.MetadataColumn;
+import org.talend.core.model.metadata.builder.connection.MetadataTable;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.properties.PropertiesFactory;
+import org.talend.core.model.properties.Property;
+import org.talend.core.ui.images.ECoreImage;
+import org.talend.repository.i18n.Messages;
+import org.talend.repository.model.IProxyRepositoryFactory;
+import org.talend.repository.model.ProxyRepositoryFactory;
+import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.model.RepositoryNodeUtilities;
+import org.talend.repository.ui.wizards.RepositoryWizard;
+import org.xml.sax.SAXException;
+
+/**
+ * DOC ggu class global comment. Detailled comment <br/>
+ * 
+ */
+public class ImportSchemaFileWizard extends RepositoryWizard implements INewWizard {
+
+    private static Logger log = Logger.getLogger(ImportSchemaFileWizard.class);
+
+    private List<MetadataColumn> columns = Collections.EMPTY_LIST;
+
+    IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+
+    private GenericSchemaWizardPage genericSchemaWizardPage;
+
+    private GenericSchemaConnection connection;
+
+    private Property connectionProperty;
+
+    private ConnectionItem connectionItem;
+
+    private File file;
+
+    private boolean initOK = true;
+
+    public ImportSchemaFileWizard(IWorkbench workbench, ISelection selection, String[] existingNames, File file) {
+        super(workbench, true);
+        this.selection = selection;
+        this.existingNames = existingNames;
+        this.file = file;
+
+        setNeedsProgressMonitor(true);
+        setDefaultPageImageDescriptor(ImageProvider.getImageDesc(ECoreImage.METADATA_TABLE_WIZ));
+
+        if (file == null) {
+            return;
+        }
+
+        initWizard();
+
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.ui.wizards.metadata.connection.genericshema.GenericSchemaWizard#addPages()
+     */
+    @Override
+    public void addPages() {
+
+        setWindowTitle(Messages.getString("ImportSchemaFileWizard.WizardTitle"));//$NON-NLS-1$
+
+        genericSchemaWizardPage = new GenericSchemaWizardPage(2, connectionItem, isRepositoryObjectEditable(), null);
+        genericSchemaWizardPage.setTitle(Messages.getString("FileTableWizardPage.descriptionCreate")); //$NON-NLS-1$
+        //$NON-NLS-2$
+        genericSchemaWizardPage.setDescription(Messages.getString("FileWizardPage.descriptionUpdateStep0")); //$NON-NLS-1$
+        addPage(genericSchemaWizardPage);
+        genericSchemaWizardPage.setPageComplete(true);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.repository.ui.wizards.metadata.connection.genericshema.GenericSchemaWizard#performFinish()
+     */
+    @Override
+    public boolean performFinish() {
+        if (genericSchemaWizardPage.isPageComplete()) {
+
+            try {
+                setPropNewName(connectionProperty);
+                factory.create(connectionItem, pathToSave);
+
+            } catch (PersistenceException e) {
+                String detailError = e.toString();
+                new ErrorDialogWidthDetailArea(getShell(), PID,
+                        Messages.getString("CommonWizard.persistenceException"), //$NON-NLS-1$
+                        detailError);
+                log.error(Messages.getString("CommonWizard.persistenceException") + "\n" + detailError); //$NON-NLS-1$ //$NON-NLS-2$
+                return false;
+            } catch (BusinessException e) {
+                String detailError = e.toString();
+                new ErrorDialogWidthDetailArea(getShell(), PID,
+                        Messages.getString("CommonWizard.persistenceException"), //$NON-NLS-1$
+                        detailError);
+                log.error(Messages.getString("CommonWizard.persistenceException") + "\n" + detailError); //$NON-NLS-1$ //$NON-NLS-2$
+                return false;
+            }
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void init(IWorkbench workbench, IStructuredSelection selection) {
+        this.selection = selection;
+
+    }
+
+    public boolean isInitOK() {
+        return initOK;
+    }
+
+    private void initWizard() {
+        try {
+
+            columns = MetadataSchema.loadMetadataColumnFromFile(file);
+
+        } catch (ParserConfigurationException e) {
+            showErrorMessages(e.toString());
+            return;
+        } catch (SAXException e) {
+            showErrorMessages(e.toString());
+            return;
+        } catch (IOException e) {
+            showErrorMessages(e.toString());
+            return;
+        }
+
+        if (selection == null || existingNames == null) {
+            initConnection();
+            pathToSave = new Path("");
+        } else {
+
+            Object obj = ((IStructuredSelection) selection).getFirstElement();
+            RepositoryNode node = (RepositoryNode) obj;
+            switch (node.getType()) {
+            case SIMPLE_FOLDER:
+                pathToSave = RepositoryNodeUtilities.getPath(node);
+                break;
+            case SYSTEM_FOLDER:
+            default:
+                pathToSave = new Path(""); //$NON-NLS-1$
+            }
+
+            switch (node.getType()) {
+            case SIMPLE_FOLDER:
+            case SYSTEM_FOLDER:
+                initConnection();
+                break;
+            default:
+                return;
+            }
+        }
+
+        connectionItem = PropertiesFactory.eINSTANCE.createGenericSchemaConnectionItem();
+        connectionItem.setProperty(connectionProperty);
+        connectionItem.setConnection(connection);
+
+        MetadataTable metadataTable = ConnectionFactory.eINSTANCE.createMetadataTable();
+        metadataTable.setId(factory.getNextId());
+        metadataTable.setLabel("metadata");
+        metadataTable.setConnection(connection);
+
+        metadataTable.getColumns().addAll(columns);
+
+        connection.getTables().add(metadataTable);
+
+    }
+
+    private void initConnection() {
+        connectionProperty = PropertiesFactory.eINSTANCE.createProperty();
+        connectionProperty.setAuthor(((RepositoryContext) CorePlugin.getContext().getProperty(
+                Context.REPOSITORY_CONTEXT_KEY)).getUser());
+        connectionProperty.setVersion(VersionUtils.DEFAULT_VERSION);
+        connectionProperty.setStatusCode(""); //$NON-NLS-1$ 
+        String name = file.getName();
+        if (name.indexOf(".") > 0) {
+            name = name.substring(0, name.indexOf("."));
+        }
+        connectionProperty.setLabel(name);
+        connectionProperty.setId(factory.getNextId());
+        connection = ConnectionFactory.eINSTANCE.createGenericSchemaConnection();
+
+    }
+
+    private void setPropNewName(Property theProperty) throws PersistenceException, BusinessException {
+        String originalLabel = theProperty.getLabel();
+
+        char j = 'a';
+        while (!factory.isNameAvailable(theProperty.getItem(), null)) {
+            String nextTry = originalLabel + "_" + (j++); //$NON-NLS-1$ //$NON-NLS-2$
+            theProperty.setLabel(nextTry);
+            if (j > 'z') {
+                setPropNewName(theProperty);
+                return;
+            }
+        }
+    }
+
+    private void showErrorMessages(String detailError) {
+        new ErrorDialogWidthDetailArea(getShell(), PID, Messages.getString("CommonWizard.persistenceException"), //$NON-NLS-1$
+                detailError);
+        log.error(Messages.getString("CommonWizard.persistenceException") + "\n" + detailError); //$NON-NLS-1$ //$NON-NLS-2$
+        initOK = false;
+    }
+}
