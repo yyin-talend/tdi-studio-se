69ecffda1d044ddca5237fcd3ac911379fb02dfb
xtan <xtan@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2007-12-05 10:35:38 +0000

    Fixed issue:2406
    About the bug "more than one interate links will generate compile error".
    
    I give 2 temporary design.
    
    (and choice one implement it first)
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@7413 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/CodeGenerator.java
+++ org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/CodeGenerator.java
@@ -378,30 +378,38 @@ public class CodeGenerator implements ICodeGenerator {
             throws CodeGeneratorException {
         StringBuffer codeComponent = new StringBuffer();
         Boolean isMarked = subProcess.isMarkedNode(node, part);
-        boolean isIterate = isIterateNode(node);
+        boolean isIterate = isIterateNode(node, incomingName);
         if ((isMarked != null) && (!isMarked)) {
             switch (part) {
             case BEGIN:
-                if (isIterate) {
-                    codeComponent.append(generateComponentCode(node, ECodePart.BEGIN, incomingName));
-                }
+                // if (isIterate) {
+                // codeComponent.append(generateComponentCode(node, ECodePart.BEGIN, incomingName));
+                // }
                 codeComponent.append(generatesTreeCode(subProcess, node, part));
                 if (!isIterate) {
                     codeComponent.append(generateComponentCode(node, ECodePart.BEGIN, incomingName));
                 }
                 break;
             case MAIN:
+                if (isIterate) {
+                    codeComponent.append(generateComponentCode(node, ECodePart.BEGIN, incomingName));
+                }
+
                 codeComponent.append(generateComponentCode(node, ECodePart.MAIN, incomingName));
                 codeComponent.append(generatesTreeCode(subProcess, node, part));
+
+                if (isIterate) {
+                    codeComponent.append(generateComponentCode(node, ECodePart.END, incomingName));
+                }
                 break;
             case END:
                 if (!isIterate) {
                     codeComponent.append(generateComponentCode(node, ECodePart.END, incomingName));
                 }
                 codeComponent.append(generatesTreeCode(subProcess, node, part));
-                if (isIterate) {
-                    codeComponent.append(generateComponentCode(node, ECodePart.END, incomingName));
-                }
+                // if (isIterate) {
+                // codeComponent.append(generateComponentCode(node, ECodePart.END, incomingName));
+                // }
                 break;
             default:
                 // do nothing
@@ -418,13 +426,17 @@ public class CodeGenerator implements ICodeGenerator {
      * @param node the node to check
      * @return true if the node is an iterate node
      */
-    private boolean isIterateNode(INode node) {
+    private boolean isIterateNode(INode node, String incomingName) {
+        // it means the first node without any income connection
+        if (incomingName == null) {
+            return false;
+        }
         boolean result = false;
         if (node != null) {
-            List<? extends IConnection> outGoingConnections = node.getOutgoingConnections();
-            if ((outGoingConnections != null) && (outGoingConnections.size() > 0)) {
-                for (IConnection connection : outGoingConnections) {
-                    if (connection.getLineStyle() == EConnectionType.ITERATE) {
+            List<? extends IConnection> inComingIterateConnection = node.getIncomingConnections(EConnectionType.ITERATE);
+            if ((inComingIterateConnection != null) && (inComingIterateConnection.size() > 0)) {
+                for (IConnection connection : inComingIterateConnection) {
+                    if (connection.getName().equals(incomingName)) {
                         result = true;
                     }
                 }
@@ -463,7 +475,7 @@ public class CodeGenerator implements ICodeGenerator {
                     if (!connection.getLineStyle().hasConnectionCategory(IConnectionCategory.MERGE)) {
                         subTreeArgument.setInputSubtreeConnection(connection);
                         code.append(generateTypedComponentCode(EInternalTemplate.SUBTREE_BEGIN, subTreeArgument));
-                        code.append(generateComponentsCode(subProcess, targetNode, part, null));
+                        code.append(generateComponentsCode(subProcess, targetNode, part, connection.getName()));
                         code.append(generateTypedComponentCode(EInternalTemplate.SUBTREE_END, subTreeArgument));
                     } else if (part == ECodePart.MAIN) {
                         subTreeArgument.setInputSubtreeConnection(connection);
