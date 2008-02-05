a09aed31d2ecac872a9a4fb3ffffbcb0b3c9de88
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-02-05 09:33:50 +0000

    Rename Component Designer to Talend Component Designer in TOS preferences
    git-svn-id: http://talendforge.org/svn/tos/trunk@8599 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.componentdesigner/src/org/talend/componentdesigner/ui/preferencepage/ComponentProjectPreferencePage.java
+++ org.talend.componentdesigner/src/org/talend/componentdesigner/ui/preferencepage/ComponentProjectPreferencePage.java
@@ -31,74 +31,68 @@ import org.talend.componentdesigner.ComponentDesigenerPlugin;
 import org.talend.componentdesigner.PluginConstant;
 
 /**
- * DOC rli  class global comment. Detailled comment
+ * DOC rli class global comment. Detailled comment
  */
 public class ComponentProjectPreferencePage extends PreferencePage implements IWorkbenchPreferencePage {
 
-	private Text directoryText;
-	
+    private Text directoryText;
 
+    public void init(IWorkbench workbench) {
+        setPreferenceStore(ComponentDesigenerPlugin.getDefault().getPreferenceStore());
+    }
 
+    @Override
+    protected Control createContents(Composite parent) {
+        Label settingsLabel = new Label(parent, SWT.None);
+        settingsLabel.setText("Choose a project folder for this component designer"); //$NON-NLS-1$
+        GridDataFactory.fillDefaults().span(2, 1).applyTo(settingsLabel);
+        Composite projSelComposite = new Composite(parent, SWT.NONE);
+        projSelComposite.setLayout(new GridLayout(9, false));
+        GridData data = new GridData(GridData.FILL_BOTH);
+        data.widthHint = 350;
+        projSelComposite.setLayoutData(data);
 
-	public void init(IWorkbench workbench) {
-		setPreferenceStore(ComponentDesigenerPlugin.getDefault()
-				.getPreferenceStore());
-	}
+        Label label = new Label(projSelComposite, SWT.None);
+        GridData gridData = new GridData();
+        gridData.horizontalSpan = 2;
+        label.setLayoutData(gridData);
+        label.setText("Component Project:");
 
+        directoryText = new Text(projSelComposite, SWT.BORDER);
+        gridData = new GridData(GridData.FILL_HORIZONTAL);
+        gridData.horizontalSpan = 6;
+        directoryText.setLayoutData(gridData);
 
-	@Override
-	protected Control createContents(Composite parent) {
-		Label settingsLabel = new Label(parent, SWT.None);
-		settingsLabel
-				.setText("Choose a project folder for this component desigener"); //$NON-NLS-1$
-		GridDataFactory.fillDefaults().span(2, 1).applyTo(settingsLabel);
-		Composite projSelComposite = new Composite(parent, SWT.NONE);
-		projSelComposite.setLayout(new GridLayout(9, false));
-		GridData data = new GridData(GridData.FILL_BOTH);
-		data.widthHint = 350;
-		projSelComposite.setLayoutData(data);
+        Button browserButton = new Button(projSelComposite, SWT.None);
+        gridData = new GridData();
+        gridData.horizontalSpan = 1;
+        browserButton.setLayoutData(gridData);
+        browserButton.setText(PluginConstant.BROWSER_LABEL);
+        browserButton.addSelectionListener(new SelectionAdapter() {
 
-		Label label = new Label(projSelComposite, SWT.None);
-		GridData gridData = new GridData();
-		gridData.horizontalSpan = 2;
-		label.setLayoutData(gridData);
-		label.setText("Component Project:");
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                directoryText.setText(getPathFromDialog());
+            }
+        });
+        if (this.getPreferenceStore().getString(PluginConstant.PROJECT_URL) != null) {
+            this.directoryText.setText(getPreferenceStore().getString(PluginConstant.PROJECT_URL));
+        }
+        return parent;
+    }
 
-		directoryText = new Text(projSelComposite, SWT.BORDER);
-		gridData = new GridData(GridData.FILL_HORIZONTAL);
-		gridData.horizontalSpan = 6;
-		directoryText.setLayoutData(gridData);
+    private String getPathFromDialog() {
+        DirectoryDialog dialog = new DirectoryDialog(this.getShell());
+        dialog.setMessage("Select Component Project Path");
+        String path = dialog.open();
+        return path == null ? PluginConstant.EMPTY_STRING : path;
+    }
 
-		Button browserButton = new Button(projSelComposite, SWT.None);
-		gridData = new GridData();
-		gridData.horizontalSpan = 1;
-		browserButton.setLayoutData(gridData);
-		browserButton.setText(PluginConstant.BROWSER_LABEL);
-		browserButton.addSelectionListener(new SelectionAdapter() {
-
-			public void widgetSelected(SelectionEvent e) {
-				directoryText.setText(getPathFromDialog());
-			}
-		});
-		if (this.getPreferenceStore().getString(PluginConstant.PROJECT_URL) != null) {
-			this.directoryText.setText(getPreferenceStore().getString(
-					PluginConstant.PROJECT_URL));
-		}
-		return parent;
-	}
-	
-
-	private String getPathFromDialog() {
-		DirectoryDialog dialog = new DirectoryDialog(this.getShell());
-		dialog.setMessage("Select Component Project Path");
-		String path = dialog.open();
-		return path == null ? PluginConstant.EMPTY_STRING : path;
-	}
-	
-	 public boolean performOk() {
-		ComponentDesigenerPlugin.getDefault().getPreferenceStore().setValue(
-				PluginConstant.PROJECT_URL, this.directoryText.getText());
-		ComponentDesigenerPlugin.getDefault().creatComponentProj();
-		return super.performOk();
-	}
+    @Override
+    public boolean performOk() {
+        ComponentDesigenerPlugin.getDefault().getPreferenceStore().setValue(PluginConstant.PROJECT_URL,
+                this.directoryText.getText());
+        ComponentDesigenerPlugin.getDefault().creatComponentProj();
+        return super.performOk();
+    }
 }
