8cec48f80c431a57c346a90a4a298ecf14ae6e56
cpei <cpei@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-02-28 07:41:24 +0000

    migration task for the renaming tFor and new property in new tLoop.
    git-svn-id: http://talendforge.org/svn/tos/trunk@11802 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/RenametFor.java
@@ -0,0 +1,55 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.AddPropertyLoopTypeConversion;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.conversions.RenameComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.ProcessItem;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class RenametFor extends AbstractJobMigrationTask {
+
+    public ExecutionResult executeOnProcess(ProcessItem item) {
+        try {
+            if (getProject().getLanguage().equals(ECodeLanguage.JAVA)) {
+                IComponentFilter filter1 = new NameComponentFilter("tFor"); //$NON-NLS-1$
+
+                IComponentConversion addProperty = new AddPropertyLoopTypeConversion();
+                IComponentConversion renameComponent = new RenameComponentConversion("tLoop");
+
+                ModifyComponentsAction.searchAndModify(item, filter1, Arrays.<IComponentConversion> asList(addProperty,
+                        renameComponent));
+
+                return ExecutionResult.SUCCESS_WITH_ALERT;
+            } else {
+                // do nothing
+                return ExecutionResult.NOTHING_TO_DO;
+            }
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
+
+}
