0e4557e60d9160c6b15b797da5b94808b916f77d
mhirt <mhirt@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-03-19 00:27:01 +0000

    Simple Java format Fix for feature:3359 Perl Migration Task
    git-svn-id: http://talendforge.org/svn/tos/trunk@12459 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/MoveExcelSheetnameToSheetlist.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/MoveExcelSheetnameToSheetlist.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.repository.model.migration;
 
 import java.util.ArrayList;
@@ -15,57 +27,53 @@ import org.talend.core.model.components.conversions.RemovePropertyComponentConve
 import org.talend.core.model.components.filters.IComponentFilter;
 import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
-import org.talend.core.model.migration.IProjectMigrationTask.ExecutionResult;
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.TalendFileFactory;
 
+/**
+ * plegall : Feature 3359 added: Perl tFileInputExcel can read multiple sheets at once (optionnaly all sheets). A
+ * migration task was added to move the old sheetname (or position) to the first sheet in the new table.
+ */
 public class MoveExcelSheetnameToSheetlist extends AbstractJobMigrationTask {
 
-	@Override
-	public ExecutionResult executeOnProcess(ProcessItem item) {
+    @Override
+    public ExecutionResult executeOnProcess(ProcessItem item) {
         if (getProject().getLanguage() == ECodeLanguage.JAVA) {
             return ExecutionResult.NOTHING_TO_DO;
         }
-		try {
-			IComponentFilter filter1 = new NameComponentFilter(
-					"tFileInputExcel"); //$NON-NLS-1$
+        try {
+            IComponentFilter filter1 = new NameComponentFilter("tFileInputExcel"); //$NON-NLS-1$
 
-			IComponentConversion addNewProperty = new IComponentConversion() {
+            IComponentConversion addNewProperty = new IComponentConversion() {
 
-				public void transform(NodeType node) {
-					ComponentUtilities.addNodeProperty(node, "SHEETLIST",
-							"TABLE");
+                public void transform(NodeType node) {
+                    ComponentUtilities.addNodeProperty(node, "SHEETLIST", "TABLE");
 
-					List<ElementValueType> values = new ArrayList<ElementValueType>();
-					ElementValueType eValue = TalendFileFactory.eINSTANCE
-							.createElementValueType();
-					eValue.setElementRef("SHEETNAME");
-					eValue.setValue(ComponentUtilities.getNodePropertyValue(
-							node, "SHEETNAME"));
-					values.add(eValue);
+                    List<ElementValueType> values = new ArrayList<ElementValueType>();
+                    ElementValueType eValue = TalendFileFactory.eINSTANCE.createElementValueType();
+                    eValue.setElementRef("SHEETNAME");
+                    eValue.setValue(ComponentUtilities.getNodePropertyValue(node, "SHEETNAME"));
+                    values.add(eValue);
 
-					ComponentUtilities.setNodeProperty(node, "SHEETLIST",
-							values);
-				}
-			};
-			IComponentConversion removeOldProperty = new RemovePropertyComponentConversion(
-					"SHEETNAME"); //$NON-NLS-1$
+                    ComponentUtilities.setNodeProperty(node, "SHEETLIST", values);
+                }
+            };
+            IComponentConversion removeOldProperty = new RemovePropertyComponentConversion("SHEETNAME"); //$NON-NLS-1$
 
-			ModifyComponentsAction.searchAndModify(item, filter1, Arrays
-					.<IComponentConversion> asList(addNewProperty,
-							removeOldProperty));
+            ModifyComponentsAction.searchAndModify(item, filter1, Arrays.<IComponentConversion> asList(addNewProperty,
+                    removeOldProperty));
 
-			return ExecutionResult.SUCCESS_WITH_ALERT;
-		} catch (Exception e) {
-			ExceptionHandler.process(e);
-			return ExecutionResult.FAILURE;
-		}
-	}
+            return ExecutionResult.SUCCESS_WITH_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
 
-	public Date getOrder() {
-		GregorianCalendar gc = new GregorianCalendar(2008, 2, 18, 10, 10, 0);
-		return gc.getTime();
-	}
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2008, 2, 18, 10, 10, 0);
+        return gc.getTime();
+    }
 }
