f7037daeb439d00ebe264ac0f9b986e3964960f7
amaumont <amaumont@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-04-01 13:12:13 +0000

    Invisible temporary components added: tAdvancedHashPersistent and tMapPersistent
    git-svn-id: http://talendforge.org/svn/tos/trunk@12927 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/commons/IMemoryLookup.java
@@ -0,0 +1,42 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+
+package org.talend.designer.components.commons;
+
+import java.io.IOException;
+
+/**
+ * DOC amaumont class global comment. Detailled comment <br/>
+ * 
+ * @param <K> K
+ * @param <V> V
+ */
+public interface IMemoryLookup<K, V> {
+
+    public void initPut();
+
+    public V put(V bean);
+
+    public void endPut();
+
+    public void initGet();
+
+    public void lookup(K key);
+
+    public boolean hasNext();
+
+    public V next();
+
+    public void endGet();
+
+}
