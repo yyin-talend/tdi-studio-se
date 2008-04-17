1f0f83d21d9d109d7c9624469afb66d7ec2a6276
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-04-17 03:22:02 +0000

    feature 2455 modified: change the migration task from project to worksapce.
    git-svn-id: http://talendforge.org/svn/tos/trunk@13327 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/preferences/migration/AddQuotesForStatLogsPreference.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/preferences/migration/AddQuotesForStatLogsPreference.java
@@ -17,9 +17,8 @@ import java.util.GregorianCalendar;
 
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.talend.core.language.ECodeLanguage;
-import org.talend.core.language.LanguageManager;
-import org.talend.core.model.general.Project;
-import org.talend.core.model.migration.AbstractProjectMigrationTask;
+import org.talend.core.model.migration.AbstractMigrationTask;
+import org.talend.core.model.migration.IWorkspaceMigrationTask;
 import org.talend.core.model.utils.TalendTextUtils;
 import org.talend.designer.core.DesignerPlugin;
 import org.talend.designer.core.model.components.EParameterName;
@@ -27,12 +26,15 @@ import org.talend.designer.core.model.components.EParameterName;
 /**
  * yzhang class global comment. Detailled comment
  */
-public class AddQuotesForStatLogsPreference extends AbstractProjectMigrationTask {
+public class AddQuotesForStatLogsPreference extends AbstractMigrationTask implements IWorkspaceMigrationTask {
 
-    private String languagePrefix = LanguageManager.getCurrentLanguage() + "_";
+    private String javaPrefix = ECodeLanguage.JAVA + "_";
 
-    private String quoteType = LanguageManager.getCurrentLanguage() == ECodeLanguage.JAVA ? TalendTextUtils.QUOTATION_MARK
-            : TalendTextUtils.SINGLE_QUOTE;
+    private String perlPrefix = ECodeLanguage.PERL + "_";
+
+    private String javaQuoteType = TalendTextUtils.QUOTATION_MARK;
+
+    private String perlQuoteType = TalendTextUtils.SINGLE_QUOTE;
 
     private String[] ids = new String[] { EParameterName.FILE_PATH.getName(), EParameterName.FILENAME_STATS.getName(),
             EParameterName.FILENAME_LOGS.getName(), EParameterName.FILENAME_METTER.getName(), EParameterName.HOST.getName(),
@@ -41,20 +43,17 @@ public class AddQuotesForStatLogsPreference extends AbstractProjectMigrationTask
             EParameterName.DBFILE.getName(), EParameterName.TABLE_STATS.getName(), EParameterName.TABLE_LOGS.getName(),
             EParameterName.TABLE_METER.getName() };
 
-    /*
-     * (non-Javadoc)
-     * 
-     * @see org.talend.core.model.migration.IProjectMigrationTask#execute(org.talend.core.model.general.Project)
+    /**
+     * yzhang Comment method "restoreValues".
      */
-    public ExecutionResult execute(Project project) {
+    private void restoreValues(String languagePrefix, String qouteType) {
         IPreferenceStore store = DesignerPlugin.getDefault().getPreferenceStore();
         for (String id : ids) {
             String value = store.getString(languagePrefix + id);
-            if (value != null && (!value.endsWith(quoteType) || !value.startsWith(quoteType))) {
-                store.setValue(id, TalendTextUtils.addQuotes(value));
+            if (value != null && (!value.endsWith(qouteType) || !value.startsWith(qouteType))) {
+                store.setValue(languagePrefix + id, TalendTextUtils.addQuotes(value));
             }
         }
-        return ExecutionResult.SUCCESS_NO_ALERT;
     }
 
     /*
@@ -66,4 +65,17 @@ public class AddQuotesForStatLogsPreference extends AbstractProjectMigrationTask
         GregorianCalendar gc = new GregorianCalendar(2008, 4, 15, 15, 0, 0);
         return gc.getTime();
     }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.IWorkspaceMigrationTask#execute()
+     */
+    public boolean execute() {
+        restoreValues(javaPrefix, javaQuoteType);
+        restoreValues(perlPrefix, perlQuoteType);
+        return true;
+
+    }
+
 }
