6b68cc89eaf362152687d6d0c6c8e69366c4163a
ggu <ggu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-04 06:19:39 +0000

    feature 2507 modification: add a new action for the connections items / schemas.
    git-svn-id: http://talendforge.org/svn/tos/trunk@13753 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/ui/actions/metadata/DetecteViewImpactAction.java
@@ -0,0 +1,140 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.ui.actions.metadata;
+
+import org.eclipse.jface.viewers.IStructuredSelection;
+import org.eclipse.jface.viewers.TreeViewer;
+import org.talend.commons.ui.image.EImage;
+import org.talend.commons.ui.image.ImageProvider;
+import org.talend.core.model.metadata.builder.connection.AbstractMetadataObject;
+import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.metadata.builder.connection.MetadataTable;
+import org.talend.core.model.metadata.builder.connection.Query;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.properties.ContextItem;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.repository.IRepositoryObject;
+import org.talend.core.model.update.RepositoryUpdateManager;
+import org.talend.repository.i18n.Messages;
+import org.talend.repository.model.ProxyRepositoryFactory;
+import org.talend.repository.model.RepositoryNode;
+import org.talend.repository.ui.actions.AContextualAction;
+import org.talend.repository.ui.views.RepositoryContentProvider.ISubRepositoryObject;
+
+/**
+ * ggu class global comment. Detailled comment
+ */
+public class DetecteViewImpactAction extends AContextualAction {
+
+    private static final String LABEL = Messages.getString("DetecteViewImpactAction.Label"); //$NON-NLS-1$
+
+    /**
+     * ggu DetectedModificationAction constructor comment.
+     */
+    public DetecteViewImpactAction() {
+        setText(LABEL);
+        setToolTipText(LABEL);
+        setImageDescriptor(ImageProvider.getImageDesc(EImage.REFRESH_ICON));
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.commons.ui.swt.actions.ITreeContextualAction#init(org.eclipse.jface.viewers.TreeViewer,
+     * org.eclipse.jface.viewers.IStructuredSelection)
+     */
+    public void init(TreeViewer viewer, IStructuredSelection selection) {
+        boolean canWork = !selection.isEmpty() && selection.size() == 1;
+        if (canWork) {
+            if (ProxyRepositoryFactory.getInstance().isUserReadOnlyOnCurrentProject()) {
+                canWork = false;
+            } else {
+                Object o = selection.getFirstElement();
+                RepositoryNode node = (RepositoryNode) o;
+                switch (node.getType()) {
+                case REPOSITORY_ELEMENT:
+                    switch (node.getObjectType()) {
+                    case METADATA_CON_TABLE:
+                    case METADATA_CON_QUERY:
+
+                    case METADATA_CONNECTIONS:
+                    case METADATA_FILE_DELIMITED:
+                    case METADATA_FILE_POSITIONAL:
+                    case METADATA_FILE_REGEXP:
+                    case METADATA_FILE_XML:
+                    case METADATA_FILE_LDIF:
+                    case METADATA_FILE_EXCEL:
+                        // case METADATA_SALESFORCE_SCHEMA:
+                        // case METADATA_LDAP_SCHEMA:
+                        // case METADATA_WSDL_SCHEMA:
+
+                    case CONTEXT:
+                        canWork = true;
+                        break;
+                    default:
+                        canWork = false;
+                    }
+                    break;
+                default:
+                    canWork = false;
+                }
+            }
+        }
+        setEnabled(canWork);
+
+    }
+
+    @Override
+    public void run() {
+        RepositoryNode node = getCurrentRepositoryNode();
+        if (node == null) {
+            return;
+        }
+        if (node.getObject() instanceof ISubRepositoryObject) {
+            ISubRepositoryObject subObject = (ISubRepositoryObject) node.getObject();
+            if (subObject != null) {
+                // schema
+                AbstractMetadataObject metadataObject = subObject.getAbstractMetadataObject();
+                if (metadataObject instanceof MetadataTable) {
+                    RepositoryUpdateManager.updateSchema((MetadataTable) metadataObject, false);
+                } else
+                // query
+                if (metadataObject instanceof Query) {
+                    RepositoryUpdateManager.updateQuery((Query) metadataObject, false);
+                }
+            }
+        } else if (node.getObject() instanceof IRepositoryObject) {
+            IRepositoryObject object = (IRepositoryObject) node.getObject();
+            if (object != null) {
+                Item item = object.getProperty().getItem();
+                if (item != null) {
+                    // context
+                    if (item instanceof ContextItem) {
+                        RepositoryUpdateManager.updateContext((ContextItem) item, false);
+                    } else
+                    // connection
+                    if (item instanceof ConnectionItem) {
+                        Connection connection = ((ConnectionItem) item).getConnection();
+                        if (connection instanceof DatabaseConnection) {
+                            RepositoryUpdateManager.updateDBConnection((DatabaseConnection) connection, false);
+                        } else {
+                            RepositoryUpdateManager.updateFileConnection(connection, false);
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+}
