8ca6e27bd1bdb50d9d8a148b67d9d92bddf046e1
qzhang <qzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-04 09:43:00 +0000

    fixed bug 3547: correct the View all Changes action.
    git-svn-id: http://talendforge.org/svn/tos/trunk@13766 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/dataset/dataset/DataSetTable.java
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/dataset/dataset/DataSetTable.java
@@ -47,7 +47,6 @@ public class DataSetTable {
 
     }
 
-
     /**
      * Create a new table element for a resultset in a given composite.
      * 
@@ -56,16 +55,17 @@ public class DataSetTable {
      * @param info text displayed in bottem left corner under table
      */
     public DataSetTable(Composite parent, final DataSet dataSet, String info) throws Exception {
-        Composite composite = new Composite(parent, SWT.FILL);        
+        Composite composite = new Composite(parent, SWT.FILL);
         GridLayout layout = new GridLayout();
-        layout.numColumns = 2;                
+        layout.numColumns = 2;
         layout.marginLeft = 0;
         layout.horizontalSpacing = 0;
         layout.verticalSpacing = 2;
         layout.marginWidth = 0;
-        layout.marginHeight = 0;        
+        layout.marginHeight = 0;
         composite.setLayout(layout);
-        
+        composite.setLayoutData(new GridData(GridData.FILL_BOTH));
+
         // check column labels & types
         String[] columnLabels = dataSet.getColumnLabels();
         int[] columnTypes = dataSet.getColumnTypes();
@@ -73,82 +73,84 @@ public class DataSetTable {
                 || columnTypes.length != columnLabels.length) {
             throw new Exception(Messages.getString("DataSetTable.invalidExcepitonMessage")); //$NON-NLS-1$
         }
-        
+
         // create table structure
-        final TableViewer tableViewer = new TableViewer(composite, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI | SWT.VIRTUAL);
-        final Table table = tableViewer.getTable();        
+        final TableViewer tableViewer = new TableViewer(composite, SWT.BORDER | SWT.V_SCROLL | SWT.H_SCROLL | SWT.MULTI
+                | SWT.VIRTUAL);
+        final Table table = tableViewer.getTable();
         tableViewer.setColumnProperties(columnLabels);
         table.setItemCount(dataSet.getRows().length);
 
         // create listener for sorting
-    	Listener sortListener = new Listener() {
-    		public void handleEvent(Event e) {
-    			
-    			// determine new sort column and direction
-    			TableColumn sortColumn = table.getSortColumn();
-    			TableColumn currentColumn = (TableColumn) e.widget;
-    			int dir = table.getSortDirection();
-    			if (sortColumn == currentColumn) {
-    				dir = dir == SWT.UP ? SWT.DOWN : SWT.UP;
-    			} else {
-    				table.setSortColumn(currentColumn);
-    				dir = SWT.UP;
-    			}
-    			
-    			// sort the data based on column and direction
-    			dataSet.sort(((Integer) currentColumn.getData("orignalColumnIndex")).intValue(), dir);
-    			
-    			// update data displayed in table
-    			table.setSortDirection(dir);
-    			table.clearAll();
-    		}
-    	};        
-        
+        Listener sortListener = new Listener() {
+
+            public void handleEvent(Event e) {
+
+                // determine new sort column and direction
+                TableColumn sortColumn = table.getSortColumn();
+                TableColumn currentColumn = (TableColumn) e.widget;
+                int dir = table.getSortDirection();
+                if (sortColumn == currentColumn) {
+                    dir = dir == SWT.UP ? SWT.DOWN : SWT.UP;
+                } else {
+                    table.setSortColumn(currentColumn);
+                    dir = SWT.UP;
+                }
+
+                // sort the data based on column and direction
+                dataSet.sort(((Integer) currentColumn.getData("orignalColumnIndex")).intValue(), dir);
+
+                // update data displayed in table
+                table.setSortDirection(dir);
+                table.clearAll();
+            }
+        };
+
         GridData tGridData = new GridData();
         tGridData.horizontalSpan = 2;
         tGridData.grabExcessHorizontalSpace = true;
         tGridData.grabExcessVerticalSpace = true;
         tGridData.horizontalAlignment = SWT.FILL;
         tGridData.verticalAlignment = SWT.FILL;
-        table.setLayoutData(tGridData);        
+        table.setLayoutData(tGridData);
         GridLayout tlayout = new GridLayout();
-        tlayout.numColumns = 2;                
+        tlayout.numColumns = 2;
         tlayout.marginLeft = 0;
-        table.setLayout(tlayout);        
+        table.setLayout(tlayout);
         table.setHeaderVisible(true);
         table.setLinesVisible(true);
 
         // store dataset for use in actions
         table.setData(dataSet);
-        
+
         // add all column headers to our table
-        for (int i = 0; i < columnLabels.length; i++) {            
+        for (int i = 0; i < columnLabels.length; i++) {
             // add column header
-            TableColumn column = new TableColumn(table, SWT.LEFT);           
+            TableColumn column = new TableColumn(table, SWT.LEFT);
             column.setText(columnLabels[i]);
             column.setMoveable(true);
-            column.setResizable(true);            
+            column.setResizable(true);
             column.addListener(SWT.Selection, sortListener);
             column.setData("orignalColumnIndex", new Integer(i));
         }
-                     
+
         tableViewer.setContentProvider(new DataSetTableContentProvider());
         tableViewer.setLabelProvider(new DataSetTableLabelProvider());
         tableViewer.setInput(dataSet);
 
         // make columns full size
         for (int i = 0; i < table.getColumnCount(); i++) {
-            table.getColumn(i).pack();      
+            table.getColumn(i).pack();
         }
-        
+
         // add status bar labels
         Label infoLabel = new Label(composite, SWT.NULL);
         infoLabel.setText(info);
-        infoLabel.setLayoutData(new GridData(SWT.LEFT, SWT.NULL, true, false));        
+        infoLabel.setLayoutData(new GridData(SWT.LEFT, SWT.NULL, true, false));
         final Label positionLabel = new Label(composite, SWT.NULL);
         positionLabel.setText(""); //$NON-NLS-1$
-        positionLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.NULL, true, false));       
-         
+        positionLabel.setLayoutData(new GridData(SWT.RIGHT, SWT.NULL, true, false));
+
         // create a TableCursor to navigate around the table
         final TableCursor cursor = new TableCursor(table, SWT.NONE);
         cursor.setBackground(table.getDisplay().getSystemColor(SWT.COLOR_LIST_SELECTION));
@@ -156,25 +158,26 @@ public class DataSetTable {
         cursor.setLayout(new FillLayout());
         cursor.setVisible(false);
         cursor.addSelectionListener(new SelectionAdapter() {
-            // when the TableEditor is over a cell, select the corresponding row in 
+
+            // when the TableEditor is over a cell, select the corresponding row in
             // the table
-            public void widgetSelected(SelectionEvent e) {                
-                table.setSelection(new TableItem[] {cursor.getRow()});
+            public void widgetSelected(SelectionEvent e) {
+                table.setSelection(new TableItem[] { cursor.getRow() });
                 cursor.setVisible(true);
-                
+
                 // update label with row/column position
                 positionLabel.setText(Messages.getString("DatabaseDetailView.Tab.RowPrefix") + " " //$NON-NLS-1$ //$NON-NLS-2$
                         + (table.indexOf(cursor.getRow()) + 1) + Messages.getString("DatabaseDetailView.Tab.ColumnPrefix") + " " //$NON-NLS-1$ //$NON-NLS-2$
-                        + (cursor.getColumn() + 1));                
+                        + (cursor.getColumn() + 1));
                 positionLabel.getParent().layout();
                 positionLabel.redraw();
             }
         });
-        
-        
+
         // add resize listener for cursor, to stop cursor from
         // taking strange shapes after being table is resized
         cursor.addControlListener(new ControlAdapter() {
+
             public void controlResized(ControlEvent e) {
                 if (cursor != null) {
                     if (cursor.getRow() == null) {
@@ -187,25 +190,22 @@ public class DataSetTable {
                 }
             }
         });
-        
-        
+
         table.addFocusListener(new FocusAdapter() {
+
             public void focusGained(FocusEvent e) {
                 Table t = (Table) e.widget;
                 if (t.getItemCount() != 0) {
                     cursor.setVisible(true);
                 }
-            }           
+            }
         });
 
- 
         // refresh tab on F5, copy cell on CTRL-C, etc
         KeyListener keyListener = new DataSetTableKeyListener(parent, table, cursor);
         cursor.addKeyListener(keyListener);
         table.addKeyListener(keyListener);
-                    
+
     }
 
 }
-
-
