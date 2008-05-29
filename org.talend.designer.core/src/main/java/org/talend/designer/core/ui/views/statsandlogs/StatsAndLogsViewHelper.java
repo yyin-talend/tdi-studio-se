78cce11826db300cbcf4b2d56579e80cbc87c589
qwei <qwei@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-05-29 06:57:43 +0000

    about stats & logs  view: fixed "save to preferences" button have a null point.
    git-svn-id: http://talendforge.org/svn/tos/trunk@14503 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/statsandlogs/StatsAndLogsViewHelper.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/statsandlogs/StatsAndLogsViewHelper.java
@@ -451,9 +451,9 @@ public class StatsAndLogsViewHelper {
                         item = UpdateRepositoryUtils.getConnectionItemByItemId(itemId);
                     }
                 }
+                PREFERENCE_STORE.setValue(LANGUAGE_PREFIX + EParameterName.PROPERTY_TYPE.getName(), propertyType);
+                PREFERENCE_STORE.setValue(LANGUAGE_PREFIX + EParameterName.REPOSITORY_PROPERTY_TYPE.getName(), itemId);
                 if (item != null) {
-                    PREFERENCE_STORE.setValue(LANGUAGE_PREFIX + EParameterName.PROPERTY_TYPE.getName(), propertyType);
-                    PREFERENCE_STORE.setValue(LANGUAGE_PREFIX + EParameterName.REPOSITORY_PROPERTY_TYPE.getName(), itemId);
                     PREFERENCE_STORE.setValue(LANGUAGE_PREFIX + EParameterName.REPOSITORY_PROPERTY_TYPE.getName()
                             + StatsAndLogsPreferencePage.CONNECTION_ITEM_LABEL, item.getProperty().getLabel());
                 }
