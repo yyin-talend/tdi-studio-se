5691728d7a3ba9dc655e172f9b6b888771a3532b
mhelleboid <mhelleboid@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-07-11 22:43:57 +0000

    svn merge -r16147:16146 from trunk to trunk
    git-svn-id: http://talendforge.org/svn/tos/trunk@16148 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/model/ComponentsFactory.java
+++ org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/model/ComponentsFactory.java
@@ -29,7 +29,6 @@ import org.eclipse.core.runtime.FileLocator;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
 import org.osgi.framework.Bundle;
-import org.talend.commons.CommonsPlugin;
 import org.talend.commons.exception.BusinessException;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.utils.io.FilesUtils;
@@ -287,10 +286,6 @@ public class ComponentsFactory implements IComponentsFactory {
     }
 
     private void loadIcons(File folder, IComponent component) {
-        if (CommonsPlugin.isHeadless()) {
-            return;
-        }
-
         ComponentIconLoading cil = new ComponentIconLoading(folder);
 
         component.setIcon32(cil.getImage32());
@@ -344,6 +339,7 @@ public class ComponentsFactory implements IComponentsFactory {
         URL url = FileLocator.toFileURL(FileLocator.find(b, new Path(IComponentsFactory.COMPONENTS_INNER_FOLDER), null));
         return url;
     }
+
     /*
      * (non-Javadoc)
      * 
