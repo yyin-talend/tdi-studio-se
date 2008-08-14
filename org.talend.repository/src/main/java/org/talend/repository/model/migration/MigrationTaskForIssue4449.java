e6e4e1fe881b61fad5bfb1d09cca69624c361a2d
cshan <cshan@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-08-14 07:12:05 +0000

    issue 4449 fixed: make this migration task faster and after it, save the item file.
    git-svn-id: http://talendforge.org/svn/tos/trunk@16988 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/MigrationTaskForIssue4449.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/MigrationTaskForIssue4449.java
@@ -15,12 +15,15 @@ package org.talend.repository.model.migration;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import org.talend.commons.exception.PersistenceException;
 import org.talend.core.language.ECodeLanguage;
 import org.talend.core.model.components.ComponentUtilities;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.ProcessItem;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+import org.talend.repository.model.ProxyRepositoryFactory;
 
 /**
  * DOC Administrator class global comment. Detailled comment
@@ -29,13 +32,22 @@ public class MigrationTaskForIssue4449 extends AbstractJobMigrationTask {
 
     public ExecutionResult executeOnProcess(ProcessItem item) {
         if (getProject().getLanguage() == ECodeLanguage.JAVA) {
+            boolean isModified = false;
             ProcessType processType = item.getProcess();
             for (Object nodeType : processType.getNode()) {
                 NodeType tmpNodeType = (NodeType) nodeType;
-                if (ComponentUtilities.getNodePropertyValue(tmpNodeType, "TABLE_ACTION") != null) {
-                    if (ComponentUtilities.getNodePropertyValue(tmpNodeType, "TABLE_ACTION").equals("CLEAR")) {
-                        ComponentUtilities.setNodeValue(tmpNodeType, "TABLE_ACTION", "TRUNCATE");
-                    }
+                ElementParameterType property = ComponentUtilities.getNodeProperty(tmpNodeType, "TABLE_ACTION");
+                if (property != null && property.getValue().equals("CLEAR")) {
+                    property.setValue("TRUNCATE");
+                    isModified = true;
+                }
+            }
+            if (isModified) {
+                ProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+                try {
+                    factory.save(item, true);
+                } catch (PersistenceException e) {
+                    throw new RuntimeException(e);
                 }
             }
             return ExecutionResult.SUCCESS_WITH_ALERT;
