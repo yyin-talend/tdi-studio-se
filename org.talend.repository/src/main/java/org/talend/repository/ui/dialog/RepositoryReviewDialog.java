a5037a3b494af395ac5b32e7db2c395b0eda8d82
ftang <ftang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-08-22 02:53:56 +0000

    Feature 4829 and 4830 modification: update the components to be able to use the property switch.
    git-svn-id: http://talendforge.org/svn/tos/trunk@17193 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/dialog/RepositoryReviewDialog.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/dialog/RepositoryReviewDialog.java
@@ -49,7 +49,6 @@ import org.talend.core.model.properties.Item;
 import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.ui.ICDCProviderService;
 import org.talend.repository.ProjectManager;
-import org.talend.repository.UpdateRepositoryUtils;
 import org.talend.repository.model.MetadataTableRepositoryObject;
 import org.talend.repository.model.ProjectRepositoryNode;
 import org.talend.repository.model.RepositoryNode;
@@ -653,7 +652,7 @@ class RepositoryTypeProcessor implements ITypeProcessor {
                         // is
                         // specified
                         // //$NON-NLS-1$
-                        String neededDbType = UpdateRepositoryUtils.getNeededDbType(currentDbType, repositoryType); //$NON-NLS-1$
+                        String neededDbType = repositoryType.substring(repositoryType.indexOf(":") + 1); //$NON-NLS-1$
                         if (!MetadataTalendType.sameDBProductType(neededDbType, currentDbType)) {
                             return false;
                         }
