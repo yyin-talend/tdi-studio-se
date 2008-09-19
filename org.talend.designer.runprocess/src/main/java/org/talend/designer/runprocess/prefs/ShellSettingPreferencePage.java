1521fd711ae78fc2f2b6549cc8896a3ba64d109f
ftang <ftang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-09-19 02:55:25 +0000

    Feature 3241 fixed: Allowing templating for exported shell-scripts.
    git-svn-id: http://talendforge.org/svn/tos/trunk@18019 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/prefs/ShellSettingPreferencePage.java
+++ org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/prefs/ShellSettingPreferencePage.java
@@ -80,52 +80,7 @@ public class ShellSettingPreferencePage extends FieldEditorPreferencePage implem
      */
     @Override
     protected void createFieldEditors() {
-
-        // commandField = new StringFieldEditor(ITalendCorePrefConstants.COMMAND_STR, "Command",
-        // getFieldEditorParent());
-        //
-        // addField(commandField);
-        // Composite parent = getFieldEditorParent();
-        // GridLayout layout = new GridLayout();
-        // layout.numColumns = 2;
-        // layout.marginWidth = 0;
-        // layout.marginHeight = 0;
-        // layout.horizontalSpacing = 8;
-        // parent.setLayout(layout);
-        //
-        // org.eclipse.swt.widgets.Button button = new org.eclipse.swt.widgets.Button(parent, SWT.NONE);
-        // button.setText("button ");
-
-        // Composite subjobGroup = new Composite(parent, SWT.BORDER);
-        // GridData layoutData = new GridData(GridData.FILL_HORIZONTAL);
-        // layoutData.horizontalSpan = 4;
-        // subjobGroup.setLayoutData(layoutData);
-        //
-        // org.eclipse.swt.widgets.Label cmdLbl = new Label(subjobGroup, SWT.NONE);
-        //
-        // cmdLbl.setText("Command");
-        // cmdLbl.setLayoutData(new GridData(GridData.FILL_BOTH));
-        //
-        // final StyledText cmdTxt = new StyledText(subjobGroup, SWT.MULTI | SWT.WRAP | SWT.BORDER | SWT.H_SCROLL |
-        // SWT.V_SCROLL);
-        // cmdTxt.setLayoutData(new GridData(GridData.FILL_BOTH));
-        // CorePlugin.getDefault().getPreferenceStore().getString(ITalendCorePrefConstants.DEFAULT_COMMAND_STR);
-        //
-        // cmdTxt.addModifyListener(new ModifyListener() {
-        //
-        // public void modifyText(ModifyEvent e) {
-        // CorePlugin.getDefault().getPreferenceStore().setValue(ITalendCorePrefConstants.DEFAULT_COMMAND_STR,
-        // cmdTxt.getText());
-        // }
-        // });
-
-        //        addField(new StringFieldEditor("", "", subjobGroup)); //$NON-NLS-1$
-        //
-        //        addField(new ColorFieldEditor(ColorUtils.SUBJOB_COLOR_NAME, "ddd", subjobGroup)); //$NON-NLS-1$
-
-        // subjobGroup.setLayout(new GridLayout(3, false));
-        // subjobGroup.layout();
-
+        // do nothing.
     }
 
     /*
@@ -134,7 +89,7 @@ public class ShellSettingPreferencePage extends FieldEditorPreferencePage implem
      * @see org.eclipse.ui.IWorkbenchPreferencePage#init(org.eclipse.ui.IWorkbench)
      */
     public void init(IWorkbench workbench) {
-
+        // do nothing.
     }
 
 }
