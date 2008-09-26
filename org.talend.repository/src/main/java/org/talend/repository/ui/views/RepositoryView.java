296d64332b07d9be78995325e894da2cc17c912d
bqian <bqian@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-09-26 06:36:56 +0000

    fix bug 0005249: CTRL + Drag and Drop
    git-svn-id: http://talendforge.org/svn/tos/trunk@18329 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/views/RepositoryView.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/views/RepositoryView.java
@@ -348,7 +348,7 @@ public class RepositoryView extends ViewPart implements IRepositoryView, ITabbed
     }
 
     protected void initDragAndDrop() {
-        int ops = DND.DROP_COPY | DND.DROP_MOVE;
+        int ops = DND.DROP_COPY | DND.DROP_MOVE | DND.DROP_LINK;
         Transfer[] transfers = new Transfer[] { LocalSelectionTransfer.getTransfer() };
 
         viewer.addDragSupport(ops, transfers, new DragSourceAdapter() {
