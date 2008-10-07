59fbda16b36a53e454421db57899c551ddcbd3cb
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-07 08:27:15 +0000

    minor 5187 fixed: add the functionality that open families when filtering.
    git-svn-id: http://talendforge.org/svn/tos/trunk@18636 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/palette/TalendPaletteViewer.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/palette/TalendPaletteViewer.java
@@ -14,8 +14,12 @@ package org.talend.designer.core.ui.editor.palette;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import org.eclipse.gef.EditDomain;
+import org.eclipse.gef.palette.PaletteDrawer;
 import org.eclipse.gef.ui.palette.PaletteViewer;
 import org.eclipse.gef.ui.parts.PaletteViewerKeyHandler;
 import org.eclipse.swt.SWT;
@@ -27,6 +31,7 @@ import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Text;
 import org.talend.core.model.components.ComponentUtilities;
 
@@ -45,10 +50,13 @@ public class TalendPaletteViewer extends PaletteViewer {
 
     private static String currentFilterText;
 
+    private ThreadPoolExecutor executor;
+
     public TalendPaletteViewer(EditDomain graphicalViewerDomain) {
         setEditDomain(graphicalViewerDomain);
         setKeyHandler(new PaletteViewerKeyHandler(this));
         setEditPartFactory(new TalendPaletteEditPartFactory());
+        executor = new ThreadPoolExecutor(1, 2, 0, TimeUnit.SECONDS, new ArrayBlockingQueue<Runnable>(3));
     }
 
     /*
@@ -120,34 +128,104 @@ public class TalendPaletteViewer extends PaletteViewer {
         text.addModifyListener(new ModifyListener() {
 
             public void modifyText(ModifyEvent e) {
-                if (setTextOnly == text) {
-                    return;
+                ExpandPaletteRunnable runnable = (ExpandPaletteRunnable) executor.getQueue().poll();
+                if (runnable != null) {
+                    runnable.stopExpand();
                 }
-
-                List<Text> disposed = new ArrayList<Text>();
-                for (Text otherText : filters) {
-
-                    if (text == otherText) {
-                        continue;
-                    }
-
-                    if (otherText.isDisposed()) {
-                        disposed.add(otherText);
-                        continue;
-                    }
-                    setTextOnly = otherText;
-                    otherText.setText(text.getText());
-                    setTextOnly = null;
-                }
-
-                currentFilterText = text.getText();
-                if (!currentFilterText.equals(SEARCH_COMPONENT)) {
-                    ComponentUtilities.filterPalette(currentFilterText.trim());
-                }
-
-                filters.removeAll(disposed);
+                filterPalette(text);
+                executor.execute(new ExpandPaletteRunnable());
             }
 
         });
+    }
+
+    /**
+     * yzhang TalendPaletteViewer class global comment. Detailled comment
+     */
+    private class ExpandPaletteRunnable implements Runnable {
+
+        private volatile boolean stop;
+
+        /*
+         * (non-Javadoc)
+         * 
+         * @see java.lang.Runnable#run()
+         */
+        public void run() {
+            Display display = Display.getDefault();
+            if (display == null) {
+                return;
+            }
+            display.syncExec(new Runnable() {
+
+                /*
+                 * (non-Javadoc)
+                 * 
+                 * @see java.lang.Runnable#run()
+                 */
+                public void run() {
+                    List children = ComponentUtilities.getPaletteRoot().getChildren();
+                    int counter = 0;
+                    for (Object obj : children) {
+                        if (stop || counter > 3) {
+                            return;
+                        }
+                        if (obj instanceof PaletteDrawer) {
+                            ((PaletteDrawer) obj).setInitialState(PaletteDrawer.INITIAL_STATE_OPEN);
+                            counter++;
+                            PaletteDrawer subDrawer = getSubDrawer((PaletteDrawer) obj);
+                            if (subDrawer != null) {
+                                subDrawer.setInitialState(PaletteDrawer.INITIAL_STATE_OPEN);
+                                counter++;
+                            }
+                        }
+
+                    }
+                }
+            });
+
+        }
+
+        public void stopExpand() {
+            stop = true;
+        }
+    }
+
+    private PaletteDrawer getSubDrawer(PaletteDrawer parent) {
+        for (Object obj : parent.getChildren()) {
+            if (obj instanceof PaletteDrawer) {
+                return (PaletteDrawer) obj;
+            }
+        }
+        return null;
+    }
+
+    private void filterPalette(Text text) {
+        if (setTextOnly == text) {
+            return;
+        }
+
+        List<Text> disposed = new ArrayList<Text>();
+        for (Text otherText : filters) {
+
+            if (text == otherText) {
+                continue;
+            }
+
+            if (otherText.isDisposed()) {
+                disposed.add(otherText);
+                continue;
+            }
+            setTextOnly = otherText;
+            otherText.setText(text.getText());
+            setTextOnly = null;
+        }
+
+        currentFilterText = text.getText();
+        if (!currentFilterText.equals(SEARCH_COMPONENT)) {
+            ComponentUtilities.filterPalette(currentFilterText.trim());
+        }
+
+        filters.removeAll(disposed);
     }
 }
