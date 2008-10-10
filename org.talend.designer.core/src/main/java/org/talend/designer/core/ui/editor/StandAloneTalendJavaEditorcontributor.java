7f8062935e2a921604299e825f22da670ce8d4eb
yzhang <yzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-10 11:40:54 +0000

    minor 4839 modified: fixed the problem that error message be shown when debug.
    git-svn-id: http://talendforge.org/svn/tos/trunk@18849 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/StandAloneTalendJavaEditorcontributor.java
@@ -0,0 +1,21 @@
+package org.talend.designer.core.ui.editor;
+
+import org.eclipse.ui.IActionBars;
+import org.eclipse.ui.part.EditorActionBarContributor;
+
+public class StandAloneTalendJavaEditorcontributor extends EditorActionBarContributor {
+
+    public StandAloneTalendJavaEditorcontributor() {
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.part.EditorActionBarContributor#init(org.eclipse.ui.IActionBars)
+     */
+    @Override
+    public void init(IActionBars bars) {
+        super.init(bars);
+    }
+
+}
