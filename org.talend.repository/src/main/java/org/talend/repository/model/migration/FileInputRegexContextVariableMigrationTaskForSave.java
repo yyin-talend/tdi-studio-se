760a2e901ebae607c6d32fe6357a635a11b856d7
xtan <xtan@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2008-10-22 09:16:06 +0000

    bug 5432 fixed: add a migrationTask to remove the quote on tFileInputRegex about the parameters "header/footer/limited"
    git-svn-id: http://talendforge.org/svn/tos/trunk@19229 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/FileInputRegexContextVariableMigrationTaskForSave.java
@@ -0,0 +1,92 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2007 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.repository.model.ProxyRepositoryFactory;
+
+/**
+ * DOC xtan. tFileInputRegx when bug:4765, it add quote in jet, and now when bug:5432, it should remove the quote.<br/>
+ */
+public class FileInputRegexContextVariableMigrationTaskForSave extends AbstractJobMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @seeorg.talend.core.model.migration.AbstractJobMigrationTask#executeOnProcess(org.talend.core.model.properties.
+     * ProcessItem)
+     */
+    @Override
+    public ExecutionResult executeOnProcess(ProcessItem item) {
+        if (getProject().getLanguage() != ECodeLanguage.JAVA) {
+            return ExecutionResult.NOTHING_TO_DO;
+        }
+        try {
+            removeQuote(item);
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            return ExecutionResult.FAILURE;
+
+        }
+    }
+
+    private boolean removeQuote(ProcessItem item) throws PersistenceException {
+
+        ProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+        boolean modified = false;
+        EList nodes = item.getProcess().getNode();
+        for (Object n : nodes) {
+            NodeType type = (NodeType) n;
+            if (type.getComponentName().equals("tFileInputRegex")) {
+                EList elementParameterList = type.getElementParameter();
+                for (Object elem : elementParameterList) {
+                    ElementParameterType elemType = (ElementParameterType) elem;
+                    if (elemType.getName().equals("HEADER") || elemType.getName().equals("FOOTER")
+                            || elemType.getName().equals("LIMIT")) {
+                        String oldV = elemType.getValue();
+                        if (oldV != null && oldV.startsWith("\"") && oldV.endsWith("\"")) {
+                            elemType.setValue(oldV.substring(1, oldV.length() - 1));
+                            modified = true;
+                        }
+
+                    }
+                }
+            }
+        }
+
+        if (modified) {
+            factory.save(item, true);
+        }
+        return modified;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.IProjectMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2008, 8, 26, 12, 0, 0);
+        return gc.getTime();
+    }
+
+}
