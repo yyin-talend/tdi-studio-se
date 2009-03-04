45fc9a36ff2267f59a8f80b08231a00690e543bd
hshen <hshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-03-04 09:55:09 +0000

    bug 6499 fixed : 3.1.0M1, Drag&Drop from DBConnection while Holding [CTRL] key retrieves schema. Shoul not See BT 0006362 (when i finished the bug 6499 ,later i found the bug 6500 was also ok)
    git-svn-id: http://talendforge.org/svn/tos/trunk@22423 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
@@ -21,6 +21,7 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Shell;
 import org.talend.core.model.components.IODataComponent;
 import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.metadata.MetadataTool;
 import org.talend.core.model.metadata.QueryUtil;
 import org.talend.core.model.metadata.builder.connection.Connection;
 import org.talend.core.model.metadata.builder.connection.MetadataTable;
@@ -245,7 +246,7 @@ public class ChangeValuesFromRepository extends ChangeMetadataCommand {
                     }
 
                     if (param.isRepositoryValueUsed()) {
-                        param.setReadOnly(true);
+                        param.setReadOnly(false);
                     }
                 }
             }
@@ -330,26 +331,29 @@ public class ChangeValuesFromRepository extends ChangeMetadataCommand {
                                     repositoryTable = item.getProperty().getId() + " - " + getFirstRepositoryTable(item); //$NON-NLS-1$
                                     repositorySchemaTypeParameter.setValue(repositoryTable);
                                 }
-                                //                                if (!"".equals(repositoryTable)) { //$NON-NLS-1$
-                                // param.getChildParameters().get(EParameterName.SCHEMA_TYPE.getName()).setValue(
-                                // EmfComponent.REPOSITORY);
-                                //
-                                // IMetadataTable table = MetadataTool.getMetadataFromRepository(repositoryTable);
-                                // // repositoryTableMap.get(repositoryTable);
-                                // if (table != null) {
-                                // table = table.clone();
-                                // setDBTableFieldValue(node, table.getTableName(), null);
-                                // setSAPFunctionName(node, table.getLabel());
-                                // table.setTableName(node.getMetadataList().get(0).getTableName());
-                                // if (!table.sameMetadataAs(node.getMetadataList().get(0))) {
-                                // ChangeMetadataCommand cmd = new ChangeMetadataCommand(node, param, null, table,
-                                // param);
-                                // cmd.setConnection(connection);
-                                // cmd.setRepositoryMode(true);
-                                // cmd.execute(true);
-                                // }
-                                // }
-                                // }
+                                if (UpdateRepositoryUtils.getMetadataTablesFromItem(item).size() == 1) {
+                                    if (!"".equals(repositoryTable)) { //$NON-NLS-1$
+                                        param.getChildParameters().get(EParameterName.SCHEMA_TYPE.getName()).setValue(
+                                                EmfComponent.REPOSITORY);
+
+                                        IMetadataTable table = MetadataTool.getMetadataFromRepository(repositoryTable);
+                                        // repositoryTableMap.get(repositoryTable);
+                                        if (table != null) {
+                                            table = table.clone();
+                                            setDBTableFieldValue(node, table.getTableName(), null);
+                                            setSAPFunctionName(node, table.getLabel());
+                                            table.setTableName(node.getMetadataList().get(0).getTableName());
+                                            if (!table.sameMetadataAs(node.getMetadataList().get(0))) {
+                                                ChangeMetadataCommand cmd = new ChangeMetadataCommand(node, param, null, table,
+                                                        param);
+                                                cmd.setConnection(connection);
+                                                cmd.setRepositoryMode(true);
+                                                cmd.execute(true);
+                                            }
+                                        }
+                                    }
+                                }
+
                             } else {
                                 Node sourceNode = getRealSourceNode((INode) elem);
                                 if (sourceNode != null) {
