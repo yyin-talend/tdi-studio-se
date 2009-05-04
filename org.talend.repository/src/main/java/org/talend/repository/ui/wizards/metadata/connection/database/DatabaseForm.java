4b073ffa7cfe88521ebb207d53f9caf1a23826f4
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-05-04 07:21:19 +0000

    bug 7204 fixed: When create DB connection in repository, can't edit the schema field for oracle.
    git-svn-id: http://talendforge.org/svn/tos/trunk@24330 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/metadata/connection/database/DatabaseForm.java
@@ -1523,7 +1523,7 @@ public class DatabaseForm extends AbstractForm {
             String s = urlDataStringConnection.getStringConnectionTemplate();
             urlConnectionStringText.setEditable(!visible);
 
-            if (s != null && s.startsWith("jdbc:jtds:sqlserver:")) { //$NON-NLS-1$
+            if (s != null && (s.startsWith("jdbc:jtds:sqlserver:"))) { //$NON-NLS-1$
                 schemaText.show();
                 schemaText.setEditable(true);
                 addContextParams(EDBParamName.Schema, true);
@@ -1592,6 +1592,7 @@ public class DatabaseForm extends AbstractForm {
             }
 
             if (urlDataStringConnection.isSchemaNeeded()) {
+                schemaText.show();
                 schemaText.setEditable(visible);
                 addContextParams(EDBParamName.Schema, visible);
             }
