a6f16f10deef3a6c93681522e8c4646702ece1e2
hshen <hshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-08-24 07:09:51 +0000

    bug 7959 fixed : Fail to retrieve schema for oracle synonyms
    git-svn-id: http://talendforge.org/svn/tos/trunk@28678 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/metadata/table/database/DatabaseTableFilterForm.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/metadata/table/database/DatabaseTableFilterForm.java
@@ -30,7 +30,6 @@ import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 import org.talend.core.CorePlugin;
-import org.talend.core.model.metadata.builder.database.ExtractMetaDataUtils;
 import org.talend.core.model.metadata.builder.database.TableInfoParameters;
 import org.talend.core.model.metadata.builder.database.ExtractMetaDataFromDataBase.ETableTypes;
 import org.talend.repository.RepositoryPlugin;
@@ -59,7 +58,9 @@ public class DatabaseTableFilterForm extends AbstractForm {
 
     private Button synonymCheck;
 
-    private Button publicSynonymCheck;
+    // hide for the bug 7959
+
+    // private Button publicSynonymCheck;
 
     private Button usedName;
 
@@ -96,9 +97,11 @@ public class DatabaseTableFilterForm extends AbstractForm {
         getTableInfoParameters().changeType(ETableTypes.TABLETYPE_TABLE, tableCheck.getSelection());
         getTableInfoParameters().changeType(ETableTypes.TABLETYPE_VIEW, viewCheck.getSelection());
         getTableInfoParameters().changeType(ETableTypes.TABLETYPE_SYNONYM, synonymCheck.getSelection());
-        if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver")) {
-            getTableInfoParameters().changeType(ETableTypes.TABLETYPE_ALL_SYNONYM, publicSynonymCheck.getSelection());
-        }
+        // hide for the bug 7959
+        // if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver"))
+        // {
+        // getTableInfoParameters().changeType(ETableTypes.TABLETYPE_ALL_SYNONYM, publicSynonymCheck.getSelection());
+        // }
 
         switchFilter();
     }
@@ -116,10 +119,12 @@ public class DatabaseTableFilterForm extends AbstractForm {
         tableCheck.setEnabled(getTableInfoParameters().isUsedName());
         viewCheck.setEnabled(getTableInfoParameters().isUsedName());
         synonymCheck.setEnabled(getTableInfoParameters().isUsedName());
-        if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver")) {
-            publicSynonymCheck.setEnabled(getTableInfoParameters().isUsedName());
-            ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
-        }
+        // hide for the bug 7959
+        // if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver"))
+        // {
+        // publicSynonymCheck.setEnabled(getTableInfoParameters().isUsedName());
+        // ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
+        // }
 
         removeButton.setEnabled(getTableInfoParameters().isUsedName());
         editButton.setEnabled(getTableInfoParameters().isUsedName());
@@ -257,12 +262,14 @@ public class DatabaseTableFilterForm extends AbstractForm {
         synonymCheck = new Button(typesFilter, SWT.CHECK);
         synonymCheck.setText(Messages.getString("DatabaseTableFilterForm.synonym")); //$NON-NLS-1$
         synonymCheck.setSelection(true);
-        if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver")) {
-            publicSynonymCheck = new Button(typesFilter, SWT.CHECK);
-            publicSynonymCheck.setText("ALL_SYNONYM");
-            publicSynonymCheck.setSelection(false);
-            // ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
-        }
+        // hide for the bug 7959
+        // if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver"))
+        // {
+        // publicSynonymCheck = new Button(typesFilter, SWT.CHECK);
+        // publicSynonymCheck.setText("ALL_SYNONYM");
+        // publicSynonymCheck.setSelection(false);
+        // // ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
+        // }
 
         Composite namecomposite = new Composite(composite2, SWT.NONE);
         gridLayout = new GridLayout();
@@ -377,31 +384,33 @@ public class DatabaseTableFilterForm extends AbstractForm {
             }
 
         });
+        // hide for the bug 7959
 
-        if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver")) {
-            publicSynonymCheck.addSelectionListener(new SelectionAdapter() {
-
-                @Override
-                public void widgetSelected(SelectionEvent e) {
-                    getTableInfoParameters().changeType(ETableTypes.TABLETYPE_ALL_SYNONYM, publicSynonymCheck.getSelection());
-                    ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
-                    if (publicSynonymCheck.getSelection()) {
-                        tableCheck.setEnabled(false);
-
-                        viewCheck.setEnabled(false);
-
-                        synonymCheck.setEnabled(false);
-                    } else {
-                        tableCheck.setEnabled(true);
-
-                        viewCheck.setEnabled(true);
-
-                        synonymCheck.setEnabled(true);
-                    }
-                }
-
-            });
-        }
+        // if (ExtractMetaDataUtils.conn != null && ExtractMetaDataUtils.conn.toString().contains("oracle.jdbc.driver"))
+        // {
+        // publicSynonymCheck.addSelectionListener(new SelectionAdapter() {
+        //
+        // @Override
+        // public void widgetSelected(SelectionEvent e) {
+        // getTableInfoParameters().changeType(ETableTypes.TABLETYPE_ALL_SYNONYM, publicSynonymCheck.getSelection());
+        // ExtractMetaDataUtils.setVale(publicSynonymCheck.getSelection());
+        // if (publicSynonymCheck.getSelection()) {
+        // tableCheck.setEnabled(false);
+        //
+        // viewCheck.setEnabled(false);
+        //
+        // synonymCheck.setEnabled(false);
+        // } else {
+        // tableCheck.setEnabled(true);
+        //
+        // viewCheck.setEnabled(true);
+        //
+        // synonymCheck.setEnabled(true);
+        // }
+        // }
+        //
+        // });
+        // }
         SelectionAdapter selectionAdapter = new SelectionAdapter() {
 
             @Override
