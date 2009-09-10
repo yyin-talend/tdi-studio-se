61a92cfe9e742ad69f7963691441fdbcd151155c
wzhang <wzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-09-10 09:14:02 +0000

    bug 7920 modified: LDAP retrive schema failed invalid escape sequence.
    git-svn-id: http://talendforge.org/svn/tos/trunk@29533 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/utils/ShadowProcessHelper.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/utils/ShadowProcessHelper.java
@@ -423,17 +423,18 @@ public class ShadowProcessHelper {
 
         LDAPSchemaBean bean = new LDAPSchemaBean();
         // TODO: added properties here...
-        bean.setHost(TalendTextUtils.addQuotes(connection.getHost().replaceAll("\\", "\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
+        bean.setHost(TalendTextUtils.addQuotes(connection.getHost().replaceAll("\\\\", "\\\\\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
         bean.setPort(connection.getPort());// int
         bean.setEncryMethod(TalendTextUtils.addQuotes(connection.getEncryptionMethodName()));
         bean.setAuthen(connection.isUseAuthen());
         bean.setAuthMethod(TalendTextUtils.addQuotes(connection.getProtocol()));
-        bean.setUser(TalendTextUtils.addQuotes(connection.getBindPrincipal().replaceAll("\\", "\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
-        bean.setPasswd(TalendTextUtils.addQuotes(connection.getBindPassword().replaceAll("\\", "\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
+        bean.setUser(TalendTextUtils.addQuotes(connection.getBindPrincipal().replaceAll("\\\\", "\\\\\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
+        bean.setPasswd(TalendTextUtils.addQuotes(connection.getBindPassword().replaceAll("\\\\", "\\\\\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
 
-        bean.setBaseDN(TalendTextUtils.addQuotes(connection.getSelectedDN().replaceAll("\\", "\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
+        bean.setBaseDN(TalendTextUtils.addQuotes(connection.getSelectedDN().replaceAll("\\\\", "\\\\\\\\"))); //$NON-NLS-1$ //$NON-NLS-2$
 
         bean.setReferrals(connection.getReferrals());
+
         bean.setAliasDereferenring(connection.getAliases());
 
         bean.setMultiValueSeparator(TalendTextUtils.addQuotes(",")); //$NON-NLS-1$
