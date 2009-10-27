c99dcdccfc144d8247acdff4ce38bb025b9b7054
wyang <wyang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-10-27 08:05:16 +0000

    bug 9595: share external_sort.jar source code.
    git-svn-id: http://talendforge.org/svn/tos/trunk@31729 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.components.libs/libs_src/external_sort/org/talend/designer/components/thash/io/beans/ILightSerializable.java
@@ -0,0 +1,16 @@
+package org.talend.designer.components.thash.io.beans;
+
+/**
+ * 
+ * Interface to load/unload instance data from/to a byte array 
+ * <br/>
+ *
+ * @param <B>
+ */
+public interface ILightSerializable<B> extends Comparable<B> {
+
+    public ILightSerializable<B> createInstance(byte[] byteArray);
+
+    public byte[] toByteArray();
+
+}
