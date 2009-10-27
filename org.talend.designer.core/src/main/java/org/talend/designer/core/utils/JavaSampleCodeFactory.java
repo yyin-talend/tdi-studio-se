8dc3cae4fdacba18ebbb052c2934bdc1bf1a48b5
hshen <hshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2009-10-27 10:45:34 +0000

    7638 fixed : tJavaRow "Generate Code" should ask for confirmation before erasing custom code
    git-svn-id: http://talendforge.org/svn/tos/trunk@31757 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaSampleCodeFactory.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaSampleCodeFactory.java
@@ -15,6 +15,7 @@ package org.talend.designer.core.utils;
 import java.util.List;
 
 import org.eclipse.gef.commands.Command;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.talend.core.model.metadata.IMetadataColumn;
 import org.talend.core.model.metadata.IMetadataTable;
 import org.talend.designer.core.i18n.Messages;
@@ -42,7 +43,8 @@ public final class JavaSampleCodeFactory implements ISampleCodeFactory {
     /*
      * (non-Javadoc)
      * 
-     * @see org.talend.designer.core.utils.ISampleCodeFactory#generateCode(org.talend.designer.core.ui.editor.nodes.Node)
+     * @see
+     * org.talend.designer.core.utils.ISampleCodeFactory#generateCode(org.talend.designer.core.ui.editor.nodes.Node)
      */
     public Command generateCodeForParameters(final Node node) {
 
@@ -69,14 +71,16 @@ public final class JavaSampleCodeFactory implements ISampleCodeFactory {
      * @return
      */
     private String generateJavaRowCode(final Node node) {
-
+        String primeVlue = "// code sample:\r\n" + "//\r\n" + "// multiply by 2 the row identifier\r\n"
+                + "// output_row.id = input_row.id * 2;\r\n" + "//\r\n" + "// lowercase the name\r\n"
+                + "// output_row.name = input_row.name.toLowerCase();";
         if (node.getMetadataList() == null || node.getMetadataList().get(0) == null) {
-            return null;
+            return primeVlue;
         }
 
         if (node.getIncomingConnections() == null || node.getIncomingConnections().isEmpty()
                 || node.getIncomingConnections().get(0).getMetadataTable() == null) {
-            return null;
+            return primeVlue;
         }
 
         IMetadataTable inputTable = node.getIncomingConnections().get(0).getMetadataTable();
@@ -86,61 +90,68 @@ public final class JavaSampleCodeFactory implements ISampleCodeFactory {
         List<IMetadataColumn> outputColumns = outputTable.getListColumns();
 
         if (inputColumns == null || inputColumns.isEmpty() || outputColumns == null || outputColumns.isEmpty()) {
-            return null;
+            return primeVlue;
         }
 
         String javaEnding = ";"; //$NON-NLS-1$
         String lineSeparator = System.getProperty("line.separator"); //$NON-NLS-1$
 
         StringBuilder builder = new StringBuilder();
+        MessageDialog mgd = new MessageDialog(null, null, null, null, 0, null, 0);
+        boolean isSelect = mgd.openConfirm(null, null, "Are you sure to replace use'code to generate code ?");
+        if (isSelect) {
+            // Add simple comment
+            builder.append(Messages.getString("JavaSampleCodeFactory.schema")).append(lineSeparator); //$NON-NLS-1$
 
-        // Add simple comment
-        builder.append(Messages.getString("JavaSampleCodeFactory.schema")).append(lineSeparator); //$NON-NLS-1$
+            int inputRowsLength = inputColumns.size();
+            int ouputRowsLength = outputColumns.size();
 
-        int inputRowsLength = inputColumns.size();
-        int ouputRowsLength = outputColumns.size();
+            if (inputRowsLength == 0 || ouputRowsLength == 0) {
+                return null;
+            }
 
-        if (inputRowsLength == 0 || ouputRowsLength == 0) {
+            if (inputRowsLength >= ouputRowsLength) {
+                for (int i = 0; i < inputRowsLength; i++) {
+
+                    String inputLabel = inputColumns.get(i).getLabel();
+
+                    String outputLabel = null;
+
+                    if (i > ouputRowsLength - 1) {
+                        outputLabel = outputColumns.get(ouputRowsLength - 1).getLabel();
+                    } else {
+                        outputLabel = outputColumns.get(i).getLabel();
+                    }
+
+                    builder
+                            .append("output_row.").append(outputLabel).append(" = ").append("input_row.").append(inputLabel).append( //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                                    javaEnding);
+
+                    builder.append(lineSeparator);
+                }
+            } else {
+                for (int i = 0; i < ouputRowsLength; i++) {
+
+                    String outputLabel = outputColumns.get(i).getLabel();
+                    String inputLabel = null;
+
+                    if (i > inputRowsLength - 1) {
+                        inputLabel = inputColumns.get(inputRowsLength - 1).getLabel();
+                    } else {
+                        inputLabel = inputColumns.get(i).getLabel();
+                    }
+
+                    builder
+                            .append("output_row.").append(outputLabel).append(" = ").append("input_row.").append(inputLabel).append( //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
+                                    javaEnding);
+                    builder.append(lineSeparator);
+                }
+            }
+
+            return builder.toString();
+        } else {
             return null;
         }
-
-        if (inputRowsLength >= ouputRowsLength) {
-            for (int i = 0; i < inputRowsLength; i++) {
-
-                String inputLabel = inputColumns.get(i).getLabel();
-
-                String outputLabel = null;
-
-                if (i > ouputRowsLength - 1) {
-                    outputLabel = outputColumns.get(ouputRowsLength - 1).getLabel();
-                } else {
-                    outputLabel = outputColumns.get(i).getLabel();
-                }
-
-                builder.append("output_row.").append(outputLabel).append(" = ").append("input_row.").append(inputLabel).append( //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-                        javaEnding);
-
-                builder.append(lineSeparator);
-            }
-        } else {
-            for (int i = 0; i < ouputRowsLength; i++) {
-
-                String outputLabel = outputColumns.get(i).getLabel();
-                String inputLabel = null;
-
-                if (i > inputRowsLength - 1) {
-                    inputLabel = inputColumns.get(inputRowsLength - 1).getLabel();
-                } else {
-                    inputLabel = inputColumns.get(i).getLabel();
-                }
-
-                builder.append("output_row.").append(outputLabel).append(" = ").append("input_row.").append(inputLabel).append( //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
-                        javaEnding);
-                builder.append(lineSeparator);
-            }
-        }
-
-        return builder.toString();
 
     }
 
