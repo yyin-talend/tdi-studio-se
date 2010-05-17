927c7f79f8bba72de260215c58c7cc04965650e3
nma <nma@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-05-17 01:32:24 +0000

    bug 12746 fixed: Refactor Unit Test for automatic testing
    git-svn-id: http://talendforge.org/svn/tos/trunk@42310 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.runprocess/src/test/java/org/talend/designer/runprocess/java/JavaProcessorTest.java
+++ org.talend.designer.runprocess/src/test/java/org/talend/designer/runprocess/java/JavaProcessorTest.java
@@ -10,7 +10,7 @@
 // 9 rue Pages 92150 Suresnes, France
 //
 // ============================================================================
-package org.talend.designer.runprocess.java;
+package java;
 
 import org.junit.Test;
 
