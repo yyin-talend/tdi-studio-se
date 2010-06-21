cc8ce87f677b81ff0ddb0486607c6f60b544fd10
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-06-21 04:18:10 +0000

    bug 13813 fixed: fix a problem of column count in the regexp wizard.
    git-svn-id: http://talendforge.org/svn/tos/trunk@44283 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/shadow/FileInputRegExpNode.java
+++ org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/shadow/FileInputRegExpNode.java
@@ -15,6 +15,8 @@ package org.talend.designer.runprocess.shadow;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.talend.core.model.utils.TalendTextUtils;
+
 /**
  * DOC chuger class global comment. Detailled comment <br/>
  * 
@@ -26,23 +28,23 @@ public class FileInputRegExpNode extends FileInputNode {
     /**
      * Constructs a new FileInputNode.
      */
-    public FileInputRegExpNode(String filename, String rowSep, String regex, int limitRows, int headerRows,
-            int footerRows, boolean removeEmptyRow, String encoding) {
+    public FileInputRegExpNode(String filename, String rowSep, String regex, int limitRows, int headerRows, int footerRows,
+            boolean removeEmptyRow, String encoding) {
         super("tFileInputRegex"); //$NON-NLS-1$
-        if(regex.length() > 1){
-            String reg = regex.substring(1, regex.length()-1);
+        if (regex.length() > 1) {
+            String reg = TalendTextUtils.removeQuotes(regex).replace("\\\\", "\\");
             Pattern pattern = Pattern.compile(reg);
             Matcher matcher = pattern.matcher(""); //$NON-NLS-1$
             int columnCount = matcher.groupCount();
-            if(columnCount > 0){
+            if (columnCount > 0) {
                 this.setColumnNumber(columnCount);
             }
         }
 
         String[] paramNames = new String[] { "FILENAME", "ROWSEPARATOR", "REGEX", "LIMIT", "HEADER", "FOOTER", //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
                 "REMOVE_EMPTY_ROW", "ENCODING" }; //$NON-NLS-1$ //$NON-NLS-2$
-        String[] paramValues = new String[] { filename, rowSep, regex, Integer.toString(limitRows),
-                Integer.toString(headerRows), Integer.toString(footerRows), Boolean.toString(removeEmptyRow), encoding };
+        String[] paramValues = new String[] { filename, rowSep, regex, Integer.toString(limitRows), Integer.toString(headerRows),
+                Integer.toString(footerRows), Boolean.toString(removeEmptyRow), encoding };
 
         for (int i = 0; i < paramNames.length; i++) {
             if (paramValues[i] != null) {
