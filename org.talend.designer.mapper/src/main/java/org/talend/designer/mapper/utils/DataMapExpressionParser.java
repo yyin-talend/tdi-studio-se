d7d2fe07af2e156378d0e091e278bbcb40ab3145
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-06-21 03:09:45 +0000

    bug 13834 fixed: avoid to rename fields if have underscore before or after the new name. (might be another column or row)
    git-svn-id: http://talendforge.org/svn/tos/trunk@44275 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/utils/DataMapExpressionParser.java
+++ org.talend.designer.mapper/src/main/java/org/talend/designer/mapper/utils/DataMapExpressionParser.java
@@ -18,10 +18,12 @@ import java.util.Set;
 import org.apache.oro.text.regex.MalformedPatternException;
 import org.apache.oro.text.regex.MatchResult;
 import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.PatternMatcher;
 import org.apache.oro.text.regex.PatternMatcherInput;
 import org.apache.oro.text.regex.Perl5Compiler;
 import org.apache.oro.text.regex.Perl5Matcher;
 import org.apache.oro.text.regex.Perl5Substitution;
+import org.apache.oro.text.regex.Substitution;
 import org.apache.oro.text.regex.Util;
 import org.talend.commons.utils.data.text.StringHelper;
 import org.talend.designer.mapper.language.ILanguage;
@@ -96,13 +98,12 @@ public class DataMapExpressionParser {
         }
     }
 
-    public String addTablePrefixToColumnName(String uniqueNameComponent, String expression,
-            TableEntryLocation[] locations, boolean prefixTableNameWithComponentName,
-            HashSet<TableEntryLocation> validColumnEntryLocations) {
+    public String addTablePrefixToColumnName(String uniqueNameComponent, String expression, TableEntryLocation[] locations,
+            boolean prefixTableNameWithComponentName, HashSet<TableEntryLocation> validColumnEntryLocations) {
         String returnedExpression = expression;
         for (TableEntryLocation location : locations) {
-            recompilePatternIfNecessary(StringHelper.replacePrms(language.getSubstPatternForPrefixColumnName(),
-                    new Object[] { Perl5Compiler.quotemeta(location.tableName), Perl5Compiler.quotemeta(location.columnName) }));
+            recompilePatternIfNecessary(StringHelper.replacePrms(language.getSubstPatternForPrefixColumnName(), new Object[] {
+                    Perl5Compiler.quotemeta(location.tableName), Perl5Compiler.quotemeta(location.columnName) }));
             if (returnedExpression != null) {
                 matcher.setMultiline(true);
                 Perl5Substitution substitution = new Perl5Substitution(language.getPrefixTableRegexp()
@@ -110,8 +111,7 @@ public class DataMapExpressionParser {
                         + language.getPrefixFieldRegexp()
                         + (validColumnEntryLocations.contains(location) ? uniqueNameComponent + "__$1__" : "") //$NON-NLS-1$ //$NON-NLS-2$
                         + "$2" + language.getSuffixFieldRegexp(), Perl5Substitution.INTERPOLATE_ALL); //$NON-NLS-1$
-                returnedExpression = Util.substitute(matcher, pattern, substitution, returnedExpression,
-                        Util.SUBSTITUTE_ALL);
+                returnedExpression = substitute(matcher, pattern, substitution, returnedExpression, Util.SUBSTITUTE_ALL);
             }
         }
         return returnedExpression;
@@ -128,8 +128,7 @@ public class DataMapExpressionParser {
                 Perl5Substitution substitution = new Perl5Substitution(
                         language.getPrefixTableRegexp() + "$1->" //$NON-NLS-1$
                                 + perlLanguage.getPrefixFieldRegexp() + "$2" + perlLanguage.getSuffixFieldRegexp(), Perl5Substitution.INTERPOLATE_ALL); //$NON-NLS-1$
-                returnedExpression = Util.substitute(matcher, pattern, substitution, returnedExpression,
-                        Util.SUBSTITUTE_ALL);
+                returnedExpression = substitute(matcher, pattern, substitution, returnedExpression, Util.SUBSTITUTE_ALL);
             }
         }
         return returnedExpression;
@@ -147,10 +146,53 @@ public class DataMapExpressionParser {
                     + language.getSuffixTableRegexp()
                     + "$3" + language.getPrefixFieldRegexp() + "$4" + newLocation.columnName + "$5" //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$
                     + language.getSuffixFieldRegexp(), Perl5Substitution.INTERPOLATE_ALL);
-            returnedExpression = Util.substitute(matcher, pattern, substitution, returnedExpression,
-                    Util.SUBSTITUTE_ALL);
+            returnedExpression = substitute(matcher, pattern, substitution, returnedExpression, Util.SUBSTITUTE_ALL);
         }
         return returnedExpression;
     }
 
+    private String substitute(PatternMatcher matcher, Pattern pattern, Substitution sub, String input, int numSubs) {
+        StringBuffer buffer = new StringBuffer(input.length());
+        PatternMatcherInput pinput = new PatternMatcherInput(input);
+
+        // Users have indicated that they expect the result to be the
+        // original input string, rather than a copy, if no substitutions
+        // are performed,
+        if (substitute(buffer, matcher, pattern, sub, pinput, numSubs) != 0)
+            return buffer.toString();
+        return input;
+    }
+
+    private int substitute(StringBuffer result, PatternMatcher matcher, Pattern pattern, Substitution sub,
+            PatternMatcherInput input, int numSubs) {
+        int beginOffset, subCount;
+        char[] inputBuffer;
+
+        subCount = 0;
+        beginOffset = input.getBeginOffset();
+        inputBuffer = input.getBuffer();
+
+        // Must be != 0 because SUBSTITUTE_ALL is represented by -1.
+        // Do NOT change to numSubs > 0.
+        while (numSubs != 0 && matcher.contains(input, pattern)) {
+            --numSubs;
+            ++subCount;
+            if (input.getMatchBeginOffset() > 0) {
+                if (input.getBuffer()[input.getMatchBeginOffset() - 1] == '_') {
+                    continue;
+                }
+            }
+            if (input.getMatchEndOffset() < input.length() - 1) {
+                if (input.getBuffer()[input.getMatchEndOffset()] == '_') {
+                    continue;
+                }
+            }
+            result.append(inputBuffer, beginOffset, input.getMatchBeginOffset() - beginOffset);
+            sub.appendSubstitution(result, matcher.getMatch(), subCount, input, matcher, pattern);
+            beginOffset = input.getMatchEndOffset();
+        }
+
+        result.append(inputBuffer, beginOffset, input.length() - beginOffset);
+        return subCount;
+    }
 }
