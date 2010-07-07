7270981b08c1ba43a57811d7bb2e424652f676aa
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-07-07 09:33:56 +0000

    feature 13367 modification: fixed a NPE for jobsettings
    git-svn-id: http://talendforge.org/svn/tos/trunk@45145 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
@@ -237,9 +237,9 @@ public class ChangeValuesFromRepository extends ChangeMetadataCommand {
             EComponentCategory currentCategory = propertyParam.getCategory();
             for (IElementParameter param : elem.getElementParameters()) {
                 String repositoryValue = param.getRepositoryValue();
-                if (("TYPE".equals(repositoryValue) || (param.isShow(elem.getElementParameters())) || (elem instanceof INode
-                        && (((INode) elem).getComponent().getName().equals("tHL7Input")) || ((INode) elem).getComponent() //$NON-NLS-1$
-                        .getName().equals("tAdvancedFileOutputXML"))) //$NON-NLS-1$
+                boolean b = elem instanceof INode && (((INode) elem).getComponent().getName().equals("tHL7Input") //$NON-NLS-1$
+                        || ((INode) elem).getComponent().getName().equals("tAdvancedFileOutputXML")); //$NON-NLS-1$
+                if (("TYPE".equals(repositoryValue) || (param.isShow(elem.getElementParameters())) || b) //$NON-NLS-1$
                         && (repositoryValue != null) && (!param.getName().equals(propertyTypeName))) {
                     IElementParameter relatedPropertyParam = elem.getElementParameterFromField(EParameterFieldType.PROPERTY_TYPE,
                             param.getCategory());
