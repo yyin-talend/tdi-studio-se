8bc6a7e0e056a42bb78e30faa4bc91f82d507b93
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-12 11:21:34 +0000

    feature 14825 add: Add a system to manage the Job status in project settings.
    git-svn-id: http://talendforge.org/svn/tos/trunk@46698 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/preference/StatusManagerSettingPage.java
+++ org.talend.repository/src/main/java/org/talend/repository/preference/StatusManagerSettingPage.java
@@ -219,7 +219,7 @@ public class StatusManagerSettingPage extends ProjectSettingPage {
             }
 
             public void treeExpanded(TreeExpansionEvent event) {
-                refreshCheckedTreeView();
+                // refreshCheckedTreeView();
             }
         });
 
