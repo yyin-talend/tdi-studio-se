f34adfa3b96e839df25c1fe7f98c9431042eebc5
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-08-13 14:36:59 +0000

    feature 14849 modified: avoid an error on reject links for Dynamic type.
    git-svn-id: http://talendforge.org/svn/tos/trunk@46801 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
@@ -2560,25 +2560,28 @@ public class Node extends Element implements INode {
             }
         }
 
-        // test empty schema in built in connections (several outputs with
-        // different schema)
+        // test in case several Dynamic type has been set or if Dynamic is not the last type in schema
         if (mainConnector != null && !noSchema && canEditSchema) {
             if (getMetadataList() != null) {
                 for (IMetadataTable meta : getMetadataList()) {
                     // count how many Dynamic Type there is, normally there should be only one.
                     int nbDynamic = 0;
                     int indexOfDynamicField = 0;
+                    int lastNotCustom = 0;
                     for (IMetadataColumn col : meta.getListColumns()) {
                         if (col.getTalendType().equals("id_Dynamic")) {
                             nbDynamic++;
                             indexOfDynamicField = meta.getListColumns().indexOf(col);
+                        }
+                        if (!col.isCustom()) {
+                            lastNotCustom = meta.getListColumns().indexOf(col);
                         }
                     }
                     if (nbDynamic > 1) {
                         String errorMessage = "There should be only one Dynamic type in one schema";
                         Problems.add(ProblemStatus.ERROR, this, errorMessage);
                     }
-                    if (nbDynamic > 0 && (indexOfDynamicField + 1) != meta.getListColumns().size()) {
+                    if (nbDynamic > 0 && (indexOfDynamicField != lastNotCustom)) {
                         String errorMessage = "Dynamic type should always be the last one in a schema";
                         Problems.add(ProblemStatus.ERROR, this, errorMessage);
                     }
@@ -2586,7 +2589,8 @@ public class Node extends Element implements INode {
             }
         }
 
-        // test in case several Dynamic type has been set or if Dynamic is not the last type in schema
+        // test empty schema in built in connections (several outputs with
+        // different schema)
         if (mainConnector != null && !noSchema && (!canEditSchema || isExternalNode())) {
             if (mainConnector.isMultiSchema()) {
                 if (getMetadataList() != null) {
