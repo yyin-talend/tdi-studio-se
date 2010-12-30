808cfe3c3d3bf2de82f61a0bd2e3bd0d68cfa674
zli <zli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2010-12-30 10:05:03 +0000

    feature 0017762 modified :  Enhance jobscript feature, add missing informations to metalanguage
    git-svn-id: http://talendforge.org/svn/tos/trunk@53353 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/AbstractMultiPageTalendEditor.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/AbstractMultiPageTalendEditor.java
@@ -101,7 +101,6 @@ import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.model.repository.IRepositoryWorkUnitListener;
 import org.talend.core.model.repository.RepositoryManager;
-import org.talend.core.model.routines.RoutinesUtil;
 import org.talend.core.properties.tab.IMultiPageTalendEditor;
 import org.talend.core.repository.model.ResourceModelUtils;
 import org.talend.core.service.IDesignerPerlService;
@@ -116,7 +115,6 @@ import org.talend.designer.core.ISyntaxCheckableEditor;
 import org.talend.designer.core.i18n.Messages;
 import org.talend.designer.core.model.process.AbstractProcessProvider;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
-import org.talend.designer.core.model.utils.emf.talendfile.RoutinesParameterType;
 import org.talend.designer.core.ui.editor.AbstractTalendEditor;
 import org.talend.designer.core.ui.editor.CodeEditorFactory;
 import org.talend.designer.core.ui.editor.TalendJavaEditor;
@@ -467,6 +465,7 @@ public abstract class AbstractMultiPageTalendEditor extends MultiPageEditorPart 
                  */
                 // ((ISyntaxCheckableEditor) codeEditor).validateSyntax();
             }
+
             codeSync();
             // for bug 5033
             if (codeEditor instanceof ISyntaxCheckableEditor && LanguageManager.getCurrentLanguage() == ECodeLanguage.PERL) {
@@ -959,14 +958,15 @@ public abstract class AbstractMultiPageTalendEditor extends MultiPageEditorPart 
         }
         // added for routines code generated switch editor 0 to 3.
         ProcessItem processItem = (ProcessItem) process.getProperty().getItem();
-        if (oldPageIndex == 2) {
-            try {
-                List<RoutinesParameterType> dependenciesInPreference = RoutinesUtil.createDependenciesInPreference();
-                processItem.getProcess().getParameters().getRoutinesParameter().addAll(dependenciesInPreference);
-            } catch (PersistenceException e) {
-                ExceptionHandler.process(e);
-            }
-        }
+
+        // if (oldPageIndex == 2) {
+        // try {
+        // List<RoutinesParameterType> dependenciesInPreference = RoutinesUtil.createDependenciesInPreference();
+        // processItem.getProcess().getParameters().getRoutinesParameter().addAll(dependenciesInPreference);
+        // } catch (PersistenceException e) {
+        // ExceptionHandler.process(e);
+        // }
+        // }
         // if some code has been generated already, for the editor we should need only the main job, not the childs.
         try {
             boolean lastGeneratedWithStats = ProcessorUtilities.getLastGeneratedWithStats(process.getId());
