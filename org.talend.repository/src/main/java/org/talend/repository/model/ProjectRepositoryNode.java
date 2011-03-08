291e1339e025cf9dc4615289d506e233c4be0b42
zli <zli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-08 08:23:53 +0000

    git-svn-id: http://talendforge.org/svn/tos/trunk@56320 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/ProjectRepositoryNode.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/ProjectRepositoryNode.java
@@ -106,7 +106,7 @@ public class ProjectRepositoryNode extends RepositoryNode implements IProjectRep
             metadataFileXmlNode, metadataFileLdifNode, metadataGenericSchemaNode, metadataLDAPSchemaNode, metadataWSDLSchemaNode,
             metadataFileExcelNode, metadataSalesforceSchemaNode, metadataSAPConnectionNode, metadataFTPConnectionNode,
             metadataEbcdicConnectionNode, metadataHL7ConnectionNode, metadataMDMConnectionNode, metadataRulesNode,
-            metadataHeaderFooterConnectionNode, jobscriptsNode, beanNode, metadataBRMSConnectionNode,
+            metadataHeaderFooterConnectionNode, jobscriptsNode, beansNode, metadataBRMSConnectionNode,
             metadataValidationRulesNode;
 
     private RepositoryNode jobletNode;
@@ -340,10 +340,10 @@ public class ProjectRepositoryNode extends RepositoryNode implements IProjectRep
             codeNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.ROUTINES);
             nodes.add(codeNode);
 
-            beanNode = new RepositoryNode(null, this, ENodeType.SYSTEM_FOLDER);
-            beanNode.setProperties(EProperties.LABEL, ERepositoryObjectType.BEAN);
-            beanNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.BEAN);
-            codeNode.getChildren().add(beanNode);
+            beansNode = new RepositoryNode(null, this, ENodeType.SYSTEM_FOLDER);
+            beansNode.setProperties(EProperties.LABEL, ERepositoryObjectType.BEANS);
+            beansNode.setProperties(EProperties.CONTENT_TYPE, ERepositoryObjectType.BEANS);
+            codeNode.getChildren().add(beansNode);
 
         } else {
             // 1. Business process
@@ -637,6 +637,9 @@ public class ProjectRepositoryNode extends RepositoryNode implements IProjectRep
                 convert(newProject, factory.getJoblets(newProject, true), jobletNode, ERepositoryObjectType.JOBLET, recBinNode);
             } else if (parent == routineNode) {
                 convert(newProject, factory.getRoutine(newProject, true), routineNode, ERepositoryObjectType.ROUTINES, recBinNode);
+            } else if (parent == beansNode) {
+                convert(newProject, factory.getCamelCamelBean(newProject, true), beansNode, ERepositoryObjectType.BEANS,
+                        recBinNode);
             } else if (parent == jobscriptsNode) {
                 convert(newProject, factory.getJobScripts(newProject, true), jobscriptsNode, ERepositoryObjectType.JOB_SCRIPT,
                         recBinNode);
