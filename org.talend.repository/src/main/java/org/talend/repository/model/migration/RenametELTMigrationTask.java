9616ac0238ccc21aae86ea5319c857b5d1002670
wwang <wwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-03-16 10:55:43 +0000

    fixed feature 18933,the migration task rename for 7 ELTXXX!
    git-svn-id: http://talendforge.org/svn/tos/trunk@56787 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/RenametELTMigrationTask.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/RenametELTMigrationTask.java
@@ -12,14 +12,23 @@
 // ============================================================================
 package org.talend.repository.model.migration;
 
+import java.util.Arrays;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.List;
 
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.language.ECodeLanguage;
 import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.conversions.RenameComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 
 /**
@@ -28,9 +37,9 @@ import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 public class RenametELTMigrationTask extends AbstractJobMigrationTask {
 
     public ExecutionResult execute(Item item) {
-        String[] source = { "tELT", "tELTAggregate", "tELTCommit", "tELTFilterColumns", "tELTFilterRows", "tELTMerge",
+        final String[] source = { "tELT", "tELTAggregate", "tELTCommit", "tELTFilterColumns", "tELTFilterRows", "tELTMerge",
                 "tELTRollback", };
-        String[] target = { "tSQLTemplate", "tSQLTemplateAggregate", "tSQLTemplateCommit", "tSQLTemplateFilterColumns",
+        final String[] target = { "tSQLTemplate", "tSQLTemplateAggregate", "tSQLTemplateCommit", "tSQLTemplateFilterColumns",
                 "tSQLTemplateFilterRows", "tSQLTemplateMerge", "tSQLTemplateRollback" };
         ProcessType processType = getProcessType(item);
         if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
@@ -38,7 +47,40 @@ public class RenametELTMigrationTask extends AbstractJobMigrationTask {
         }
         try {
             for (int i = 0; i < source.length; i++) {
-                ModifyComponentsAction.searchAndRename(item, processType, source[i], target[i]); //$NON-NLS-1$ //$NON-NLS-2$           
+                final int j = i;
+                IComponentFilter filter = new NameComponentFilter(source[i]);
+                RenameComponentConversion renameConversion = new RenameComponentConversion(target[i]);
+                IComponentConversion changeNodeNameConversion = new IComponentConversion() {
+
+                    public void transform(NodeType node) {
+
+                        ProcessType item = (ProcessType) node.eContainer();
+                        for (Object o : item.getNode()) {
+                            NodeType nt = (NodeType) o;
+                            for (Object o1 : nt.getElementParameter()) {
+                                ElementParameterType t = (ElementParameterType) o1;
+                                String value = t.getValue();
+                                if (value != null) {
+                                    if (value.contains(source[j])) {
+                                        String replaceAll = value.replaceAll(source[j], target[j]);
+                                        t.setValue(replaceAll);
+                                    }
+                                }
+                                if ("TABLE".equals(t.getField())) {
+                                    for (ElementValueType type : (List<ElementValueType>) t.getElementValue()) {
+                                        if (type.getValue() != null && type.getValue().contains(source[j])) {
+                                            String replaceAll = type.getValue().replaceAll(source[j], target[j]);
+                                            type.setValue(replaceAll);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+
+                    }
+                };
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(renameConversion, changeNodeNameConversion));
             }
             return ExecutionResult.SUCCESS_NO_ALERT;
         } catch (Exception e) {
@@ -48,7 +90,7 @@ public class RenametELTMigrationTask extends AbstractJobMigrationTask {
     }
 
     public Date getOrder() {
-        GregorianCalendar gc = new GregorianCalendar(2011, 3, 14, 12, 0, 0);
+        GregorianCalendar gc = new GregorianCalendar(2011, 3, 16, 12, 0, 0);
         return gc.getTime();
     }
 }
