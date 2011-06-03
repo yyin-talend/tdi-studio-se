3c14884a796973d2b8a13587ab7d99378bb580a9
hywang <hywang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-03 06:44:27 +0000

    bug 0021587 fixed: SAXParseException: unable to load a particular project
    git-svn-id: http://talendforge.org/svn/tos/trunk@61724 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/AutoUpdateRelationsMigrationTask.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/AutoUpdateRelationsMigrationTask.java
@@ -15,19 +15,30 @@ package org.talend.repository.model.migration;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import org.eclipse.emf.ecore.resource.Resource;
+import org.eclipse.emf.ecore.xmi.XMIResource;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.model.general.Project;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.relationship.RelationshipItemBuilder;
 import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.model.repository.RepositoryObject;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.core.repository.utils.XmiResourceManager;
 
 /**
  * This migration task must be run each time, not only one time. Not standard migration task. This is to try to keep the
  * relation cache updated each time, no matter of bug / manual update from user or such.
  */
 public class AutoUpdateRelationsMigrationTask extends AbstractJobMigrationTask {
+
+    private XmiResourceManager rM;
+
+    public AutoUpdateRelationsMigrationTask() {
+        rM = ProxyRepositoryFactory.getInstance().getRepositoryFactoryFromProvider().getResourceManager();
+    }
 
     @Override
     public ExecutionResult execute(Project project) {
@@ -40,6 +51,18 @@ public class AutoUpdateRelationsMigrationTask extends AbstractJobMigrationTask {
     public ExecutionResult execute(Item item) {
         try {
             RelationshipItemBuilder.getInstance().addOrUpdateItem(item, true);
+            // should make both the item resource and property resource to be unreachable objects so that JVM can
+            // release the memory when gc
+            if (item instanceof ProcessItem) {
+                Resource processResource = ((ProcessItem) item).getProcess().eResource();
+                Resource propertyResource = item.eResource();
+                if (processResource != null && processResource.isLoaded() && (processResource instanceof XMIResource)) {
+                    propertyResource.unload();
+                    processResource.unload();
+                    rM.resourceSet.getResources().remove(propertyResource);
+                    rM.resourceSet.getResources().remove(processResource);
+                }
+            }
         } catch (Exception e) {
             ExceptionHandler.process(new Exception("Error in item:" + item.getProperty().getLabel(), e));
         }
