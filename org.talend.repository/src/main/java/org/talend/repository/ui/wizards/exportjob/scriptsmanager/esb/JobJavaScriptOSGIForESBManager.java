defc33858a8d8ed9f54b5d386dbb1376e2aa55ed
rzubairov <rzubairov@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-06-20 16:40:02 +0000

    OSGI export: javax.xml.soap import added
    git-svn-id: http://talendforge.org/svn/tos/trunk@62941 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
@@ -259,7 +259,7 @@ public class JobJavaScriptOSGIForESBManager extends JobJavaScriptsManager {
         a.put(new Attributes.Name("Bundle-Version"), jobVersion); //$NON-NLS-1$
         a.put(new Attributes.Name("Bundle-ManifestVersion"), "2"); //$NON-NLS-1$ //$NON-NLS-2$
         a.put(new Attributes.Name("Export-Package"), packageName); //$NON-NLS-1$
-        a.put(new Attributes.Name("Import-Package"), "routines.system.api;resolution:=optional,org.dom4j;resolution:=optional,org.dom4j.io;resolution:=optional,org.dom4j.tree;resolution:=optional,org.jaxen;resolution:=optional"); //$NON-NLS-1$ //$NON-NLS-2$
+        a.put(new Attributes.Name("Import-Package"), "routines.system.api;resolution:=optional,org.dom4j;resolution:=optional,org.dom4j.io;resolution:=optional,org.dom4j.tree;resolution:=optional,org.jaxen;resolution:=optional,javax.xml.soap;resolution:=optional"); //$NON-NLS-1$ //$NON-NLS-2$
         a.put(new Attributes.Name("Bundle-ClassPath"), getClassPath(libResource)); //$NON-NLS-1$
 
         return manifest;
