b2f232d4b676ea4dd11f12f6996b9b8fd3d0355e
gldu <gldu@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-07-12 11:00:26 +0000

    bug 22167 fixed: Job generation very slow with latest patch
    git-svn-id: http://talendforge.org/svn/tos/trunk@64230 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessor.java
+++ org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessor.java
@@ -432,6 +432,11 @@ public class JavaProcessor extends Processor implements IJavaBreakpointListener 
             boolean f = true;
             while (f) {
                 long time2 = System.currentTimeMillis();
+                try {
+                    Thread.sleep(100);
+                } catch (InterruptedException e) {
+                    ExceptionHandler.process(e);
+                }
                 if (time2 - time1 > 30000) {
                     if (job.getResult() == null || !job.getResult().isOK()) {
                         f = false;
