d2760f4ded861dfd098aac635c19f1491513a390
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-09-26 10:15:53 +0000

    feature TDI-17240 modified: add review dialog.
    git-svn-id: http://talendforge.org/svn/tos/trunk@68869 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.exchange/src/main/java/org/talend/designer/components/exchange/ExchangePlugin.java
+++ org.talend.designer.components.exchange/src/main/java/org/talend/designer/components/exchange/ExchangePlugin.java
@@ -1,5 +1,6 @@
 package org.talend.designer.components.exchange;
 
+import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.osgi.framework.BundleContext;
 
@@ -8,43 +9,49 @@ import org.osgi.framework.BundleContext;
  */
 public class ExchangePlugin extends AbstractUIPlugin {
 
-	// The plug-in ID
-	public static final String PLUGIN_ID = "org.talend.designer.components.exchange"; //$NON-NLS-1$
+    // The plug-in ID
+    public static final String PLUGIN_ID = "org.talend.designer.components.exchange"; //$NON-NLS-1$
 
-	// The shared instance
-	private static ExchangePlugin plugin;
-	
-	/**
-	 * The constructor
-	 */
-	public ExchangePlugin() {
-	}
+    // The shared instance
+    private static ExchangePlugin plugin;
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
-	 */
-	public void start(BundleContext context) throws Exception {
-		super.start(context);
-		plugin = this;
-	}
+    /**
+     * The constructor
+     */
+    public ExchangePlugin() {
+    }
 
-	/*
-	 * (non-Javadoc)
-	 * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
-	 */
-	public void stop(BundleContext context) throws Exception {
-		plugin = null;
-		super.stop(context);
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#start(org.osgi.framework.BundleContext)
+     */
+    public void start(BundleContext context) throws Exception {
+        super.start(context);
+        plugin = this;
+    }
 
-	/**
-	 * Returns the shared instance
-	 *
-	 * @return the shared instance
-	 */
-	public static ExchangePlugin getDefault() {
-		return plugin;
-	}
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.plugin.AbstractUIPlugin#stop(org.osgi.framework.BundleContext)
+     */
+    public void stop(BundleContext context) throws Exception {
+        plugin = null;
+        super.stop(context);
+    }
+
+    /**
+     * Returns the shared instance
+     * 
+     * @return the shared instance
+     */
+    public static ExchangePlugin getDefault() {
+        return plugin;
+    }
+
+    public static ImageDescriptor getImageDescriptor(String path) {
+        return imageDescriptorFromPlugin(PLUGIN_ID, path);
+    }
 
 }
