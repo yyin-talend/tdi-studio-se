9f4ee3ce1e104c8ac3817ae82e71d039e9cd5b81
rdubois <rdubois@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2011-10-06 13:17:34 +0000

    feature TDI-17908: tXMLMap: manage the output tables generation as in the tMap
    git-svn-id: http://talendforge.org/svn/tos/trunk@69368 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.xmlmap/src/main/java/org/talend/designer/xmlmap/generation/GenerationManager.java
+++ org.talend.designer.xmlmap/src/main/java/org/talend/designer/xmlmap/generation/GenerationManager.java
@@ -14,43 +14,78 @@ package org.talend.designer.xmlmap.generation;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import org.talend.commons.utils.data.text.StringHelper;
 import org.talend.core.model.process.BlockCode;
 
-
 /**
- * DOC rdubois  class global comment. Detailled comment
- * <br/>
- *
+ * DOC rdubois class global comment. Detailled comment <br/>
+ * 
  * $Id: talend.epf 1 2006-09-29 17:06:40Z rdubois $
- *
+ * 
  */
 public class GenerationManager {
-    private List<BlockCode> clocksCodeToClose;
-    
-    
-    public GenerationManager() {
-        
-    }
-    
-    public List<BlockCode> getBlocksCodeToClose() {
-        return clocksCodeToClose;
-    }
+	private List<BlockCode> clocksCodeToClose;
+
+    private static final String PREFIX_VARIABLE_NAME = ""; //$NON-NLS-1$
+
+    private static final String PREFIX_FIELD_NAME = "."; //$NON-NLS-1$
+
+    private static final String SUFFIX_FIELD_NAME = ""; //$NON-NLS-1$
+
+    private static final String PREFIX_TABLE_NAME = PREFIX_VARIABLE_NAME;
 
     /**
-     * DOC rdubois Comment method "getBlocksCodeToClose".
+     * {0} and {1} must be replaced respectively by the table name and the column name.
      */
-    public List<BlockCode> setBlocksCodeToClose(BlockCode blockCode) {
-        return clocksCodeToClose;
-    }
+    private static final String TEMPLATE_GENERATED_CODE_TABLE_COLUMN_VARIABLE = PREFIX_TABLE_NAME
+            + "{1}" + PREFIX_FIELD_NAME + "{2}" //$NON-NLS-1$ //$NON-NLS-2$
+            + SUFFIX_FIELD_NAME;
 
-    /**
-     * DOC rdubois Comment method "getBlocksCodeToClose".
-     */
-    public void addBlocksCodeToClose(BlockCode blockCode) {
-        if (clocksCodeToClose == null) {
-            clocksCodeToClose = new ArrayList<BlockCode>();
-        }
-        clocksCodeToClose.add(0, blockCode);
-    }
+	public GenerationManager() {
+
+	}
+
+	public List<BlockCode> getBlocksCodeToClose() {
+		return clocksCodeToClose;
+	}
+
+	/**
+	 * DOC rdubois Comment method "getBlocksCodeToClose".
+	 */
+	public List<BlockCode> setBlocksCodeToClose(BlockCode blockCode) {
+		return clocksCodeToClose;
+	}
+
+	/**
+	 * DOC rdubois Comment method "getBlocksCodeToClose".
+	 */
+	public void addBlocksCodeToClose(BlockCode blockCode) {
+		if (clocksCodeToClose == null) {
+			clocksCodeToClose = new ArrayList<BlockCode>();
+		}
+		clocksCodeToClose.add(0, blockCode);
+	}
+
+	public String getGeneratedCodeTableColumnVariable(
+			String uniqueNameComponent, String tableName, String columnName,
+			boolean prefixTableNameWithComponentName) {
+		String template = null;
+		if (prefixTableNameWithComponentName) {
+			template = getTemplateGeneratedCodeTableColumnVariableWithComponentNamePrefix();
+		} else {
+			template = getTemplateGeneratedCodeTableColumnVariable();
+		}
+		return StringHelper.replacePrms(template, new Object[] {
+				uniqueNameComponent, tableName, columnName });
+	}
+
+	public String getTemplateGeneratedCodeTableColumnVariableWithComponentNamePrefix() {
+		return TEMPLATE_GENERATED_CODE_TABLE_COLUMN_VARIABLE;
+	}
+
+	public String getTemplateGeneratedCodeTableColumnVariable() {
+		return TEMPLATE_GENERATED_CODE_TABLE_COLUMN_VARIABLE;
+	}
 
 }
