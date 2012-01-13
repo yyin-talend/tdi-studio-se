5e85581a3c3ef0c8d877e1db4c37cbb43a9ccccf
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-01-13 09:06:08 +0000

    modification: fixed the comment error for checkstyle
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@76712 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/MergeTosMetadataMigrationTask.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/MergeTosMetadataMigrationTask.java
@@ -1,6 +1,5 @@
 // ============================================================================
 //
-
 // Copyright (C) 2006-2011 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
