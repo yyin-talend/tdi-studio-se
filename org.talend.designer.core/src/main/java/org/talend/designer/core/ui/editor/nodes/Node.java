c852fe3553f21052744a66ad1106cf38325659cd
rdubois <rdubois@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-02-28 16:53:19 +0000

    merge r79005 from trunk to branch 5.0 TDI-19941 fixed: Manage the "real" ON_COMPONENT_OK in the virtual components.
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@79006 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
@@ -234,6 +234,8 @@ public class Node extends Element implements IGraphicalNode {
 
     private Boolean takeSchema = null;
 
+    private EConnectionType virtualLinkTo;
+
     /**
      * Getter for index.
      * 
@@ -3469,7 +3471,7 @@ public class Node extends Element implements IGraphicalNode {
     public boolean isVirtualGenerateNode() {
         return false;
     }
-
+    
     /**
      * ftang Comment method "checkStartLinks".
      */
@@ -3641,6 +3643,14 @@ public class Node extends Element implements IGraphicalNode {
     public List<ModuleNeeded> getModulesNeeded() {
         // same as the component, but an override is possible in the AbstractNode when generate the code
         return component.getModulesNeeded();
+    }
+
+    public EConnectionType getVirtualLinkTo() {
+        return this.virtualLinkTo;
+    }
+
+    public void setVirtualLinkTo(EConnectionType virtualLinkTo) {
+        this.virtualLinkTo=virtualLinkTo;
     }
 
 }
