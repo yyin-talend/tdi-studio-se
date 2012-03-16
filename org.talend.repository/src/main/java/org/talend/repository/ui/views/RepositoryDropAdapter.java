a0fc0a3ce74572a72f4b25188bea01b99867d642
hqzhang <hqzhang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-03-16 10:26:23 +0000

    Bug TDI-20080 fixed: Move a job into Referential view does'nt work or is not smooth.
    Root cause for moving job is not smooth as before:
    
    Before really move the job, the code checks whether the job is locked or not, this check spends some time because it will not only check the local user, but also the remote users. And current there is a check every time dragging a job to a new target(including folders and jobs), so it will wait at every job and folder which the job might pass when dragging it to another folder.
    
    Fix Solution: 
    Do not check the lock status when draging a job until the draging is finished, before really moving the job, check the lock status, if it's locked,  prompt a message to tell users that the job is locked
    
    Root cause for moving job doesn't work:
    
    When the mouse moves over the drop target, if it's a valid drop, RepositoryDropAdapter records the event.detail as operation(DND.DROP_MOVE, DND.DROP_COPY, DND.DROP_DEFAULT), and when drop the object, we perform the copying or moving operation according to the recorded operation(DND.DROP_MOVE, DND.DROP_COPY,DROP_DEFAULT).  Sometimes when moving  over the drop target, the operation is recorded as DND.DROP_DEFAULT, but when dropping the object, the event.detail == DND.DROP_MOVE. This could be a bug of eclipse, as the recorded operation does not match the actually operation.
    
    Fix Solution: 
    Record the drop event, and distinguish the operation type by the drop event.
    
    git-svn-id: http://talendforge.org/svn/tos/trunk@79972 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/views/RepositoryDropAdapter.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/views/RepositoryDropAdapter.java
@@ -12,11 +12,17 @@
 // ============================================================================
 package org.talend.repository.ui.views;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.List;
 
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.jface.dialogs.ProgressMonitorDialog;
+import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.StructuredViewer;
 import org.eclipse.swt.dnd.DND;
+import org.eclipse.swt.dnd.DropTargetEvent;
 import org.eclipse.swt.dnd.TransferData;
 import org.eclipse.ui.part.PluginDropAdapter;
 import org.talend.commons.exception.LoginException;
@@ -35,6 +41,8 @@ import org.talend.core.repository.model.ProxyRepositoryFactory;
 import org.talend.core.repository.utils.AbstractResourceChangesService;
 import org.talend.core.repository.utils.TDQServiceRegister;
 import org.talend.repository.RepositoryWorkUnit;
+import org.talend.repository.i18n.Messages;
+import org.talend.repository.model.ERepositoryStatus;
 import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.IRepositoryNode.ENodeType;
 import org.talend.repository.model.IRepositoryNode.EProperties;
@@ -54,6 +62,14 @@ public class RepositoryDropAdapter extends PluginDropAdapter {
         super(viewer);
     }
 
+    private DropTargetEvent event;
+
+    @Override
+    public void drop(DropTargetEvent event) {
+        this.event = event;
+        super.drop(event);
+    }
+
     /*
      * (non-Javadoc)
      * 
@@ -66,66 +82,34 @@ public class RepositoryDropAdapter extends PluginDropAdapter {
         boolean toReturn = true;
 
         try {
-            RepositoryWorkUnit<Object> repositoryWorkUnit = null;
             switch (operation) {
             case DND.DROP_COPY:
-                String copyName = "User action : Copy Object"; //$NON-NLS-1$
-                repositoryWorkUnit = new RepositoryWorkUnit<Object>(copyName, CopyObjectAction.getInstance()) {
-
-                    @Override
-                    protected void run() throws LoginException, PersistenceException {
-                        try {
-                            for (Object obj : ((StructuredSelection) data).toArray()) {
-                                final RepositoryNode sourceNode = (RepositoryNode) obj;
-                                CopyObjectAction.getInstance().execute(sourceNode, targetNode);
-                            }
-                        } catch (Exception e) {
-                            throw new PersistenceException(e);
-                        }
-                    }
-                };
-                ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(repositoryWorkUnit);
+                RunnableWithReturnValue runnable = new CopyRunnable(
+                        Messages.getString("RepositoryDropAdapter_copyingItems"), data, targetNode); //$NON-NLS-1$
+                runInProgressDialog(runnable);
+                toReturn = (Boolean) runnable.getReturnValue();
                 break;
             case DND.DROP_MOVE:
-                // MOD gdbu 2011-9-29 TDQ-3546
-                List<?> selectItems = ((org.eclipse.jface.viewers.TreeSelection) data).toList();
-                for (Object object : selectItems) {
-                    if (object instanceof RepositoryNode) {
-                        RepositoryNode repositoryNode = (RepositoryNode) object;
-                        ERepositoryObjectType sourceType = (ERepositoryObjectType) repositoryNode
-                                .getProperties(EProperties.CONTENT_TYPE);
-                        if (sourceType == ERepositoryObjectType.METADATA_CONNECTIONS
-                                || sourceType == ERepositoryObjectType.METADATA_FILE_DELIMITED
-                                || sourceType == ERepositoryObjectType.METADATA_MDMCONNECTION) {
-
-                            AbstractResourceChangesService resourceChangeService = TDQServiceRegister.getInstance()
-                                    .getResourceChangeService(AbstractResourceChangesService.class);
-                            boolean judgeCanMoveOrNotByDependency = judgeCanMoveOrNotByDependency(repositoryNode,
-                                    resourceChangeService);
-                            if (!judgeCanMoveOrNotByDependency) {
-                                return false;
-                            }
-                        }
-                    }
-                }
-                // ~TDQ-3546
-                String moveName = "User action : Move Object"; //$NON-NLS-1$
-                repositoryWorkUnit = new RepositoryWorkUnit<Object>(moveName, MoveObjectAction.getInstance()) {
-
-                    @Override
-                    protected void run() throws LoginException, PersistenceException {
-                        try {
-                            for (Object obj : ((StructuredSelection) data).toArray()) {
-                                final RepositoryNode sourceNode = (RepositoryNode) obj;
-                                MoveObjectAction.getInstance().execute(sourceNode, targetNode, true);
-                            }
-                        } catch (Exception e) {
-                            throw new PersistenceException(e);
-                        }
-                    }
-                };
-                ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(repositoryWorkUnit);
+                runnable = new MoveRunnable(Messages.getString("RepositoryDropAdapter_movingItems"), data, targetNode); //$NON-NLS-1$
+                runInProgressDialog(runnable);
+                toReturn = (Boolean) runnable.getReturnValue();
                 break;
+            // TDI-20080 hqzhang, operation value(actually is event.detail) on drag enter state maybe different from
+            // value on drop
+            // state, this could be a bug of ViewerDropAdapter
+            case DND.DROP_DEFAULT:
+                if (event.detail == DND.DROP_COPY) {
+                    runnable = new CopyRunnable(Messages.getString("RepositoryDropAdapter_copyingItems"), data, targetNode); //$NON-NLS-1$
+                    runInProgressDialog(runnable);
+                    toReturn = (Boolean) runnable.getReturnValue();
+                    break;
+                }
+                if (event.detail == DND.DROP_MOVE) {
+                    runnable = new MoveRunnable(Messages.getString("RepositoryDropAdapter_movingItems"), data, targetNode); //$NON-NLS-1$
+                    runInProgressDialog(runnable);
+                    toReturn = (Boolean) runnable.getReturnValue();
+                    break;
+                }
             default:
                 // Nothing to do
             }
@@ -173,8 +157,9 @@ public class RepositoryDropAdapter extends PluginDropAdapter {
      */
     @Override
     public boolean validateDrop(Object target, int operation, TransferData transferType) {
+        if (target == null)
+            return false;
         super.validateDrop(target, operation, transferType);
-
         boolean isValid = true;
         for (Object obj : ((StructuredSelection) getViewer().getSelection()).toArray()) {
             RepositoryNode sourceNode = (RepositoryNode) obj;
@@ -210,28 +195,186 @@ public class RepositoryDropAdapter extends PluginDropAdapter {
             case DND.DROP_COPY:
                 isValid = CopyObjectAction.getInstance().validateAction(sourceNode, (RepositoryNode) target);
                 break;
-            case DND.DROP_NONE:
+            // case DND.DROP_NONE:
             case DND.DROP_MOVE:
                 isValid = MoveObjectAction.getInstance().validateAction(sourceNode, (RepositoryNode) target, true);
                 break;
             case DND.DROP_DEFAULT:
             case DND.Drop: // hywang
                 isValid = MoveObjectAction.getInstance().validateAction(sourceNode, (RepositoryNode) target, true);
-                boolean isLock = false;
-                isLock = MoveObjectAction.getInstance().isLock(sourceNode);
-                if (isLock) {
-                    isValid = false;
-                }
                 break;
             default:
                 isValid = false;
             }
-
-            if (!isValid) {
-                return isValid;
-            }
+            // if (!isValid) {
+            // return isValid;
+            // }
         }
 
         return isValid;
+    }
+
+    private void runInProgressDialog(IRunnableWithProgress runnable) {
+        try {
+            ProgressMonitorDialog progress = new ProgressMonitorDialog(getViewer().getControl().getShell());
+            progress.run(false, false, runnable);
+        } catch (InvocationTargetException e) {
+            ExceptionHandler.process(e);
+        } catch (InterruptedException e) {
+            ExceptionHandler.process(e);
+        }
+    }
+
+    /**
+     * hqzhang.
+     */
+    class CopyRunnable extends RunnableWithReturnValue {
+
+        private Object data;
+
+        private RepositoryNode targetNode;
+
+        public CopyRunnable(String taskName, Object data, RepositoryNode targetNode) {
+            super(taskName);
+            this.data = data;
+            this.targetNode = targetNode;
+        }
+
+        public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+            monitor.beginTask(getTaskName(), IProgressMonitor.UNKNOWN);
+            String copyName = "User action : Copy Object"; //$NON-NLS-1$
+            RepositoryWorkUnit<Object> repositoryWorkUnit = new RepositoryWorkUnit<Object>(copyName,
+                    CopyObjectAction.getInstance()) {
+
+                @Override
+                protected void run() throws LoginException, PersistenceException {
+                    try {
+                        for (Object obj : ((StructuredSelection) data).toArray()) {
+                            final RepositoryNode sourceNode = (RepositoryNode) obj;
+                            CopyObjectAction.getInstance().execute(sourceNode, targetNode);
+                        }
+                    } catch (Exception e) {
+                        throw new PersistenceException(e);
+                    }
+                }
+            };
+            ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(repositoryWorkUnit);
+            setReturnValue(true);
+            monitor.done();
+        }
+
+    }
+
+    /**
+     * hqzhang.
+     */
+    class MoveRunnable extends RunnableWithReturnValue {
+
+        private Object data;
+
+        private RepositoryNode targetNode;
+
+        public MoveRunnable(String taskName, Object data, RepositoryNode targetNode) {
+            super(taskName);
+            this.data = data;
+            this.targetNode = targetNode;
+        }
+
+        public void run(final IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+            monitor.beginTask(getTaskName(), IProgressMonitor.UNKNOWN);
+            // MOD gdbu 2011-10-9 TDQ-3545
+            List<?> selectItems = ((org.eclipse.jface.viewers.TreeSelection) data).toList();
+            for (Object object : selectItems) {
+                if (object instanceof RepositoryNode) {
+                    RepositoryNode repositoryNode = (RepositoryNode) object;
+                    boolean isLock = false;
+                    monitor.subTask(Messages.getString("RepositoryDropAdapter.checkingLockStatus") + repositoryNode.getObject().getLabel()); //$NON-NLS-1$
+                    isLock = MoveObjectAction.getInstance().isLock(repositoryNode);
+                    if (isLock) {
+                        String errorMsg = null;
+                        IRepositoryViewObject objectToCopy = repositoryNode.getObject();
+                        if (ProxyRepositoryFactory.getInstance().getStatus(objectToCopy) == ERepositoryStatus.LOCK_BY_USER) {
+                            errorMsg = Messages.getString("RepositoryDropAdapter_lockedByYou"); //$NON-NLS-1$
+                        }
+                        if (ProxyRepositoryFactory.getInstance().getStatus(objectToCopy) == ERepositoryStatus.LOCK_BY_OTHER) {
+                            errorMsg = Messages.getString("RepositoryDropAdapter_lockedByOthers"); //$NON-NLS-1$
+                        }
+                        MessageDialog.openInformation(getViewer().getControl().getShell(),
+                                Messages.getString("RepositoryDropAdapter_moveTitle"), //$NON-NLS-1$
+                                errorMsg); //$NON-NLS-1$ 
+                        setReturnValue(false);
+                        return;
+                    }
+                    ERepositoryObjectType sourceType = (ERepositoryObjectType) repositoryNode
+                            .getProperties(EProperties.CONTENT_TYPE);
+                    if (sourceType == ERepositoryObjectType.METADATA_CONNECTIONS
+                            || sourceType == ERepositoryObjectType.METADATA_FILE_DELIMITED
+                            || sourceType == ERepositoryObjectType.METADATA_MDMCONNECTION) {
+
+                        AbstractResourceChangesService resourceChangeService = TDQServiceRegister.getInstance()
+                                .getResourceChangeService(AbstractResourceChangesService.class);
+                        boolean judgeCanMoveOrNotByDependency = judgeCanMoveOrNotByDependency(repositoryNode,
+                                resourceChangeService);
+                        if (!judgeCanMoveOrNotByDependency) {
+                            setReturnValue(false);
+                            return;
+                        }
+                    }
+                }
+            }
+            // ~TDQ-3545
+
+            String moveName = "User action : Move Object"; //$NON-NLS-1$
+            RepositoryWorkUnit<Object> repositoryWorkUnit = new RepositoryWorkUnit<Object>(moveName,
+                    MoveObjectAction.getInstance()) {
+
+                @Override
+                protected void run() throws LoginException, PersistenceException {
+                    try {
+                        for (Object obj : ((StructuredSelection) data).toArray()) {
+                            final RepositoryNode sourceNode = (RepositoryNode) obj;
+                            monitor.subTask(Messages.getString("RepositoryDropAdapter.moving") + sourceNode.getObject().getLabel()); //$NON-NLS-1$
+                            MoveObjectAction.getInstance().execute(sourceNode, targetNode, true);
+                        }
+                    } catch (Exception e) {
+                        throw new PersistenceException(e);
+                    }
+                }
+            };
+            ProxyRepositoryFactory.getInstance().executeRepositoryWorkUnit(repositoryWorkUnit);
+            setReturnValue(true);
+            monitor.done();
+        }
+    }
+
+    /**
+     * hqzhang.
+     */
+    abstract class RunnableWithReturnValue implements IRunnableWithProgress {
+
+        public RunnableWithReturnValue(String taskName) {
+            this.taskName = taskName;
+        }
+
+        private String taskName;
+
+        private Object returnValue;
+
+        protected Object getReturnValue() {
+            return this.returnValue;
+        }
+
+        protected void setReturnValue(Object returnValue) {
+            this.returnValue = returnValue;
+        }
+
+        protected String getTaskName() {
+            return this.taskName;
+        }
+
+        protected void setTaskName(String taskName) {
+            this.taskName = taskName;
+        }
+
     }
 }
