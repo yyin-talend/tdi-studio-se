ce1466fa2939745078d5f20a3e59881873b98cb0
jzhao <jzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-16 10:04:54 +0000

    Fix New Feature TDI-8197: tFileOutputExcel 'Write excel2007 file format(xlsx)' memory error http://jira.talendforge.org/browse/TDI-8197
    git-svn-id: http://talendforge.org/svn/tos/trunk@81703 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.libs/libs_src/talendExcel/org/talend/ExcelTool.java
+++ org.talend.designer.components.libs/libs_src/talendExcel/org/talend/ExcelTool.java
@@ -3,12 +3,11 @@ package org.talend;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 import org.apache.poi.ss.usermodel.Cell;
@@ -18,304 +17,353 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
+import org.apache.poi.xssf.streaming.SXSSFSheet;
+import org.apache.poi.xssf.streaming.SXSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 public class ExcelTool {
 
-    private Workbook wb = null;
+	private Workbook wb = null;
 
-    private Workbook preWb = null;
+	private Workbook preWb = null;
 
-    private String sheetName = null;
+	private String sheetName = null;
 
-    private Sheet sheet = null;
+	private Sheet sheet = null;
 
-    private Sheet preSheet = null;
+	private Sheet preSheet = null;
 
-    private Row curRow = null;
+	private Row curRow = null;
 
-    private Row preRow = null;
+	private Row preRow = null;
 
-    private Cell curCell = null;
+	private Cell curCell = null;
 
-    private Cell preCell = null;
+	private Cell preCell = null;
 
-    private boolean appendWorkbook = false;
+	private boolean appendWorkbook = false;
 
-    private boolean appendSheet = false;
+	private boolean appendSheet = false;
 
-    private int startX = 0;
+	private int startX = 0;
 
-    private int curY = 0;
+	private int curY = 0;
 
-    private int xOffset = 0;
+	private int xOffset = 0;
 
-    private boolean isAbsY = false;
+	private boolean isAbsY = false;
 
-    private int absX = 0;
+	private int absX = 0;
 
-    private int absY = 0;
+	private int absY = 0;
 
-    private boolean keepCellFormat = false;
+	private boolean keepCellFormat = false;
 
-    private Font font = null;
+	private Font font = null;
 
-    private Map<String, CellStyle> cellStylesMapping = null;
+	private Map<String, CellStyle> cellStylesMapping = null;
 
-    public ExcelTool() {
-        cellStylesMapping = new HashMap<String, CellStyle>();
-    }
+	private int rowAccessWindowSize = SXSSFWorkbook.DEFAULT_WINDOW_SIZE;
 
-    public void setAppend(boolean appendWorkbook, boolean appendSheet) {
-        this.appendWorkbook = appendWorkbook;
-        this.appendSheet = appendSheet;
-    }
+	public ExcelTool() {
+		cellStylesMapping = new HashMap<String, CellStyle>();
+	}
 
-    public void setXY(boolean isAbsY, int absX, int absY, boolean keepFormat) {
-        this.isAbsY = isAbsY;
-        this.absX = absX;
-        this.absY = absY;
-        this.keepCellFormat = keepFormat;
-    }
+	public void setAppend(boolean appendWorkbook, boolean appendSheet) {
+		this.appendWorkbook = appendWorkbook;
+		this.appendSheet = appendSheet;
+	}
 
-    public void setSheet(String sheetName) {
-        this.sheetName = sheetName;
-    }
+	public void setXY(boolean isAbsY, int absX, int absY, boolean keepFormat) {
+		this.isAbsY = isAbsY;
+		this.absX = absX;
+		this.absY = absY;
+		this.keepCellFormat = keepFormat;
+	}
 
-    public void prepareStream() throws Exception {
-        wb = new XSSFWorkbook();
-        sheet = wb.createSheet(sheetName);
-        if (isAbsY) {
-            startX = absX;
-            curY = absY;
-        }
-    }
+	public void setSheet(String sheetName) {
+		this.sheetName = sheetName;
+	}
 
-    public void prepareXlsxFile(String fileName) throws Exception {
-        File xlsxFile = new File(fileName);
-        if (xlsxFile.exists()) {
-            if (isAbsY && keepCellFormat) {
-                initPreXlsx(fileName);
-            }
-            InputStream inp = new FileInputStream(fileName);
-            wb = WorkbookFactory.create(inp);
-            if (appendWorkbook) {
-                sheet = wb.getSheet(sheetName);
-                if (sheet != null) {
-                    if (appendSheet) {
-                        curY = sheet.getLastRowNum() + 1;
-                    } else {
-                        wb.removeSheetAt(wb.getSheetIndex(sheetName));
-                        sheet = wb.createSheet(sheetName);
-                    }
-                } else {
-                    sheet = wb.createSheet(sheetName);
-                }
-            } else {
-                List<Integer> indexs = new ArrayList<Integer>();
-                for (XSSFSheet sheet : (XSSFWorkbook) wb) {
-                    indexs.add(wb.getSheetIndex(sheet));
-                }
-                for (int i = indexs.size() - 1; i >= 0; i--) {
-                    wb.removeSheetAt(indexs.get(i));
-                }
-                sheet = wb.createSheet(sheetName);
-            }
-        } else {
-            wb = new XSSFWorkbook();
-            sheet = wb.createSheet(sheetName);
-        }
-        if (isAbsY) {
-            startX = absX;
-            curY = absY;
-        }
-    }
-    
-    /**
-     * get the start insert row index.
-     * @return
-     */
-    public int getStartRow() {
-    	return curY;
-    }
+	public void prepareStream() throws Exception {
+		wb = new SXSSFWorkbook(rowAccessWindowSize);
+		sheet = wb.createSheet(sheetName);
+		if (isAbsY) {
+			startX = absX;
+			curY = absY;
+		}
+	}
 
-    private void initPreXlsx(String fileName) throws Exception {
-        InputStream preIns = new FileInputStream(fileName);
-        preWb = WorkbookFactory.create(preIns);
-        preSheet = preWb.getSheet(sheetName);
-    }
+	public void prepareXlsxFile(String fileName) throws Exception {
+		File xlsxFile = new File(fileName);
+		if (xlsxFile.exists()) {
+			if (isAbsY && keepCellFormat) {
+				initPreXlsx(fileName);
+			}
+			InputStream inp = new FileInputStream(fileName);
+			wb = WorkbookFactory.create(inp);
+			if (appendWorkbook) {
+				sheet = wb.getSheet(sheetName);
+				if (sheet != null) {
+					if (appendSheet) {
+						curY = sheet.getLastRowNum() + 1;
+					} else {
+						wb.removeSheetAt(wb.getSheetIndex(sheetName));
+						sheet = wb.createSheet(sheetName);
+					}
+				} else {
+					sheet = wb.createSheet(sheetName);
+				}
+			} else {
+				for (int i = wb.getNumberOfSheets() - 1; i >= 0; i--) {
+					wb.removeSheetAt(i);
+				}
+				sheet = wb.createSheet(sheetName);
+			}
+		} else {
+			wb = new SXSSFWorkbook(rowAccessWindowSize);
+			sheet = wb.createSheet(sheetName);
+		}
+		if (isAbsY) {
+			startX = absX;
+			curY = absY;
+		}
+	}
 
-    public void setFont(String fontName) {
-        if (!"".equals(fontName)) {
-            font = wb.createFont();
-            font.setFontName(fontName);
-        }
-    }
+	/**
+	 * get the start insert row index.
+	 * 
+	 * @return
+	 */
+	public int getStartRow() {
+		return curY;
+	}
 
-    public void addRow() {
-        if (isAbsY && keepCellFormat) {
-            if (preSheet != null) {
-                preRow = preSheet.getRow(curY);
-            } else {
-                preRow = null;
-            }
-        }
-        curRow = sheet.getRow(curY);
-        if (curRow == null)
-            curRow = sheet.createRow(curY);
-        curY = curY + 1;
-        xOffset = 0;
-    }
+	private void initPreXlsx(String fileName) throws Exception {
+		InputStream preIns = new FileInputStream(fileName);
+		preWb = WorkbookFactory.create(preIns);
+		preSheet = preWb.getSheet(sheetName);
+	}
 
-    private void addCell() {
-        if (isAbsY && keepCellFormat) {
-            if (preRow != null) {
-                preCell = preRow.getCell(startX + xOffset);
-            } else {
-                preCell = null;
-            }
-        }
-        curCell = curRow.createCell(startX + xOffset);
-        xOffset++;
-    }
+	public void setFont(String fontName) {
+		if (!"".equals(fontName)) {
+			font = wb.createFont();
+			font.setFontName(fontName);
+		}
+	}
 
-    private CellStyle getNormalCellStyle() {
-        CellStyle preCellStyle = getPreCellStyle();
-        if (preCellStyle == null) {
-            if (cellStylesMapping.get("normal") != null) {
-                return cellStylesMapping.get("normal");
-            } else {
-                CellStyle style = wb.createCellStyle();
-                if (font != null) {
-                    style.setFont(font);
-                }
-                cellStylesMapping.put("normal", style);
-                return style;
-            }
-        } else {
-            return preCellStyle;
-        }
+	public void addRow() {
+		if (isAbsY && keepCellFormat) {
+			if (preSheet != null) {
+				preRow = preSheet.getRow(curY);
+			} else {
+				preRow = null;
+			}
+		}
+		curRow = sheet.getRow(curY);
+		if (curRow == null)
+			curRow = sheet.createRow(curY);
+		curY = curY + 1;
+		xOffset = 0;
+	}
 
-    }
+	private void addCell() {
+		if (isAbsY && keepCellFormat) {
+			if (preRow != null) {
+				preCell = preRow.getCell(startX + xOffset);
+			} else {
+				preCell = null;
+			}
+		}
+		curCell = curRow.createCell(startX + xOffset);
+		xOffset++;
+	}
 
-    private CellStyle getDateCellStyle(String pattern) {
-        CellStyle preCellStyle = getPreCellStyle();
-        if (preCellStyle == null) {
-            if (cellStylesMapping.get(pattern) != null) {
-                return cellStylesMapping.get(pattern);
-            } else {
-                CellStyle style = wb.createCellStyle();
-                if (font != null) {
-                    style.setFont(font);
-                }
-                if (pattern != null || !"".equals(pattern)) {
-                    style.setDataFormat(wb.getCreationHelper().createDataFormat().getFormat(pattern));
-                }
-                cellStylesMapping.put(pattern, style);
-                return style;
-            }
-        } else {
-            return preCellStyle;
-        }
-    }
+	private CellStyle getNormalCellStyle() {
+		CellStyle preCellStyle = getPreCellStyle();
+		if (preCellStyle == null) {
+			if (cellStylesMapping.get("normal") != null) {
+				return cellStylesMapping.get("normal");
+			} else {
+				CellStyle style = wb.createCellStyle();
+				if (font != null) {
+					style.setFont(font);
+				}
+				cellStylesMapping.put("normal", style);
+				return style;
+			}
+		} else {
+			return preCellStyle;
+		}
 
-    private CellStyle getPreCellStyle() {
-        if (isAbsY && keepCellFormat) {
-            if (preCell == null) {
-                return null;
-            } else {
-                CellStyle preCellStyle = preCell.getCellStyle();
-                CellStyle targetCellStyle = wb.createCellStyle();
-                targetCellStyle.cloneStyleFrom(preCellStyle);
-                return targetCellStyle;
-            }
-        } else {
-            return null;
-        }
-    }
+	}
 
-    public void addCellValue(boolean booleanValue) {
-        addCell();
-        curCell.setCellValue(booleanValue);
-        curCell.setCellStyle(getNormalCellStyle());
-    }
+	private CellStyle getDateCellStyle(String pattern) {
+		CellStyle preCellStyle = getPreCellStyle();
+		if (preCellStyle == null) {
+			if (cellStylesMapping.get(pattern) != null) {
+				return cellStylesMapping.get(pattern);
+			} else {
+				CellStyle style = wb.createCellStyle();
+				if (font != null) {
+					style.setFont(font);
+				}
+				if (pattern != null || !"".equals(pattern)) {
+					style.setDataFormat(wb.getCreationHelper()
+							.createDataFormat().getFormat(pattern));
+				}
+				cellStylesMapping.put(pattern, style);
+				return style;
+			}
+		} else {
+			return preCellStyle;
+		}
+	}
 
-    public void addCellValue(Date dateValue, String pattern) {
-        addCell();
-        curCell.setCellValue(dateValue);
-        curCell.setCellStyle(getDateCellStyle(pattern));
-    }
+	private CellStyle getPreCellStyle() {
+		if (isAbsY && keepCellFormat) {
+			if (preCell == null) {
+				return null;
+			} else {
+				CellStyle preCellStyle = preCell.getCellStyle();
+				CellStyle targetCellStyle = wb.createCellStyle();
+				targetCellStyle.cloneStyleFrom(preCellStyle);
+				return targetCellStyle;
+			}
+		} else {
+			return null;
+		}
+	}
 
-    public void addCellValue(double doubleValue) {
-        addCell();
-        curCell.setCellValue(doubleValue);
-        curCell.setCellStyle(getNormalCellStyle());
-    }
+	public void addCellValue(boolean booleanValue) {
+		addCell();
+		curCell.setCellValue(booleanValue);
+		curCell.setCellStyle(getNormalCellStyle());
+	}
 
-    public void addCellValue(String stringValue) {
-        addCell();
-        curCell.setCellValue(stringValue);
-        curCell.setCellStyle(getNormalCellStyle());
-    }
+	public void addCellValue(Date dateValue, String pattern) {
+		addCell();
+		curCell.setCellValue(dateValue);
+		curCell.setCellStyle(getDateCellStyle(pattern));
+	}
 
-    public void setColAutoSize(int colNum) {
-        sheet.autoSizeColumn(startX + colNum, true);
-    }
+	public void addCellValue(double doubleValue) {
+		addCell();
+		curCell.setCellValue(doubleValue);
+		curCell.setCellStyle(getNormalCellStyle());
+	}
 
-    public void writeExcel(OutputStream outputStream) throws Exception {
-        wb.write(outputStream);
-    }
+	public void addCellValue(String stringValue) {
+		addCell();
+		curCell.setCellValue(stringValue);
+		curCell.setCellStyle(getNormalCellStyle());
+	}
 
-    public void writeExcel(String fileName, boolean createDir) throws Exception {
-        if (createDir) {
-            File file = new File(fileName);
-            File pFile = file.getParentFile();
-            if (pFile != null && !pFile.exists()) {
-                pFile.mkdirs();
-            }
-        }
-        FileOutputStream fileOutput = new FileOutputStream(fileName);
-        wb.write(fileOutput);
-        fileOutput.close();
-    }
+	public void setColAutoSize(int colNum) {
+		sheet.autoSizeColumn(startX + colNum, true);
+	}
 
-    public static void main(String[] args) throws Exception {
-        // ExcelTool tool = new ExcelTool();
-        // tool.setSheet("sheet1");
-        // tool.setAppend(false, false);
-        // tool.setXY(true, 0, 0, true);
-        // tool.prepareXlsxFile("c:/xlsxTest/out.xlsx");
-        // tool.setFont("");
-        // tool.addRow();
-        // tool.addCellValue("hello world!!!");
-        // tool.setColAutoSize(0);
-        // tool.writeExcel("c:/xlsxTest/out.xlsx", true);
+	public void setRowAccessWindowSize(int rowAccessWindowSize) {
+		this.rowAccessWindowSize = rowAccessWindowSize;
+	}
 
-        // ExcelTool tool = new ExcelTool();
-        // tool.setSheet("sheet1");
-        // tool.setAppend(true, true);
-        // tool.setXY(true, 0, 0, true);
-        // tool.setFont("");
-        // // tool.prepareXlsxFile("c:/xlsxTest/out.xlsx");
-        // tool.addRow();
-        // tool.addCellValue("hello world!!!!!!!");
-        // tool.setColAutoSize(0);
-        // OutputStream outputStream = new FileOutputStream("c:/xlsxTest/out.xlsx");
-        // tool.writeExcel(outputStream);
+	public void writeExcel(OutputStream outputStream) throws Exception {
+		wb.write(outputStream);
+	}
 
-        OutputStream out = new java.io.FileOutputStream("C:/xlsxTest/out.xlsx");
-        ExcelTool tool = new ExcelTool();
-        tool.setSheet("sheet1");
-        tool.setAppend(false, false);
-        tool.setXY(true, 0, 0, true);
-        // tool.prepareXlsxFile("c:/xlsxTest/out.xlsx");
-        tool.prepareStream();
-        tool.setFont("");
-        tool.addRow();
-        tool.addCellValue("hello world!!!");
-        tool.setColAutoSize(0);
-        tool.writeExcel(out);
+	public void writeExcel(String fileName, boolean createDir) throws Exception {
+		if (createDir) {
+			File file = new File(fileName);
+			File pFile = file.getParentFile();
+			if (pFile != null && !pFile.exists()) {
+				pFile.mkdirs();
+			}
+		}
+		FileOutputStream fileOutput = new FileOutputStream(fileName);
+		wb.write(fileOutput);
+		fileOutput.close();
+	}
 
-    }
+	public void flushRowInMemory(int rowNum, int flushSize) {
+
+		if (flushSize <= 0) {
+			flushSize = 0;
+		}
+		if (flushSize == 0) {
+			flush(flushSize);
+		} else {
+			if (rowNum % flushSize == 0) {
+				flush(flushSize);
+			}
+		}
+
+	}
+
+	public void flush(int flushSize) {
+		try {
+			if (wb instanceof XSSFWorkbook) {
+				(new SXSSFSheet(new SXSSFWorkbook((XSSFWorkbook) wb, -1),
+						(XSSFSheet) sheet)).flushRows(flushSize);
+			}
+			if (wb instanceof SXSSFWorkbook) {
+				if (sheet instanceof XSSFSheet) {
+					(new SXSSFSheet((SXSSFWorkbook) wb, (XSSFSheet) sheet))
+							.flushRows(flushSize);
+				}
+				if (sheet instanceof SXSSFSheet) {
+					((SXSSFSheet) sheet).flushRows(flushSize);
+				}
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static void main(String[] args) throws Exception {
+		// ExcelTool tool = new ExcelTool();
+		// tool.setSheet("sheet1");
+		// tool.setAppend(false, false);
+		// tool.setXY(true, 0, 0, true);
+		// tool.prepareXlsxFile("c:/xlsxTest/out.xlsx");
+		// tool.setFont("");
+		// tool.addRow();
+		// tool.addCellValue("hello world!!!");
+		// tool.setColAutoSize(0);
+		// tool.writeExcel("c:/xlsxTest/out.xlsx", true);
+
+		// ExcelTool tool = new ExcelTool();
+		// tool.setSheet("sheet1");
+		// tool.setAppend(true, true);
+		// tool.setXY(true, 0, 0, true);
+		// tool.setFont("");
+		// // tool.prepareXlsxFile("c:/xlsxTest/out.xlsx");
+		// tool.addRow();
+		// tool.addCellValue("hello world!!!!!!!");
+		// tool.setColAutoSize(0);
+		// OutputStream outputStream = new
+		// FileOutputStream("c:/xlsxTest/out.xlsx");
+		// tool.writeExcel(outputStream);
+
+		org.talend.ExcelTool xlsxTool_tFileOutputExcel_1 = new org.talend.ExcelTool();
+		xlsxTool_tFileOutputExcel_1.setSheet("Sheet1");
+		xlsxTool_tFileOutputExcel_1.setAppend(true, true);
+		xlsxTool_tFileOutputExcel_1.setXY(false, 0, 20, false);
+		xlsxTool_tFileOutputExcel_1.setRowAccessWindowSize(100);
+		xlsxTool_tFileOutputExcel_1.prepareXlsxFile("C:/test/out3.xlsx");
+
+		xlsxTool_tFileOutputExcel_1.setFont("ARIAL");
+		for (int i = 0; i < 50000; i++) {
+			xlsxTool_tFileOutputExcel_1.addRow();
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.addCellValue("2222222222");
+			xlsxTool_tFileOutputExcel_1.flushRowInMemory(i + 1, 100);
+		}
+		xlsxTool_tFileOutputExcel_1.writeExcel("C:/test/out3.xlsx", false);
+
+	}
 }
