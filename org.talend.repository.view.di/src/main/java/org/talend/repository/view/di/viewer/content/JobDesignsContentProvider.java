1a2404baacc2f38c7420b9f6a8711bf7879107dd
mwang <mwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-04-23 09:46:28 +0000

    For bug TDI-20571, using CNF saveable protocol.
    git-svn-id: http://talendforge.org/svn/tos/trunk@82310 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository.view.di/src/main/java/org/talend/repository/view/di/viewer/content/JobDesignsContentProvider.java
+++ org.talend.repository.view.di/src/main/java/org/talend/repository/view/di/viewer/content/JobDesignsContentProvider.java
@@ -12,12 +12,21 @@
 // ============================================================================
 package org.talend.repository.view.di.viewer.content;
 
+import org.eclipse.core.runtime.IAdaptable;
+import org.eclipse.ui.IEditorPart;
+import org.eclipse.ui.PlatformUI;
+import org.eclipse.ui.Saveable;
+import org.eclipse.ui.navigator.SaveablesProvider;
+import org.talend.core.model.process.IProcess2;
 import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.designer.core.IMultiPageTalendEditor;
+import org.talend.repository.model.IRepositoryNode;
 import org.talend.repository.model.ProjectRepositoryNode;
 import org.talend.repository.model.RepositoryNode;
 import org.talend.repository.viewer.content.ProjectRepoDirectChildrenNodeContentProvider;
+import org.talend.repository.viewer.content.TalendSaveablePart;
 
-public class JobDesignsContentProvider extends ProjectRepoDirectChildrenNodeContentProvider {
+public class JobDesignsContentProvider extends ProjectRepoDirectChildrenNodeContentProvider implements IAdaptable {
 
     /*
      * (non-Javadoc)
@@ -29,5 +38,66 @@ public class JobDesignsContentProvider extends ProjectRepoDirectChildrenNodeCont
     @Override
     protected RepositoryNode getTopLevelNodeFromProjectRepositoryNode(ProjectRepositoryNode projectNode) {
         return projectNode.getRootRepositoryNode(ERepositoryObjectType.PROCESS);
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.navigator.SaveablesProvider#getSaveables()
+     */
+    @Override
+    public Saveable[] getSaveables() {
+        Saveable[] saveables = null;
+        IEditorPart currEditorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        if (currEditorPart == null)
+            saveables = new Saveable[0];
+        else {
+            if (currEditorPart.isDirty()) {
+                saveables = new Saveable[1];
+                saveables[0] = new TalendSaveablePart(currEditorPart);
+            }
+        }
+        return saveables;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.navigator.SaveablesProvider#getElements(org.eclipse.ui.Saveable)
+     */
+    @Override
+    public Object[] getElements(Saveable saveable) {
+        if (saveable instanceof IMultiPageTalendEditor) {
+            IMultiPageTalendEditor multiPageTE = (IMultiPageTalendEditor) saveable;
+            IProcess2 process = multiPageTE.getProcess();
+            IRepositoryNode repNode = process.getRepositoryNode();
+            return new Object[] { repNode };
+        }
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.ui.navigator.SaveablesProvider#getSaveable(java.lang.Object)
+     */
+    @Override
+    public Saveable getSaveable(Object element) {
+        Saveable saveable = null;
+        IEditorPart currEditorPart = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage().getActiveEditor();
+        if (currEditorPart != null && currEditorPart.isDirty())
+            saveable = new TalendSaveablePart(currEditorPart);
+
+        return saveable;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.eclipse.core.runtime.IAdaptable#getAdapter(java.lang.Class)
+     */
+    @Override
+    public Object getAdapter(Class adapter) {
+        return null;
     }
 }
