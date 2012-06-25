a7dec6bdf57a08f87cb36adcb7eea4ed17b35928
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-06-25 05:38:52 +0000

    bug TDI-20733 fixed:Postgres ELT and ETL components act differently
    git-svn-id: http://talendforge.org/svn/tos/trunk@86235 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/MapExpressionParser.java
+++ org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/MapExpressionParser.java
@@ -8,9 +8,13 @@ import java.util.Map;
 import org.apache.oro.text.regex.MalformedPatternException;
 import org.apache.oro.text.regex.MatchResult;
 import org.apache.oro.text.regex.Pattern;
+import org.apache.oro.text.regex.PatternMatcher;
 import org.apache.oro.text.regex.PatternMatcherInput;
 import org.apache.oro.text.regex.Perl5Compiler;
 import org.apache.oro.text.regex.Perl5Matcher;
+import org.apache.oro.text.regex.Perl5Substitution;
+import org.apache.oro.text.regex.Substitution;
+import org.apache.oro.text.regex.Util;
 
 public class MapExpressionParser {
 
@@ -67,5 +71,62 @@ public class MapExpressionParser {
                 throw new RuntimeException(e);
             }
         }
+    }
+
+    public String replaceLocation(String expression, String tempPattern, String sub) {
+        String returnedExpression = expression;
+
+        recompilePatternIfNecessary(tempPattern);
+        if (returnedExpression != null) {
+            matcher.setMultiline(true);
+            Perl5Substitution substitution = new Perl5Substitution(sub);
+            returnedExpression = substitute(matcher, pattern, substitution, returnedExpression, Util.SUBSTITUTE_ALL);
+        }
+        return returnedExpression;
+    }
+
+    private String substitute(PatternMatcher matcher, Pattern pattern, Substitution sub, String input, int numSubs) {
+        StringBuffer buffer = new StringBuffer(input.length());
+        PatternMatcherInput pinput = new PatternMatcherInput(input);
+
+        // Users have indicated that they expect the result to be the
+        // original input string, rather than a copy, if no substitutions
+        // are performed,
+        if (substitute(buffer, matcher, pattern, sub, pinput, numSubs) != 0)
+            return buffer.toString();
+        return input;
+    }
+
+    private int substitute(StringBuffer result, PatternMatcher matcher, Pattern pattern, Substitution sub,
+            PatternMatcherInput input, int numSubs) {
+        int beginOffset, subCount;
+        char[] inputBuffer;
+
+        subCount = 0;
+        beginOffset = input.getBeginOffset();
+        inputBuffer = input.getBuffer();
+
+        // Must be != 0 because SUBSTITUTE_ALL is represented by -1.
+        // Do NOT change to numSubs > 0.
+        while (numSubs != 0 && matcher.contains(input, pattern)) {
+            --numSubs;
+            ++subCount;
+            if (input.getMatchBeginOffset() > 0) {
+                if (input.getBuffer()[input.getMatchBeginOffset() - 1] == '_') {
+                    continue;
+                }
+            }
+            if (input.getMatchEndOffset() < input.length() - 1) {
+                if (input.getBuffer()[input.getMatchEndOffset()] == '_') {
+                    continue;
+                }
+            }
+            result.append(inputBuffer, beginOffset, input.getMatchBeginOffset() - beginOffset);
+            sub.appendSubstitution(result, matcher.getMatch(), subCount, input, matcher, pattern);
+            beginOffset = input.getMatchEndOffset();
+        }
+
+        result.append(inputBuffer, beginOffset, input.length() - beginOffset);
+        return subCount;
     }
 }
