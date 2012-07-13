88e9437a6a6187ac1e9b4009570269724f5551a9
fwang <fwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-07-13 02:39:52 +0000

    bug TDI-21027 fixed:Component Creation: Missing a scrollbar in the creation Wizzard
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@87357 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.componentdesigner/src/org/talend/componentdesigner/ui/wizard/creatcomponent/AbstractXMLConfigPage.java
+++ org.talend.componentdesigner/src/org/talend/componentdesigner/ui/wizard/creatcomponent/AbstractXMLConfigPage.java
@@ -18,8 +18,10 @@ import java.util.List;
 import java.util.Map;
 
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.ScrolledComposite;
 import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
@@ -71,6 +73,8 @@ public abstract class AbstractXMLConfigPage extends AbstractComponentPage {
 
     protected Map<String, String> attrsToolTipsMap = null;
 
+    private ScrolledComposite scrolledComposite;
+
     /**
      * DOC rli WizardXMLConfigPage constructor comment.
      * 
@@ -92,9 +96,10 @@ public abstract class AbstractXMLConfigPage extends AbstractComponentPage {
     @Override
     protected void createPageContent(Composite parent) {
         Composite topComposite = new Composite(parent, SWT.NONE);
-        topComposite.setLayout(new GridLayout(2, false));
-        GridData data = new GridData(GridData.FILL_BOTH);
-        topComposite.setLayoutData(data);
+        // topComposite.setLayout(new GridLayout(2, false));
+        // GridData data = new GridData(GridData.FILL_BOTH);
+        // topComposite.setLayoutData(data);
+        topComposite.setLayout(new FillLayout());
         availableXmlTree = new Tree(topComposite, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
         GridData treeGD = new GridData(GridData.FILL_BOTH);
         treeGD.widthHint = 110;
@@ -102,7 +107,9 @@ public abstract class AbstractXMLConfigPage extends AbstractComponentPage {
         treeGD.grabExcessVerticalSpace = true;
         availableXmlTree.setLayoutData(treeGD);
 
-        rightComposite = new Composite(topComposite, SWT.NONE);
+        scrolledComposite = new ScrolledComposite(topComposite, SWT.H_SCROLL | SWT.V_SCROLL | SWT.BORDER);
+        // rightComposite = new Composite(topComposite, SWT.NONE);
+        rightComposite = new Composite(scrolledComposite, SWT.NONE);
         GridData rightPanelGd = new GridData(GridData.FILL_BOTH);
         rightPanelGd.widthHint = 180;
         rightComposite.setLayoutData(rightPanelGd);
@@ -112,6 +119,10 @@ public abstract class AbstractXMLConfigPage extends AbstractComponentPage {
         label.setText(Messages.getString("AbstractXMLConfigPage.SetProperties")); //$NON-NLS-1$
         GridData labelGd = new GridData(GridData.FILL_HORIZONTAL);
         label.setLayoutData(labelGd);
+
+        scrolledComposite.setContent(rightComposite);
+        scrolledComposite.setExpandHorizontal(true);
+        scrolledComposite.setExpandVertical(true);
 
         this.setControl(topComposite);
     }
@@ -182,11 +193,27 @@ public abstract class AbstractXMLConfigPage extends AbstractComponentPage {
                     nodeAttrCompsite.dispose();
                 }
                 rebuildAttrComposite(treeNodeData);
+                setComponentMinSize(treeNodeData.getTreeNode().getValue().toString());
                 rightComposite.layout();
             }
 
         });
 
+    }
+
+    private void setComponentMinSize(String label) {
+        if (("HEADER").equals(label)) {
+            scrolledComposite.setMinSize(0, 0);
+            scrolledComposite.setMinSize(280, 750);
+        } else if (("CONNECTOR").equals(label)) {
+            scrolledComposite.setMinSize(0, 0);
+            scrolledComposite.setMinSize(280, 500);
+        } else if (("PARAMETER").equals(label)) {
+            scrolledComposite.setMinSize(0, 0);
+            scrolledComposite.setMinSize(280, 630);
+        } else {
+            scrolledComposite.setMinSize(0, 0);
+        }
     }
 
     /*
