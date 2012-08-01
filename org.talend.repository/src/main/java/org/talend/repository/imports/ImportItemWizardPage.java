12d946577e0d4bbff6624f6275bef5ed591f89c9
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-08-01 10:08:18 +0000

    bug TDI-21880 fixed:Auto Generated doc of job/joblet:if import a new job/joblet, this function not work
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@88438 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/imports/ImportItemWizardPage.java
+++ org.talend.repository/src/main/java/org/talend/repository/imports/ImportItemWizardPage.java
@@ -888,7 +888,7 @@ class ImportItemWizardPage extends WizardPage {
                 try {
                     service.createNodeDocumentationItemFromItem(item);
                 } catch (Exception e) {
-                    e.printStackTrace();
+                    ExceptionHandler.process(e);
                 }
             }
             IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
