f96b8698656aa991a337953ac4f7da9887646552
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-08-16 10:12:38 +0000

    bug TDI-8282 fixed: Connection properties should not be editable
    git-svn-id: http://talendforge.org/svn/tos/trunk@89241 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/ProcessUpdateManager.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/ProcessUpdateManager.java
@@ -1433,7 +1433,7 @@ public class ProcessUpdateManager extends AbstractUpdateManager {
                     }
                 }
 
-                IRepositoryViewObject lastVersion = UpdateRepositoryUtils.getRepositoryObjectById(propertyValue, true);
+                IRepositoryViewObject lastVersion = UpdateRepositoryUtils.getRepositoryObjectById(propertyValue);
                 UpdateCheckResult result = null;
 
                 Connection repositoryConnection = null;
