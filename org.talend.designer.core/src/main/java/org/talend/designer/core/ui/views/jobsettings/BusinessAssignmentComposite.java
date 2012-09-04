ca145cfc74f40245b5f3c0acd10aa39a206f4657
fwang <fwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-04 07:26:32 +0000

    bug TDI-21747 fixed:Business model:assignment:r-click the service in assignment list, many function lost
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@90074 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/BusinessAssignmentComposite.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/BusinessAssignmentComposite.java
@@ -547,4 +547,12 @@ public class BusinessAssignmentComposite extends AbstractTabComposite {
         return new StructuredSelection(repositoryNode);
     }
 
+    @Override
+    public void refresh() {
+        super.refresh();
+        if (tableViewer != null) {
+            tableViewer.refresh();
+        }
+    }
+
 }
