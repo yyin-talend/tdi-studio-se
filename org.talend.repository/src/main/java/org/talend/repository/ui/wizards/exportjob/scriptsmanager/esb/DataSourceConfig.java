7360759a84bf18eaadad05732316b409f94972b8
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-14 03:35:23 +0000

    TDQ-6054 TDQ-6131 make report support to matching of JUDI.
    git-svn-id: http://talendforge.org/svn/tos/trunk@90696 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.repository.ui.wizards.exportjob.scriptsmanager.esb;
 
 import java.net.URI;
@@ -7,7 +19,9 @@ import org.talend.core.model.properties.ProcessItem;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.repository.utils.EmfModelUtils;
 
-// OSGi DataSource
+/**
+ * OSGi DataSource
+ */
 public class DataSourceConfig {
 
 	private static final String DB_JDBC = "tJDBCConnection"; //$NON-NLS-1$
--- org.talend.repository/src/main/java/org/talend/repository/utils/EmfModelUtils.java
+++ org.talend.repository/src/main/java/org/talend/repository/utils/EmfModelUtils.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.repository.utils;
 
 import java.util.ArrayList;
@@ -8,6 +20,10 @@ import org.talend.core.model.properties.ProcessItem;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 
+/**
+ * 
+ * DOC zshen class global comment. Detailled comment
+ */
 public class EmfModelUtils {
 
 	private EmfModelUtils() {
