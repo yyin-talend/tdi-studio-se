becd73b66539334e847edd681f9e8d397371ac53
jjzhou <jjzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-18 04:01:06 +0000

    fix Bug TDI-22675:After using tFileCopy, the source file sometime remains open
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_1@90850 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.libs/libs_src/filecopy/org/talend/FileCopy.java
+++ org.talend.designer.components.libs/libs_src/filecopy/org/talend/FileCopy.java
@@ -35,7 +35,9 @@ public class FileCopy {
 
     public static void copyFile(String srcFileName, String desFileName, boolean delSrc) throws Exception {
 
-        FileInputStream srcInputStream = new FileInputStream(srcFileName);
+        FileInputStream srcInputStream = null;
+        try{
+        srcInputStream = new FileInputStream(srcFileName);
         long lastModified = new File(srcFileName).lastModified();
         int available = srcInputStream.available();
         if (available > L_SIZE) {// X > 100M
@@ -47,6 +49,11 @@ public class FileCopy {
         }
         // keep modification_time
         new File(desFileName).setLastModified(lastModified);
+        }finally{
+            if(srcInputStream!=null){
+                srcInputStream.close();
+            }
+        }
     }
 
     private static void copyFileS(String srcFileName, FileInputStream srcInputStream, String desFileName, boolean delSrc)
