2405604b3e0490a6725907490f843afff8be4053
ldong <ldong@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-09-19 08:41:44 +0000

    Feature TDI-22773 improved:Adding file description in zip of exportJob (Studio and commandline).
    git-svn-id: http://talendforge.org/svn/tos/trunk@90985 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/JobInfoBuilder.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/JobInfoBuilder.java
@@ -20,6 +20,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Map;
 import java.util.Properties;
 
@@ -36,38 +37,39 @@ import org.talend.repository.model.IProxyRepositoryFactory;
 import org.talend.repository.ui.wizards.exportjob.scriptsmanager.JobScriptsManager.ExportChoice;
 
 public class JobInfoBuilder {
-	private String dir = null;
 
-	private String jobPropertyFile = null;
+    private String dir = null;
+
+    private String jobPropertyFile = null;
 
     private String filename = null;
-    
+
     private ExportFileResource process = null;
-    
+
     private static final String UNDER_LINE_CHAR = "_"; //$NON-NLS-1$
-    
+
     private static final String PROJECT_ID = "projectId"; //$NON-NLS-1$
-    
-    private static final String PROJECT_NAME = "projectName"; //$NON-NLS-1$
-    
+
+    private static final String PROJECT_NAME = "project"; //$NON-NLS-1$
+
     private static final String JOB_ID = "jobId"; //$NON-NLS-1$
-    
+
     private static final String JOB_NAME = "job"; //$NON-NLS-1$
-    
+
     private static final String JOB_VERSION = "jobVersion"; //$NON-NLS-1$
-    
+
     private static final String DATE = "date"; //$NON-NLS-1$
-    
+
     private static final String BRANCH = "branch"; //$NON-NLS-1$
-    
+
     private static final String COMMANDLINE_VERSION = "cmdLineVersion"; //$NON-NLS-1$
-    
+
     private static final String CONTEXT_NAME = "contextName"; //$NON-NLS-1$
-    
+
     private static final String APPLY_CONTEXY_CHILDREN = "applyContextToChildren"; //$NON-NLS-1$
-    
+
     private static final String ADD_STATIC_CODE = "statistics"; //$NON-NLS-1$
-    
+
     private Map<ExportChoice, Object> exportChoice;
 
     /**
@@ -78,16 +80,16 @@ public class JobInfoBuilder {
      * @param jarName
      * @param includeDirs
      */
-    JobInfoBuilder(ExportFileResource process,String root, String jobInfoFile) {
-    	this.process = process;
+    JobInfoBuilder(ExportFileResource process, String root, String jobInfoFile) {
+        this.process = process;
         this.dir = root;
         this.jobPropertyFile = jobInfoFile;
         File file = new File(jobInfoFile);
         this.filename = file.getName();
     }
-    
-    JobInfoBuilder(ExportFileResource process,Map<ExportChoice, Object> exportChoice,String root, String jobInfoFile) {
-    	this.process = process;
+
+    JobInfoBuilder(ExportFileResource process, Map<ExportChoice, Object> exportChoice, String root, String jobInfoFile) {
+        this.process = process;
         this.dir = root;
         this.jobPropertyFile = jobInfoFile;
         File file = new File(jobInfoFile);
@@ -95,8 +97,8 @@ public class JobInfoBuilder {
         this.exportChoice = exportChoice;
     }
 
-    private Properties getPropery(){
-    	Properties property = new Properties();
+    private Properties getPropery() {
+        Properties property = new Properties();
         return property;
     }
 
@@ -106,58 +108,59 @@ public class JobInfoBuilder {
      * @throws Exception
      */
     public void buildProperty() {
-    	Properties property = getPropery();
+        Properties property = getPropery();
         exportJobInfo(property);
     }
 
     private void exportJobInfo(Properties propertyFile) {
-    	File jobInfoFile = new File(jobPropertyFile);
-    	try {
-			if (!jobInfoFile.exists())   
-				jobInfoFile.createNewFile();   
-			InputStream fis = new FileInputStream(jobInfoFile);   
-			propertyFile.load(fis);   
-			fis.close();
-			OutputStream fos = new FileOutputStream(jobPropertyFile);
-			setProperty(propertyFile);
-			propertyFile.store(fos, "just for tac");
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}   
+        File jobInfoFile = new File(jobPropertyFile);
+        try {
+            if (!jobInfoFile.exists())
+                jobInfoFile.createNewFile();
+            InputStream fis = new FileInputStream(jobInfoFile);
+            propertyFile.load(fis);
+            fis.close();
+            OutputStream fos = new FileOutputStream(jobPropertyFile);
+            setProperty(propertyFile);
+            propertyFile.store(fos, "just for tac");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
-    
-    private void setProperty(Properties propertyFile){
-    	ProcessItem processItem = (ProcessItem) process.getItem();
-        JobInfo jobInfo = new JobInfo(processItem, processItem.getProcess().getDefaultContext(), processItem.getProperty().getVersion());
+
+    private void setProperty(Properties propertyFile) {
+        ProcessItem processItem = (ProcessItem) process.getItem();
+        JobInfo jobInfo = new JobInfo(processItem, processItem.getProcess().getDefaultContext(), processItem.getProperty()
+                .getVersion());
         Project currentProject = ProjectManager.getInstance().getCurrentProject();
         propertyFile.setProperty(PROJECT_ID, String.valueOf(currentProject.getEmfProject().getId()));
-        propertyFile.setProperty(PROJECT_NAME, currentProject.getLabel());
-        
+        propertyFile.setProperty(PROJECT_NAME, currentProject.getTechnicalLabel());
+
         String branchKey = IProxyRepositoryFactory.BRANCH_SELECTION + UNDER_LINE_CHAR + currentProject.getTechnicalLabel();
         Context ctx = CoreRuntimePlugin.getInstance().getContext();
         RepositoryContext rc = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
-        if (rc.getFields().containsKey(branchKey)
-                && rc.getFields().get(branchKey) != null) {
-           String branchSelection = rc.getFields().get(branchKey);
-           propertyFile.setProperty(BRANCH, branchSelection);
-            
+        if (rc.getFields().containsKey(branchKey) && rc.getFields().get(branchKey) != null) {
+            String branchSelection = rc.getFields().get(branchKey);
+            propertyFile.setProperty(BRANCH, branchSelection);
+
         }
-        
+
         propertyFile.setProperty(JOB_ID, jobInfo.getJobId());
         propertyFile.setProperty(JOB_NAME, jobInfo.getJobName());
         propertyFile.setProperty(JOB_VERSION, jobInfo.getJobVersion());
         propertyFile.setProperty(CONTEXT_NAME, jobInfo.getContextName());
-        
-        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
-        propertyFile.setProperty(DATE, df.format(System.currentTimeMillis()));
-        
-        if(exportChoice != null){
-        	propertyFile.setProperty(APPLY_CONTEXY_CHILDREN, String.valueOf(exportChoice.get(ExportChoice.applyToChildren)));
-        	propertyFile.setProperty(ADD_STATIC_CODE, String.valueOf(exportChoice.get(ExportChoice.addStatistics)));
+
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HHmmssS");
+        Date currentDate = new Date();
+        propertyFile.setProperty(DATE, df.format(currentDate));
+
+        if (exportChoice != null) {
+            propertyFile.setProperty(APPLY_CONTEXY_CHILDREN, String.valueOf(exportChoice.get(ExportChoice.applyToChildren)));
+            propertyFile.setProperty(ADD_STATIC_CODE, String.valueOf(exportChoice.get(ExportChoice.addStatistics)));
         }
         propertyFile.setProperty(COMMANDLINE_VERSION, VersionUtils.getVersion());
-        
+
     }
 }
