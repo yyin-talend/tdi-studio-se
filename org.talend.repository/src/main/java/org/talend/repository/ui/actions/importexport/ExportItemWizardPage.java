d119b016940de6aad7568104c6e69c259a5d1088
cli <cli@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-19 07:21:47 +0000

    TDI-22791: fixed the NPE.  at org.talend.repository.ui.actions.importexport.ExportItemWizardPage.createItemRoot(ExportItemWizardPage.java:567)
    git-svn-id: http://talendforge.org/svn/tos/trunk@92464 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/actions/importexport/ExportItemWizardPage.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/actions/importexport/ExportItemWizardPage.java
@@ -564,7 +564,7 @@ class ExportItemWizardPage extends WizardPage {
             // when first open the exportItem dialog ,the exportPath maybe empty,need judge
             if (exportArchivePath != null && exportArchivePath.trim().length() > 0) {
                 File beforeExportArchiveFolder = new File(exportArchivePath).getParentFile();
-                if (beforeExportArchiveFolder.exists()) {
+                if (beforeExportArchiveFolder == null || beforeExportArchiveFolder.exists()) {
                     String newPath = new File(beforeExportArchiveFolder, arcFileName + FileConstants.ZIP_FILE_SUFFIX)
                             .getAbsolutePath();
                     this.archivePathField.setText(newPath);
