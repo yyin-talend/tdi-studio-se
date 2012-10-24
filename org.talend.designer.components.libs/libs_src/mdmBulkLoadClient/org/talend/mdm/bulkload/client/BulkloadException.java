a569ad1e2015776a7aa69648e11da64ffe7ee72c
jjzhou <jjzhou@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-24 09:38:41 +0000

    fix  WorkItem TDI-23381:Update tMDMBulkload library https://jira.talendforge.org/browse/TDI-23381
    git-svn-id: http://talendforge.org/svn/tos/trunk@92677 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.components.libs/libs_src/mdmBulkLoadClient/org/talend/mdm/bulkload/client/BulkloadException.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (C) 2006-2012 Talend Inc. - www.talend.com
+ *
+ * This source code is available under agreement available at
+ * %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+ *
+ * You should have received a copy of the agreement
+ * along with this program; if not, write to Talend SA
+ * 9 rue Pages 92150 Suresnes, France
+ */
+
+package org.talend.mdm.bulkload.client;
+
+import java.io.PrintStream;
+import java.io.PrintWriter;
+
+/**
+ *
+ */
+public class BulkloadException extends RuntimeException {
+
+    private static final String SERVER_EXCEPTION_SEPARATOR = "== SERVER EXCEPTION ==";
+
+    private static final String LINE_SEPARATOR  = System.getProperty("line.separator"); //$NON-NLS-1$
+
+    private final String serverException;
+
+    public BulkloadException(String serverException) {
+        super("An exception happened during bulk load on MDM server.");
+        this.serverException = serverException;
+    }
+
+    public String getServerException() {
+        return serverException;
+    }
+
+    @Override
+    public void printStackTrace(PrintStream s) {
+        super.printStackTrace(s);
+        s.append(LINE_SEPARATOR);
+        s.append(SERVER_EXCEPTION_SEPARATOR);
+        s.append(LINE_SEPARATOR);
+        s.append(serverException);
+    }
+
+    @Override
+    public void printStackTrace(PrintWriter s) {
+        super.printStackTrace(s);
+        s.println(SERVER_EXCEPTION_SEPARATOR);
+        s.println(serverException);
+    }
+}
