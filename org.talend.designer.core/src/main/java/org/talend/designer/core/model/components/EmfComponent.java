8d4422373353df6aa5abdd76e4a917957eb6a2ed
zshen <zshen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-10-29 04:08:44 +0000

    TDQ-5330 TDQ-6341 backport on branch5.0
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_0@92922 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EmfComponent.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EmfComponent.java
@@ -200,6 +200,8 @@ public class EmfComponent extends AbstractComponent {
 
     private boolean isAlreadyLoad = false;
 
+    private boolean needSingleOutput = false;
+
     // weak ref used so that memory is not used by a static ComponentResourceFactoryImpl instance
     private static SoftReference<ComponentResourceFactoryImpl> compResFactorySoftRef;
 
@@ -3093,6 +3095,9 @@ public class EmfComponent extends AbstractComponent {
                 ExceptionHandler.process(e);
             }
         }
+        if (needSingleOutput) {
+            return false;
+        }
         return compType.getHEADER().isISMULTIPLYINGOUTPUTS();
     }
 
@@ -3459,4 +3464,23 @@ public class EmfComponent extends AbstractComponent {
     public String getSourceBundleName() {
         return bundleName;
     }
+
+    /**
+     * Getter for needSingleOutput.
+     * 
+     * @return the needSingleOutput
+     */
+    public boolean isNeedSingleOutput() {
+        return this.needSingleOutput;
+    }
+
+    /**
+     * Sets the needSingleOutput.
+     * 
+     * @param needSingleOutput the needSingleOutput to set
+     */
+    public void setNeedSingleOutput(boolean needSingleOutput) {
+        this.needSingleOutput = needSingleOutput;
+    }
+
 }
