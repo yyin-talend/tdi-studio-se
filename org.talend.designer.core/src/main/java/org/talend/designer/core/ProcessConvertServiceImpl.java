410ef81eb5fc4bbfd973a5006b9334507dff835d
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2012-12-28 09:11:26 +0000

    bug TDI-24308 fixed : The IProcess is a Process instead of RouteProcess when exporting a Route Item
    git-svn-id: http://talendforge.org/svn/tos/trunk@96124 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ProcessConvertServiceImpl.java
@@ -0,0 +1,25 @@
+package org.talend.designer.core;
+
+import org.talend.core.model.process.IProcess;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.designer.core.ui.editor.process.Process;
+
+public class ProcessConvertServiceImpl implements IProcessConvertService {
+
+    public ProcessConvertServiceImpl() {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public IProcess getProcessFromItem(Item item, boolean loadScreenshots) {
+        if (item instanceof ProcessItem) {
+            Process process = null;
+            process = new Process(item.getProperty());
+            process.loadXmlFile(loadScreenshots);
+            return process;
+        }
+        return null;
+    }
+
+}
