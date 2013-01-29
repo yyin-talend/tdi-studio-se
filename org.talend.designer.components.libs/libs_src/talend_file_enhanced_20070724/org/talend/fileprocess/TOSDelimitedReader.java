598a5054dc805fb0914e1798d0da341c4420b135
wwang <wwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-29 02:41:03 +0000

    Fix Bug TDI-24552 : tFileInputFullRow component produce additional rows https://jira.talendforge.org/browse/TDI-24552
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@97421 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.components.libs/libs_src/talend_file_enhanced_20070724/org/talend/fileprocess/TOSDelimitedReader.java
+++ org.talend.designer.components.libs/libs_src/talend_file_enhanced_20070724/org/talend/fileprocess/TOSDelimitedReader.java
@@ -2,7 +2,7 @@
 //
 // Talend Community Edition
 //
-// Copyright (C) 2006-2012 Talend - www.talend.com
+// Copyright (C) 2006-2013 Talend - www.talend.com
 //
 // This library is free software; you can redistribute it and/or
 // modify it under the terms of the GNU Lesser General Public
@@ -565,6 +565,14 @@ public class TOSDelimitedReader {
             if (readCount < maxReadLength) {
                 if (readCount == -1) {
                     streamEndMeet = true;
+                } else {
+                    if(inputStream.markSupported()) {
+                        inputStream.mark(1);
+                        if(inputStream.read() == -1) {
+                            streamEndMeet = true;
+                        }
+                        inputStream.reset();
+                    }
                 }
             }
 
