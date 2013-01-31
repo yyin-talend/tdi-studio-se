c2cc1dd9d23a30d510329c08921c3ee244bb9f69
mwang <mwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-01-31 10:06:46 +0000

    For feature: TDI-24405 Support Map/Reduce job in Studio, sub-task: TDI-24489 Implement the "Convert" function between common job and M/R job
    git-svn-id: http://talendforge.org/svn/tos/trunk@97598 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ProcessConvertServiceImpl.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ProcessConvertServiceImpl.java
@@ -2,9 +2,8 @@ package org.talend.designer.core;
 
 import org.talend.core.model.process.IProcess;
 import org.talend.core.model.properties.Item;
-import org.talend.core.model.properties.JobletProcessItem;
-import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.model.properties.impl.ProcessItemImpl;
+import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.designer.core.ui.editor.process.Process;
 
 public class ProcessConvertServiceImpl implements IProcessConvertService {
@@ -22,6 +21,40 @@ public class ProcessConvertServiceImpl implements IProcessConvertService {
             return process;
         }
         return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#doConvert(org.talend.core.model.properties.Item,
+     * org.talend.core.model.repository.IRepositoryViewObject)
+     */
+    @Override
+    public Item doConvert(Item item, IRepositoryViewObject repViewObject) {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#isOriginalItemDeleted()
+     */
+    @Override
+    public boolean isOriginalItemDeleted() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.designer.core.IProcessConvertService#isNewItemCreated()
+     */
+    @Override
+    public boolean isNewItemCreated() {
+        // TODO Auto-generated method stub
+        return false;
     }
 
 }
