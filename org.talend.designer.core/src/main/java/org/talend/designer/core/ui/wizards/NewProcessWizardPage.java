e5860571e46b12662f37855ccbe179f24cde1686
mwang <mwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-02-25 03:38:35 +0000

    git-svn-id: http://talendforge.org/svn/tos/trunk@98613 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/wizards/NewProcessWizardPage.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/wizards/NewProcessWizardPage.java
@@ -12,14 +12,21 @@
 // ============================================================================
 package org.talend.designer.core.ui.wizards;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IStatus;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.properties.Property;
 import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.designer.core.i18n.Messages;
+import org.talend.repository.model.IProxyRepositoryService;
 import org.talend.repository.ui.wizards.PropertiesWizardPage;
 
 /**
@@ -46,6 +53,7 @@ public class NewProcessWizardPage extends PropertiesWizardPage {
     /**
      * @see org.eclipse.jface.dialogs.IDialogPage#createControl(org.eclipse.swt.widgets.Composite)
      */
+    @Override
     public void createControl(Composite parent) {
         Composite container = new Composite(parent, SWT.NONE);
         GridLayout layout = new GridLayout(2, false);
@@ -59,6 +67,7 @@ public class NewProcessWizardPage extends PropertiesWizardPage {
         // setPageComplete(false);
     }
 
+    @Override
     public ERepositoryObjectType getRepositoryObjectType() {
         return ERepositoryObjectType.PROCESS;
     }
@@ -68,11 +77,46 @@ public class NewProcessWizardPage extends PropertiesWizardPage {
      * 
      * @see org.talend.repository.ui.wizards.PropertiesWizardPage#evaluateTextField()
      */
+    @Override
     protected void evaluateTextField() {
         super.evaluateTextField();
         if (nameStatus.getSeverity() == IStatus.OK) {
             evaluateNameInRoutine();
         }
+    }
+
+    @Override
+    protected List<IRepositoryViewObject> loadRepViewObjectWithOtherTypes() throws PersistenceException {
+        List<IRepositoryViewObject> list = new ArrayList<IRepositoryViewObject>();
+
+        // List for m/r process
+        ERepositoryObjectType mrRepObjType = ERepositoryObjectType.valueOf(ERepositoryObjectType.class, "PROCESS_MR");//$NON-NLS-1$
+        if (mrRepObjType != null) {
+            if (GlobalServiceRegister.getDefault().isServiceRegistered(IProxyRepositoryService.class)) {
+                IProxyRepositoryService service = (IProxyRepositoryService) GlobalServiceRegister.getDefault().getService(
+                        IProxyRepositoryService.class);
+
+                List<IRepositoryViewObject> mrList = service.getProxyRepositoryFactory().getAll(mrRepObjType, true, false);
+                if (mrList != null && mrList.size() > 0) {
+                    list.addAll(mrList);
+                }
+            }
+        }
+
+        // List for routine
+        if (ERepositoryObjectType.ROUTINES != null) {
+            if (GlobalServiceRegister.getDefault().isServiceRegistered(IProxyRepositoryService.class)) {
+                IProxyRepositoryService service = (IProxyRepositoryService) GlobalServiceRegister.getDefault().getService(
+                        IProxyRepositoryService.class);
+
+                List<IRepositoryViewObject> mrList = service.getProxyRepositoryFactory().getAll(ERepositoryObjectType.ROUTINES,
+                        true, false);
+                if (mrList != null && mrList.size() > 0) {
+                    list.addAll(mrList);
+                }
+            }
+        }
+        return list;
     }
 
 }
