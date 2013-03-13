a3f0e1e966053a519417323cc2fda3322f4d99c7
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-03-13 02:00:34 +0000

    TDI-25064 : fix problem of classcast.
    git-svn-id: http://talendforge.org/svn/tos/trunk@99454 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeValuesFromRepository.java
@@ -271,8 +271,9 @@ public class ChangeValuesFromRepository extends ChangeMetadataCommand {
                         && (((INode) elem).getComponent().getName().equals("tHL7Input") //$NON-NLS-1$
                                 || ((INode) elem).getComponent().getName().equals("tAdvancedFileOutputXML") //$NON-NLS-1$
                                 || ((INode) elem).getComponent().getName().equals("tMDMOutput")
-                                || ((INode) elem).getComponent().getName().equals("tWebService") || ((INode) elem).getComponent()
-                                .getName().equals("tCreateTable")) || ((INode) elem).getComponent().getName().equals("tWriteJSONField"); //$NON-NLS-1$
+                                || ((INode) elem).getComponent().getName().equals("tWebService")
+                                || ((INode) elem).getComponent().getName().equals("tCreateTable") || ((INode) elem)
+                                .getComponent().getName().equals("tWriteJSONField")); //$NON-NLS-1$
 
                 if (("TYPE".equals(repositoryValue) || (param.isShow(elem.getElementParameters())) || b) //$NON-NLS-1$
                         && (repositoryValue != null) && (!param.getName().equals(propertyTypeName))) {
