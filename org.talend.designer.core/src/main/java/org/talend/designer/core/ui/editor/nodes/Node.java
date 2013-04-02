3ea9d47d3b9bd89a68e8bc8442ccfdd3fc3134b4
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-04-02 08:36:14 +0000

    merge r100427 from trunk to branch 5.1 Fix Bug TDI-25167 : Behaviour of tRunJob component is different between version 5.1.2 and 5.2.1 https://jira.talendforge.org/browse/TDI-25167
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_1@100443 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
@@ -3006,7 +3006,7 @@ public class Node extends Element implements IGraphicalNode {
                         && (mainConnector.getMaxLinkOutput() != 0)) {
                     if (table.getListColumns().size() == 0) {
                         String errorMessage = Messages.getString("Node.noSchemaDefined"); //$NON-NLS-1$
-                        Problems.add(ProblemStatus.ERROR, this, errorMessage);
+                        Problems.add(ProblemStatus.WARNING, this, errorMessage);
                         noSchema = true;
                     }
                 }
