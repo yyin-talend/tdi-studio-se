05a022ec7bbba721bc24fac7d10534408127c306
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-05-17 07:03:35 +0000

    merge r102889 from trunk to branch 5.2 bug TDI-25599 fixed : existing connection_parameter lost after drag and drop the joblet into a job .
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@102890 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/jobletcontainer/JobletUtil.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/jobletcontainer/JobletUtil.java
@@ -279,6 +279,8 @@ public class JobletUtil {
                     list.addAll((List) paValue);
                     cloneElement.setValue(list);
                 } else {
+                    // TDI-25599
+                    cloneElement.setContextMode(elementPara.isContextMode());
                     cloneElement.setValue(elementPara.getValue());
                 }
 
