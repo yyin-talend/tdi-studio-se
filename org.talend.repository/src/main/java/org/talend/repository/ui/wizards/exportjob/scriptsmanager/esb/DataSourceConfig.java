27a3653e7c62a34224226f52bc6e89617415de8f
amarkevich <amarkevich@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-06-04 07:52:13 +0000

    TESB-8767 When using a datasource alias in JDBC components for connection pooling the tick box to switch it on and off does not function properly.
    git-svn-id: http://talendforge.org/svn/tos/trunk@103712 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
+++ org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
@@ -12,8 +12,9 @@
 // ============================================================================
 package org.talend.repository.ui.wizards.exportjob.scriptsmanager.esb;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.HashSet;
-import java.util.Set;
 
 import org.talend.core.model.process.JobInfo;
 import org.talend.core.model.properties.ProcessItem;
@@ -29,32 +30,32 @@ public class DataSourceConfig {
     private DataSourceConfig() {
     }
 
-    public static Set<String> getAliases(ProcessItem processItem) {
+    public static Collection<String> getAliases(ProcessItem processItem) {
         return getAliases(processItem, new HashSet<String>());
     }
 
-    private static Set<String> getAliases(ProcessItem processItem, Set<String> jobNameVersionChecked) {
-        Set<String> aliases = new HashSet<String>();
+    private static Collection<String> getAliases(ProcessItem processItem, Collection<String> jobNameVersionChecked) {
         String jobName = processItem.getProperty().getLabel();
         String jobVersion = processItem.getProperty().getVersion();
-        String id = jobName + "_" + jobVersion; //$NON-NLS-1$
-        if (jobNameVersionChecked.contains(id)) {
-            return aliases; // no need to add more to the list, just return the empty list
+        String id = jobName + '_' + jobVersion;
+        if (!jobNameVersionChecked.add(id)) {
+            return Collections.emptyList(); // no need to add more to the list, just return the empty list
         }
-        jobNameVersionChecked.add(id);
+        Collection<String> aliases = new HashSet<String>();
         for (Object o : processItem.getProcess().getNode()) {
             if (o instanceof NodeType) {
                 NodeType node = (NodeType) o;
-                if (EmfModelUtils.isComponentActive(node)) {
+                if (EmfModelUtils.isComponentActive(node)
+                        && EmfModelUtils.computeCheckElementValue("SPECIFY_DATASOURCE_ALIAS", node)) { //$NON-NLS-1$
                     String alias = EmfModelUtils.computeTextElementValue("DATASOURCE_ALIAS", node); //$NON-NLS-1$
-                    if (!"".equals(alias)) {
+                    if (!alias.isEmpty()) {
                         aliases.add(alias);
                     }
                 }
             }
         }
-        Set<JobInfo> subjobInfos = ProcessorUtilities.getChildrenJobInfo(processItem);
-        for (JobInfo subjobInfo : subjobInfos) {
+
+        for (JobInfo subjobInfo : ProcessorUtilities.getChildrenJobInfo(processItem)) {
             aliases.addAll(getAliases(subjobInfo.getProcessItem(), jobNameVersionChecked));
         }
         return aliases;
