adaebc6fe80303cc55524063dcec59298a960d1d
wchen <wchen@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-09 03:59:08 +0000

    bug TDI-25946 fixed: HL7 connection wizard is not parsing columns correctly
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@105047 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.hl7/src/org/talend/designer/hl7/model/PrimitiveModel.java
+++ org.talend.designer.hl7/src/org/talend/designer/hl7/model/PrimitiveModel.java
@@ -15,7 +15,11 @@ package org.talend.designer.hl7.model;
 import org.talend.core.model.utils.TalendTextUtils;
 
 import ca.uhn.hl7v2.model.Primitive;
+import ca.uhn.hl7v2.model.Type;
 
+/**
+ * DOC gcui class global comment. Detailled comment
+ */
 public class PrimitiveModel extends AbstractStructureModel {
 
     private int componentindex;
@@ -37,18 +41,24 @@ public class PrimitiveModel extends AbstractStructureModel {
 
     @Override
     protected void generateDisplayName() {
-        String parentName = "";
+        String parentName = ""; //$NON-NLS-1$
         if (parent instanceof TypeModel) {
             TypeModel tm = (TypeModel) parent;
-            String extensionName = tm.getType().getName();
+            Type type = tm.getType();
             String parentDisplayName = tm.getDisplayName();
-            // int extensionIndex = parentDisplayName.indexOf(extensionName);
-            int extensionIndex = parentDisplayName.lastIndexOf(extensionName);// changed for bug TDI-19467
-            parentName = parentDisplayName.substring(0, extensionIndex - 1);
+            parentName = parentDisplayName;
+            if (type != null) {
+                String extensionName = type.getName();
+                // int extensionIndex = parentDisplayName.indexOf(extensionName);
+                int extensionIndex = parentDisplayName.lastIndexOf(extensionName);// changed for bug TDI-19467
+                parentName = parentDisplayName.substring(0, extensionIndex - 1);
+            }
         }
+        String typeDispaly = primitive == null ? "" : TalendTextUtils.LBRACKET + this.primitive.getName()
+                + TalendTextUtils.RBRACKET;
 
-        this.displayName = parentName + "-" + this.componentindex + "-" + this.subcomponentindex + TalendTextUtils.LBRACKET
-                + this.primitive.getName() + TalendTextUtils.RBRACKET;
+        this.displayName = parentName + "-" + this.componentindex + "-" + this.subcomponentindex + typeDispaly; //$NON-NLS-1$ //$NON-NLS-2$
+
     }
 
 }
