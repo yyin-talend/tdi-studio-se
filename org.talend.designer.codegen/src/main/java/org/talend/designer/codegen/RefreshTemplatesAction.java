eea3b3b6f333f3d8e2b64d3cc8c7ea807eff0404
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-07-25 04:04:32 +0000

    TDI-26838 : fix problem of graphic disposed for the update of component. use the same method everywhere for any refresh of templates needed.
    git-svn-id: http://talendforge.org/svn/tos/trunk@105799 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/RefreshTemplatesAction.java
+++ org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/RefreshTemplatesAction.java
@@ -13,15 +13,6 @@
 package org.talend.designer.codegen;
 
 import org.eclipse.jface.action.Action;
-import org.eclipse.ui.PlatformUI;
-import org.talend.core.CorePlugin;
-import org.talend.core.GlobalServiceRegister;
-import org.talend.core.model.components.ComponentCompilations;
-import org.talend.core.model.process.Element;
-import org.talend.designer.codegen.model.CodeGeneratorEmittersPoolFactory;
-import org.talend.designer.core.IDesignerCoreService;
-import org.talend.designer.core.ui.views.properties.IComponentSettingsView;
-import org.talend.repository.model.ComponentsFactoryProvider;
 
 /**
  * DOC mhirt class global comment. Detailled comment <br/>
@@ -43,28 +34,8 @@ public class RefreshTemplatesAction extends Action {
      */
     @Override
     public void run() {
-        // TDI-25866:In case select a component and sctrl+shift+f3,need clean its componentSetting view
-        IComponentSettingsView viewer = (IComponentSettingsView) PlatformUI.getWorkbench().getActiveWorkbenchWindow()
-                .getActivePage().findView(IComponentSettingsView.ID);
-
-        Element oldComponent = null;
-
-        if (viewer != null) {
-            oldComponent = viewer.getElement();
-            viewer.cleanDisplay();
-        }
-        ComponentCompilations.deleteMarkers();
-        ComponentsFactoryProvider.getInstance().resetCache();
-        CorePlugin.getDefault().getLibrariesService().syncLibraries();
-        CodeGeneratorEmittersPoolFactory.initialize();
-        // achen modify to record ctrl+shift+f3 is pressed to fix bug 0006107
-        IDesignerCoreService designerCoreService = (IDesignerCoreService) GlobalServiceRegister.getDefault().getService(
-                IDesignerCoreService.class);
-        designerCoreService.getLastGeneratedJobsDateMap().clear();
-
-        if (oldComponent != null) {
-            viewer.setElement(oldComponent);
-        }
+        CodeGeneratorService service = new CodeGeneratorService();
+        service.refreshTemplates();
     }
 
 }
