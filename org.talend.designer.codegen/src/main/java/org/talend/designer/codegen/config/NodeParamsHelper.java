cbb03b2d22b4fefe814cc8745054b48de3cbd616
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-12 11:56:50 +0000

    [TESB-11028] tESBConsumer/BPM Connector: user Credentials propagation use alias/passwprd - [TESB-11060] Update UI Add copyright & author.
    git-svn-id: http://talendforge.org/svn/tos/trunk@107907 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodeParamsHelper.java
+++ org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodeParamsHelper.java
@@ -1,3 +1,15 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
 package org.talend.designer.codegen.config;
 
 import java.util.ArrayList;
@@ -27,6 +39,9 @@ import org.talend.designer.runprocess.ItemCacheManager;
 /**
  * Use for wrap a {@link INode} to provide faster and convenient methods to get
  * parameters.
+ * 
+ * @author GaoZone
+ * @update 2013-9-12 [TESB-11028]
  */
 public class NodeParamsHelper {
 
