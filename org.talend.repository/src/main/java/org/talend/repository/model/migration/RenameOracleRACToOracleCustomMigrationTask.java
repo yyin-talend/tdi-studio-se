a6d9f5980efb214d6cd24bfa8a5de2917a1815a6
hcyi <hcyi@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-13 07:32:38 +0000

    Feature TDI-27433 added : Rename the name Oracle RAC to Oracle Custom .
    git-svn-id: http://talendforge.org/svn/tos/trunk@107973 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/RenameOracleRACToOracleCustomMigrationTask.java
@@ -0,0 +1,77 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.database.EDatabaseTypeName;
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.migration.AbstractItemMigrationTask;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
+
+/**
+ * DOC hcyi class global comment. Detailled comment
+ */
+public class RenameOracleRACToOracleCustomMigrationTask extends AbstractItemMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        ConnectionItem connectionItem = (ConnectionItem) item;
+        DatabaseConnection connection = (DatabaseConnection) connectionItem.getConnection();
+        if (connection != null) {
+            if (connection.getDatabaseType().equals("Oracle RAC")) {
+                connection.setDatabaseType(EDatabaseTypeName.ORACLE_CUSTOM.getDisplayName());
+                try {
+                    ProxyRepositoryFactory.getInstance().save(item);
+                } catch (PersistenceException e) {
+                    ExceptionHandler.process(e);
+                    return ExecutionResult.FAILURE;
+                }
+                return ExecutionResult.SUCCESS_NO_ALERT;
+            } else {
+                return ExecutionResult.NOTHING_TO_DO;
+            }
+        }
+        return ExecutionResult.NOTHING_TO_DO;
+    }
+
+    @Override
+    public List<ERepositoryObjectType> getTypes() {
+        List<ERepositoryObjectType> toReturn = new ArrayList<ERepositoryObjectType>();
+        toReturn.add(ERepositoryObjectType.METADATA_CONNECTIONS);
+        return toReturn;
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IProjectMigrationTask#getOrder()
+     */
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2013, 9, 13, 1, 0, 0);
+        return gc.getTime();
+    }
+}
