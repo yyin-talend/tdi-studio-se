2a5c16a017785b613f69913b54dad862450a5e56
nrousseau <nrousseau@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-09-22 06:36:29 +0000

    merge r108279 from trunk to branch 5.3 TDI-27364 : fix the version problem for psql for the version change to have the same as the component.
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@108281 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/properties/MultipleThreadDynamicComposite.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/properties/MultipleThreadDynamicComposite.java
@@ -215,7 +215,7 @@ public class MultipleThreadDynamicComposite extends ScrolledComposite implements
                         if (item instanceof ConnectionItem) {
                             ConnectionItem connectionItem = (ConnectionItem) item;
                             Connection connection = connectionItem.getConnection();
-                            if (connection.isReadOnly()) {
+                            if (connection == null || connection.isReadOnly()) {
                                 continue;
                             }
                             for (Object tableObj : ConnectionHelper.getTables(connection)) {
