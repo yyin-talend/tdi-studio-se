11b801ecf7d041f0e19fb4dce2149710f9e0c613
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-08 02:51:05 +0000

    [TESB-11135] Duplicate conditions of node parameters.(showIf in xml file and if-else in javajet). Update method NodeParamsHelper.getDisplayValue(IElementParameter) use ElementParameterParser.getDisplayValue(IElementParameter), instead of copy code.
    git-svn-id: http://talendforge.org/svn/tos/trunk@108928 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodeParamsHelper.java
+++ org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodeParamsHelper.java
@@ -19,22 +19,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
-import org.talend.commons.exception.ExceptionHandler;
-import org.talend.core.model.process.EParameterFieldType;
 import org.talend.core.model.process.ElementParameterParser;
 import org.talend.core.model.process.IElementParameter;
 import org.talend.core.model.process.INode;
-import org.talend.core.model.process.IProcess;
-import org.talend.core.model.properties.Item;
-import org.talend.core.model.properties.ProcessItem;
-import org.talend.core.model.properties.RulesItem;
-import org.talend.core.model.relationship.RelationshipItemBuilder;
-import org.talend.core.model.repository.IRepositoryViewObject;
-import org.talend.core.model.utils.JavaResourcesHelper;
-import org.talend.core.runtime.CoreRuntimePlugin;
-import org.talend.core.utils.TalendQuoteUtils;
-import org.talend.designer.core.model.utils.emf.talendfile.ContextType;
-import org.talend.designer.runprocess.ItemCacheManager;
 
 /**
  * Use for wrap a {@link INode} to provide faster and convenient methods to get
@@ -228,146 +215,18 @@ public class NodeParamsHelper {
     }
 
     /**
-     * Gets the display value. Copied from
-     * 
-     * @param param
-     *            the param
-     * @return the display value
-     *         {@code ElementParameterParser.getDisplayValue(final IElementParameter param)}
-     */
-    @SuppressWarnings("unchecked")
+	 * Gets the display value.
+	 * 
+	 * @param param
+	 *            the param
+	 * @return the display value
+	 *         {@code ElementParameterParser.getDisplayValue(final IElementParameter param)}
+	 */
     private static String getDisplayValue(final IElementParameter param) {
-        if (param == null) {
-            return "";
-        }
-        Object value = param.getValue();
-
-        if (value instanceof String) {
-
-            if (param.getName().equals("PROCESS_TYPE_VERSION") && value.equals(RelationshipItemBuilder.LATEST_VERSION)) { //$NON-NLS-1$
-                String jobId = (String) param.getParentParameter().getChildParameters()
-                        .get("PROCESS_TYPE_PROCESS").getValue(); //$NON-NLS-1$
-                ProcessItem processItem = ItemCacheManager.getProcessItem(jobId);
-                if (processItem == null) {
-                    return ""; //$NON-NLS-1$
-                }
-                return processItem.getProperty().getVersion();
-            }
-            if (param.getName().equals("PROCESS_TYPE_CONTEXT")) { //$NON-NLS-1$
-                String jobId = (String) param.getParentParameter().getChildParameters()
-                        .get("PROCESS_TYPE_PROCESS").getValue(); //$NON-NLS-1$
-                ProcessItem processItem = ItemCacheManager.getProcessItem(jobId);
-                if (processItem == null) {
-                    return ""; //$NON-NLS-1$
-                }
-                // check if the selected context exists, if not, use the default
-                // context of the job.
-                boolean contextExists = false;
-                for (Object object : processItem.getProcess().getContext()) {
-                    if (object instanceof ContextType) {
-                        if (((ContextType) object).getName() != null && ((ContextType) object).getName().equals(value)) {
-                            contextExists = true;
-                            continue;
-                        }
-                    }
-                }
-                if (!contextExists) {
-                    return processItem.getProcess().getDefaultContext();
-                }
-                return (String) value;
-            }
-
-            if (param.getName().equals("SELECTED_JOB_NAME")) {
-                String jobId = (String) param.getChildParameters().get("PROCESS_TYPE_PROCESS").getValue(); //$NON-NLS-1$
-                ProcessItem processItem = ItemCacheManager.getProcessItem(jobId);
-                if (processItem == null) {
-                    return ""; //$NON-NLS-1$
-                }
-                return processItem.getProperty().getLabel();
-            }
-            // hywang add for 6484
-            if ("SELECTED_FILE".equals(param.getRepositoryValue())) { //$NON-NLS-1$
-                IElementParameter propertyParam = param.getElement().getElementParameter(
-                        "PROPERTY:REPOSITORY_PROPERTY_TYPE"); //$NON-NLS-1$
-                if (propertyParam != null && propertyParam.getValue() != null && !propertyParam.getValue().equals("")) { //$NON-NLS-1$
-                    try {
-                        IRepositoryViewObject object = CoreRuntimePlugin.getInstance().getProxyRepositoryFactory()
-                                .getLastVersion((String) propertyParam.getValue());
-                        if (object != null) {
-                            Item item = object.getProperty().getItem();
-                            String extension = null;
-
-                            String rule = ""; //$NON-NLS-1$
-                            String processLabelAndVersion = null;
-                            if (item instanceof RulesItem) {
-                                RulesItem rulesItem = (RulesItem) item;
-                                extension = rulesItem.getExtension();
-                                if (param.getElement() instanceof INode) {
-                                    INode node = (INode) param.getElement();
-                                    IProcess process = node.getProcess();
-                                    String jobLabel = process.getName();
-                                    String jobVersion = process.getVersion();
-                                    processLabelAndVersion = JavaResourcesHelper.getJobFolderName(jobLabel, jobVersion);
-                                }
-
-                                rule = "rules/final/" + processLabelAndVersion + "/" + rulesItem.getProperty().getLabel() //$NON-NLS-1$ //$NON-NLS-2$
-                                        + rulesItem.getProperty().getVersion() + extension;
-                            }
-                            return TalendQuoteUtils.addQuotes(rule);
-                        } else {
-                            return param.getValue().toString();
-                        }
-                    } catch (Exception e) {
-                        ExceptionHandler.process(e);
-                    }
-
-                }
-            }
-
-            return (String) value;
-        }
-        if (param.getFieldType() == EParameterFieldType.RADIO || param.getFieldType() == EParameterFieldType.CHECK
-                || param.getFieldType() == EParameterFieldType.AS400_CHECK) {
-            if (value instanceof Boolean) {
-                return ((Boolean) param.getValue()).toString();
-            } else {
-                return Boolean.FALSE.toString();
-            }
-        }
-
-        if (param.getFieldType() == EParameterFieldType.TABLE) {
-            List<Map<String, Object>> tableValues = (List<Map<String, Object>>) param.getValue();
-            String[] items = param.getListItemsDisplayCodeName();
-            String stringValues = "{"; //$NON-NLS-1$
-            for (int i = 0; i < tableValues.size(); i++) {
-                Map<String, Object> lineValues = tableValues.get(i);
-                stringValues += "["; //$NON-NLS-1$
-                for (int j = 0; j < items.length; j++) {
-
-                    Object currentValue = lineValues.get(items[j]);
-                    if (currentValue instanceof Integer) {
-                        IElementParameter tmpParam = (IElementParameter) param.getListItemsValue()[j];
-                        if (tmpParam.getListItemsDisplayName().length != 0) {
-                            stringValues += tmpParam.getListItemsDisplayName()[(Integer) currentValue];
-                        }
-                    } else {
-                        stringValues += currentValue;
-                    }
-
-                    if (j != (items.length - 1)) {
-                        stringValues += ","; //$NON-NLS-1$
-                    }
-                }
-                stringValues += "]"; //$NON-NLS-1$
-                if (i != (tableValues.size() - 1)) {
-                    stringValues += ","; //$NON-NLS-1$
-                }
-            }
-            stringValues += "}"; //$NON-NLS-1$
-            return stringValues;
-        }
-
-        return new String(""); //$NON-NLS-1$
+		if (param == null) {
+			return "";
+		}
+		return ElementParameterParser.getStringElementParameterValue(param);
     }
 
 }
