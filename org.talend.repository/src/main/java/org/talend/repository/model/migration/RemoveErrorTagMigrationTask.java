07ecab289074be31bd33f02c3177a426fff09fa6
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-17 07:21:32 +0000

    bug TDI-27525 fixed : Red cross on a Database Icon in the repository
    git-svn-id: http://talendforge.org/svn/tos/trunk@109517 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/RemoveErrorTagMigrationTask.java
@@ -0,0 +1,51 @@
+package org.talend.repository.model.migration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Iterator;
+
+import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
+import org.talend.core.model.migration.AbstractItemMigrationTask;
+import org.talend.core.model.properties.ConnectionItem;
+import org.talend.core.model.properties.InformationLevel;
+import org.talend.core.model.properties.Item;
+import org.talend.migration.IMigrationTask.ExecutionResult;
+
+public class RemoveErrorTagMigrationTask extends AbstractItemMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2013, 10, 17, 14, 0, 0);
+        return gc.getTime();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        ConnectionItem connectionItem = (ConnectionItem) item;
+        DatabaseConnection connection = (DatabaseConnection) connectionItem.getConnection();
+        if (connection != null) {
+            Iterator it = connectionItem.getProperty().getInformations().iterator();
+            while (it.hasNext()) {
+                org.talend.core.model.properties.Information Information = (org.talend.core.model.properties.Information) it
+                        .next();
+                if (Information.getLevel().equals(InformationLevel.ERROR_LITERAL)
+                        || Information.getLevel().equals(InformationLevel.WARN_LITERAL)) {
+                    it.remove();
+                }
+            }
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        }
+        return ExecutionResult.NOTHING_TO_DO;
+    }
+}
