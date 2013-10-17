b0e272f8c2cec971841e4001fbe36e0503721c7e
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-17 09:12:47 +0000

    bug TDI-27525 fixed : Red cross on a Database Icon in the repository
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_3@109531 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.repository/src/main/java/org/talend/repository/model/migration/RemoveErrorTagMigrationTask.java
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/RemoveErrorTagMigrationTask.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Iterator;
+import java.util.List;
 
 import org.talend.core.model.metadata.builder.connection.DatabaseConnection;
 import org.talend.core.model.migration.AbstractItemMigrationTask;
@@ -11,6 +12,10 @@ import org.talend.core.model.properties.ConnectionItem;
 import org.talend.core.model.properties.InformationLevel;
 import org.talend.core.model.properties.Item;
 import org.talend.migration.IMigrationTask.ExecutionResult;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.repository.model.ProxyRepositoryFactory;
 
 public class RemoveErrorTagMigrationTask extends AbstractItemMigrationTask {
 
@@ -35,17 +40,34 @@ public class RemoveErrorTagMigrationTask extends AbstractItemMigrationTask {
         ConnectionItem connectionItem = (ConnectionItem) item;
         DatabaseConnection connection = (DatabaseConnection) connectionItem.getConnection();
         if (connection != null) {
+            boolean flag = false;
             Iterator it = connectionItem.getProperty().getInformations().iterator();
             while (it.hasNext()) {
                 org.talend.core.model.properties.Information Information = (org.talend.core.model.properties.Information) it
                         .next();
-                if (Information.getLevel().equals(InformationLevel.ERROR_LITERAL)
-                        || Information.getLevel().equals(InformationLevel.WARN_LITERAL)) {
+                if (Information.getLevel().getName().equals(InformationLevel.ERROR_LITERAL.getName())
+                        || Information.getLevel().getName().equals(InformationLevel.WARN_LITERAL.getName())) {
                     it.remove();
+                    flag = true;
                 }
             }
-            return ExecutionResult.SUCCESS_NO_ALERT;
+            if (flag) {
+                try {
+                    ProxyRepositoryFactory.getInstance().save(item);
+                } catch (PersistenceException e) {
+                    ExceptionHandler.process(e);
+                    return ExecutionResult.FAILURE;
+                }
+                return ExecutionResult.SUCCESS_NO_ALERT;
+            }
         }
         return ExecutionResult.NOTHING_TO_DO;
+    }
+
+    @Override
+    public List<ERepositoryObjectType> getTypes() {
+        List<ERepositoryObjectType> toReturn = new ArrayList<ERepositoryObjectType>();
+        toReturn.add(ERepositoryObjectType.METADATA_CONNECTIONS);
+        return toReturn;
     }
 }
