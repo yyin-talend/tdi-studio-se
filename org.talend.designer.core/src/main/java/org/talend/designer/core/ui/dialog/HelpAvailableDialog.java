e064aac94d640b7eed9e6d16c6fe616b4fef77c6
zgao <zgao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-22 09:20:20 +0000

    [TUP-1048] [User Experience] map the help views with the actual feature in the Studio Use inner panel of dialog.
    git-svn-id: http://talendforge.org/svn/tos/trunk@109766 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/dialog/HelpAvailableDialog.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/dialog/HelpAvailableDialog.java
@@ -12,12 +12,15 @@
 // ============================================================================
 package org.talend.designer.core.ui.dialog;
 
-import org.eclipse.jface.dialogs.Dialog;
 import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.jface.dialogs.TrayDialog;
 import org.eclipse.jface.window.IShellProvider;
-import org.eclipse.swt.events.HelpEvent;
-import org.eclipse.swt.events.HelpListener;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.help.IWorkbenchHelpSystem;
@@ -26,15 +29,12 @@ import org.eclipse.ui.help.IWorkbenchHelpSystem;
 /**
  * This Dialog is similar to TraDialog both support helpAvailable feature, but this one override the UI of Help button
  * to a normal labeled button instead of a "<img src="http://www.eclipse.org/mylyn/images/icon-help.gif">" image button.
- * Help button is visible by default(can be set by {@link #setHelpAvailable(boolean)}),just need to override
- * {@link #helpPressed()} method.
+ * Help button is visible by default(can be set by {@link #setHelpAvailable(boolean)}).
  * 
  * @author GaoZone
- * @version 1.0
+ * @version 1.1
  */
-public abstract class HelpAvailableDialog extends Dialog {
-
-    private boolean helpAvailable = true;
+public abstract class HelpAvailableDialog extends TrayDialog {
 
     public HelpAvailableDialog(IShellProvider parentShell) {
         super(parentShell);
@@ -47,29 +47,9 @@ public abstract class HelpAvailableDialog extends Dialog {
     @Override
     protected void configureShell(Shell newShell) {
         super.configureShell(newShell);
-        // Register help listener on the shell
-        newShell.addHelpListener(new HelpListener() {
-            public void helpRequested(HelpEvent event) {
-                if (isHelpAvailable()) {
-                    helpPressed();
-                }
-            }
-        });
-    }
-
-    /**
-     * Perform help behavior. Override when necessary.</br>example open help part inner the dialog:</br>
-     * {@code PlatformUI.getWorkbench().getHelpSystem().setHelp(getShell(), HELP_CONTEXT_ID);}</br>or open independent
-     * dialog:</br> {@code PlatformUI.getWorkbench().getHelpSystem().displayHelp(HELP_CONTEXT_ID);}
-     */
-    protected void helpPressed() {
+        setHelpAvailable(true);
         IWorkbenchHelpSystem helpSystem = PlatformUI.getWorkbench().getHelpSystem();
-        String helpContext = getHelpContextId();
-        if (helpContext == null) {
-            helpSystem.displayHelp();
-        } else {
-            helpSystem.displayHelp(helpContext);
-        }
+        helpSystem.setHelp(newShell, getHelpContextId());
     }
 
     /**
@@ -82,6 +62,28 @@ public abstract class HelpAvailableDialog extends Dialog {
         return null;
     }
 
+    @Override
+    protected Control createButtonBar(Composite parent) {
+        Composite composite = new Composite(parent, SWT.NONE);
+        // create a layout with spacing and margins appropriate for the font
+        // size.
+        GridLayout layout = new GridLayout();
+        layout.numColumns = 0; // this is incremented by createButton
+        layout.makeColumnsEqualWidth = true;
+        layout.marginWidth = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_MARGIN);
+        layout.marginHeight = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_MARGIN);
+        layout.horizontalSpacing = convertHorizontalDLUsToPixels(IDialogConstants.HORIZONTAL_SPACING);
+        layout.verticalSpacing = convertVerticalDLUsToPixels(IDialogConstants.VERTICAL_SPACING);
+        composite.setLayout(layout);
+        GridData data = new GridData(GridData.HORIZONTAL_ALIGN_END | GridData.VERTICAL_ALIGN_CENTER);
+        composite.setLayoutData(data);
+        composite.setFont(parent.getFont());
+
+        // Add the buttons to the button bar.
+        createButtonsForButtonBar(composite);
+        return composite;
+    }
+
     /**
      * Add create Help Button if {@link #isHelpAvailable()} = true.
      * 
@@ -92,23 +94,26 @@ public abstract class HelpAvailableDialog extends Dialog {
         if (isHelpAvailable()) {
             createButton(parent, IDialogConstants.HELP_ID, IDialogConstants.HELP_LABEL, false);
         }
-        super.createButtonsForButtonBar(parent);
+        // create OK and Cancel buttons by default
+        createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+        createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
     }
 
     @Override
     protected void buttonPressed(int buttonId) {
         if (IDialogConstants.HELP_ID == buttonId) {
-            helpPressed();
+            if (getShell() != null) {
+                Control c = getShell().getDisplay().getFocusControl();
+                while (c != null) {
+                    if (c.isListening(SWT.Help)) {
+                        c.notifyListeners(SWT.Help, new Event());
+                        break;
+                    }
+                    c = c.getParent();
+                }
+            }
         } else {
             super.buttonPressed(buttonId);
         }
-    }
-
-    protected boolean isHelpAvailable() {
-        return helpAvailable;
-    }
-
-    public void setHelpAvailable(boolean helpAvailable) {
-        this.helpAvailable = helpAvailable;
     }
 }
