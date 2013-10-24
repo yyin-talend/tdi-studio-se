d524131efe28c1b2bbf1b97fb98759344400afdb
wwang <wwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2013-10-24 06:04:08 +0000

    Fix Feature TDI-26945 : add database support for hive components https://jira.talendforge.org/browse/TDI-26945
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_2@109905 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeDBName4Hive.java
@@ -0,0 +1,90 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2013 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class ChangeDBName4Hive extends AbstractJobMigrationTask {
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = { "tHiveConnection", "tHiveRow", "tHiveInput", "tELTHiveMap" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
+
+        IComponentConversion changeDBName4Hive = new IComponentConversion() {
+
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType database = ComponentUtilities.getNodeProperty(node, "DBNAME"); //$NON-NLS-1$
+
+                if (database == null) {
+                    return;
+                }
+
+                String dbname = database.getValue();
+                if (dbname == null) {
+                    return;
+                }
+
+                String value = dbname.trim();
+                if ("".equals(value) || "\"\"".equals(value) || "\"default\"".equals(value)) {//$NON-NLS-1$//$NON-NLS-2$//$NON-NLS-3$
+                    return;
+                }
+
+                database.setValue("\"\"");//$NON-NLS-1$
+            }
+
+        };
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(changeDBName4Hive));
+            } catch (PersistenceException e) {
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+
+    }
+
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2013, 9, 24, 10, 0, 0);
+        return gc.getTime();
+    }
+}
