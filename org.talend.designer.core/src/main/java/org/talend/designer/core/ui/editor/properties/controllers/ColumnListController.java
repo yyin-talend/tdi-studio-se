dda9b6b96ef915c5d4124896d36a08075871963b
ycbai <ycbai@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-03 08:32:44 +0000

    TDI-28502 : the component must NOT see its own generated column
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@112929 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/ColumnListController.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/ColumnListController.java
@@ -759,6 +759,9 @@ public class ColumnListController extends AbstractElementPropertySectionControll
 
         if (table != null) {
             for (IMetadataColumn column : table.getListColumns()) {
+                if (column.isCustom()) { // for TDI-28502
+                    continue;
+                }
                 // add for bug 12034
                 String label = column.getLabel();
                 //                if (element instanceof INode && ((INode) element).getComponent().getName().endsWith("tFileInputXML")) {//$NON-NLS-1$
@@ -792,6 +795,9 @@ public class ColumnListController extends AbstractElementPropertySectionControll
             IMetadataTable table = connection.getMetadataTable();
             if (table != null) {
                 for (IMetadataColumn column : table.getListColumns()) {
+                    if (column.isCustom()) { // for TDI-28502
+                        continue;
+                    }
                     columnList.add(column.getLabel());
                 }
             }
