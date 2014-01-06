96c73e809084e5651df636509ece0277b2735b39
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-06 08:59:30 +0000

    bug TDI-7370:Bad object sorting in Outline view
    git-svn-id: http://talendforge.org/svn/tos/trunk@112999 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/outline/ProcessTreeEditPart.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/outline/ProcessTreeEditPart.java
@@ -14,9 +14,16 @@ package org.talend.designer.core.ui.editor.outline;
 
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.eclipse.gef.EditPart;
 import org.eclipse.gef.editparts.AbstractTreeEditPart;
+import org.talend.designer.core.ui.editor.nodes.Node;
 import org.talend.designer.core.ui.editor.process.Process;
 
 /**
@@ -79,6 +86,85 @@ public class ProcessTreeEditPart extends AbstractTreeEditPart implements Propert
 
     /*
      * (non-Javadoc)
+     * 
+     * @see org.eclipse.gef.editparts.AbstractEditPart#refreshChildren()
+     */
+    @SuppressWarnings("unchecked")
+    @Override
+    protected void refreshChildren() {
+        Map modelToEditPart = new HashMap();
+        List children = getChildren();
+        int i;
+        for (i = 0; i < children.size(); i++) {
+            EditPart editPart = (EditPart) children.get(i);
+            modelToEditPart.put(editPart.getModel(), editPart);
+        }
+
+        List modelObjects = getModelChildren();
+        Collections.sort(modelObjects, new Comparator<Node>() {
+
+            @Override
+            public int compare(Node o1, Node o2) {
+                String s1 = o1.getUniqueName().toLowerCase();
+                String s2 = o2.getUniqueName().toLowerCase();
+                if (s1.charAt(0) > s2.charAt(0)) {
+                    return 1;
+                } else if (s1.charAt(0) < s2.charAt(0)) {
+                    return -1;
+                } else { // The alphanumeric section
+                    String alphabet1 = s1.replaceAll("\\d+$", "");
+                    String alphabet2 = s2.replaceAll("\\d+$", "");
+                    // If you don't want to case-sensitive, or
+                    // compareTo
+                    int cmpAlphabet = alphabet1.compareToIgnoreCase(alphabet2);
+                    if (cmpAlphabet != 0) {
+                        return cmpAlphabet;
+                    }
+                    // Digital section
+                    String numeric1 = s1.replaceAll("^[a-zA-Z_]+", "");
+                    String numeric2 = s2.replaceAll("^[a-zA-Z_]+", "");
+                    if ("".equals(numeric1)) {
+                        /*
+                         * Even if the numeric2 is the empty string also does not matter, of course, if the comparison
+                         * is not String ( or other immutable objects ) is another matter
+                         */
+                        return -1;
+                    }
+                    if ("".equals(numeric2)) {
+                        return 1;
+                    }
+                    int result = Integer.parseInt(numeric1) - Integer.parseInt(numeric2);
+                    return result;
+                }
+            }
+
+        });
+
+        for (i = 0; i < modelObjects.size(); i++) {
+            Object model = modelObjects.get(i);
+            if (i >= children.size() || ((EditPart) children.get(i)).getModel() != model) {
+                EditPart editPart = (EditPart) modelToEditPart.get(model);
+                if (editPart != null) {
+                    reorderChild(editPart, i);
+                } else {
+                    editPart = createChild(model);
+                    addChild(editPart, i);
+                }
+            }
+        }
+
+        List trash = new ArrayList();
+        for (; i < children.size(); i++)
+            trash.add(children.get(i));
+
+        for (i = 0; i < trash.size(); i++) {
+            EditPart ep = (EditPart) trash.get(i);
+            removeChild(ep);
+        }
+    }
+
+    /*
+     * ss (non-Javadoc)
      * 
      * @see org.eclipse.gef.editparts.AbstractEditPart#deactivate()
      */
