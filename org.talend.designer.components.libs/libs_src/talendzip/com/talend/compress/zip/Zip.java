a2e715160b9952006efbdcd11c0090a1ccc84650
wwang <wwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-07 04:22:11 +0000

    Fix Bug TDI-28402 : Impossible to delete a zip file after "OnComponentError" using tFileUnarchive https://jira.talendforge.org/browse/TDI-28402
    git-svn-id: http://talendforge.org/svn/tos/branches/branch-5_4@113029 f6f1c999-d317-4740-80b0-e6d1abc6f99e

---
+++ org.talend.designer.components.libs/libs_src/talendzip/com/talend/compress/zip/Zip.java
@@ -0,0 +1,177 @@
+package com.talend.compress.zip;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Zip {
+
+	private String sourceDir;
+	private String targetZip;
+	private boolean overwriteExistTargetZip = true;
+	private boolean makeTargetDir = false;
+	private int compressLevel = 4;
+	private String encoding = "ISO-8859-15";
+
+	private boolean allFiles = true;
+	private String namePatternFilter = null;
+	private boolean containSubDir = true;
+
+	private boolean isEncrypted = false;
+	private String password = null;
+
+	private String zip64Mode = null;
+
+	public void setZip64Mode(String zip64Mode) {
+		this.zip64Mode = zip64Mode;
+	}
+
+	public void setEncoding(String encoding) {
+		this.encoding = encoding;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEncrypted(boolean isEncrypted) {
+		this.isEncrypted = isEncrypted;
+	}
+
+	public void setContainSubDir(boolean containSubDir) {
+		this.containSubDir = containSubDir;
+	}
+
+	public void setAllFiles(boolean allFiles) {
+		this.allFiles = allFiles;
+	}
+
+	public void setNamePatternFilter(String namePatternFilter) {
+		this.namePatternFilter = namePatternFilter;
+	}
+
+	public void setCompressLevel(int compressLevel) {
+		this.compressLevel = compressLevel;
+	}
+
+	public void setMakeTargetDir(boolean makeTargetDir) {
+		this.makeTargetDir = makeTargetDir;
+	}
+
+	public Zip(String sourceDir, String targetZip) {
+		this.sourceDir = sourceDir;
+		this.targetZip = targetZip;
+	}
+
+	public void setOverwriteExistTargetZip(boolean overwriteExistTargetZip) {
+		this.overwriteExistTargetZip = overwriteExistTargetZip;
+	}
+
+	public void doZip() throws Exception {
+		File target = new java.io.File(targetZip);
+		if (target.exists()) {
+			if (!overwriteExistTargetZip) {
+				throw (new java.lang.Exception("File already exist!"));
+			} else {
+				target.delete();
+			}
+		}
+
+		if (makeTargetDir) {
+			String zipDir = targetZip.contains("\\") ? targetZip.substring(0,
+					targetZip.lastIndexOf("\\")) : targetZip.substring(0,
+					targetZip.lastIndexOf("/"));
+			File zDir = new java.io.File(zipDir);
+			zDir.mkdirs();
+		}
+
+		final File source = new File(sourceDir);
+		final List<File> list = new ArrayList<File>();
+		source.listFiles(new java.io.FilenameFilter() {
+			public boolean accept(java.io.File dir, String name) {
+				java.io.File file = new java.io.File(dir, name);
+				if (file.isFile()) {
+					if (allFiles || name.matches(namePatternFilter)) {
+						list.add(file);
+						return true;
+					}
+				} else if (containSubDir) {
+					file.listFiles(this);
+				}
+				return false;
+			}
+		});
+
+		if (list.size() < 1) {
+			return;
+		}
+
+		doZip1(source, list);
+	}
+
+	// apache common compress impl
+	private void doZip1(final File source, final List<File> list)
+			throws Exception {
+		int beginIndex = source.getPath().length() + 1;
+
+		java.io.OutputStream output_stream = null;
+		try {
+			output_stream = new java.io.FileOutputStream(targetZip);
+			if (isEncrypted && !"".equals(password)) {
+				output_stream = new javax.crypto.CipherOutputStream(
+						output_stream,
+						org.talend.archive.IntegrityUtil.createCipher(
+								javax.crypto.Cipher.ENCRYPT_MODE, password));
+			}
+		} catch (Exception e) {
+			if (output_stream != null) {
+				output_stream.close();
+			}
+			throw e;
+		}
+
+		org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream out = null;
+		out = new org.apache.commons.compress.archivers.zip.ZipArchiveOutputStream(
+				new java.io.BufferedOutputStream(output_stream));
+		out.setLevel(compressLevel);
+		// set filename & comment encoding
+		out.setEncoding(encoding);
+		if ("ALWAYS".equals(zip64Mode)) {
+			out.setUseZip64(org.apache.commons.compress.archivers.zip.Zip64Mode.Always);
+		} else if ("NEVER".equals(zip64Mode)) {
+			out.setUseZip64(org.apache.commons.compress.archivers.zip.Zip64Mode.Never);
+		}
+
+		try {
+			for (int i = 0; i < list.size(); i++) {
+				org.apache.commons.compress.archivers.zip.ZipArchiveEntry entry = new org.apache.commons.compress.archivers.zip.ZipArchiveEntry(
+						list.get(i).getPath().substring(beginIndex));
+				entry.setTime(list.get(i).lastModified());
+				out.putArchiveEntry(entry);
+
+				java.io.BufferedInputStream in = null;
+				try {
+					in = new java.io.BufferedInputStream(
+							new java.io.FileInputStream(list.get(i)));
+					int readLen;
+					byte[] buf = new byte[1024];
+					while ((readLen = in.read(buf, 0, 1024)) != -1) {
+						out.write(buf, 0, readLen);
+					}
+				} finally {
+					if (in != null) {
+						in.close();
+					}
+				}
+
+				out.closeArchiveEntry();
+				out.flush();
+			}
+		} finally {
+			if (out != null) {
+				out.close();
+			}
+		}
+	}
+
+}
