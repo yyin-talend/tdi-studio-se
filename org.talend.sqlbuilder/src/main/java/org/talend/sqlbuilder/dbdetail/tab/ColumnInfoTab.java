b8eea64325e9fd696baa1c1a1ec7acd81e594d2e
hwang <hwang@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-01-16 10:04:06 +0000

    TDI-28578:Metadata wizard doesn't display tables starting with '/'
    git-svn-id: http://talendforge.org/svn/tos/trunk@113461 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/dbdetail/tab/ColumnInfoTab.java
+++ org.talend.sqlbuilder/src/main/java/org/talend/sqlbuilder/dbdetail/tab/ColumnInfoTab.java
@@ -21,6 +21,7 @@ import net.sourceforge.squirrel_sql.fw.sql.ITableInfo;
 
 import org.talend.core.model.metadata.builder.database.ExtractMetaDataFromDataBase;
 import org.talend.core.model.metadata.builder.database.ExtractMetaDataUtils;
+import org.talend.core.model.metadata.builder.util.MetadataConnectionUtils;
 import org.talend.sqlbuilder.Messages;
 import org.talend.sqlbuilder.dataset.dataset.DataSet;
 import org.talend.sqlbuilder.dbstructure.nodes.INode;
@@ -67,8 +68,15 @@ public class ColumnInfoTab extends AbstractDataSetTab {
                         .getColumns(ti.getCatalogName(), ti.getSchemaName(), realTableName, "%"); //$NON-NLS-1$
 
             } else {
-
-                resultSet = node.getSession().getMetaData().getColumns(tableNode.getTableInfo());
+                // https://jira.talendforge.org/browse/TDI-28578
+                String tableName = ti.getSimpleName();
+                boolean isOracle = MetadataConnectionUtils.isOracle(treeNode.getMetaData().getJDBCMetaData());
+                if (isOracle && tableName.contains("/")) {
+                    tableName = tableName.replaceAll("/", "//");
+                }
+                resultSet = node.getSession().getMetaData().getJDBCMetaData()
+                        .getColumns(ti.getCatalogName(), ti.getSchemaName(), tableName, "%");
+                // resultSet = node.getSession().getMetaData().getColumns(tableNode.getTableInfo());
             }
 
             DataSet dataSet = new DataSet(null, resultSet, new int[] { 4, 5, 6, 7, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18 });
