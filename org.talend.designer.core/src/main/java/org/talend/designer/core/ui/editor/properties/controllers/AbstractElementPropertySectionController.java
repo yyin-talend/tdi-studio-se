ea2dd63f3b670beca2e82f5ec49b821c05a012f6
plv <plv@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-02-21 07:36:29 +0000

    bug TDI-28675 fixed: Incompatibility jdbc driver 6 Vertica on Vertica 6 Server
    git-svn-id: http://talendforge.org/svn/tos/trunk@114298 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/AbstractElementPropertySectionController.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/AbstractElementPropertySectionController.java
@@ -72,6 +72,7 @@ import org.talend.commons.utils.generation.CodeGenerationUtils;
 import org.talend.core.CorePlugin;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.context.RepositoryContext;
+import org.talend.core.database.EDatabase4DriverClassName;
 import org.talend.core.database.EDatabaseTypeName;
 import org.talend.core.database.conn.ConnParameterKeys;
 import org.talend.core.database.conn.version.EDatabaseVersion4Drivers;
@@ -1434,6 +1435,16 @@ public abstract class AbstractElementPropertySectionController implements Proper
         connParameters.setDriverJar(TalendTextUtils.removeQuotes(driverJar));
 
         String driverClass = getValueFromRepositoryName(element, EConnectionParameterName.DRIVER_CLASS.getName());
+        String dbVersion = getValueFromRepositoryName(element, "DB_VERSION");
+        if ("VERTICA_6_0".equals(dbVersion)) {
+            connParameters.setDbVersion(EDatabaseVersion4Drivers.VERTICA_6.getVersionValue());
+            driverClass = EDatabase4DriverClassName.VERTICA2.getDriverClass();
+        } else if (EDatabaseVersion4Drivers.VERTICA_5_1.getVersionValue().equals(dbVersion)
+                || EDatabaseVersion4Drivers.VERTICA_6_1_X.getVersionValue().equals(dbVersion)) {
+            connParameters.setDbVersion(dbVersion);
+            driverClass = EDatabase4DriverClassName.VERTICA2.getDriverClass();
+        }
+
         connParameters.setDriverClass(TalendTextUtils.removeQuotes(driverClass));
 
         if (driverClass != null && !"".equals(driverClass)
@@ -1553,8 +1564,17 @@ public abstract class AbstractElementPropertySectionController implements Proper
             }
         }
         connParameters.setUrl(url);
-        connParameters.setDriverClass(TalendTextUtils.removeQuotesIfExist(getParameterValueWithContext(element,
-                EConnectionParameterName.DRIVER_CLASS.getName(), context)));
+
+        String driverClass = TalendTextUtils.removeQuotesIfExist(getParameterValueWithContext(element,
+                EConnectionParameterName.DRIVER_CLASS.getName(), context));
+        String dbVersion = getValueFromRepositoryName(element, "DB_VERSION");
+        if (EDatabaseVersion4Drivers.VERTICA_6.getVersionValue().equals(dbVersion)
+                || EDatabaseVersion4Drivers.VERTICA_5_1.getVersionValue().equals(dbVersion)
+                || EDatabaseVersion4Drivers.VERTICA_6_1_X.getVersionValue().equals(dbVersion)) {
+            driverClass = EDatabase4DriverClassName.VERTICA2.getDriverClass();
+        }
+        connParameters.setDriverClass(driverClass);
+
         connParameters.setDriverJar(TalendTextUtils.removeQuotesIfExist(getParameterValueWithContext(element,
                 EConnectionParameterName.DRIVER_JAR.getName(), context)));
 
