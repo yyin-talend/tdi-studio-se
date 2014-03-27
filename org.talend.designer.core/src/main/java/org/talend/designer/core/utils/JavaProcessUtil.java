af6646439b5dccdb0cf59d880615fc5b0a647415
zwzhao <zwzhao@f6f1c999-d317-4740-80b0-e6d1abc6f99e>
2014-03-27 06:02:46 +0000

    bug TDI-28591 fixed : tLibraryLoad with context value as Lib Path didn't work correctlly
    git-svn-id: http://talendforge.org/svn/tos/trunk@115563 f6f1c999-d317-4740-80b0-e6d1abc6f99e

--- org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
@@ -412,20 +412,9 @@ public class JavaProcessUtil {
                                 String var = ContextParameterUtils.getVariableFromCode(text);
                                 if (var.equals(contextPara.getName())) {
                                     String values = context.getContextParameter(contextPara.getName()).getValue();
-
-                                    if (curParam.getName().equals(EParameterName.DRIVER_JAR.getName()) && values.contains(";")) { //$NON-NLS-1$
-                                        String[] jars = values.split(";"); //$NON-NLS-1$
-                                        for (String jar2 : jars) {
-                                            String jar = jar2;
-                                            jar = jar.substring(jar.lastIndexOf("\\") + 1); //$NON-NLS-1$
-                                            ModuleNeeded module = new ModuleNeeded(null, jar, null, true);
-                                            modulesNeeded.add(module);
-                                        }
-                                    } else {
-                                        values = values.substring(values.lastIndexOf("\\") + 1); //$NON-NLS-1$
-                                        ModuleNeeded module = new ModuleNeeded(null, values, null, true);
-                                        modulesNeeded.add(module);
-                                    }
+                                    values = values.substring(values.lastIndexOf("\\") + 1); //$NON-NLS-1$
+                                    ModuleNeeded module = new ModuleNeeded(null, values, null, true);
+                                    modulesNeeded.add(module);
                                 }
                             }
                         }
