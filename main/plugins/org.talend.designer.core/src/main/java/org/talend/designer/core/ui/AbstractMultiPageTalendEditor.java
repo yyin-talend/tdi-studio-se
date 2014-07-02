5e509ead5d9689abd3d49c16efc19507673bd7bb
ldong-talend <ldong@talend.com>
2014-07-02 15:12:33 -0700

    feature TDI-29337 added:Simplify the display of the context view.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/AbstractMultiPageTalendEditor.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/AbstractMultiPageTalendEditor.java
@@ -568,7 +568,8 @@ public abstract class AbstractMultiPageTalendEditor extends MultiPageEditorPart 
             if (view != null) {
                 ContextsView contextsView = (ContextsView) view;
                 contextsView.getContextViewComposite().setTabEnable(flag);
-                contextsView.getContextViewComposite().getContextTemplateComposite().getViewer().getTree().setEnabled(flag);
+                contextsView.getContextViewComposite().getContextTableValueComposite().getViewer().getTree().setEnabled(flag);
+                // contextsView.getContextViewComposite().getContextTemplateComposite().getViewer().getTree().setEnabled(flag);
             }
         }
     }
