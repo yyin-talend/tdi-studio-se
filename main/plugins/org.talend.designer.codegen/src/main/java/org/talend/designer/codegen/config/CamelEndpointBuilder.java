a4d6b7d9907eda94e46f149932d9208a72ba4650
Zhangbohong GAO <zgao@talend.com>
2014-07-10 14:12:28 +0800

    [TESB-13644] cAMQP & cMQTT: finalyze Refactoring cMQTT code generator. Provide common tools to create endpoint uri - CamelEndpointBuilder https://jira.talendforge.org/browse/TESB-13644

---
+++ main/plugins/org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/CamelEndpointBuilder.java
@@ -0,0 +1,323 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.codegen.config;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.commons.lang.StringUtils;
+import org.talend.core.model.process.ElementParameterParser;
+import org.talend.core.model.process.IElementParameter;
+import org.talend.core.model.process.INode;
+
+/**
+ * Use to build camel endpoint.</br>The generated endpoint is different from
+ * real endpoint query,It generates java source code expression.</br>for example
+ * to build endpoint <code>comp,NAME(expression),PATH(expression)</code> with
+ * query <code>{P=V,P2=V2}</code></br> It will generate :</br>
+ * <code>"comp:"+NAME+":"+PATH+"?"+P+"="+V+"&"+p2+"="+V2</code></br> instead of
+ * :</br>
+ * <code style="text-decoration: line-through;">comp:name:path?P=V&P2=V2</code>
+ * </br>
+ * <p>
+ * In javadoc of methods, <b>Direct String</b> means String value, eg: hello
+ * (without quotes)</br> <b>Expression String</b> means the source code
+ * expression to generate a String value, eg: "a"+context.b+"c" (with quotes)
+ * </p>
+ * 
+ * @author GaoZone
+ * @since 5.6
+ * @version 0.4
+ */
+public class CamelEndpointBuilder {
+
+	private static final String QUOTED_EMPTY = "\"\""/* "" */;
+
+	private String component;
+	private String name;
+	private List<String> paths;
+
+	/** The params map. k-v both are Expression. */
+	private final Map<String, String> paramsMap;
+
+	CamelEndpointBuilder() {
+		paramsMap = new LinkedHashMap<String, String>();
+		paths = new ArrayList<String>();
+	}
+
+	/**
+	 * Provider append wrapped expression to {@link StringBuilder}
+	 */
+	private final static class SBTool {
+		private static void appendDirectString(StringBuilder sb, String string) {
+			sb.append("+\"").append(string).append('\"');
+		}
+
+		/**
+		 * Append expression to {@link StringBuilder}.
+		 * 
+		 * @param sb
+		 *            the sb
+		 * @param expression
+		 *            expression represents source to generate a String
+		 */
+		private static void appendExpression(StringBuilder sb, String expression) {
+			sb.append("+").append(expression);
+		}
+	}
+
+	/**
+	 * Some appender may need to override {@link #finish()}.
+	 */
+	private class ParamAppender {
+		public CamelEndpointBuilder finish() {
+			return CamelEndpointBuilder.this;
+		}
+	}
+
+	/**
+	 * Add a param that value is build from a Appendable style.</br>
+	 * {@link #finish()} must be called at last.
+	 * <code>.append(xx).append(xx).finish()</code>
+	 */
+	public final class BuildingValueParamAppender extends ParamAppender {
+
+		private final String key;
+		private final StringBuilder valueBuilder;
+
+		private BuildingValueParamAppender(String key) {
+			this.key = key;
+			valueBuilder = new StringBuilder();
+		}
+
+		@Override
+		public CamelEndpointBuilder finish() {
+			if(valueBuilder.length() == 0) {
+				return super.finish();
+			}
+			valueBuilder.deleteCharAt(0);
+			// remove the first appended +, because generated exp will be +"dir"+E2+E3
+			return addParam(key, valueBuilder.toString());
+		}
+
+		public BuildingValueParamAppender appendDirectString(String directString) {
+			SBTool.appendDirectString(valueBuilder, directString);
+			return this;
+		}
+
+		public BuildingValueParamAppender appendExpression(String expression) {
+			SBTool.appendExpression(valueBuilder, expression);
+			return this;
+		}
+
+	}
+
+	public final class NodeParamNotDefaultAppender extends ParamAppender {
+
+		private final boolean visibleRequired;
+		private INode node;
+
+		private NodeParamNotDefaultAppender(INode node, boolean visibleRequired) {
+			this.visibleRequired = visibleRequired;
+			this.node = node;
+		}
+
+		/**
+		 * Adds the param.
+		 *
+		 * @param key
+		 *            the param name
+		 * @param nodeParamKey
+		 *            the node param key, <code>__XXX__</code> style.
+		 * @param defaultValue
+		 *            the default value, will skip add if value equals default.
+		 * @return the node param not default appender
+		 */
+		public NodeParamNotDefaultAppender addParam(String key, String nodeParamKey, String defaultValue) {
+			if (node == null) {
+				return this;
+			}
+			IElementParameter param = node.getElementParameter(nodeParamKey);
+			if (param == null) {
+				return this;
+			}
+			if (visibleRequired && !param.isShow(node.getElementParametersWithChildrens())) {
+				return this;
+			}
+			String value = ElementParameterParser.getStringElementParameterValue(param);
+			addParamInConditions(key, value, false, defaultValue);
+			return this;
+		}
+	}
+
+	/**
+	 * Gets the builder.
+	 *
+	 * @return the builder
+	 */
+	public static CamelEndpointBuilder getBuilder() {
+		return new CamelEndpointBuilder();
+	}
+
+	public String build() {
+		StringBuilder sb = new StringBuilder();
+		sb.append('\"').append(component).append(":").append('\"');
+		// "comp:"
+		SBTool.appendExpression(sb, name);
+		// "comp:"+name
+		SBTool.appendDirectString(sb, ":");
+		// "component:"+name+":"
+		sb.append(buildPath()).append(buildParamQuery());
+		// comp+":"+name+":"+path+"?p="+v+"&p2="+v2
+		String build = sb.toString();
+		//merge "a"+"b" to "ab"
+		return StringUtils.remove(build, "\"+\"");
+	}
+
+	/**
+	 * Builds the path.
+	 * 
+	 * @return the char sequence, eg: P1+"/"+P2+"/"+P3
+	 */
+	private CharSequence buildPath() {
+		if (paths.isEmpty()) {
+			return "";
+		}
+		return "+" + StringUtils.join(paths, "+\"/\"+"/* "/" */);
+	}
+
+	/**
+	 * Builds the param query.
+	 * 
+	 * @return the char sequence, eg: +"?p="+v+"&p2="+v2
+	 */
+	private CharSequence buildParamQuery() {
+		StringBuilder sb = new StringBuilder();
+		for (Entry<String, String> entry : paramsMap.entrySet()) {
+			SBTool.appendDirectString(sb, "&");
+			SBTool.appendExpression(sb, entry.getKey());
+			SBTool.appendDirectString(sb, "=");
+			SBTool.appendExpression(sb, entry.getValue());
+		}
+		if (sb.length() > 0) {
+			// fix first & to ?.
+			sb.setCharAt(sb.indexOf("&"), '?');
+		}
+		return sb;
+	}
+
+	public CamelEndpointBuilder addParamInConditions(String key, String valueExp, boolean skipWhenKeyExist,
+			String skipWhenValueIsThis) {
+		key = StringUtils.trimToNull(key);
+		if (key == null) {
+			return this;
+		}
+		String keyExp = "\"" + key + "\"";// quoted key, forming to expression.
+		return addExpressionParamInConditions(keyExp, valueExp, skipWhenKeyExist, skipWhenValueIsThis);
+	}
+
+	/**
+	 * Adds the or update param.
+	 *
+	 * @param key
+	 *            the key , <b>Direct String</b>
+	 * @param value
+	 *            source expression to build value. <b>Expression String</b>
+	 * @return the camel endpoint builder
+	 */
+	public CamelEndpointBuilder addParam(String key, String value) {
+		return addParamInConditions(key, value, false, null);
+	}
+
+	public CamelEndpointBuilder addExpressionParamInConditions(String keyExp, String valueExp,
+			boolean skipWhenKeyExist, String skipWhenValueIsThis) {
+		keyExp = StringUtils.trimToNull(keyExp);
+		if (keyExp == null || keyExp.equals(QUOTED_EMPTY)) {
+			return this;
+		}
+		valueExp = StringUtils.trimToNull(valueExp);
+		if (valueExp == null || valueExp.equals(QUOTED_EMPTY)) {
+			return this;
+		}
+		if (skipWhenKeyExist && paramsMap.containsKey(keyExp)) {
+			return this;
+		}
+		if (valueExp.equals(skipWhenValueIsThis)) {
+			return this;
+		}
+		paramsMap.put(keyExp, valueExp);
+		return this;
+	}
+
+	public CamelEndpointBuilder addParamIfNotDefault(String key, String value, String defaultValue) {
+		return addParamInConditions(key, value, false, defaultValue);
+	}
+
+	public CamelEndpointBuilder addParamIfNotExist(String key, String value) {
+		return addParamInConditions(key, value, true, null);
+	}
+
+	public CamelEndpointBuilder addParams(Collection<Map<String, String>> tableValues) {
+		if (tableValues == null) {
+			return this;
+		}
+		for (Map<String, String> map : tableValues) {
+			addExpressionParamInConditions(map.get("NAME"), map.get("VALUE"), false, null);
+		}
+		return this;
+	}
+
+	/**
+	 * Sets the component.
+	 *
+	 * @param component
+	 *            the component name, <b>Direct String</b>.
+	 */
+	public void setComponent(String component) {
+		this.component = component;
+	}
+
+	/**
+	 * Sets the name.
+	 *
+	 * @param name
+	 *            the name, <b>Expression String</b>
+	 * @return
+	 */
+	public CamelEndpointBuilder setName(String name) {
+		this.name = name;
+		return this;
+	}
+
+	public CamelEndpointBuilder setDirectName(String directName) {
+		this.name = '\"' + directName + '\"';
+		return this;
+	}
+
+	public CamelEndpointBuilder appendPath(String pathExpression) {
+		paths.add(pathExpression);
+		return this;
+	}
+
+	public NodeParamNotDefaultAppender getNodeParamNotDefaultAppender(INode node) {
+		return new NodeParamNotDefaultAppender(node, false);
+	}
+
+	public BuildingValueParamAppender getBuildingValueParamAppender(String paramName) {
+		return new BuildingValueParamAppender(paramName);
+	}
+}
