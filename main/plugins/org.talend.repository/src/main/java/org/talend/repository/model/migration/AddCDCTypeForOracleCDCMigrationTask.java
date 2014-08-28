1cb925f46c67ef3ba15066147f2e0f8ac952a33d
jzhao <jzhao@talend.com>
2014-08-28 09:32:06 +0800

    Fixed Feature TDI-29787: Implement the tOracleCDC component on Oracle 12 https://jira.talendforge.org/browse/TDI-29787 Note:Add Migration Task

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddCDCTypeForOracleCDCMigrationTask.java
@@ -0,0 +1,88 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC jzhao class global comment. Detailled comment
+ */
+public class AddCDCTypeForOracleCDCMigrationTask extends
+		AbstractJobMigrationTask {
+
+	@Override
+	public ExecutionResult execute(Item item) {
+		ProcessType processType = getProcessType(item);
+		IComponentFilter filter = new NameComponentFilter("tOracleCDC");
+		try {
+			ModifyComponentsAction
+					.searchAndModify(
+							item,
+							processType,
+							filter,
+							Arrays.<IComponentConversion> asList(new IComponentConversion() {
+
+								public void transform(NodeType node) {
+									ElementParameterType logMode = ComponentUtilities
+											.getNodeProperty(node,
+													"CDC_TYPE_MODE");
+									if (logMode == null) {
+										return;
+									} else {
+										ComponentUtilities
+												.addNodeProperty(node,
+														"CDC_MODE",
+														"CLOSED_LIST");
+										if ("true".equalsIgnoreCase(logMode
+												.getValue())) {
+											ComponentUtilities.setNodeValue(
+													node, "CDC_MODE", "LOG");
+										} else {
+											ComponentUtilities
+													.setNodeValue(node,
+															"CDC_MODE",
+															"TRIGGER");
+										}
+
+									}
+								}
+							}));
+		} catch (PersistenceException e) {
+			ExceptionHandler.process(e);
+			return ExecutionResult.FAILURE;
+		}
+
+		return ExecutionResult.SUCCESS_NO_ALERT;
+
+	}
+
+	public Date getOrder() {
+		GregorianCalendar gc = new GregorianCalendar(2014, 7, 27, 12, 0, 0);
+		return gc.getTime();
+	}
+}
