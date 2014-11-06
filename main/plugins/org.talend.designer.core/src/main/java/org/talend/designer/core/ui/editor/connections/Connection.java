45d1a7f73488a0bd84d23d8bea749b6e0bdf2dfa
hcyi <hcyi@talend.com>
2014-11-06 16:26:33 +0800

    TDI-30811:tSalesforcebulkexec/tSalesforceOutput link tLogRow with main line has compile error.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/connections/Connection.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/connections/Connection.java
@@ -45,6 +45,7 @@ import org.talend.designer.core.DesignerPlugin;
 import org.talend.designer.core.i18n.Messages;
 import org.talend.designer.core.model.components.EParameterName;
 import org.talend.designer.core.model.components.ElementParameter;
+import org.talend.designer.core.ui.editor.cmd.ChangeMetadataCommand;
 import org.talend.designer.core.ui.editor.nodes.Node;
 import org.talend.designer.core.ui.editor.process.Process;
 import org.talend.designer.core.ui.editor.properties.controllers.ColumnListController;
@@ -198,6 +199,15 @@ public class Connection extends Element implements IConnection, IPerformance {
                     if (!ComponentCategory.CATEGORY_4_MAPREDUCE.getName().equals(process2.getComponentsType())) {
                         createMeterParameters(process2);
                     }
+                }
+            }
+            // TDI-30811:tSalesforcebulkexec/tSalesforceOutput link tLogRow with main line has compile error
+            IElementParameter schemaTypeParam = source.getElementParameterFromField(EParameterFieldType.SCHEMA_TYPE);
+            if (schemaTypeParam != null) {
+                IMetadataTable metadataTable = source.getMetadataFromConnector(schemaTypeParam.getContext());
+                if (metadataTable != null) {
+                    ChangeMetadataCommand cmd = new ChangeMetadataCommand(source, schemaTypeParam, metadataTable, metadataTable);
+                    cmd.execute(true);
                 }
             }
         }
