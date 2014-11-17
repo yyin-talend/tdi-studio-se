41d8e3c00f5a953247398b799f98bb0582bbc060
hwang <hwang@talend.com>
2014-11-17 16:41:32 +0800

    TDI-30951:CheckPoint: The subjob behind OnSubjobError checkpoint wouldn't execute after user launched recovery from the checkpoint

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/properties/ComponentSettingsView.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/properties/ComponentSettingsView.java
@@ -695,7 +695,8 @@ public class ComponentSettingsView extends ViewPart implements IComponentSetting
                 Object propertyValue = conn.getPropertyValue(Connection.LINESTYLE_PROP);
                 if (propertyValue.equals(EConnectionType.ON_COMPONENT_OK)
                         || propertyValue.equals(EConnectionType.ON_COMPONENT_ERROR)
-                        || propertyValue.equals(EConnectionType.RUN_IF)) {
+                        || propertyValue.equals(EConnectionType.RUN_IF) || propertyValue.equals(EConnectionType.ON_SUBJOB_ERROR)
+                        || propertyValue.equals(EConnectionType.PARALLELIZE)) {
                     return true;
                 } else {
                     if (isAvoidRecoveryByConditions(conn)) {
@@ -709,8 +710,7 @@ public class ComponentSettingsView extends ViewPart implements IComponentSetting
 
     private boolean isNeedRecoveryCategory(Object currentConnPropertyValue, boolean isNormalJob, boolean isMrStormJob) {
         // subjob_ok and subjob_error both need the recovery category
-        boolean isSubJobConn = currentConnPropertyValue.equals(EConnectionType.ON_SUBJOB_OK)
-                || currentConnPropertyValue.equals(EConnectionType.ON_SUBJOB_ERROR);
+        boolean isSubJobConn = currentConnPropertyValue.equals(EConnectionType.ON_SUBJOB_OK);
         return (isSubJobConn && isNormalJob) || isMrStormJob;
     }
 
