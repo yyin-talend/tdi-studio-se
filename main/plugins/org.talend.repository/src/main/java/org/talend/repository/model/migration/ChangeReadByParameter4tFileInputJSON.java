0c74bf05b693d97b141dac60444bf1e01216f8de
wang wei <wwang@talend.com>
2014-12-11 14:34:13 +0800

    TDI-28800: support new json-path api for tFileInputJSON https://jira.talendforge.org/browse/TDI-28800

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeReadByParameter4tFileInputJSON.java
@@ -0,0 +1,91 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class ChangeReadByParameter4tFileInputJSON extends
+		AbstractJobMigrationTask {
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = {"tFileInputJSON"}; //$NON-NLS-1$
+        
+    	IComponentConversion action = new IComponentConversion() {
+
+	        public void transform(NodeType node) {
+	            if(node == null) {
+	                return;
+	            }
+	            
+	        	ElementParameterType originalElement = ComponentUtilities.getNodeProperty(node, "READBYXPATH"); //$NON-NLS-1$
+	        	if (originalElement == null) {
+	        		return;
+	        	}
+	        	
+        		String content = originalElement.getValue();
+        		if(content == null) {
+        		    return;
+        		}
+        		
+        		ComponentUtilities.addNodeProperty(node, "READ_BY","CLOSED_LIST");
+        		if("false".equals(content)) {
+        			ComponentUtilities.setNodeValue(node, "READ_BY", "JSONPATH_WITHOUTPUT_LOOP");
+        		} else {//read by xpath
+        			ComponentUtilities.setNodeValue(node, "READ_BY", "XPATH");
+        		}
+	        }
+        
+    	};
+    	
+    	for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays
+                        .<IComponentConversion> asList(action));
+            } catch (PersistenceException e) {
+                // TODO Auto-generated catch block
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+
+    }
+
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2014, 11, 15, 14, 0, 0);
+        return gc.getTime();
+    }
+}
