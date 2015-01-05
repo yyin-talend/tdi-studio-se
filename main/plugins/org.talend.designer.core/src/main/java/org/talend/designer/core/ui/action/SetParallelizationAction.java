a8059247b3b7504b4190df9a59b4838c53636fdb
hwang <hwang@talend.com>
2015-01-05 18:07:31 +0800

    TDI-31397:radiobox:"Departion row" is missing after joblet when enable partitioning

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/action/SetParallelizationAction.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/action/SetParallelizationAction.java
@@ -51,7 +51,35 @@ public class SetParallelizationAction extends SelectionAction {
         }
         if (parts.size() == 1) {
             Object o = parts.get(0);
-            return calculateAction(o);
+            if (o instanceof SubjobContainerPart) {
+                SubjobContainerPart part = (SubjobContainerPart) o;
+                SubjobContainer subjob = (SubjobContainer) part.getModel();
+                if (subjob.getProcess().getComponentsType().equals(ComponentCategory.CATEGORY_4_DI.getName())) {
+                    if (subjob.isDisplayed()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+
+            } else if (o instanceof NodePart) {
+                NodePart part = (NodePart) o;
+                Node node = (Node) part.getModel();
+                if (node.getProcess().getComponentsType().equals(ComponentCategory.CATEGORY_4_DI.getName())) {
+                    if (node.isStart()) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                } else {
+                    return false;
+                }
+
+            } else {
+                return false;
+            }
         }
         return false;
     }
@@ -89,54 +117,4 @@ public class SetParallelizationAction extends SelectionAction {
         }
     }
 
-    private boolean calculateAction(Object object) {
-        if (object instanceof SubjobContainerPart) {
-            SubjobContainerPart part = (SubjobContainerPart) object;
-            SubjobContainer subjob = (SubjobContainer) part.getModel();
-            if (subjob.getProcess().getComponentsType().equals(ComponentCategory.CATEGORY_4_DI.getName())) {
-                if (subjob.isDisplayed()) {
-                    for (NodeContainer nodeCon : subjob.getNodeContainers()) {
-                        if (nodeCon.getNode().isJoblet()) {
-                            return false;
-                        }
-                    }
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-
-        } else if (object instanceof NodePart) {
-            NodePart part = (NodePart) object;
-            Node node = (Node) part.getModel();
-            if (node.getProcess().getComponentsType().equals(ComponentCategory.CATEGORY_4_DI.getName())) {
-                if (node.isJoblet()) {
-                    return false;
-                } else if (node.isStart()) {
-                    SubjobContainer subjob = node.getNodeContainer().getSubjobContainer();
-                    if (subjob == null) {
-                        return false;
-                    }
-                    for (NodeContainer nodeCon : subjob.getNodeContainers()) {
-                        if (node.getUniqueName().equals(nodeCon.getNode().getUniqueName())) {
-                            continue;
-                        }
-                        if (nodeCon.getNode().isJoblet()) {
-                            return false;
-                        }
-                    }
-
-                    return true;
-                } else {
-                    return false;
-                }
-            } else {
-                return false;
-            }
-
-        }
-        return false;
-    }
 }
