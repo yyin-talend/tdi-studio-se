fca69ecee33f7504be11c0f07a9ab7dc06b0290d
ycbai <ycbai@talend.com>
2015-01-15 15:45:24 +0800

    TDI-29844: "Show the table list for the current connection" buttton is unuseable for tMysqlInput when use an existing connection

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/AbstractElementPropertySectionController.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/AbstractElementPropertySectionController.java
@@ -1923,7 +1923,15 @@ public abstract class AbstractElementPropertySectionController implements Proper
 
     protected boolean isConnectionExist() {
         setAllConnectionParameters(null, elem);
-        ISQLBuilderService service = (ISQLBuilderService) GlobalServiceRegister.getDefault().getService(ISQLBuilderService.class);
+
+        ISQLBuilderService service = null;
+        if (GlobalServiceRegister.getDefault().isServiceRegistered(ISQLBuilderService.class)) {
+            service = (ISQLBuilderService) GlobalServiceRegister.getDefault().getService(ISQLBuilderService.class);
+        }
+        if (service == null) {
+            return false;
+        }
+
         if (contextManager != null && contextManager.getDefaultContext().getContextParameterList().size() != 0) {
             initConnectionParametersWithContext(elem, contextManager.getDefaultContext());
         }
