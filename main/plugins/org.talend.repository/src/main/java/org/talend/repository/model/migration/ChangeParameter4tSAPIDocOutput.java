1308bd1f9f7f45fa70acb682436d7454b7d2af64
wang wei <wwang@talend.com>
2015-02-10 09:43:04 +0800

    TDI-31536: Support streaming in tSAPIdoc component https://jira.talendforge.org/browse/TDI-31536

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeParameter4tSAPIDocOutput.java
@@ -0,0 +1,79 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class ChangeParameter4tSAPIDocOutput extends
+		AbstractJobMigrationTask {
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = {"tSAPIDocOutput"};  //$NON-NLS-1$ 
+        
+    	IComponentConversion conversion = new IComponentConversion() {
+
+	        public void transform(NodeType node) {
+	            if(node == null) {
+	                return;
+	            }
+	            
+	        	ElementParameterType parameter = ComponentUtilities.getNodeProperty(node, "FROM_XML"); //$NON-NLS-1$
+	        	if (parameter == null) {
+	        		ComponentUtilities.addNodeProperty(node, "FROM_XML", "CHECK"); //$NON-NLS-1$ $NON-NLS-2$
+	        		ComponentUtilities.setNodeValue(node, "FROM_XML", "true"); //$NON-NLS-1$ $NON-NLS-2$
+	        	}
+	        }
+        
+    	};
+    	
+    	for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays
+                        .<IComponentConversion> asList(conversion));
+            } catch (PersistenceException e) {
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+
+    }
+
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2015, 1, 10, 10, 0, 0);
+        return gc.getTime();
+    }
+}
