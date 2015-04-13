39cd9e073e224391ea56746b4175ee32ec40ce17
xhkong <xhkong@talend.com>
2015-04-13 11:18:01 +0800

    fix bug TUP-2694 Sometimes it's impossible to save the routines https://jira.talendforge.org/browse/TUP-2694

--- org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/StandAloneTalendJavaEditor.java
+++ org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/StandAloneTalendJavaEditor.java
@@ -344,6 +344,7 @@ public class StandAloneTalendJavaEditor extends CompilationUnitEditor implements
                 }
             };
             repFactory.executeRepositoryWorkUnit(repositoryWorkUnit);
+            repositoryWorkUnit.throwPersistenceExceptionIfAny();
 
             // for bug 11930: Unable to save Routines.* in db project
 
@@ -357,12 +358,12 @@ public class StandAloneTalendJavaEditor extends CompilationUnitEditor implements
 
     }
 
-    private void refreshJobAndSave(final IProxyRepositoryFactory repFactory) {
-        try {
-            // cause it to update MaxInformationLevel
-            repFactory.save(item);
-        } catch (Exception e) {
-        }
+    private void refreshJobAndSave(final IProxyRepositoryFactory repFactory) throws PersistenceException {
+        // try {
+        // cause it to update MaxInformationLevel
+        repFactory.save(item);
+        // } catch (Exception e) {
+        // }
         // update editor image
         setTitleImage(getTitleImage());
 
