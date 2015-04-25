f389e1e2d11e7e9f4c0c15120ff3ca711d7e93b0
Pierre Teyssier <pteyssier@talend.com>
2015-04-25 19:36:54 +0800

    TUP-2712 Replace the icons of the repository https://jira.talendforge.org/browse/TUP-2712

--- main/plugins/org.talend.designer.components.exchange/src/main/java/org/talend/designer/components/exchange/ui/dialog/ExchangeService.java
+++ main/plugins/org.talend.designer.components.exchange/src/main/java/org/talend/designer/components/exchange/ui/dialog/ExchangeService.java
@@ -14,18 +14,18 @@ package org.talend.designer.components.exchange.ui.dialog;
 
 import java.io.IOException;
 
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.ui.IEditorPart;
 import org.eclipse.ui.IWorkbenchPage;
 import org.eclipse.ui.PartInitException;
 import org.eclipse.ui.PlatformUI;
+import org.talend.commons.ui.gmf.util.DisplayUtils;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.model.general.Project;
 import org.talend.core.service.IExchangeService;
-import org.talend.designer.components.exchange.i18n.Messages;
 import org.talend.designer.components.exchange.ui.views.ExchangeEditorInput;
 import org.talend.designer.components.exchange.util.ExchangeWebService;
+import org.talend.registration.wizards.register.TalendForgeDialog;
 import org.talend.repository.ProjectManager;
 
 import us.monoid.json.JSONException;
@@ -42,22 +42,33 @@ public class ExchangeService implements IExchangeService {
      * 
      * @see org.talend.core.model.general.IExchangeService#openExchangeDialog()
      */
+    @Override
     public String openExchangeDialog() {
         ImportExchangeDialog dialog = new ImportExchangeDialog(Display.getCurrent().getActiveShell());
         dialog.open();
         return dialog.getSelectFile();
     }
 
+    @Override
     public void openExchangeEditor() {
         IWorkbenchPage activePage = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
         if (activePage != null) {
             Project proj = ProjectManager.getInstance().getCurrentProject();
             if (proj.getExchangeUser() == null
                     || checkUserAndPass(proj.getExchangeUser().getUsername(), proj.getExchangeUser().getPassword()) != null) {
-                MessageDialog.openError(null, "Exchange", Messages.getString("Exchange.logon.error"));
-                return;
+                if (proj != null) {
+                    if (proj.getExchangeUser().getUsername() == "" && proj.getExchangeUser().getPassword() == "") {
+                        String userEmail = null;
+                        if (proj.getAuthor() != null) {
+                            userEmail = proj.getAuthor().getLogin();
+                        }
+                        TalendForgeDialog tfDialog = new TalendForgeDialog(DisplayUtils.getDefaultShell(), userEmail);
+                        tfDialog.open();
+                    }
+                    // MessageDialog.openError(null, "Exchange", Messages.getString("Exchange.logon.error"));
+                    return;
+                }
             }
-
             try {
                 ExchangeEditorInput input = new ExchangeEditorInput();
                 IEditorPart findEditor = activePage.findEditor(input);
@@ -73,6 +84,7 @@ public class ExchangeService implements IExchangeService {
 
     }
 
+    @Override
     public String checkUserAndPass(String username, String password) {
         String errorMessage = "Wrong user or password";
         if (username == null || "".equals(username) || password == null || "".equals(password)) {
