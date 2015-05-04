ea4053ae0bd75e3982c8694995b55a8c669be6bc
Wu Liu <wliu@talend.com>
2015-05-04 17:04:10 +0800

    TUP-1884 : Support for Java 8 Use ucanaccess project to make access components support jdbc.

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddJDBCODBCMode4AccessMigrationTask.java
@@ -0,0 +1,86 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC PLV class global comment. Detailled comment
+ */
+public class AddJDBCODBCMode4AccessMigrationTask extends AbstractJobMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2015, 5, 4, 12, 0, 0);
+        return gc.getTime();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
+        if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
+            return ExecutionResult.NOTHING_TO_DO;
+        }
+        String[] componentsName = new String[] { "tAccessConnection", "tAccessInput", "tAccessOutput", "tAccessRow" };
+
+        try {
+
+            for (String element : componentsName) {
+                IComponentFilter filter = new NameComponentFilter(element);
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(new IComponentConversion() {
+
+                            @Override
+                            public void transform(NodeType node) {
+                                if (ComponentUtilities.getNodeProperty(node, "JDBC_ODBC") == null) {
+                                    ComponentUtilities.addNodeProperty(node, "JDBC_ODBC", "CLOSED_LIST");
+                                    ComponentUtilities.getNodeProperty(node, "JDBC_ODBC").setValue("ODBC");
+                                }
+
+                            }
+
+                        }));
+            }
+
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
+
+}
