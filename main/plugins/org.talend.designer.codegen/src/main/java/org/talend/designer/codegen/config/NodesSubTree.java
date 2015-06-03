71d71c8ea80c12f50e55afc3c40469ced1a92dde
amarkevich <amarkevich@talend.com>
2015-06-03 16:34:31 +0300

    Revert "TDI-31266: OutputFIleDelimited having issues with records over 8K when used with other components https://jira.talendforge.org/browse/TDI-31266 use @Deprecated instead of remove subTreeContainsParallelIterate"
    This reverts commit c0adfb0c14ea7967d0404a515dcc385d0ab91edb.

--- main/plugins/org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodesSubTree.java
+++ main/plugins/org.talend.designer.codegen/src/main/java/org/talend/designer/codegen/config/NodesSubTree.java
@@ -60,9 +60,6 @@ public class NodesSubTree {
 
     List<IConnection> allMainSubTreeConnections = null;
 
-    @Deprecated
-    boolean subTreeContainsParallelIterate = false;
-
     List<INode> mergeBranchStarts;
 
     List<INode> mergeNodes;
@@ -546,18 +543,6 @@ public class NodesSubTree {
      */
     public void setAllMainSubTreeConnections(List<IConnection> allMainSubTreeConnections) {
         this.allMainSubTreeConnections = allMainSubTreeConnections;
-    }
-
-    @Deprecated
-    /*
-     * use org.talend.core.model.process.AbstractNode.getParallelIterator()
-     */
-    public boolean subTreeContainsParallelIterate() {
-        return subTreeContainsParallelIterate;
-    }
-
-    public void setSubTreeContainsParallelIterate(boolean subTreeContainsParallelIterate) {
-        this.subTreeContainsParallelIterate = subTreeContainsParallelIterate;
     }
 
     /**
