5f54e3350f5548c8cd1284fd8ecd255a9346f831
GGu <ggu@talend.com>
2015-06-11 15:25:35 +0800

    TUP-3008: clean some maven files.

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DeleteAllJobWhenStartUp.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DeleteAllJobWhenStartUp.java
@@ -12,10 +12,15 @@
 // ============================================================================
 package org.talend.designer.runprocess;
 
+import java.io.File;
+
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFolder;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.NullProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.ui.IStartup;
 import org.talend.commons.exception.ExceptionHandler;
@@ -63,18 +68,30 @@ public class DeleteAllJobWhenStartUp implements IStartup {
         ITalendProcessJavaProject talendJavaProject = processService.getTalendProcessJavaProject();
         if (talendJavaProject != null) {
             IJavaProject jProject = talendJavaProject.getJavaProject();
-            IProgressMonitor moniter = new NullProgressMonitor();
+            IProgressMonitor monitor = new NullProgressMonitor();
             try {
                 if (!jProject.isOpen()) {
-                    jProject.open(moniter);
+                    jProject.open(monitor);
                 }
                 // empty the src/main/java...
                 IFolder srcFolder = talendJavaProject.getSrcFolder();
-                talendJavaProject.cleanFolder(moniter, srcFolder);
+                talendJavaProject.cleanFolder(monitor, srcFolder);
+                // contexts
+                IFolder resourcesFolder = talendJavaProject.getResourcesFolder();
+                emptyContexts(monitor, resourcesFolder, talendJavaProject);
 
-                // empty the outputs, target/classes
-                IFolder outputFolder = talendJavaProject.getOutputFolder();
-                talendJavaProject.cleanFolder(moniter, outputFolder);
+                // empty the outputs, target
+                IFolder targetFolder = talendJavaProject.getTargetFolder();
+                talendJavaProject.cleanFolder(monitor, targetFolder);
+
+                // empty the src/test/java
+                IFolder testSrcFolder = talendJavaProject.getTestSrcFolder();
+                talendJavaProject.cleanFolder(monitor, testSrcFolder);
+
+                // empty the src/test/java (main for contexts)
+                IFolder testResourcesFolder = talendJavaProject.getTestResourcesFolder();
+                // emptyContexts(moniter,testResourcesFolder,talendJavaProject); //seems only contexts, so clean all.
+                talendJavaProject.cleanFolder(monitor, testResourcesFolder);
 
                 // empty lib/...
                 if (GlobalServiceRegister.getDefault().isServiceRegistered(ILibrariesService.class)) {
@@ -86,13 +103,17 @@ public class DeleteAllJobWhenStartUp implements IStartup {
                 }
 
                 // rules
-                IFolder rulesResFolder = talendJavaProject.getResourceSubFolder(moniter, JavaUtils.JAVA_RULES_DIRECTORY);
-                talendJavaProject.cleanFolder(moniter, rulesResFolder);
+                IFolder rulesResFolder = talendJavaProject.getResourceSubFolder(monitor, JavaUtils.JAVA_RULES_DIRECTORY);
+                talendJavaProject.cleanFolder(monitor, rulesResFolder);
 
                 // sqltempalte
-                IFolder sqlTemplateResFolder = talendJavaProject.getResourceSubFolder(moniter,
+                IFolder sqlTemplateResFolder = talendJavaProject.getResourceSubFolder(monitor,
                         JavaUtils.JAVA_SQLPATTERNS_DIRECTORY);
-                talendJavaProject.cleanFolder(moniter, sqlTemplateResFolder);
+                talendJavaProject.cleanFolder(monitor, sqlTemplateResFolder);
+
+                // temp folder sometimes ?
+                IFolder tempFolder = talendJavaProject.getProject().getFolder("temp"); //$NON-NLS-1$
+                talendJavaProject.cleanFolder(monitor, tempFolder);
 
                 // anything else to do? esb?
 
@@ -102,5 +123,47 @@ public class DeleteAllJobWhenStartUp implements IStartup {
         }
         executed = true;
 
+    }
+
+    private void emptyContexts(IProgressMonitor monitor, IContainer baseContainer, ITalendProcessJavaProject talendJavaProject)
+            throws CoreException {
+        IPath location = baseContainer.getLocation();
+        File[] listFiles = location.toFile().listFiles();
+        if (listFiles != null) { // for each project sub folder
+            for (File child : listFiles) {
+                File testContextFile = findTestContextFile(child);
+                if (testContextFile != null) {
+                    IPath testContextPath = new Path(testContextFile.toString());
+                    IPath testRelativePath = testContextPath.makeRelativeTo(location);
+                    String projectSegment = testRelativePath.segment(0);
+                    IFolder folder = baseContainer.getFolder(new Path(projectSegment));
+                    talendJavaProject.cleanFolder(monitor, folder);
+                    if (folder.exists()) { // also delete it
+                        folder.delete(true, monitor);
+                    }
+                }
+            }
+        }
+    }
+
+    private File findTestContextFile(File file) {
+        if (file != null) {
+            if (file.getName().endsWith(JavaUtils.JAVA_CONTEXT_EXTENSION)
+                    && file.getParentFile().getName().equals(JavaUtils.JAVA_CONTEXTS_DIRECTORY)) {
+                return file;
+            }
+            if (file.isDirectory()) {
+                File[] listFiles = file.listFiles();
+                if (listFiles != null) {
+                    for (File f : listFiles) {
+                        File contextFile = findTestContextFile(f);
+                        if (contextFile != null) {
+                            return contextFile;
+                        }
+                    }
+                }
+            }
+        }
+        return null;
     }
 }
--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/CleanMavenFilesLoginTask.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/CleanMavenFilesLoginTask.java
@@ -14,6 +14,10 @@ package org.talend.designer.runprocess.maven;
 
 import java.lang.reflect.InvocationTargetException;
 
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.talend.commons.exception.ExceptionHandler;
@@ -30,17 +34,30 @@ public class CleanMavenFilesLoginTask extends AbstractLoginTask implements IRunn
 
     @Override
     public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
-        if (GlobalServiceRegister.getDefault().isServiceRegistered(IRunProcessService.class)) {
-            IRunProcessService service = (IRunProcessService) GlobalServiceRegister.getDefault().getService(
-                    IRunProcessService.class);
-            ITalendProcessJavaProject talendProcessJavaProject = service.getTalendProcessJavaProject();
-            if (talendProcessJavaProject != null) {
+        final IWorkspaceRunnable runnable = new IWorkspaceRunnable() {
+
+            @Override
+            public void run(IProgressMonitor monitor) {
                 try {
-                    talendProcessJavaProject.cleanMavenFiles(monitor);
+                    if (GlobalServiceRegister.getDefault().isServiceRegistered(IRunProcessService.class)) {
+                        IRunProcessService service = (IRunProcessService) GlobalServiceRegister.getDefault().getService(
+                                IRunProcessService.class);
+                        ITalendProcessJavaProject talendProcessJavaProject = service.getTalendProcessJavaProject();
+                        if (talendProcessJavaProject != null) {
+                            talendProcessJavaProject.cleanMavenFiles(monitor);
+                        }
+                    }
                 } catch (Exception e) {
                     ExceptionHandler.process(e);
                 }
             }
+        };
+
+        try {
+            IWorkspace workspace = ResourcesPlugin.getWorkspace();
+            workspace.run(runnable, workspace.getRoot(), IWorkspace.AVOID_UPDATE, monitor);
+        } catch (CoreException e) {
+            throw new InvocationTargetException(e);
         }
 
     }
