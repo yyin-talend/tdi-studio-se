3cae2142230bc2bad3b9ce5e06b40a39cb9b5a5e
GGu <ggu@talend.com>
2015-06-12 10:26:36 +0800

    TUP-3010: change the script with context.

--- main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.bat
+++ main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.bat
@@ -1,3 +1,3 @@
 %~d0
 cd %~dp0
-java -Xms256M -Xmx1024M -cp ${talend.job.bat.classpath} ${talend.job.class} ${talend.job.context} ${talend.job.bat.addition} %* 
+java -Xms256M -Xmx1024M -cp ${talend.job.bat.classpath} ${talend.job.class} ${talend.job.bat.addition} %* 
--- main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.sh
+++ main/plugins/org.talend.designer.runprocess/resources/templates/Job_run_template.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`
 ROOT_PATH=`pwd`
-java -Xms256M -Xmx1024M -cp ${talend.job.sh.classpath} ${talend.job.class} ${talend.job.context} ${talend.job.sh.addition} "$@" 
+java -Xms256M -Xmx1024M -cp ${talend.job.sh.classpath} ${talend.job.class} ${talend.job.sh.addition} "$@" 
