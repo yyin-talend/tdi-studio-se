4515d613a9a96fe5fcfa7a7c7b97d384ae7c6d73
rdubois <rdubois.contact@gmail.com>
2015-06-24 19:25:16 +0200

    TDI-32416 - Edit the migration task to make it work with Big Data items.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeDefaultValue4JSONComponents.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeDefaultValue4JSONComponents.java
@@ -23,7 +23,7 @@ import org.talend.core.model.components.ModifyComponentsAction;
 import org.talend.core.model.components.conversions.IComponentConversion;
 import org.talend.core.model.components.filters.IComponentFilter;
 import org.talend.core.model.components.filters.NameComponentFilter;
-import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.migration.AbstractAllJobMigrationTask;
 import org.talend.core.model.properties.Item;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
@@ -32,37 +32,38 @@ import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 /**
  * DOC Administrator class global comment. Detailled comment
  */
-public class ChangeDefaultValue4JSONComponents extends
-		AbstractJobMigrationTask {
+public class ChangeDefaultValue4JSONComponents extends AbstractAllJobMigrationTask {
 
     @Override
     public ExecutionResult execute(Item item) {
         final ProcessType processType = getProcessType(item);
-        String[] compNames = {"tFileInputJSON","tExtractJSONFields"}; //$NON-NLS-1$ //$NON-NLS-2$
-        
-    	IComponentConversion conversion = new IComponentConversion() {
+        String[] compNames = {
+                "tFileInputJSON", "tExtractJSONFields", "tExtractAbstractJSONFields", "tFileInputJSON", "tInputFormatJSON" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$
 
-	        public void transform(NodeType node) {
-	            if(node == null) {
-	                return;
-	            }
-	            
-	        	ElementParameterType parameter = ComponentUtilities.getNodeProperty(node, "READ_BY"); //$NON-NLS-1$
-	        	
-	        	if (parameter == null) {
-	        		ComponentUtilities.addNodeProperty(node, "READ_BY", "CLOSED_LIST"); //$NON-NLS-1$ //$NON-NLS-2$
-	        		ComponentUtilities.setNodeValue(node, "READ_BY", "XPATH"); //$NON-NLS-1$ //$NON-NLS-2$
-	        	}
-	        }
-        
-    	};
-    	
-    	for (String name : compNames) {
+        IComponentConversion conversion = new IComponentConversion() {
+
+            @Override
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType parameter = ComponentUtilities.getNodeProperty(node, "READ_BY"); //$NON-NLS-1$
+
+                if (parameter == null) {
+                    ComponentUtilities.addNodeProperty(node, "READ_BY", "CLOSED_LIST"); //$NON-NLS-1$ //$NON-NLS-2$
+                    ComponentUtilities.setNodeValue(node, "READ_BY", "XPATH"); //$NON-NLS-1$ //$NON-NLS-2$
+                }
+            }
+
+        };
+
+        for (String name : compNames) {
             IComponentFilter filter = new NameComponentFilter(name);
 
             try {
-                ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays
-                        .<IComponentConversion> asList(conversion));
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(conversion));
             } catch (PersistenceException e) {
                 // TODO Auto-generated catch block
                 ExceptionHandler.process(e);
@@ -74,6 +75,7 @@ public class ChangeDefaultValue4JSONComponents extends
 
     }
 
+    @Override
     public Date getOrder() {
         GregorianCalendar gc = new GregorianCalendar(2015, 5, 24, 11, 0, 0);
         return gc.getTime();
