88eeff44f04eb0394cf8632a6334e27cb33137cc
wluo-talend <wluo@talend.com>
2015-06-29 19:55:45 -0700

    TDI-31824: upload source code for talend-microsoft-crm-2015-on-premises-0.2.jar https://jira.talendforge.org/browse/TDI-31824

---
+++ main/plugins/org.talend.designer.components.libs/libs_src/crm_2015_on_premises/com/microsoft/schemas/xrm/_2013/metadata/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://schemas.microsoft.com/xrm/2013/Metadata", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.microsoft.schemas.xrm._2013.metadata;
