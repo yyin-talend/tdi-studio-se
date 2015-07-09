455d9d3321435623503ae287e70d2bac5aa14ac1
hcyi <hcyi@talend.com>
2015-07-09 17:27:23 +0800

    TUP-3005:Spark and Spark Streaming components are not displayed in the Palette Settings.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/palettesettings/RootPaletteItem.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/palettesettings/RootPaletteItem.java
@@ -14,10 +14,13 @@ package org.talend.repository.preference.palettesettings;
 
 import org.eclipse.core.runtime.Assert;
 import org.eclipse.jface.resource.ImageDescriptor;
+import org.talend.commons.ui.runtime.image.ECoreImage;
+import org.talend.commons.ui.runtime.image.ImageProvider;
+import org.talend.core.PluginChecker;
 import org.talend.core.model.components.ComponentCategory;
 import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.repository.utils.ConvertJobsUtil;
 import org.talend.core.ui.component.preference.provider.IPaletteItem;
-import org.talend.core.ui.images.CoreImageProvider;
 
 /**
  * created by nrousseau on Aug 11, 2014 Detailled comment
@@ -28,31 +31,42 @@ public class RootPaletteItem extends AbstractPaletteItem implements IPaletteItem
     public RootPaletteItem(ComponentCategory category) {
         setCategory(category);
         ERepositoryObjectType type;
+        ImageDescriptor imageDesc;
         switch (category) {
         case CATEGORY_4_CAMEL:
             type = ERepositoryObjectType.valueOf("ROUTES"); //$NON-NLS-1$
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.ROUTES_ICON);
             break;
         case CATEGORY_4_MAPREDUCE:
             type = ERepositoryObjectType.valueOf("PROCESS_MR"); //$NON-NLS-1$
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.PROCESS_BATCH_MR_ICON);
             break;
         case CATEGORY_4_STORM:
             type = ERepositoryObjectType.valueOf("PROCESS_STORM"); //$NON-NLS-1$
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.PROCESS_STREAMING_STORM_ICON);
             break;
         case CATEGORY_4_SPARK:
             type = ERepositoryObjectType.valueOf("PROCESS_MR"); //$NON-NLS-1$
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.PROCESS_BATCH_SPARK_ICON);
             break;
         case CATEGORY_4_SPARKSTREAMING:
             type = ERepositoryObjectType.valueOf("PROCESS_STORM"); //$NON-NLS-1$
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.PROCESS_STREAMING_SPARK_ICON);
             break;
         case CATEGORY_4_DI:
         default:
             type = ERepositoryObjectType.PROCESS;
+            imageDesc = ImageProvider.getImageDesc(ECoreImage.PROCESS_ICON);
             break;
         }
         Assert.isNotNull(type);
         String desc = category.getDesc();
-        setLabel(type.getLabel() + ("".equals(desc) ? "" : " (" + desc + ")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
-        ImageDescriptor imageDesc = CoreImageProvider.getImageDesc(type);
+        String label = type.getLabel();
+        if (ERepositoryObjectType.PROCESS.equals(type)
+                && (PluginChecker.isStormPluginLoader() || PluginChecker.isMapReducePluginLoader())) {
+            label = ConvertJobsUtil.JobType.STANDARD.getDisplayName();
+        }
+        setLabel(label + ("".equals(desc) ? "" : " (" + desc + ")")); //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$
         setImageDesc(imageDesc);
     }
 }
