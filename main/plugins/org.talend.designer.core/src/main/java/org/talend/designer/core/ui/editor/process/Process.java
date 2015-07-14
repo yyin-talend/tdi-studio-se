00bdbaa412b9d9679706496d810e448e34d724d3
zwxue <zwxue@talend.com>
2015-07-14 11:09:43 +0800

    TDI-32692: fix the thread problem caused by previous modification.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/Process.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/Process.java
@@ -3032,7 +3032,7 @@ public class Process extends Element implements IProcess2, IGEFProcess, ILastVer
 
     @Override
     public void checkStartNodes() {
-        for (INode node : nodes) {
+        for (INode node : new ArrayList<>(nodes)) {
             if ((Boolean) node.getPropertyValue(EParameterName.STARTABLE.getName())) {
                 if (node.isActivate()) {
                     boolean checkIfCanBeStart = node.checkIfCanBeStart();
@@ -3301,7 +3301,7 @@ public class Process extends Element implements IProcess2, IGEFProcess, ILastVer
     }
 
     private void getMatchingNodes(String componentName, List<INode> matchingNodes, List<INode> generatingNodes) {
-        for (INode node : generatingNodes) {
+        for (INode node : new ArrayList<>(generatingNodes)) {
             if (node.isActivate()) {
                 if (componentName == null) { // means all nodes will be
                     // returned
