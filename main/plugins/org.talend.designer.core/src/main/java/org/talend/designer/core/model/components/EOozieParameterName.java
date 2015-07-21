beab460c94dbf894e11ecbe39ec1dcb6260c67f4
wdliu <wdliu@talend.com>
2015-07-21 12:12:37 +0800

    TDI-32972 Bug : DI Studio Part TDI-32972:In the Oozie settings view, all fields are greyed out even though I am not using properties from Repository

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EOozieParameterName.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EOozieParameterName.java
@@ -18,12 +18,40 @@ import org.talend.designer.core.i18n.Messages;
  * DOC ycbai class global comment. Detailled comment
  */
 public enum EOozieParameterName {
+    
+    PROPERTY_TYPENAME(Messages.getString("EOozieParameterName.propertyType")),
 
     HADOOP_APP_PATH(Messages.getString("EOozieParameterName.hadoopAppPath")), //$NON-NLS-1$
 
     JOBID_FOR_OOZIE(Messages.getString("EOozieParameterName.jobId")), //$NON-NLS-1$
 
     REPOSITORY_CONNECTION_ID(Messages.getString("EOozieParameterName.repositoryConnectionId")), //$NON-NLS-1$
+    
+    HADOOP_DISTRIBUTION(Messages.getString("EOozieParameterName.hadoopDistribution")),
+    
+    HADOOP_VERSION(Messages.getString("EOozieParameterName.hadoopVersion")),
+    
+    ENABLE_KERBEROS(Messages.getString("EOozieParameterName.enableKerberos")),
+    
+    NAME_NODE_PRINCIPAL(Messages.getString("EOozieParameterName.nameNodePrincipal")),
+    
+    USERNAME(Messages.getString("EOozieParameterName.userName")),
+    
+    GROUP(Messages.getString("EOozieParameterName.group")),
+    
+    USE_KEYTAB(Messages.getString("EOozieParameterName.useKeytab")),
+    
+    KT_PRINCIPAL(Messages.getString("EOozieParameterName.ktPrincipal")),
+    
+    KEY_TAB(Messages.getString("EOozieParameterName.keytab")),
+    
+    ENABLE_OO_KERBEROS(Messages.getString("EOozieParameterName.enableOoKerberos")),
+    
+    NAME_NODE_END_POINT(Messages.getString("EOozieParameterName.nameNodeEndPoint")),
+    
+    JOB_TRACKER_ENDPOINT(Messages.getString("EOozieParameterName.jobTrackerEndPoint")),
+    
+    OOZIE_END_POINT(Messages.getString("EOozieParameterName.oozieEndPointValue")),
     ;
 
     private String displayName;
@@ -39,4 +67,5 @@ public enum EOozieParameterName {
     public String getDisplayName() {
         return this.displayName;
     }
+    
 }
