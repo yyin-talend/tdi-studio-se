c60a8d5baee12e9e2fb5108bebe6addd3f71bdb1
CHEN Bin <bchen@talend.com>
2015-07-24 15:30:24 +0800

    [TBD-1969] migration task for : change the default value to Datastax and 2.0 for cassandra components https://jira.talendforge.org/browse/TBD-1969

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeDefaultValueForCassandraAPIVersion.java
@@ -0,0 +1,93 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2014 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractAllJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * created by bchen on Jul 20, 2015 Detailled comment
+ *
+ */
+public class ChangeDefaultValueForCassandraAPIVersion extends AbstractAllJobMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2015, 6, 20, 12, 0, 0);
+        return gc.getTime();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = { "tCassandraConnection", "tCassandraInput", "tCassandraOutput", "tCassandraRow" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ 
+
+        IComponentConversion conversion = new IComponentConversion() {
+
+            @Override
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType parameter = ComponentUtilities.getNodeProperty(node, "API_TYPE"); //$NON-NLS-1$
+
+                if (parameter == null) {
+                    ComponentUtilities.addNodeProperty(node, "API_TYPE", "CLOSED_LIST"); //$NON-NLS-1$ //$NON-NLS-2$
+                    ComponentUtilities.setNodeValue(node, "API_TYPE", "HECTOR"); //$NON-NLS-1$ //$NON-NLS-2$
+                }
+            }
+
+        };
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(conversion));
+            } catch (PersistenceException e) {
+                // TODO Auto-generated catch block
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+    }
+
+}
