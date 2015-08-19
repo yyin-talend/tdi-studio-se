bb7f8360659724cf57a5e9c8a0eb2b668cf327ad
wluo-talend <wluo@talend.com>
2015-08-19 23:55:59 +0800

    TDI-32402: remove support dynamic type https://jira.talendforge.org/browse/TDI-32402

--- main/plugins/org.talend.designer.components.libs/libs_src/salesforce-wave/src/com/talend/salesforce/wave/SalesforceWaveHelper.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/salesforce-wave/src/com/talend/salesforce/wave/SalesforceWaveHelper.java
@@ -3,22 +3,10 @@
  */
 package com.talend.salesforce.wave;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.List;
 
 public class SalesforceWaveHelper{
     public static String generateJsonMetadata(java.io.File jsonFile,List<MetadataColumnRuntime> imatadataColumnList,java.util.Map<String,Object> customedConfig){
-        boolean containsDynamicColumn = SalesforceWaveHelper.containsDynamicColumn(imatadataColumnList);
-        List<MetadataColumnRuntime> normalMetadataColumnList = imatadataColumnList;
-        if(containsDynamicColumn){
-            String[] columnsFromHeader = SalesforceWaveHelper.extractHeaderFromCSVFile(new File((String)customedConfig.get("csvPath")),customedConfig);
-            normalMetadataColumnList = SalesforceWaveHelper.buildMetadataColumnRuntime(columnsFromHeader, imatadataColumnList);
-        }
-
         String jsonMetadataStr = null;
         StringBuilder strBuilder = new StringBuilder("{");//The left brace
             strBuilder.append("\"fileFormat\":{");
@@ -41,7 +29,7 @@ public class SalesforceWaveHelper{
             */
             strBuilder.append(",\"fields\":[");
             int counter = 0;
-            for(MetadataColumnRuntime imetadataColumn : normalMetadataColumnList){
+            for(MetadataColumnRuntime imetadataColumn : imatadataColumnList){
                String defaultValue = imetadataColumn.getDefault();
                if(counter != 0){
                  strBuilder.append(",");
@@ -140,109 +128,4 @@ public class SalesforceWaveHelper{
         public static boolean isValidValue(String value){
                return value.matches("^[A-Za-z0-9_]+$");
         }
-
-        public static boolean containsDynamicColumn(List<MetadataColumnRuntime> imatadataColumnList){
-               int dynamicColumnCounter = 0;
-               for(MetadataColumnRuntime column: imatadataColumnList){
-                    if("id_Dynamic".equals(column.getTalendType())){
-                        dynamicColumnCounter ++;
-                    }
-               }
-
-               if(dynamicColumnCounter == 0){
-                    return false;
-               }else if(dynamicColumnCounter == 1){
-                    return true;
-               }else{
-                    throw new RuntimeException("Only one dynamic column allowed.");
-               }
-        }
-
-        //assume there is a header in the csv file
-        public static String[] extractHeaderFromCSVFile(File csvFile, java.util.Map<String,Object> customedConfig){
-               BufferedReader buffReader = null;
-               String header = null;
-               try{
-                   buffReader = new BufferedReader(new InputStreamReader(new FileInputStream(csvFile),(String)customedConfig.get("encoding")));
-                   boolean flag = true;
-                   while(flag){
-                        header = buffReader.readLine();
-                        if(!"".equals(header)){
-                            flag = false;
-                        }
-                   }
-               }catch(java.io.FileNotFoundException e){
-                    e.printStackTrace();
-               }catch(java.io.IOException e){
-                  e.printStackTrace();
-               }catch(java.lang.Exception e){
-                  e.printStackTrace();
-               }finally{
-                  if(buffReader != null){
-                      try{
-                          buffReader.close();
-                      }catch(java.io.IOException e){
-                          e.printStackTrace();
-                      }
-                  }
-               }
-
-               String[] headerArr = header.split((String)customedConfig.get("fieldsDelimitedBy"));
-               int lgth = headerArr.length;
-               String[] removedDoubleQuote = new String[lgth];
-               for(int i = 0; i < lgth; i++){
-                  if(headerArr[i].startsWith("\"") && headerArr[i].endsWith("\"")){
-                     removedDoubleQuote[i] = headerArr[i].substring(1,headerArr[i].length()-1);
-                  }else{
-                    removedDoubleQuote[i] = headerArr[i];
-                  }
-               }
-               return removedDoubleQuote;
-        }
-
-      public static List<MetadataColumnRuntime> buildMetadataColumnRuntime(String[] entireColumns, List<MetadataColumnRuntime> containsDynamicColumnList){
-           List<MetadataColumnRuntime> normalMetadataColumnList = new ArrayList<MetadataColumnRuntime>();
-           int lgth = containsDynamicColumnList.size();
-           int lgthx = entireColumns.length;
-           int dynamicIndex = 0;
-
-           if(lgthx < lgth){
-               throw new RuntimeException("The length of the entireColumns must greater then the length of containsDynamicColumnList.");
-           }
-           if(lgth <= 0 || lgthx <= 0){
-               throw new IllegalArgumentException("The length of the array must greater than 0.");
-           }
-
-           for(MetadataColumnRuntime metadataColumn : containsDynamicColumnList){
-              if("id_Dynamic".equals(metadataColumn.getTalendType())){
-                 break;
-              }
-              dynamicIndex ++;
-           }
-
-           MetadataColumnRuntime metadataColumn = containsDynamicColumnList.get(dynamicIndex);
-           for(int i = 0; i < dynamicIndex; i++){
-                normalMetadataColumnList.add(containsDynamicColumnList.get(i));
-           }
-
-           for(int j = dynamicIndex; j < (lgthx - lgth + dynamicIndex + 1); j++){
-                normalMetadataColumnList.add(
-                        new MetadataColumnRuntime.Builder()
-                        .label(entireColumns[j])
-                        .talendType("id_String")
-                        .pattern(metadataColumn.getPattern())
-                        .length(metadataColumn.getLength())
-                        .precision(metadataColumn.getPrecision())
-                        .defaultValue(metadataColumn.getDefault())
-                        .comment(metadataColumn.getComment())
-                        .isPrimaryKey(metadataColumn.isPrimaryKey())
-                        .build()
-                    );
-           }
-
-           for(int k = dynamicIndex + 1; k < lgth; k++){
-                normalMetadataColumnList.add(containsDynamicColumnList.get(k));
-           }
-           return normalMetadataColumnList;
-       }
     }
