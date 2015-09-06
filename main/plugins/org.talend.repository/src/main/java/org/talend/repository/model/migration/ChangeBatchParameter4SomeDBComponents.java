8124b121c1549f2fdc958951d7fc8fc6ac68f065
wang wei <wwang@talend.com>
2015-09-06 16:39:14 +0800

    TDI-33830: Need a migration task for tFirebirdOutput/tGreenPlumOutput/tIngresOutput/tInterbaseOutput/tParAccelOutput/tPostgresPlusOutput/tSQLiteOutput to keep the use batch unchecked when import old job to new studio https://jira.talendforge.org/browse/TDI-33830

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeBatchParameter4SomeDBComponents.java
@@ -0,0 +1,83 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class ChangeBatchParameter4SomeDBComponents extends AbstractJobMigrationTask {
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = {
+                "tFirebirdOutput", "tGreenplumOutput", "tIngresOutput", "tInterbaseOutput", "tParAccelOutput", "tPostgresPlusOutput", "tSQLiteOutput" };
+
+        IComponentConversion conversion = new IComponentConversion() {
+
+            @Override
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType parameter = ComponentUtilities.getNodeProperty(node, "USE_BATCH");
+
+                if (parameter == null) {
+                    ComponentUtilities.addNodeProperty(node, "USE_BATCH", "CHECK");
+                    ComponentUtilities.setNodeValue(node, "USE_BATCH", "false");
+                }
+            }
+
+        };
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(conversion));
+            } catch (PersistenceException e) {
+                // TODO Auto-generated catch block
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+
+    }
+
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2015, 8, 6, 16, 30, 0);
+        return gc.getTime();
+    }
+}
