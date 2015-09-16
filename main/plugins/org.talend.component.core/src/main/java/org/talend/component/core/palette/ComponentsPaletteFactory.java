db4c3332f6b3586a2d310963056f6bebd42a7e7d
hcyi <hcyi@talend.com>
2015-09-16 14:18:31 +0800

    TUP-3495:Display of component in the palette(TUP-3462)

---
+++ main/plugins/org.talend.component.core/src/main/java/org/talend/component/core/palette/ComponentsPaletteFactory.java
@@ -0,0 +1,60 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2015 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.component.core.palette;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.eclipse.gef.palette.PaletteEntry;
+import org.eclipse.gef.palette.PaletteGroup;
+import org.eclipse.gef.palette.PaletteRoot;
+import org.talend.component.core.utils.ComponentsUtils;
+import org.talend.core.model.components.IComponentsFactory;
+import org.talend.core.ui.component.ComponentPaletteUtilities;
+import org.talend.designer.core.ui.editor.TalendEditorPaletteFactory;
+
+/**
+ * 
+ * created by hcyi on Sep 14, 2015 Detailled comment
+ *
+ */
+public final class ComponentsPaletteFactory {
+
+    private static PaletteRoot paletteRoot;
+
+    public static List<PaletteEntry> createPalette() {
+        List<PaletteEntry> list = null;
+        paletteRoot = ComponentPaletteUtilities.getPaletteRoot();
+        Object defaultEntry = paletteRoot.getChildren().get(0);
+        if (defaultEntry instanceof PaletteGroup) {
+            list = createLinkNodes((PaletteGroup) defaultEntry);
+        }
+        return list;
+    }
+
+    public static PaletteRoot createPalette(IComponentsFactory compFac, PaletteRoot root) {
+        ComponentsUtils.loadComponents(ComponentsUtils.getComponentService());
+        paletteRoot = TalendEditorPaletteFactory.createPalette(compFac);
+        Object defaultEntry = paletteRoot.getChildren().get(0);
+        if (defaultEntry instanceof PaletteGroup) {
+            createLinkNodes((PaletteGroup) defaultEntry);
+        }
+        return paletteRoot;
+    }
+
+    private static List<PaletteEntry> createLinkNodes(PaletteGroup toolGroup) {
+        ArrayList<PaletteEntry> arrayList = new ArrayList<PaletteEntry>();
+        // to do
+        return arrayList;
+    }
+}
