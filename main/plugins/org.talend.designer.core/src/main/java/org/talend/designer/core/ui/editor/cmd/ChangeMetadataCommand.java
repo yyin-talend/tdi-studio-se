f38ddc10575af8767a38bf429f85e25e9d40cc8f
hcyi <hcyi@talend.com>
2015-12-21 15:40:03 +0800

    TDI-34445:'Base Schema' value changes back to 'Build-in' even when 'update from repository' is selected

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeMetadataCommand.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/ChangeMetadataCommand.java
@@ -397,6 +397,7 @@ public class ChangeMetadataCommand extends Command {
                                         dataComp.setColumnNameChanged(columnNameChanged);
                                     }
                                 }
+                                cmd.setRepositoryMode(repositoryMode);
                                 cmd.execute(true);
                                 propagatedChange.add(cmd);
                             }
