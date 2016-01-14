4fc6868b2060664443c312baf1a5a9d595826792
wchen-talend <wchen@talend.com>
2016-01-14 17:19:29 +0800

    TDI-35074:When do compare joblet, the joblet will be locked, after close the joblet editor page, the joblet is always in lock mode.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ICreateMRProcessService.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ICreateMRProcessService.java
@@ -5,7 +5,7 @@
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
 //
-// You should have received a copy of the  agreement
+// You should have received a copy of the agreement
 // along with this program; if not, write to Talend SA
 // 9 rue Pages 92150 Suresnes, France
 //
@@ -19,13 +19,15 @@ import org.talend.core.model.properties.Property;
 import org.talend.designer.core.ui.editor.ProcessEditorInput;
 import org.talend.designer.core.ui.editor.process.Process;
 
-
 /**
- * DOC talend  class global comment. Detailled comment
+ * DOC talend class global comment. Detailled comment
  */
 public interface ICreateMRProcessService extends IService {
 
     public Process createMRProcess(Property property);
-    
+
     public ProcessEditorInput getMRProcessEditorInput(ProcessItem processItem, boolean load) throws PersistenceException;
+
+    public ProcessEditorInput getMRProcessEditorInput(ProcessItem processItem, boolean load, boolean lastVersion, boolean readOnly)
+            throws PersistenceException;
 }
