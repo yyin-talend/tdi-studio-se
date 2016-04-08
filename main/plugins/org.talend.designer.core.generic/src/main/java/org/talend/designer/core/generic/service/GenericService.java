74472df0b1fb84bf06308060f82c2a10c0d40b95
zwxue <zwxue@talend.com>
2016-04-08 15:07:18 +0800

    TUP-4232 feature: Add more informations to the export logs menu.

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/service/GenericService.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/service/GenericService.java
@@ -12,9 +12,17 @@
 // ============================================================================
 package org.talend.designer.core.generic.service;
 
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+
+import org.talend.components.api.component.ComponentDefinition;
 import org.talend.core.model.process.IElementParameter;
 import org.talend.core.runtime.services.IGenericService;
 import org.talend.designer.core.generic.model.GenericElementParameter;
+import org.talend.designer.core.generic.utils.ComponentsUtils;
 
 /**
  * created by ycbai on 2016年3月24日 Detailled comment
@@ -28,6 +36,24 @@ public class GenericService implements IGenericService {
             return ((GenericElementParameter) parameter).callBeforeActivate();
         }
         return false;
+    }
+
+    @Override
+    public List<Map<String, String>> getAllGenericComponentsInfo() {
+        List<Map<String, String>> genericComponents = new ArrayList<>();
+        Set<ComponentDefinition> definitions = ComponentsUtils.getComponentService().getAllComponents();
+        if (definitions != null) {
+            for (ComponentDefinition definition : definitions) {
+                Map<String, String> componentInfo = new LinkedHashMap<>();
+                componentInfo.put("Name", definition.getName()); //$NON-NLS-1$
+                componentInfo.put("Title", definition.getTitle()); //$NON-NLS-1$
+                componentInfo.put("Partitioning", definition.getPartitioning()); //$NON-NLS-1$
+                componentInfo.put("Maven-GroupId", definition.getMavenGroupId()); //$NON-NLS-1$
+                componentInfo.put("Maven-ArtifactId", definition.getMavenArtifactId()); //$NON-NLS-1$
+                genericComponents.add(componentInfo);
+            }
+        }
+        return genericComponents;
     }
 
 }
