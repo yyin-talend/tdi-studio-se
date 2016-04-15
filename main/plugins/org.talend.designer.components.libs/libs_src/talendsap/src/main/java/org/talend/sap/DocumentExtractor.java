f6b7c338e7558ed79afbda825dcadb2cbda97194
wang wei <wwang@talend.com>
2016-04-15 10:48:57 +0800

    TDI-35695: performance issue about tSAPBapi component https://jira.talendforge.org/browse/TDI-35695

--- main/plugins/org.talend.designer.components.libs/libs_src/talendsap/src/main/java/org/talend/sap/DocumentExtractor.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talendsap/src/main/java/org/talend/sap/DocumentExtractor.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.dom4j.Document;
+import org.dom4j.Element;
 import org.dom4j.Node;
 import org.dom4j.XPath;
 
@@ -62,28 +63,38 @@ public class DocumentExtractor {
 	}
 	
 	public List<List<String>> getTableResult(String tableName,List<String> names) {
-		XPath xpath = org.dom4j.DocumentHelper.createXPath(
-			sb.append("/").append(function).append("/TABLES/").append(tableName).append("/item")
-			.append("|")
-			.append("/").append(function).append("/CHANGING/").append(tableName).append("/item")
-			.toString()
-		);
-		sb.setLength(0);
-		List<Node> nodes = xpath.selectNodes(doc);
-		if(nodes == null || nodes.isEmpty()) {
-			return null;
-		}
-		
 		List<List<String>> result = new ArrayList<List<String>>();
 		
-		for(Node node : nodes) {
+		Element functionElement = doc.getRootElement();
+		if (functionElement == null) {
+			return result;
+		}
+
+		Element tablesOrChangingElement = functionElement.element("TABLES");
+		if (tablesOrChangingElement == null) {
+			tablesOrChangingElement = functionElement.element("CHANGING");
+		}
+		
+		if(tablesOrChangingElement == null) {
+			return result;
+		}
+
+		Element tableElement = tablesOrChangingElement.element(tableName);
+
+		if (tableElement == null) {
+			return result;
+		}
+		
+		List<Element> elements = tableElement.elements("item");
+		
+		for(Element element : elements) {
 			List<String> row = new ArrayList<String>();
 			for(String name : names) {
-				Node subNode = node.selectSingleNode(name);
-				if(subNode == null) {
+				Element subElement = element.element(name);
+				if(subElement == null) {
 					row.add(null);
 				} else {
-					row.add(subNode.getText());
+					row.add(subElement.getText());
 				}
 			}
 			result.add(row);
