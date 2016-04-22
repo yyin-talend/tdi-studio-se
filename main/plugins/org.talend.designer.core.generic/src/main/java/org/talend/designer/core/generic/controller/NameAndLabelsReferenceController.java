5b94dda8f0e9e9d37421a783c6b76088150f79bb
CHEN Bin <bchen@talend.com>
2016-04-22 10:29:16 +0800

    [TUP-4450] fixed the problem when the component has one more Widget.WidgetType.NAME_SELECTION_REFERENCE

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/controller/NameAndLabelsReferenceController.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/controller/NameAndLabelsReferenceController.java
@@ -54,6 +54,8 @@ public class NameAndLabelsReferenceController extends AbstractElementPropertySec
 
     private static final String MODULE = "MODULE"; //$NON-NLS-1$
 
+    private static final String PARAM_OBJ = "PARAM_OBJ"; //$NON-NLS-1$
+
     public NameAndLabelsReferenceController(IDynamicProperty dp) {
         super(dp);
     }
@@ -63,8 +65,9 @@ public class NameAndLabelsReferenceController extends AbstractElementPropertySec
     }
 
     public Command createCommand(Button button) {
-        if (curParameter instanceof GenericElementParameter) {
-            GenericElementParameter gParam = (GenericElementParameter) curParameter;
+        Object paramObj = button.getData(PARAM_OBJ);
+        if (paramObj instanceof GenericElementParameter) {
+            GenericElementParameter gParam = (GenericElementParameter) paramObj;
             if (gParam != null) {
                 callBeforeActive(gParam);
                 NameAndLabelsDialog nameAndLabelsDialog = new NameAndLabelsDialog(composite.getShell(),
@@ -109,6 +112,7 @@ public class NameAndLabelsReferenceController extends AbstractElementPropertySec
         data.height = STANDARD_HEIGHT - 2;
         btnEdit.setLayoutData(data);
         btnEdit.setData(NAME, MODULE);
+        btnEdit.setData(PARAM_OBJ, param);
         btnEdit.setData(PARAMETER_NAME, param.getName());
         // btnEdit.setEnabled(!param.isReadOnly());
         btnEdit.addSelectionListener(new SelectionAdapter() {
