7588def85a0071cd0eb26cb55732d91fe1a6b257
Mike Yan <yyan@talend.com>
2016-04-25 10:50:42 +0800

    TESB-17238: Fixed cannot export subjobs data source alias in blueprint

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/DataSourceConfig.java
@@ -21,64 +21,107 @@ import org.talend.core.CorePlugin;
 import org.talend.core.model.process.IElementParameter;
 import org.talend.core.model.process.INode;
 import org.talend.core.model.process.IProcess;
+import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.utils.TalendQuoteUtils;
 import org.talend.designer.core.IDesignerCoreService;
+import org.talend.designer.runprocess.ItemCacheManager;
 
 /**
  * OSGi DataSource
  */
 public class DataSourceConfig {
 
-	private DataSourceConfig() {
-	}
+    private static final String TRUN_JOB = "tRunJob"; //$NON-NLS-1$
 
-	public static Collection<String> getAliases(IProcess processItem) {
-		return getAliases(processItem, new HashSet<String>());
-	}
+    private DataSourceConfig() {
+    }
 
-	private static Collection<String> getAliases(IProcess processItem,
-			Collection<String> jobNameVersionChecked) {
-		if(processItem == null){
-			return Collections.emptyList();
-		}
-		String jobName = processItem.getName();
-		String jobVersion = processItem.getVersion();
-		String id = jobName + '_' + jobVersion;
-		if (!jobNameVersionChecked.add(id)) {
-			return Collections.emptyList(); // no need to add more to the list,
-											// just return the empty list
-		}
-		Collection<String> aliases = new HashSet<String>();
-		
-		List<? extends INode> generatingNodes = processItem.getGeneratingNodes();
-		for(INode node: generatingNodes){
-			String datasourceAlias = getDatasourceAliasFrom(node);
-			if(datasourceAlias != null && !datasourceAlias.isEmpty()){
-				aliases.add(datasourceAlias);
-			}
-		}
+    public static Collection<String> getAliases(IProcess processItem) {
+        return getAliases(processItem, new HashSet<String>());
+    }
 
-		return aliases;
-	}
-	
-	private static String getDatasourceAliasFrom(INode node) {
-		if(!node.isActivate()){
-			return null;
-		}
-		IElementParameter isSpecifyAlias = node.getElementParameter("SPECIFY_DATASOURCE_ALIAS");
-		if(isSpecifyAlias == null || Boolean.FALSE.equals(isSpecifyAlias.getValue())){
-			return null;
-		}
-		IElementParameter aliasParam = node.getElementParameter("DATASOURCE_ALIAS");
-		if(aliasParam == null){
-			return null;
-		}
-		IDesignerCoreService designerCoreService = CorePlugin.getDefault().getDesignerCoreService();
-		boolean evaluate = designerCoreService.evaluate(isSpecifyAlias.getShowIf(), node.getElementParameters());
-		if(!evaluate){
-			return null;
-		}
-		String result = aliasParam.getValue()==null?null:aliasParam.getValue().toString();
-		return TalendQuoteUtils.removeQuotes(result.trim());
-	}
+    private static Collection<String> getAliases(IProcess processItem, Collection<String> jobNameVersionChecked) {
+        if (processItem == null) {
+            return Collections.emptyList();
+        }
+        String jobName = processItem.getName();
+        String jobVersion = processItem.getVersion();
+        String id = jobName + '_' + jobVersion;
+        if (!jobNameVersionChecked.add(id)) {
+            return Collections.emptyList(); // no need to add more to the list,
+                                            // just return the empty list
+        }
+        Collection<String> aliases = new HashSet<String>();
+
+        List<? extends INode> generatingNodes = processItem.getGeneratingNodes();
+        for (INode node : generatingNodes) {
+            getDatasourceAliasesFrom(node, aliases);
+        }
+        return aliases;
+    }
+
+    @Deprecated
+    private static String getDatasourceAliasFrom(INode node) {
+        if (!node.isActivate()) {
+            return null;
+        }
+        IElementParameter isSpecifyAlias = node.getElementParameter("SPECIFY_DATASOURCE_ALIAS"); //$NON-NLS-1$
+        if (isSpecifyAlias == null || Boolean.FALSE.equals(isSpecifyAlias.getValue())) {
+            return null;
+        }
+        IElementParameter aliasParam = node.getElementParameter("DATASOURCE_ALIAS"); //$NON-NLS-1$
+        if (aliasParam == null) {
+            return null;
+        }
+        IDesignerCoreService designerCoreService = CorePlugin.getDefault().getDesignerCoreService();
+        boolean evaluate = designerCoreService.evaluate(isSpecifyAlias.getShowIf(), node.getElementParameters());
+        if (!evaluate) {
+            return null;
+        }
+        String result = aliasParam.getValue() == null ? null : aliasParam.getValue().toString();
+        return TalendQuoteUtils.removeQuotes(result.trim());
+    }
+
+    // TESB-17238:Include sub jobs datasource alias
+    private static void getDatasourceAliasesFrom(INode node, Collection<String> ds) {
+        if (!node.isActivate()) {
+            return;
+        }
+        if (TRUN_JOB.equals(node.getComponent().getName())) {
+            List<? extends INode> subNodes = getSubProcessNodesFromTRunjob(node);
+            if (subNodes != null) {
+                for (INode n : subNodes) {
+                    getDatasourceAliasesFrom(n, ds);
+                }
+            }
+        }
+        IElementParameter isSpecifyAlias = node.getElementParameter("SPECIFY_DATASOURCE_ALIAS"); //$NON-NLS-1$
+        if (isSpecifyAlias == null || Boolean.FALSE.equals(isSpecifyAlias.getValue())) {
+            return;
+        }
+        IElementParameter aliasParam = node.getElementParameter("DATASOURCE_ALIAS"); //$NON-NLS-1$
+        if (aliasParam == null) {
+            return;
+        }
+        IDesignerCoreService designerCoreService = CorePlugin.getDefault().getDesignerCoreService();
+        boolean evaluate = designerCoreService.evaluate(isSpecifyAlias.getShowIf(), node.getElementParameters());
+        if (!evaluate) {
+            return;
+        }
+        String result = aliasParam.getValue() == null ? null : aliasParam.getValue().toString();
+
+        ds.add(TalendQuoteUtils.removeQuotes(result.trim()));
+    }
+
+    private static List<? extends INode> getSubProcessNodesFromTRunjob(INode tRunJobNode) {
+        String processId = (String) tRunJobNode.getElementParameter("PROCESS:PROCESS_TYPE_PROCESS").getValue(); //$NON-NLS-1$
+        if (processId != null && !"".equals(processId)) { //$NON-NLS-1$
+            ProcessItem processItem = ItemCacheManager.getProcessItem(processId);
+            IDesignerCoreService service = CorePlugin.getDefault().getDesignerCoreService();
+            if (processItem != null && service != null) {
+                return service.getProcessFromItem(processItem).getGeneratingNodes();
+            }
+        }
+        return null;
+    }
 }
