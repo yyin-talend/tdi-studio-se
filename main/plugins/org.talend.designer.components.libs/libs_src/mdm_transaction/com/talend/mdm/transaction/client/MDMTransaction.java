585b1642f91ed6cc9a4fa7cd85d99936057b0362
Pingwen Lin <pwlin@talend.com>
2016-05-30 11:55:48 +0800

    TMDM-9111 : MDM transaction handling is wrong in multithreaded environment

--- main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/com/talend/mdm/transaction/client/MDMTransaction.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/com/talend/mdm/transaction/client/MDMTransaction.java
@@ -12,75 +12,85 @@ import org.apache.commons.httpclient.methods.PostMethod;
 
 public class MDMTransaction {
 
-	private String url;
-	private String id;
-	private String username;
-	private String password;
-	
-	public void commit() throws IOException {
-		HttpClient client = new HttpClient();
-		client.getState().setCredentials(AuthScope.ANY,
-				new UsernamePasswordCredentials(username, password));
+    private String url;
+    private String id;
+    private String username;
+    private String password;
+    private String sessionId;
 
-		HttpMethod method = new PostMethod(url + "/" + id);
-		method.setDoAuthentication(true);
-		try {
-			client.executeMethod(method);
-		} catch (HttpException e) {
-			throw e;
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			method.releaseConnection();
-		}
+    public void commit() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
 
-		int statuscode = method.getStatusCode();
-		if (statuscode >= 400) {
-		    throw new MDMTransactionException("Commit failed. The commit operation has returned the code " + statuscode + ".");
-		}
-	}
+        HttpMethod method = new PostMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            method.setRequestHeader("Cookie", "JSESSIONID=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
+        }
 
-	public void rollback() throws IOException {
-		HttpClient client = new HttpClient();
-		client.getState().setCredentials(AuthScope.ANY,
-				new UsernamePasswordCredentials(username, password));
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException("Commit failed. The commit operation has returned the code " + statuscode + ".");
+        }
+    }
 
-		HttpMethod method = new DeleteMethod(url + "/" + id);
-		method.setDoAuthentication(true);
-		try {
-			client.executeMethod(method);
-		} catch (HttpException e) {
-			throw e;
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			method.releaseConnection();
-		}
+    public void rollback() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
 
-		int statuscode = method.getStatusCode();
-		if (statuscode >= 400) {
-		    throw new MDMTransactionException("Rollback failed. The rollback operation has returned the code " + statuscode + ".");
-		}
-	}
-	
-	public void setUrl(String url) {
-		this.url = url;
-	}
+        HttpMethod method = new DeleteMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            method.setRequestHeader("Cookie", "JSESSIONID=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
+        }
 
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getId() {
-		return id;
-	}
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException("Rollback failed. The rollback operation has returned the code " + statuscode + ".");
+        }
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
+    public void setUrl(String url) {
+        this.url = url;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
 }
--- main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/com/talend/mdm/transaction/client/MDMTransactionClient.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/com/talend/mdm/transaction/client/MDMTransactionClient.java
@@ -6,70 +6,98 @@ import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
 
 public class MDMTransactionClient {
-	
-	public static MDMTransaction newTransaction(String url, String username, String password) throws IOException {
-		HttpClient client = new HttpClient();
-		client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
-		
-		PutMethod put = new PutMethod(url);
-		put.setDoAuthentication(true);
-		String tid = "";
-		try {
-			client.executeMethod(put);
-			tid = put.getResponseBodyAsString();
-		} catch (HttpException e) {
-			throw e;
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			put.releaseConnection();
-		}
-		
-		MDMTransaction result = new MDMTransaction();
-		result.setUrl(url);
-		result.setId(tid);
-		result.setUsername(username);
-		result.setPassword(password);
-		
-		return result;
-	}
-	
-	public static String getMDMTransactionURL(String url) {
-		if(url == null || "".equals(url)) {
-			return "";
-		}
-		
-		int count = 3;
-		int i=0;
-		for(;i<url.length();i++) {
-			char c = url.charAt(i);
-			if('/' == c) {
-				count--;
-			}
-			if(count == 0) {
-				break;
-			}
-		}
-		
-		String result = url.substring(0, i);
-		result += "/datamanager/services/transactions";
-		
-		return result;
-	}
 
-	public static void main(String[] args) throws IOException {
-		MDMTransaction mt = MDMTransactionClient.newTransaction("http://localhost:8080/datamanager/services/transactions", "administrator", "administrator");
-		mt.commit();
-		
-		MDMTransaction mt1 = MDMTransactionClient.newTransaction("http://localhost:8080/datamanager/services/transactions", "administrator", "administrator");
-		mt1.rollback();
-		
-		String url = "http://localhost:8080/talend/TalendPort";
-		String mdmurl = MDMTransactionClient.getMDMTransactionURL(url);
-		System.out.println(mdmurl);
-	}
-	
+    public static MDMTransaction newTransaction(String url, String username, String password, String sessionId) throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        PutMethod put = new PutMethod(url);
+        put.setDoAuthentication(true);
+        String tid = "";
+        try {
+            put.setRequestHeader("Cookie", "JSESSIONID=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
+            client.executeMethod(put);
+            tid = put.getResponseBodyAsString();
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            put.releaseConnection();
+        }
+
+        MDMTransaction result = new MDMTransaction();
+        result.setUrl(url);
+        result.setId(tid);
+        result.setUsername(username);
+        result.setPassword(password);
+        result.setSessionId(sessionId);
+
+        return result;
+    }
+
+    public static String getSessionID(String url, String username, String password) throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        GetMethod get = new GetMethod(url);
+        get.setDoAuthentication(true);
+        String sessionID = "";
+        try {
+            client.executeMethod(get);
+            String setCookie = get.getResponseHeader("Set-Cookie").getValue(); //$NON-NLS-1$
+            int beginIndex = setCookie.indexOf("JSESSIONID=") + 11; //$NON-NLS-1$
+            int endIndex = setCookie.indexOf(";", beginIndex); //$NON-NLS-1$
+            sessionID = setCookie.substring(beginIndex, endIndex);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            get.releaseConnection();
+        }
+        return sessionID;
+    }
+
+    public static String getMDMTransactionURL(String url) {
+        if(url == null || "".equals(url)) {
+            return "";
+        }
+
+        int count = 3;
+        int i=0;
+        for(;i<url.length();i++) {
+            char c = url.charAt(i);
+            if('/' == c) {
+                count--;
+            }
+            if(count == 0) {
+                break;
+            }
+        }
+
+        String result = url.substring(0, i);
+        result += "/datamanager/services/transactions";
+
+        return result;
+    }
+
+    public static void main(String[] args) throws IOException {
+        String sessionID = MDMTransactionClient.getSessionID("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator");
+        System.out.println(sessionID);
+
+        MDMTransaction mt = MDMTransactionClient.newTransaction("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator", sessionID);
+        mt.commit();
+
+        MDMTransaction mt1 = MDMTransactionClient.newTransaction("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator", sessionID);
+        mt1.rollback();
+
+        String url = "http://localhost:8000/talend/TalendPort";
+        String mdmurl = MDMTransactionClient.getMDMTransactionURL(url);
+        System.out.println(mdmurl);
+    }
+
 }
