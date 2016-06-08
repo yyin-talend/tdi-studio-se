d995e62ea86168452007673b1c5c1b3bb7b09e50
hwang <hwang@talend.com>
2016-06-08 17:38:15 +0800

    TDI-35007:positioning of drag and drop objects on designer space in Studio

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/TalendEditorDropTargetListener.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/process/TalendEditorDropTargetListener.java
@@ -64,6 +64,7 @@ import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.dialogs.ListDialog;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.runtime.model.repository.ERepositoryStatus;
+import org.talend.commons.ui.gmf.draw2d.AnimatableZoomManager;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.commons.ui.runtime.image.ImageUtils.ICON_SIZE;
 import org.talend.core.CorePlugin;
@@ -2007,7 +2008,8 @@ public class TalendEditorDropTargetListener extends TemplateTransferDropTargetLi
             viewOriginalPosition = viewport.getViewLocation();
         }
         Point point = new Point(originalPoint.x + viewOriginalPosition.x, originalPoint.y + viewOriginalPosition.y);
-
+        point.x =  (int) (point.x/AnimatableZoomManager.currentZoom);
+        point.y =  (int) (point.y/AnimatableZoomManager.currentZoom);
         org.talend.designer.core.ui.editor.connections.Connection targetConnection = null;
         if (selectedConnectionPart != null) {
             targetConnection = (org.talend.designer.core.ui.editor.connections.Connection) selectedConnectionPart.getModel();
