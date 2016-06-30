3daf9e5f0d32196d4b54850e284e3e36a808b7bc
cmeng-talend <cmeng@talend.com>
2016-06-30 17:43:17 +0800

    TDI-36772 fix: No response when click restart button when I start studio with cutom workspace https://jira.talendforge.org/browse/TDI-36772

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginProjectPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginProjectPage.java
@@ -992,16 +992,16 @@ public class LoginProjectPage extends AbstractLoginActionPage {
     }
 
     public void finishPressed() {
-        // This is for check if the reference project need to update
-        try {
-            saveUpdateStatus(getProject());
-        } catch (JSONException e) {
-            // TODO Auto-generated catch block
-            ExceptionHandler.process(e);
-        }
         if (LoginHelper.isRestart) {
             loginDialog.okPressed();
         } else {
+            // This is for check if the reference project need to update
+            try {
+                saveUpdateStatus(getProject());
+            } catch (Exception e) {
+                // show exception
+                MessageBoxExceptionHandler.process(e, loginHelper.getUsableShell());
+            }
             boolean isLogInOk = loginHelper.logIn(getConnection(), getProject());
             if (isLogInOk) {
                 LoginHelper.setAlwaysAskAtStartup(alwaysAsk.getSelection());
@@ -1024,8 +1024,9 @@ public class LoginProjectPage extends AbstractLoginActionPage {
         RepositoryContext repositoryContext = (RepositoryContext) ctx.getProperty(Context.REPOSITORY_CONTEXT_KEY);
         PreferenceManipulator prefManipulator = new PreferenceManipulator();
         String url = project.getEmfProject().getUrl();
-        if (url == null || !"git".equals(getStorage(url)))
+        if (url == null || !"git".equals(getStorage(url))) {
             return;
+        }
         String location = getLocation(url);
         String projectName = project.getTechnicalLabel();
         String branch = getBranch();
