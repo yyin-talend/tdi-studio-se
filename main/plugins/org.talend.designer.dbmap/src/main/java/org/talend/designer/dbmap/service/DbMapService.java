7d5636566bc29eda950b1d31b7a09b1d737b20fb
zwxue <zwxue@talend.com>
2016-07-20 11:48:35 +0800

    TDI-35297: tELTPostgressqlMap not functioning on other schemas apart from public schema.

--- main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/service/DbMapService.java
+++ main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/service/DbMapService.java
@@ -12,9 +12,17 @@
 // ============================================================================
 package org.talend.designer.dbmap.service;
 
+import java.util.List;
+
 import org.talend.core.model.process.IExternalNode;
 import org.talend.core.service.IDbMapService;
+import org.talend.designer.core.model.utils.emf.talendfile.AbstractExternalData;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.dbmap.DbMapComponent;
+import org.talend.designer.dbmap.model.emf.dbmap.DBMapData;
+import org.talend.designer.dbmap.model.emf.dbmap.DBMapperTableEntry;
+import org.talend.designer.dbmap.model.emf.dbmap.InputTable;
+import org.talend.designer.dbmap.model.emf.dbmap.OutputTable;
 
 /**
  * DOC talend class global comment. Detailled comment
@@ -27,6 +35,37 @@ public class DbMapService implements IDbMapService {
             return true;
         }
         return false;
+    }
+
+    @Override
+    public void updateEMFDBMapData(NodeType nodeType, String oldValue, String newValue) {
+        AbstractExternalData nodeData = nodeType.getNodeData();
+        if (nodeData instanceof DBMapData) {
+            DBMapData dbMapData = (DBMapData) nodeData;
+            for (InputTable input : dbMapData.getInputTables()) {
+                if (input.getName().equals(oldValue) || input.getTableName().equals(oldValue)) {
+                    input.setName(newValue);
+                    input.setTableName(newValue);
+                }
+            }
+            for (OutputTable output : dbMapData.getOutputTables()) {
+                List<DBMapperTableEntry> entries = output.getDBMapperTableEntries();
+                for (DBMapperTableEntry entry : entries) {
+                    String expression = entry.getExpression();
+                    if (expression != null && !"".equals(expression.trim())) { //$NON-NLS-1$
+                        int index = expression.lastIndexOf("."); //$NON-NLS-1$
+                        // at least "a.b"
+                        if (index > 0) {
+                            String connectionName = expression.substring(0, index);
+                            if (oldValue.equals(connectionName)) {
+                                entry.setExpression(newValue + expression.substring(index, expression.length()));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        
     }
 
 }
