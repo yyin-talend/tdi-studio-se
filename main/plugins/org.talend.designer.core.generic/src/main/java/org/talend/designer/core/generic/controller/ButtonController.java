8f3ba8a7ac50d7c87c7b5c4444e2fceb53e5b54f
ycbai <ycbai@talend.com>
2016-08-24 14:19:15 +0800

    TUP-5106: Generic widget display issues for new component framework

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/controller/ButtonController.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/controller/ButtonController.java
@@ -25,10 +25,12 @@ import org.eclipse.swt.layout.FormData;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.ui.views.properties.tabbed.ITabbedPropertyConstants;
 import org.talend.commons.ui.runtime.image.ImageProvider;
 import org.talend.core.model.process.IElementParameter;
 import org.talend.core.ui.CoreUIPlugin;
 import org.talend.core.ui.properties.tab.IDynamicProperty;
+import org.talend.designer.core.ui.editor.cmd.PropertyChangeCommand;
 import org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController;
 
 /**
@@ -46,7 +48,8 @@ public class ButtonController extends AbstractElementPropertySectionController {
         IElementParameter parameter = (IElementParameter) button.getData();
         if (parameter != null) {
             callBeforeActive(parameter);
-            parameter.setValue(null); // so as to invoke listeners to perform some actions.
+            // so as to invoke listeners to perform some actions.
+            return new PropertyChangeCommand(elem, parameter.getName(), null);
         }
         return null;
     }
@@ -55,22 +58,32 @@ public class ButtonController extends AbstractElementPropertySectionController {
     public Control createControl(Composite subComposite, IElementParameter param, int numInRow, int nbInRow, int top,
             Control lastControl) {
         Button theBtn = getWidgetFactory().createButton(subComposite, "", SWT.PUSH); //$NON-NLS-1$
+        theBtn.setBackground(subComposite.getBackground());
         if (param.getDisplayName().equals("")) { //$NON-NLS-1$
             theBtn.setImage(ImageProvider.getImage(CoreUIPlugin.getImageDescriptor(DOTS_BUTTON)));
         } else {
             theBtn.setText(param.getDisplayName());
         }
         FormData data = new FormData();
-        if (lastControl != null) {
-            reLayoutOtherControls(subComposite, numInRow, theBtn);
-            data.left = new FormAttachment(lastControl, 0);
+        if (isInWizard()) {
+            if (lastControl != null) {
+                reLayoutOtherControls(subComposite, numInRow, theBtn);
+                data.left = new FormAttachment(lastControl, 0);
+            } else {
+                data.right = new FormAttachment(100, -10);
+            }
         } else {
-            data.right = new FormAttachment(100, -10);
+            if (lastControl != null) {
+                data.left = new FormAttachment(lastControl, 0);
+            } else {
+                data.left = new FormAttachment((((numInRow - 1) * MAX_PERCENT) / nbInRow), 0);
+            }
         }
         data.top = new FormAttachment(0, top);
         theBtn.setLayoutData(data);
         theBtn.setEnabled(!param.isReadOnly());
         theBtn.setData(param);
+        hashCurControls.put(param.getName(), theBtn);
         theBtn.addSelectionListener(new SelectionAdapter() {
 
             @Override
@@ -79,6 +92,8 @@ public class ButtonController extends AbstractElementPropertySectionController {
                 executeCommand(cmd);
             }
         });
+        Point initialSize = theBtn.computeSize(SWT.DEFAULT, SWT.DEFAULT);
+        dynamicProperty.setCurRowSize(initialSize.y + ITabbedPropertyConstants.VSPACE);
         return theBtn;
     }
 
