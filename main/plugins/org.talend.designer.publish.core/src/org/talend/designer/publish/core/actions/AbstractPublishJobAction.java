ab375ff4a9b7937b32b118cbadb89649dbbddf1e
zwxue <zwxue@talend.com>
2016-08-30 18:16:36 +0800

    TDI-36420: "apply context to children" doesn't work for the published normal task in TAC.

--- main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
+++ main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
@@ -160,9 +160,12 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
             exportChoiceMap.put(ExportChoice.includeLibs, true);
 
             ProcessItem processItem = (ProcessItem) node.getObject().getProperty().getItem();
-
-            BuildJobManager.getInstance().buildJob(tmpJob.getAbsolutePath(), processItem, processItem.getProperty().getVersion(),
-                    processItem.getProcess().getDefaultContext(), exportChoiceMap, exportType, monitor);
+ 			String contextName = (String) exportChoiceMap.get(ExportChoice.contextName);
+			if (contextName == null) {
+				contextName = processItem.getProcess().getDefaultContext();
+			}
+			BuildJobManager.getInstance().buildJob(tmpJob.getAbsolutePath(), processItem, jobVersion, contextName,
+					exportChoiceMap, exportType, monitor);
 
             monitor.beginTask("Deploy to Artifact Repository....", IProgressMonitor.UNKNOWN);
             FeaturesModel featuresModel = getFeatureModel(tmpJob);
