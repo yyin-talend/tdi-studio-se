200958a961a4ebd0001673e78d57f49ba922a249
zwxue <zwxue@talend.com>
2016-08-30 18:16:36 +0800

    TDI-36420: "apply context to children" doesn't work for the published normal task in TAC.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JobScriptsExportWizardPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JobScriptsExportWizardPage.java
@@ -1470,7 +1470,8 @@ public abstract class JobScriptsExportWizardPage extends WizardFileSystemResourc
     }
 
     protected boolean buildJobWithMaven(JobExportType jobExportType, IProgressMonitor monitor) {
-        String context = (contextCombo == null || contextCombo.isDisposed()) ? IContext.DEFAULT : contextCombo.getText();
+		String context = (contextCombo == null || contextCombo.isDisposed())
+				? processItem.getProcess().getDefaultContext() : contextCombo.getText();
         try {
             String destination = getDestinationValue();
             int separatorIndex = destination.lastIndexOf(File.separator);
