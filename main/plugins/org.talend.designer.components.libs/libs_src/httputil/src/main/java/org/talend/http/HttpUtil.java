52a68c80dc1d2d02d8e0574ea6e9e9724b781aee
wang wei <wwang@talend.com>
2016-09-22 16:14:35 +0800

    TDI-37177: fix the util class to return the better exception information https://jira.talendforge.org/browse/TDI-37177

--- main/plugins/org.talend.designer.components.libs/libs_src/httputil/src/main/java/org/talend/http/HttpUtil.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/httputil/src/main/java/org/talend/http/HttpUtil.java
@@ -1,40 +1,74 @@
 package org.talend.http;
 
 import java.util.List;
+
+import org.apache.http.entity.ContentType;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.ContentType;
+
+import net.sf.json.JSONObject;
+
 import java.util.ArrayList;
 
 public class HttpUtil {
-    
-    public static void handHttpResponse(org.apache.http.HttpResponse response) throws java.io.IOException {
-        if(response == null) {
-            return;
-        }
-        
-        if(response.getStatusLine().getStatusCode() != 200) {//login fail
-            if (response.getStatusLine().getStatusCode() == 400) {
-                String returnedValue = org.apache.http.util.EntityUtils.toString(response.getEntity());
-                net.sf.json.JSONObject object = net.sf.json.JSONObject.fromObject(returnedValue);
-                throw new RuntimeException(object.getString("cause"));
-            } else if (response.getStatusLine().getStatusCode() == 403) {
-                String returnedValue = org.apache.http.util.EntityUtils.toString(response.getEntity());
-                net.sf.json.JSONObject object = net.sf.json.JSONObject.fromObject(returnedValue);
-                throw new RuntimeException(object.getString("message"));
+	
+	public static void handHttpResponse(org.apache.http.HttpResponse response) throws java.io.IOException {
+	    if(response == null) {
+	        return;
+	    }
+	    
+	    if(response.getStatusLine().getStatusCode() != 200) {
+            String message = "";
+            
+            String returnedValue = EntityUtils.toString(response.getEntity());
+            
+            ContentType contentType = ContentType.get(response.getEntity());
+            if (contentType != null && ContentType.APPLICATION_JSON.getMimeType().equals(contentType.getMimeType())) {
+                JSONObject object = JSONObject.fromObject(returnedValue);
+                
+                message = getValueFromJson(object, "message");
+                String cause = getValueFromJson(object, "cause");
+                if (!cause.isEmpty()) {
+                    if (!message.isEmpty()) {
+                        message = message + "\n" + cause;
+                    } else {
+                        message = cause;
+                    }
+                }
+                
+                if (message.isEmpty()) {
+                    message = response.getStatusLine().getReasonPhrase();
+                }
             } else {
-                throw new RuntimeException(response.getStatusLine().getReasonPhrase());
+                if (returnedValue.isEmpty()) {
+                    message = response.getStatusLine().getReasonPhrase();
+                }
             }
+            
+            throw new RuntimeException(message);
         }
+	}
+	
+	private static String getValueFromJson(JSONObject object, String key) {
+        String message = "";
+        try {
+            message = object.getString(key);
+        } catch (Exception e) {
+            message = "";
+        }
+        return message;
     }
-    
-    public static void handHttpResponse(java.net.HttpURLConnection connection) throws java.io.IOException {
-        if(connection == null) {
-            return;
-        }
-        
-        int responseCode = connection.getResponseCode();
-        String responseMessage = connection.getResponseMessage();
+	
+	public static void handHttpResponse(java.net.HttpURLConnection connection) throws java.io.IOException {
+	    if(connection == null) {
+	        return;
+	    }
+	    
+	    int responseCode = connection.getResponseCode();
+	    String responseMessage = connection.getResponseMessage();
         if(responseCode != 200) {//login fail
             throw new RuntimeException("" + responseCode + " : " + responseMessage);
         }
     }
-    
+	
 }
