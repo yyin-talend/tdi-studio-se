56e3de1c02f4041fce58a27f34e1e04647f04889
msjian <msjian@talend.com>
2016-10-13 14:56:26 +0800

    TDQ-7446: fix TDQ-7348 pattern list in tMultiPatternCheck show more than once.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/TableController.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/TableController.java
@@ -203,8 +203,8 @@ public class TableController extends AbstractElementPropertySectionController {
             } else {
                 formData.right = new FormAttachment(100, -ITabbedPropertyConstants.HSPACE);
             }
-            formData.left = new FormAttachment((((nbInRow - numInRow) * MAX_PERCENT) / nbInRow),
-                    currentLabelWidth2 + ITabbedPropertyConstants.HSPACE);
+            formData.left = new FormAttachment((((nbInRow - numInRow) * MAX_PERCENT) / nbInRow), currentLabelWidth2
+                    + ITabbedPropertyConstants.HSPACE);
 
             formData = (FormData) labelLabel2.getLayoutData();
             formData.right = new FormAttachment(mainComposite, 0);
@@ -355,16 +355,45 @@ public class TableController extends AbstractElementPropertySectionController {
                 String[] customizedShowIfs = param.getListItemsShowIf();
                 dqPatternService.overridePatternList(typeParam, param);
                 // Add the customized value:
-                param.setListItemsValue(ArrayUtils.addAll(param.getListItemsValue(), customizedValue));
-                param.setListItemsDisplayCodeName((String[]) ArrayUtils.addAll(param.getListItemsDisplayCodeName(),
+                param.setListItemsValue(mergeWithoutDuplicate(param.getListItemsValue(), customizedValue));
+                param.setListItemsDisplayCodeName((String[]) mergeWithoutDuplicate(param.getListItemsDisplayCodeName(),
                         customizedDisplayCodeName));
-                param.setListItemsDisplayName((String[]) ArrayUtils.addAll(param.getListItemsDisplayName(), customizedDisplayName));
-                param.setListItemsNotShowIf((String[]) ArrayUtils.addAll(new String[param.getListItemsShowIf().length],
+                param.setListItemsDisplayName((String[]) mergeWithoutDuplicate(param.getListItemsDisplayName(),
+                        customizedDisplayName));
+                param.setListItemsNotShowIf(mergeWithDuplicate(new String[param.getListItemsShowIf().length],
                         customizedNotShowIfs));
-                param.setListItemsShowIf((String[]) ArrayUtils.addAll(new String[param.getListItemsShowIf().length],
-                        customizedShowIfs));
+                param.setListItemsShowIf(mergeWithDuplicate(new String[param.getListItemsShowIf().length], customizedShowIfs));
             }
         }
+    }
+
+    /**
+     * Adds all the elements of "b" arrays into "a" array without the duplicate one in "a", and return "a".
+     * 
+     * @param a
+     * @param b
+     * @return
+     */
+    private Object[] mergeWithoutDuplicate(Object[] a, Object[] b) {
+        if (b == null || b.length == 0) {
+            return a;
+        }
+        for (Object valueB : b) {
+            if (!ArrayUtils.contains(a, valueB)) {
+                a = ArrayUtils.add(a, valueB);
+            }
+        }
+        return a;
+    }
+
+    private String[] mergeWithDuplicate(String[] a, String[] b) {
+        if (b == null || b.length == 0) {
+            return a;
+        }
+        for (String valueB : b) {
+            a = (String[]) ArrayUtils.add(a, valueB);
+        }
+        return a;
     }
 
     private boolean isDQPatternList(IElementParameter param) {
