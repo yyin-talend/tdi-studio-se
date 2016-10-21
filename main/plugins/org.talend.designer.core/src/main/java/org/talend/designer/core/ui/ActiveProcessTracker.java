a1c5ab818044bda3fcd00da17510d6e539c12805
wchen-talend <wchen@talend.com>
2016-10-21 13:24:46 +0800

    TUP-6195:Should better restore the jobsetting view after close a job, now show unhandle error when switch pages

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/ActiveProcessTracker.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/ActiveProcessTracker.java
@@ -240,15 +240,7 @@ public class ActiveProcessTracker implements IPartListener {
         if (part instanceof AbstractMultiPageTalendEditor) {
             AbstractMultiPageTalendEditor mpte = (AbstractMultiPageTalendEditor) part;
             mpte.beforeDispose();
-        }
-        IWorkbenchPage page = PlatformUI.getWorkbench().getActiveWorkbenchWindow().getActivePage();
-        if (page != null) {
-            if (page.getActiveEditor() != null) {
-                if (!page.getActiveEditor().getSite().getId().equals(DISTARTID)
-                        && !page.getActiveEditor().getSite().getId().equals(MDMSTARTID)) {
-                    JobSettings.switchToCurJobSettingsView();
-                }
-            }
+            JobSettings.switchToCurJobSettingsView();
         }
 
     }
