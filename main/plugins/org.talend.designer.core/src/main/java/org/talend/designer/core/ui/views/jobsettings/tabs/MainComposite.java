1fe2966f7177014903211dc4a754db8a4adbb835
hwang <hwang@talend.com>
2016-11-21 15:10:54 +0800

    TUP-15118：A wrong warning shown when convert MR job which is converted

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/tabs/MainComposite.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/views/jobsettings/tabs/MainComposite.java
@@ -608,15 +608,12 @@ public class MainComposite extends AbstractTabComposite {
                         Item originalItem = repositoryObject.getProperty().getItem();
                         if (ConvertJobsUtil.isNeedConvert(originalItem, originalJobType, originalFramework, true)) {
                             boolean hasTestCase = ConvertJobsUtil.hasTestCase(repositoryObject.getProperty());
-                            if (hasTestCase
-                                    && !MessageDialogWithToggle
-                                            .openConfirm(null, "Warning",
-                                                    "Warning: You will lost all the testcases when you do converting, do you want to continue?")) {
-                                return;
-                            }
                             // Convert
                             final Item newItem = ConvertJobsUtil.createOperation(originalName, originalJobType,
                                     originalFramework, repositoryObject);
+                            if(newItem!=null){
+                                ConvertJobsUtil.convertTestcases(newItem,repositoryObject,originalJobType);
+                            }
                             RepositoryWorkUnit repositoryWorkUnit = new RepositoryWorkUnit("Convert job") { //$NON-NLS-1$
 
                                 @Override
