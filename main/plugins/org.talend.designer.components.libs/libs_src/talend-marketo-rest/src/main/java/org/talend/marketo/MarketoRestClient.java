b3fc530234a63839fa200869b029a4c87c723489
pyzhou <pyzhou@talend.com>
2016-11-29 19:17:45 +0800

    Bug/6.3.1/tdi 37860 (#825)
    * TDI-37860 : TMarketoInput has NullPointerException when Access token is invalid
    *  TDI-37860 Update the jars
    * TDI-37860 : Add the Exception errorMessage to other similar method.
    * TDI-37860 : Correct the maven path.

--- main/plugins/org.talend.designer.components.libs/libs_src/talend-marketo-rest/src/main/java/org/talend/marketo/MarketoRestClient.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-marketo-rest/src/main/java/org/talend/marketo/MarketoRestClient.java
@@ -233,8 +233,11 @@ public class MarketoRestClient {
         if (nextPageToken != null) {
             webClient.query("nextPageToken", nextPageToken);
         }
-
-        return (ResultBasic) executeGetRequest(ResultBasic.class);
+        ResultBasic rb = (ResultBasic) executeGetRequest(ResultBasic.class);
+        if(rb.isSuccess()){
+        	return rb;
+        }
+        throw new Exception(rb.getErrors().toString());
     }
 
     public Integer getListIdByName(String listName) throws Exception {
@@ -251,6 +254,8 @@ public class MarketoRestClient {
                     return listObject.getId();
                 }
             }
+        }else if(!getResponse.isSuccess()){
+        	throw new Exception(getResponse.getErrors().toString());
         }
         return null;
     }
@@ -278,7 +283,11 @@ public class MarketoRestClient {
         if (listId != null) {
             webClient.query("listId", listId);
         }
-        return (ResultGetLeadActivities) executeGetRequest(ResultGetLeadActivities.class);
+        ResultGetLeadActivities rga = (ResultGetLeadActivities) executeGetRequest(ResultGetLeadActivities.class);
+        if(rga.isSuccess()){
+        	return rga;
+        }
+        throw new Exception(rga.getErrors().toString());
     }
 
     public Map<Integer, String> getServerActivityTypes() throws Exception {
@@ -315,8 +324,12 @@ public class MarketoRestClient {
         if (fields != null && fields.length > 0) {
             webClient.query("fields", MarketoUtils.unionString(",", fields));
         }
+        ResultGetLeadChanges rglc = (ResultGetLeadChanges) executeGetRequest(ResultGetLeadChanges.class);
+        if(rglc.isSuccess()){
+        	return rglc;
+        }
+        throw new Exception(rglc.getErrors().toString());
 
-        return (ResultGetLeadChanges) executeGetRequest(ResultGetLeadChanges.class);
     }
 
     public ResultSync syncLead(String action, String lookupField,
@@ -351,8 +364,12 @@ public class MarketoRestClient {
             inputJson.addProperty("partitionName", partitionName);
         }
         inputJson.add("input", gson.toJsonTree(leadsObjects));
+        ResultSync rs = (ResultSync) executePostRequest(ResultSync.class,inputJson);
+        if(rs.isSuccess()){
+        	return rs;
+        }
+        throw new Exception(rs.getErrors().toString());
 
-        return (ResultSync) executePostRequest(ResultSync.class, inputJson);
     }
 
     public ResultSync listOperation(String operation, int listId,
@@ -383,7 +400,13 @@ public class MarketoRestClient {
         // inputData.put("input", json);
         JsonObject jsonObj = new JsonObject();
         jsonObj.add("input", json);
-        return (ResultSync) executePostRequest(ResultSync.class, jsonObj);
+        
+        ResultSync rs = (ResultSync) executePostRequest(ResultSync.class, jsonObj);
+        if(rs.isSuccess()){
+        	return rs;
+        }
+        throw new Exception(rs.getErrors().toString());
+        
     }
 
     public Map<String, String> readActivity(LeadActivityRecord record) {
