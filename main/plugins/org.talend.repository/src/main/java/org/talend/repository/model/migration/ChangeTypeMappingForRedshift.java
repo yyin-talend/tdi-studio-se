612b04cbe4a887940c64e153d069f0f0a93c64fe
Chen Bin <bchen@talend.com>
2016-12-06 16:55:12 +0800

    TBD-4260: migration task for redshift component to correct type mapping

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeTypeMappingForRedshift.java
@@ -0,0 +1,87 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractAllJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class ChangeTypeMappingForRedshift extends AbstractAllJobMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2016, 11, 6, 12, 0, 0);
+        return gc.getTime();
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+        String[] compNames = { "tRedshiftInput", "tRedshiftOutput", "tRedshiftLookupInput" }; //$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ 
+
+        IComponentConversion conversion = new IComponentConversion() {
+
+            @Override
+            public void transform(NodeType node) {
+                if (node == null) {
+                    return;
+                }
+
+                ElementParameterType typeMapping = ComponentUtilities.getNodeProperty(node, "MAPPING"); //$NON-NLS-1$
+                if (typeMapping != null && "redshift_new_id".equals(typeMapping.getValue())) {
+                    ComponentUtilities.setNodeValue(node, "MAPPING", "redshift_id"); //$NON-NLS-1$ //$NON-NLS-2$
+                }
+            }
+
+        };
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(conversion));
+            } catch (PersistenceException e) {
+                // TODO Auto-generated catch block
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+    }
+
+}
