5ad64d9184e83b665fc314ed6d07f2303ccf2e10
zwxue <zwxue@talend.com>
2016-12-09 14:16:33 +0800

    TUP-16573: cmdline generate time increase as the tasks number increase.

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DeleteAllJobWhenStartUp.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DeleteAllJobWhenStartUp.java
@@ -30,9 +30,7 @@ import org.eclipse.core.runtime.jobs.ISchedulingRule;
 import org.eclipse.jdt.core.IJavaProject;
 import org.eclipse.ui.IStartup;
 import org.talend.commons.exception.ExceptionHandler;
-import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.utils.generation.JavaUtils;
-import org.talend.core.CorePlugin;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.runtime.process.ITalendProcessJavaProject;
 import org.talend.designer.maven.model.TalendMavenConstants;
@@ -49,11 +47,13 @@ public class DeleteAllJobWhenStartUp implements IStartup {
 
     public static boolean executed;
 
-    private boolean startUnderPluginModel;
-
-    public void startup(boolean pluginModel) {
-        startUnderPluginModel = pluginModel;
+    public void startup() {
         earlyStartup();
+    }
+    
+    @Deprecated
+    public void startup(boolean pluginModel) {
+        startup();
     }
 
     /*
@@ -63,10 +63,6 @@ public class DeleteAllJobWhenStartUp implements IStartup {
      */
     @Override
     public void earlyStartup() {
-
-        if (!startUnderPluginModel && !CorePlugin.getDefault().getRepositoryService().isRCPMode()) {
-            return;
-        }
         if (!GlobalServiceRegister.getDefault().isServiceRegistered(IRunProcessService.class)) {
             return;
         }
