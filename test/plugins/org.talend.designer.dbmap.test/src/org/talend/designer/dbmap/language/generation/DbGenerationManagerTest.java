51026b9b5bcbcc21d4c4f20a890211804b82e343
GuoLiang <guol@talend.com>
2016-12-19 16:44:29 +0800

    bugfix recheck: TUP-14431 ELT Map queries : compilation error with (#877)
    quotes

---
+++ test/plugins/org.talend.designer.dbmap.test/src/org/talend/designer/dbmap/language/generation/DbGenerationManagerTest.java
@@ -0,0 +1,100 @@
+package org.talend.designer.dbmap.language.generation;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.talend.core.model.metadata.IMetadataColumn;
+import org.talend.core.model.metadata.IMetadataTable;
+import org.talend.core.model.metadata.MetadataColumn;
+import org.talend.core.model.metadata.MetadataTable;
+import org.talend.core.model.process.IConnection;
+import org.talend.designer.core.ui.editor.connections.Connection;
+import org.talend.designer.dbmap.DbMapComponent;
+import org.talend.designer.dbmap.external.data.ExternalDbMapData;
+import org.talend.designer.dbmap.external.data.ExternalDbMapEntry;
+import org.talend.designer.dbmap.external.data.ExternalDbMapTable;
+
+public class DbGenerationManagerTest {
+
+    private DbMapComponent dbMapComponent;
+
+    private DbGenerationManager dbManager;
+
+    private ExternalDbMapEntry extMapEntry;
+
+    private List<ExternalDbMapEntry> tableEntries;
+
+    @Before
+    public void setUp() throws Exception {
+        dbMapComponent = new DbMapComponent();
+
+        ExternalDbMapData externalDbMapData = new ExternalDbMapData();
+        dbMapComponent.setExternalData(externalDbMapData);
+
+        List<ExternalDbMapTable> inputTables = new ArrayList<>();
+        externalDbMapData.setInputTables(inputTables);
+
+        ExternalDbMapTable externalDbMapTable = new ExternalDbMapTable();
+        inputTables.add(externalDbMapTable);
+        externalDbMapTable.setName("t1");
+        externalDbMapTable.setAlias("t1");
+        externalDbMapTable.setTableName("t1");
+        externalDbMapTable.setJoinType("NO_JOIN");
+        
+        tableEntries = new ArrayList<>();
+        externalDbMapTable.setMetadataTableEntries(tableEntries);
+        extMapEntry = new ExternalDbMapEntry("id", "t1.id");
+        tableEntries.add(extMapEntry);
+
+        List<IConnection> incomingConnections = new ArrayList<IConnection>();
+        String[] columns = new String[] { "\\\"id\\\"" };
+        incomingConnections.add(createConnection("t1", "t1", "id", columns));
+        dbMapComponent.setIncomingConnections(incomingConnections);
+
+        dbManager = new GenericDbGenerationManager();
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dbMapComponent = null;
+        dbManager = null;
+        extMapEntry = null;
+        tableEntries = null;
+    }
+
+    @Test
+    public void testInitExpression() {
+        checkValue("t1.\\\"id\\\"");
+    }
+
+    public void checkValue(String expression) {
+        Assert.assertEquals(expression, dbManager.initExpression(dbMapComponent, extMapEntry));
+    }
+
+    private IConnection createConnection(String schemaName, String tableName, String label, String[] columns) {
+        Connection connection = mock(Connection.class);
+        when(connection.getName()).thenReturn(tableName);
+        IMetadataTable metadataTable = new MetadataTable();
+        metadataTable.setLabel(tableName);
+        metadataTable.setTableName(tableName);
+        List<IMetadataColumn> listColumns = new ArrayList<IMetadataColumn>();
+        for (String columnName : columns) {
+            IMetadataColumn column = new MetadataColumn();
+            column.setLabel(label);
+            column.setOriginalDbColumnName(columnName);
+            listColumns.add(column);
+        }
+        metadataTable.setListColumns(listColumns);
+        when(connection.getMetadataTable()).thenReturn(metadataTable);
+
+        return connection;
+    }
+}
