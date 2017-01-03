8ddf518949c476cd53fbb4125c1e9a3161e7637c
wang wei <wwang@talend.com>
2017-01-03 14:23:22 +0800

    fix(TDI-36824): tDatasetOutput: Allow limit to be infinite if null (or empty) is set

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/Component.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/Component.java
@@ -1242,7 +1242,7 @@ public class Component extends AbstractBasicComponent {
             return "\"" + value.replace("\\\"", "\\\\\"").replace("\"", "\\\"") + "\"";//$NON-NLS-1$ //$NON-NLS-2$ //$NON-NLS-3$ //$NON-NLS-4$ //$NON-NLS-5$ //$NON-NLS-6$
         }
         if (GenericTypeUtils.isIntegerType(property) && ContextParameterUtils.isContainContextParam(value)) {
-            value = "Integer.valueOf(" + value + ")";
+            value = "routines.system.ObjectUtil.nonNull(" + value + ") ? Integer.valueOf(" + value + ") : null";
         }
         return value;
     }
