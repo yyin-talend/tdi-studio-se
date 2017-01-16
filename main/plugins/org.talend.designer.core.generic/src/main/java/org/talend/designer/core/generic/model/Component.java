5e3a8503436cbae27234839b97eb09532a38bd95
dlin-talend <dlin@talend.com>
2017-01-16 10:33:57 +0800

    TUP-16598: add codegenutil to org.talend.libraries.custom and set to (#928)
    component framework by default

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/Component.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/Component.java
@@ -1173,6 +1173,9 @@ public class Component extends AbstractBasicComponent {
             ModuleNeeded moduleNeeded = new ModuleNeeded(getName(), "", true,
                     "mvn:org.talend.libraries/slf4j-log4j12-1.7.2/6.0.0");
             componentImportNeedsList.add(moduleNeeded);
+            moduleNeeded = new ModuleNeeded(getName(), "", true,
+                    "mvn:org.talend.libraries/talend-codegen-utils/0.17.0-SNAPSHOT");
+            componentImportNeedsList.add(moduleNeeded);
             return componentImportNeedsList;
         }
     }
