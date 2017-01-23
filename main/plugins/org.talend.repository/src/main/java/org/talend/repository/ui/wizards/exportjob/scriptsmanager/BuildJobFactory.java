21954eb7b576fd26c425ace4188febbcb39c355b
GGu <ggu@talend.com>
2017-01-23 10:45:55 +0800

    TUP-17000: Impl build handler for standalone.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/BuildJobFactory.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/BuildJobFactory.java
@@ -12,10 +12,15 @@
 // ============================================================================
 package org.talend.repository.ui.wizards.exportjob.scriptsmanager;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import org.talend.core.model.properties.ProcessItem;
 import org.talend.core.runtime.process.IBuildJobHandler;
+import org.talend.core.runtime.repository.build.AbstractBuildProvider;
+import org.talend.core.runtime.repository.build.BuildExportManager;
+import org.talend.core.runtime.repository.build.IBuildJobParameters;
+import org.talend.core.runtime.repository.build.IBuildParametes;
 import org.talend.repository.ui.wizards.exportjob.JavaJobScriptsExportWSWizardPage.JobExportType;
 import org.talend.repository.ui.wizards.exportjob.handler.BuildJobHandler;
 import org.talend.repository.ui.wizards.exportjob.scriptsmanager.JobScriptsManager.ExportChoice;
@@ -38,10 +43,25 @@ public class BuildJobFactory {
      */
     public static IBuildJobHandler createBuildJobHandler(ProcessItem processItem, String contextName, String version,
             Map<ExportChoice, Object> exportChoiceMap, JobExportType jobExportType) {
+
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put(IBuildParametes.ITEM, processItem);
+        parameters.put(IBuildParametes.VERSION, version);
+        parameters.put(IBuildJobParameters.CONTEXT_GROUP, contextName);
+        parameters.put(IBuildJobParameters.CHOICE_OPTION, exportChoiceMap);
+
         IBuildJobHandler handler = null;
         switch (jobExportType) {
         case POJO:
-            handler = new BuildJobHandler(processItem, version, contextName, exportChoiceMap);
+            final AbstractBuildProvider buildProvider = BuildExportManager.getInstance().getBuildProvider("Standalone",
+                    parameters);
+            if (buildProvider != null) {
+                handler = buildProvider.createHandler(parameters);
+            }
+            if (handler == null) {
+                // TODO, should remove
+                handler = new BuildJobHandler(processItem, version, contextName, exportChoiceMap);
+            }
             break;
         case WSWAR:
         case WSZIP:
