61d1e6a63a7306a702e0706d63e865213107a159
zshen-talend <zshen@talend.com>
2017-03-02 17:17:00 +0800

    bug fixed TDQ-13053 fixed nullPointerException with mutilpass case (#1000)

--- main/plugins/org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/lookup/memory/SwooshMultipassAdvaluedMemoryLookup.java
+++ main/plugins/org.talend.designer.components.localprovider/src/main/java/org/talend/designer/components/lookup/memory/SwooshMultipassAdvaluedMemoryLookup.java
@@ -1,47 +1,38 @@
 package org.talend.designer.components.lookup.memory;
 
-import java.util.Collection;
-import java.util.HashMap;
-import java.util.List;
-
-import org.apache.commons.collections.list.GrowthList;
-import org.talend.commons.utils.data.map.MultiLazyValuesMap;
-import org.talend.designer.components.lookup.common.ICommonLookup.MATCHING_MODE;
-
-
 public class SwooshMultipassAdvaluedMemoryLookup<V> extends AdvancedMemoryLookup<V> {
 
     public SwooshMultipassAdvaluedMemoryLookup(MATCHING_MODE matchingMode, boolean b, boolean c) {
         super(matchingMode, false, false);
-//        
-//        mapOfCol = new MultiLazyValuesMap(new HashMap()) {
-//
-//            @Override
-//            public Collection instanciateNewCollection() {
-//                return new GrowthList(3);
-//            }
-//
-//            @Override
-//            public Object put(Object key, Object value) {
-//                Object v = get(key);
-//                if (v != null) {
-//                    if (v instanceof List) {
-//                        ((List) v).add(value);
-//                    } else {
-//                        Collection list = instanciateNewCollection();
-//                        list.add(v);
-//                        list.add(value);
-//                        map.put(key, list);
-//                    }
-//                } else {
-//                    return map.put(key, value);
-//                }
-//                return v;
-//            }
-//            
-//            
-//
-//        };
+        //
+        // mapOfCol = new MultiLazyValuesMap(new HashMap()) {
+        //
+        // @Override
+        // public Collection instanciateNewCollection() {
+        // return new GrowthList(3);
+        // }
+        //
+        // @Override
+        // public Object put(Object key, Object value) {
+        // Object v = get(key);
+        // if (v != null) {
+        // if (v instanceof List) {
+        // ((List) v).add(value);
+        // } else {
+        // Collection list = instanciateNewCollection();
+        // list.add(v);
+        // list.add(value);
+        // map.put(key, list);
+        // }
+        // } else {
+        // return map.put(key, value);
+        // }
+        // return v;
+        // }
+        //
+        //
+        //
+        // };
 
     }
 
@@ -49,12 +40,15 @@ public class SwooshMultipassAdvaluedMemoryLookup<V> extends AdvancedMemoryLookup
         return new SwooshMultipassAdvaluedMemoryLookup<V>(matchingMode, false, false);
     }
 
-    public V put(V master,V value) {
+    public V put(V master, V value) {
         if (value != null) {
-                    arrayIsDirty = true;
-                    V previousValue = (V) mapOfCol.put(master, value);
-                    incrementCountValues(value, previousValue);
-                    return previousValue;
+            arrayIsDirty = true;
+            V previousValue = (V) mapOfCol.put(master, value);
+            incrementCountValues(value, previousValue);
+            if (getMatchingMode() == MATCHING_MODE.ALL_ROWS) {
+                getList().add(value);
+            }
+            return previousValue;
         }
         return null;
     }
