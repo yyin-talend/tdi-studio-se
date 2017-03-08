1915ba9a3efccfb651cc0c3cb156b4d84b346cbd
Anas KHABALI <akhabali@talend.com>
2017-03-08 09:53:12 +0100

    fix(TDI-38294) migration task for jobs using azureBlob cpt to tcomp (#1024)
    This migration task move jobs, using Azure Blob components, from old
    format to new TCOMP format
    
    * tAzureStorageConnection
    * tAzureStorageContainerCreate
    * tAzureStorageContainerDelete
    * tAzureStorageContainerExist
    * tAzureStorageDelete
    * tAzureStorageGet
    * tAzureStorageList
    * tAzureStoragePut

---
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewAzureBlobMigrationTask.java
@@ -0,0 +1,63 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.core.generic.model.migration;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.Properties;
+
+import org.talend.designer.core.generic.utils.ParameterUtilTool;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+
+public class NewAzureBlobMigrationTask extends NewComponentFrameworkMigrationTask {
+
+    @Override
+    public Date getOrder() {
+        return new GregorianCalendar(2017, 1, 24, 16, 0, 0).getTime();
+    }
+
+    @Override
+    protected Properties getPropertiesFromFile() {
+        Properties props = new Properties();
+        InputStream in = getClass().getResourceAsStream("NewAzureBlobMigrationTask.properties");//$NON-NLS-1$
+        try {
+            props.load(in);
+            in.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return props;
+    }
+
+    @Override
+    protected ElementParameterType getParameterType(NodeType node, String paramName) {
+
+        ElementParameterType paramType = ParameterUtilTool.findParameterType(node, paramName);
+        if (node != null && paramType != null) {
+            Object value = ParameterUtilTool.convertParameterValue(paramType);
+            if ("CONTAINER_ACL".equals(paramName)) {
+                if ("CONTAINER_ACL_PRIVATE".equals(String.valueOf(value))) {
+                    paramType.setValue("Private");
+                } else {
+                    paramType.setValue("Public");
+                }
+            }
+        }
+        return paramType;
+    }
+
+}
