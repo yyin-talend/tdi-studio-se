df0bf75a835dffa8ece63271bc378effd10eb7c2
Emmanuel GALLOIS <emmanuel.gallois@gmail.com>
2017-04-18 08:20:59 +0100

    bugfix(TDI-38465) : manage correctly Apimode for old jobs (#1154)

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewMarketoMigrationTask.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewMarketoMigrationTask.java
@@ -12,8 +12,6 @@ import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 
 public class NewMarketoMigrationTask extends NewComponentFrameworkMigrationTask {
 
-    Boolean hasAPI = Boolean.FALSE;
-
     @Override
     public Date getOrder() {
         return new GregorianCalendar(2017, 2, 24, 10, 0, 0).getTime();
@@ -34,62 +32,47 @@ public class NewMarketoMigrationTask extends NewComponentFrameworkMigrationTask 
 
     @Override
     protected ElementParameterType getParameterType(NodeType node, String paramName) {
-        // Not sure that it's the right place for that...
+        ElementParameterType paramType = ParameterUtilTool.findParameterType(node, paramName);
         //
         // Very old job didn't support REST API
-        // This means it has neither USE_REST_API nor USE_SOAP_API so migrate to SOAP
+        // This means it has neither USE_REST_API nor USE_SOAP_API so migrate to
+        // SOAP
         //
-        if (node != null && !hasAPI) {
-            ElementParameterType apiParamType = ParameterUtilTool.findParameterType(node, "USE_SOAP_API");
-            if (apiParamType == null) {
-               ParameterUtilTool.addParameterType(node, "RADIO","USE_SOAP_API", "true");
-            }
-            hasAPI = true;
+        if ("USE_SOAP_API".equals(paramName) && paramType == null) {
+            ParameterUtilTool.addParameterType(node, "RADIO", "USE_SOAP_API", "true");
+            paramType = ParameterUtilTool.findParameterType(node, paramName);
         }
-        //
-        String componentName  = node.getComponentName();
-        ElementParameterType paramType = ParameterUtilTool.findParameterType(node, paramName);
         if (node != null && paramType != null) {
+            String componentName = node.getComponentName();
             Object value = ParameterUtilTool.convertParameterValue(paramType);
-            if("USE_SOAP_API".equals(paramName)){
-                if("true".equals(String.valueOf(value))) {
+            if ("USE_SOAP_API".equals(paramName)) {
+                if ("true".equals(String.valueOf(value))) {
                     paramType.setValue("SOAP");
                 } else {
                     paramType.setValue("REST");
                 }
             }
-            // MAX_RETURN should be taken in account when OPERATION=getMutipleLeads and LEAD_SELECTOR=LeadKeySelector
+            // MAX_RETURN should be taken in account when
+            // OPERATION=getMutipleLeads and LEAD_SELECTOR=LeadKeySelector
             // Otherwise, we feed the value with BATCH_SIZE.
             if ("MAX_RETURN".equals(paramName) && "tMarketoInput".equals(componentName)) {
                 ElementParameterType operation = ParameterUtilTool.findParameterType(node, "OPERATION");
                 ElementParameterType leadSelector = ParameterUtilTool.findParameterType(node, "LEAD_SELECTOR");
                 ElementParameterType batchSize = ParameterUtilTool.findParameterType(node, "BATCH_SIZE");
+                ElementParameterType maxReturn = ParameterUtilTool.findParameterType(node, "MAX_RETURN");
+                Object batchSizeValue = ParameterUtilTool.convertParameterValue(batchSize);
                 if (operation != null && leadSelector != null) {
                     Object operationValue = ParameterUtilTool.convertParameterValue(operation);
                     Object leadSelectorValue = ParameterUtilTool.convertParameterValue(leadSelector);
-                    if ( !("getMutipleLeads".equals(String.valueOf(operationValue))      &&
-                           "LeadKeySelector".equals(String.valueOf(leadSelectorValue)) ) &&
-                            batchSize != null) {
-                         Object batchSizeValue = ParameterUtilTool.convertParameterValue(batchSize);
+                    Object maxReturnValue = ParameterUtilTool.convertParameterValue(maxReturn);
+                    if ("getMutipleLeads".equals(String.valueOf(operationValue))
+                            && "LeadKeySelector".equals(String.valueOf(leadSelectorValue)) && maxReturnValue != null) {
+                        paramType.setValue(String.valueOf(maxReturnValue));
+                    } else {
                         paramType.setValue(String.valueOf(batchSizeValue));
                     }
-                }
-            }
-            // BATCH_SIZE should be taken in account when OPERATION!=getMutipleLeads and LEAD_SELECTOR!=LeadKeySelector
-            // Otherwise, we feed the value with MAX_RETURN.
-            if ("BATCH_SIZE".equals(paramName) && "tMarketoInput".equals(componentName)) {
-                ElementParameterType operation = ParameterUtilTool.findParameterType(node, "OPERATION");
-                ElementParameterType leadSelector = ParameterUtilTool.findParameterType(node, "LEAD_SELECTOR");
-                ElementParameterType maxReturn = ParameterUtilTool.findParameterType(node, "MAX_RETURN");
-                if (operation != null && leadSelector != null) {
-                   Object operationValue = ParameterUtilTool.convertParameterValue(operation);
-                   Object leadSelectorValue = ParameterUtilTool.convertParameterValue(leadSelector);
-                   if ("getMutipleLeads".equals(String.valueOf(operationValue)) &&
-                       "LeadKeySelector".equals(String.valueOf(leadSelectorValue)) &&
-                        maxReturn != null) {
-                        Object maxReturnValue = ParameterUtilTool.convertParameterValue(maxReturn);
-                        paramType.setValue(String.valueOf(maxReturnValue));
-                   }
+                } else {
+                    paramType.setValue(String.valueOf(batchSizeValue));
                 }
             }
         }
