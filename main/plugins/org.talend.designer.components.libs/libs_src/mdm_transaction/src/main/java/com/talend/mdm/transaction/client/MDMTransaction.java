0418ab9a11f542b4fc004344b0375826820806e2
Pingwen Lin <pwlin@talend.com>
2017-05-24 14:22:46 +0800

    TPS-1404 [5.6.1]MDM Transaction handling is wrong in multithreaded environment(TMDM-9530) (#1302)
    * TPS-1404 : [5.6.1]MDM Transaction handling is wrong in multithreaded environment(TMDM-9530)
    TMDM-9111 : MDM transaction handling is wrong in multithreaded environment
    TMDM-9530 : throw error 'Failed to acquire lock within 30 seconds' while using mdm transaction in the

---
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransaction.java
@@ -0,0 +1,109 @@
+package com.talend.mdm.transaction.client;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.DeleteMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+public class MDMTransaction {
+
+    public static final String JVM_STICKY_SESSION = "sticky_session"; //$NON-NLS-1$
+    public static final String DEFAULT_STICKY_SESSION = "JSESSIONID"; //$NON-NLS-1$
+
+    private String url;
+    private String id;
+    private String username;
+    private String password;
+    private String sessionId;
+
+    public void commit() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+        client.getParams().setAuthenticationPreemptive(true);
+
+        HttpMethod method = new PostMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            method.setRequestHeader("Cookie", getStickySession() + "=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
+        }
+
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException("Commit failed. The commit operation has returned the code " + statuscode + ".");
+        }
+    }
+
+    public void rollback() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY,
+                new UsernamePasswordCredentials(username, password));
+        client.getParams().setAuthenticationPreemptive(true);
+
+        HttpMethod method = new DeleteMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            method.setRequestHeader("Cookie", getStickySession() + "=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
+        }
+
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException("Rollback failed. The rollback operation has returned the code " + statuscode + ".");
+        }
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public static String getStickySession() {
+        String stickySession = System.getProperty(JVM_STICKY_SESSION);
+        if(stickySession == null) {
+            stickySession = DEFAULT_STICKY_SESSION;
+        }
+        return stickySession;
+    }
+}
---
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransactionClient.java
@@ -0,0 +1,132 @@
+package com.talend.mdm.transaction.client;
+
+import java.io.IOException;
+
+import org.apache.commons.httpclient.Header;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.UsernamePasswordCredentials;
+import org.apache.commons.httpclient.auth.AuthScope;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PutMethod;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class MDMTransactionClient {
+
+    private static final Log LOG = LogFactory.getLog(MDMTransactionClient.class);
+
+    public static MDMTransaction newTransaction(String url, String username, String password) throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        client.getParams().setAuthenticationPreemptive(true);
+
+        PutMethod put = new PutMethod(url);
+        put.setDoAuthentication(true);
+        String tid;
+        String sessionID;
+        try {
+            client.executeMethod(put);
+            tid = put.getResponseBodyAsString();
+            sessionID = parseSessionID(put);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            put.releaseConnection();
+        }
+
+        MDMTransaction result = new MDMTransaction();
+        result.setUrl(url);
+        result.setId(tid);
+        result.setUsername(username);
+        result.setPassword(password);
+        result.setSessionId(sessionID);
+
+        return result;
+    }
+
+    public static String getSessionID(String url, String username, String password) throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+        client.getParams().setAuthenticationPreemptive(true);
+        GetMethod get = new GetMethod(url);
+        get.setDoAuthentication(true);
+        String sessionID;
+        try {
+            client.executeMethod(get);
+            sessionID = parseSessionID(get);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            get.releaseConnection();
+        }
+        return sessionID;
+    }
+
+    private static String parseSessionID(HttpMethod method) {
+        String sessionID = null;
+        String stickySession = MDMTransaction.getStickySession();
+        Header[] setCookie = method.getResponseHeaders("Set-Cookie"); //$NON-NLS-1$
+        for(Header header : setCookie) {
+            String headerValue = header.getValue();
+            if(headerValue.startsWith(stickySession + "=")) { //$NON-NLS-1$
+                int beginIndex = (stickySession + "=").length(); //$NON-NLS-1$
+                int endIndex = headerValue.indexOf(";", beginIndex); //$NON-NLS-1$
+                sessionID = headerValue.substring(beginIndex, endIndex);
+                break;
+            }
+        }
+        if(sessionID == null) {
+            if(LOG.isDebugEnabled()) {
+                LOG.warn("Cookie for sticky session not found!"); //$NON-NLS-1$
+            }
+            sessionID = ""; //$NON-NLS-1$
+        }
+        return sessionID;
+    }
+
+    public static String getMDMTransactionURL(String url) {
+        if(url == null || "".equals(url)) {
+            return "";
+        }
+
+        int count = 3;
+        int i=0;
+        for(;i<url.length();i++) {
+            char c = url.charAt(i);
+            if('/' == c) {
+                count--;
+            }
+            if(count == 0) {
+                break;
+            }
+        }
+
+        String result = url.substring(0, i);
+        result += "/datamanager/services/transactions"; //$NON-NLS-1$
+
+        return result;
+    }
+
+    @SuppressWarnings("nls")
+    public static void main(String[] args) throws IOException {
+        String sessionID = MDMTransactionClient.getSessionID("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator");
+        System.out.println(sessionID);
+
+        MDMTransaction mt = MDMTransactionClient.newTransaction("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator");
+        mt.commit();
+
+        MDMTransaction mt1 = MDMTransactionClient.newTransaction("http://localhost:8000/datamanager/services/transactions", "administrator", "administrator");
+        mt1.rollback();
+
+        String url = "http://localhost:8000/talend/TalendPort";
+        String mdmurl = MDMTransactionClient.getMDMTransactionURL(url);
+        System.out.println(mdmurl);
+    }
+
+}
