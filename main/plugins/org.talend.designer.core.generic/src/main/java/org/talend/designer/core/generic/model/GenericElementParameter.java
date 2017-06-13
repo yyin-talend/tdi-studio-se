8b4d89c43684910c546a89f5b29c67e34cbd7d4a
cmeng-talend <cmeng@talend.com>
2017-06-13 11:42:12 +0800

    fix(TUP-16516): after change context group under repository re-import the whole project again will get error https://jira.talendforge.org/browse/TUP-16516
    Conflicts:
    	main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/GenericElementParameter.java
    
    Conflicts:
    	main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/GenericElementParameter.java

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/GenericElementParameter.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/GenericElementParameter.java
@@ -40,6 +40,7 @@ import org.talend.core.model.process.EConnectionType;
 import org.talend.core.model.process.EParameterFieldType;
 import org.talend.core.model.process.IConnection;
 import org.talend.core.model.process.IElement;
+import org.talend.core.model.process.IGenericElementParameter;
 import org.talend.core.model.process.INode;
 import org.talend.core.model.process.INodeConnector;
 import org.talend.core.model.process.IProcess;
@@ -61,7 +62,7 @@ import org.talend.designer.core.ui.editor.nodes.Node;
  * created by ycbai on 2015年9月24日 Detailled comment
  *
  */
-public class GenericElementParameter extends ElementParameter {
+public class GenericElementParameter extends ElementParameter implements IGenericElementParameter {
 
     private ComponentProperties rootProperties;
 
@@ -527,7 +528,8 @@ public class GenericElementParameter extends ElementParameter {
             property.setTaggedValue(IGenericConstants.IS_DYNAMIC, mode);
         }
     }
-    
+
+    @Override
     public void setAskPropagate(Boolean askPropagate) {
         this.askPropagate = askPropagate;
     }
