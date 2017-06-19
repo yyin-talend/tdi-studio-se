99588773d09cea74ae2880e2d237bab2ed6a373c
zwxue <zwxue@talend.com>
2017-06-19 11:55:44 +0800

    fix(TUP-17726):java version for reference project changed to 1.8 after (#1404)
    migration.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddJavaVersionMigrationTask.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddJavaVersionMigrationTask.java
@@ -15,6 +15,7 @@ package org.talend.repository.model.migration;
 import java.util.Date;
 import java.util.GregorianCalendar;
 
+import org.apache.commons.lang.StringUtils;
 import org.eclipse.jdt.core.JavaCore;
 import org.talend.commons.utils.generation.JavaUtils;
 import org.talend.core.model.general.Project;
@@ -26,9 +27,9 @@ public class AddJavaVersionMigrationTask extends AbstractProjectMigrationTask {
 
     @Override
     public ExecutionResult execute(Project project) {
-        String javaVersion = JavaUtils.getProjectJavaVersion();
-        if (javaVersion == null || !JavaUtils.AVAILABLE_VERSIONS.contains(javaVersion)) {
-            ProjectPreferenceManager manager = new ProjectPreferenceManager(project, CoreRuntimePlugin.PLUGIN_ID);
+        ProjectPreferenceManager manager = new ProjectPreferenceManager(project, CoreRuntimePlugin.PLUGIN_ID);
+        String javaVersion = manager.getValue(JavaUtils.PROJECT_JAVA_VERSION_KEY);
+        if (StringUtils.isBlank(javaVersion) || !JavaUtils.AVAILABLE_VERSIONS.contains(javaVersion)) {
             manager.setValue(JavaUtils.PROJECT_JAVA_VERSION_KEY, JavaCore.VERSION_1_7);
             manager.save();
             return ExecutionResult.SUCCESS_NO_ALERT;
