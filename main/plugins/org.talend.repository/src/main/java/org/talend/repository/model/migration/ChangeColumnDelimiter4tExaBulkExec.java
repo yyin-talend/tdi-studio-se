6c03b8c9dcf84426c54f012ae1160e622f75075d
wang wei <wwang@talend.com>
2017-07-04 11:23:26 +0800

    fix(TDI-39087): tExaBulkExec can not have empty string as field delimiter (#1480)
    * fix(TDI-39087): tExaBulkExec can not have empty string as field
    delimiter
    
    * fix(TDI-39087): fix the version

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ChangeColumnDelimiter4tExaBulkExec.java
@@ -0,0 +1,84 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2016 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class ChangeColumnDelimiter4tExaBulkExec extends AbstractJobMigrationTask {
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org.talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
+        if (processType == null) {
+            return ExecutionResult.NOTHING_TO_DO;
+        }
+        String[] componentsName = new String[] { "tEXABulkExec" }; //$NON-NLS-1$
+
+        try {
+
+            for (String element : componentsName) {
+                IComponentFilter filter = new NameComponentFilter(element);
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(new IComponentConversion() {
+
+                            @Override
+                            public void transform(NodeType node) {
+                                ElementParameterType ept = ComponentUtilities.getNodeProperty(node, "COLUMN_DELIMITER");//$NON-NLS-1$
+                                if (ept != null) {
+                                    if ("\"\"".equals(ept.getValue())) {//$NON-NLS-1$
+                                        ComponentUtilities.setNodeValue(node, "COLUMN_DELIMITER", "\"\\\"\""); //$NON-NLS-1$//$NON-NLS-2$
+                                    }
+                                }
+                            }
+
+                        }));
+            }
+
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
+
+    /*
+     * (non-Javadoc)
+     * 
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2017, 6, 4, 11, 0, 0);
+        return gc.getTime();
+    }
+
+}
