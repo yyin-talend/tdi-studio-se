b94052d7fb81aae9f4997d0349462814a98ec1b0
Dmytro Chmyga <dmytro.chmyga@synapse.com>
2017-11-06 07:32:13 +0200

    Revert "feat(TCOMP-291):property value from current record (#1366)"
    This reverts commit 47836512adc6b21c5326a8c42594221b210816e5.

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/constants/IGenericConstants.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/constants/IGenericConstants.java
@@ -66,6 +66,5 @@ public interface IGenericConstants {
      */
     public static final String LINE_SEPARATOR_REPLACED_TO = "LINE_SEPARATOR_REPLACED_TO";//$NON-NLS-1$
 
-    public static final String DYNAMIC_PROPERTY_VALUE = "DYNAMIC_PROPERTY_VALUE"; //$NON-NLS-1$
 
 }
