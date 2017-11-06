476ae1efef7e6a3b8bf7081159788f474f969942
wchen-talend <wchen@talend.com>
2017-11-06 10:08:52 +0800

    fix(TUP-18738):UHG - Configuring SVN Polling from TAC (#1681)
    * fix(TUP-18738):UHG - Configuring SVN Polling from TAC
    
    * fix(TUP-18738):UHG - Configuring SVN Polling from TAC

---
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/preferences/PreferencePreInitailizeTask.java
@@ -0,0 +1,49 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2017 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.core.ui.preferences;
+
+import java.lang.reflect.InvocationTargetException;
+
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.prefs.ITalendCorePrefConstants;
+import org.talend.designer.core.DesignerPlugin;
+import org.talend.login.AbstractLoginTask;
+
+/**
+ * 
+ * DOC wchen class global comment. Detailled comment
+ */
+public class PreferencePreInitailizeTask extends AbstractLoginTask {
+
+    @Override
+    public void run(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
+        IPreferenceStore store = DesignerPlugin.getDefault().getPreferenceStore();
+        String pAutoCheck = System.getProperty("svn.update.info.check");
+        if (pAutoCheck != null) {
+            boolean enableAutoCheckSvn = Boolean.valueOf(pAutoCheck);
+            store.setValue(ITalendCorePrefConstants.SVN_UPDATE_INFO_AUTO_CHECK, enableAutoCheckSvn);
+        }
+        String pInterval = System.getProperty("svn.update.info.interval");
+        if (pInterval != null) {
+            try {
+                int svnCheckInterval = Integer.parseInt(pInterval);
+                store.setValue(ITalendCorePrefConstants.SVN_UPDATE_INFO_AUTO_CHECK_TIME_INTERVAL, svnCheckInterval);
+            } catch (NumberFormatException e) {
+                ExceptionHandler.process(e);
+            }
+        }
+    }
+
+}
