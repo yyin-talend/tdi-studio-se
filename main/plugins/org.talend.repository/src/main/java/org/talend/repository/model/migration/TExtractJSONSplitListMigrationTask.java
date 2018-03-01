501f300877b6f9bea79673a3104172558d4304c3
pyzhou <pyzhou@talend.com>
2018-03-01 14:28:03 +0800

    fix(TDI-34082):tExtractJsonField split list. (#2016)

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/TExtractJSONSplitListMigrationTask.java
@@ -0,0 +1,73 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.ui.runtime.exception.ExceptionHandler;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+/**
+ * DOC Administrator class global comment. Detailled comment
+ */
+public class TExtractJSONSplitListMigrationTask extends AbstractJobMigrationTask {
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
+        if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
+            return ExecutionResult.NOTHING_TO_DO;
+        }
+        IComponentFilter filter = new NameComponentFilter("tExtractJSONFields");
+        try {
+            ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays
+                    .<IComponentConversion> asList(new IComponentConversion() {
+
+                        public void transform(NodeType node) {
+                            if (ComponentUtilities.getNodeProperty(node, "SPLIT_LIST") == null) {//$NON-NLS-1$
+                                ComponentUtilities.addNodeProperty(node, "SPLIT_LIST", "CHECK");//$NON-NLS-1$
+                                ElementParameterType splitList = ComponentUtilities.getNodeProperty(node,
+                                        "SPLIT_LIST"); //$NON-NLS-1$
+                                splitList.setValue("false");
+                            }
+
+             
+                        }
+                    }));
+        } catch (PersistenceException e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+
+        return ExecutionResult.SUCCESS_NO_ALERT;
+
+    }
+
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2018, 2, 1, 11, 0, 0);
+        return gc.getTime();
+    }
+}
