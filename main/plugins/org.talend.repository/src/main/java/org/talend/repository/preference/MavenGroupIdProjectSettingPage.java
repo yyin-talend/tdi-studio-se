b60aec3a407f914c97c834f59ac6adb5564d976c
zwxue <zwxue@talend.com>
2018-03-12 09:53:36 +0800

    fix(TUP-19458):project groupId may keep to empty. (#2064)

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/MavenGroupIdProjectSettingPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/MavenGroupIdProjectSettingPage.java
@@ -12,14 +12,16 @@
 // ============================================================================
 package org.talend.repository.preference;
 
-import org.eclipse.jface.preference.BooleanFieldEditor;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.events.ModifyEvent;
 import org.eclipse.swt.events.ModifyListener;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Point;
 import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.swt.widgets.Text;
@@ -40,11 +42,17 @@ public class MavenGroupIdProjectSettingPage extends AbstractProjectSettingPage {
 
     private Text groupIdText;
 
-    BooleanFieldEditor appendFolderButton;
+    private Button appendFolderButton;
+
+    private Label skipGroupIdLabel;
+
+    private Button skipGroupIdButton;
 
     private String oldGroupId;
 
     private boolean oldAppendFolder;
+
+    private boolean oldSkipGroupId;
 
     public MavenGroupIdProjectSettingPage() {
         noDefaultAndApplyButton();
@@ -60,17 +68,13 @@ public class MavenGroupIdProjectSettingPage extends AbstractProjectSettingPage {
         Composite parent = getFieldEditorParent();
         parent.setLayout(new GridLayout());
 
-        Composite compsite = new Composite(parent, SWT.NONE);
-        compsite.setLayout(new GridLayout(2, false));
+        Composite textCompsite = new Composite(parent, SWT.NONE);
+        textCompsite.setLayout(new GridLayout(2, false));
 
-        Label label = new Label(compsite, SWT.NONE);
-        label.setText(Messages.getString("MavenGroupIdProjectSettingPage.groupIdLabel")); //$NON-NLS-1$
-        GridData labelData = new GridData();
-        label.setLayoutData(labelData);
+        Label groupIdLabel = new Label(textCompsite, SWT.NONE);
+        groupIdLabel.setText(Messages.getString("MavenGroupIdProjectSettingPage.groupIdLabel")); //$NON-NLS-1$
 
-        groupIdText = new Text(compsite, SWT.BORDER);
-        oldGroupId = getPreferenceStore().getString(MavenConstants.PROJECT_GROUPID);
-        groupIdText.setText(oldGroupId);
+        groupIdText = new Text(textCompsite, SWT.BORDER);
         GC gc = new GC(groupIdText);
         String defaultGroupId = "org.example." + ProjectManager.getInstance().getCurrentProject().getTechnicalLabel(); //$NON-NLS-1$
         Point labelSize = gc.stringExtent(defaultGroupId);
@@ -79,13 +83,45 @@ public class MavenGroupIdProjectSettingPage extends AbstractProjectSettingPage {
         GridData textData = new GridData();
         textData.widthHint = hint;
         groupIdText.setLayoutData(textData);
+
+        Composite checkComposite = new Composite(parent, SWT.NONE);
+        checkComposite.setLayout(new GridLayout(2, false));
+
+        appendFolderButton = new Button(checkComposite, SWT.CHECK);
+        Label appendFolderButtonLabel = new Label(checkComposite, SWT.NONE);
+        appendFolderButtonLabel.setText(Messages.getString("MavenGroupIdProjectSettingPage.appendFolderLabel")); //$NON-NLS-1$
+
+        skipGroupIdButton = new Button(checkComposite, SWT.CHECK);
+        skipGroupIdLabel = new Label(checkComposite, SWT.NONE);
+        skipGroupIdLabel.setText(Messages.getString("MavenGroupIdProjectSettingPage.skipGroupIdLabel")); //$NON-NLS-1$
+
+        initFields();
+        addListeners();
+
+    }
+
+    private void initFields() {
+        oldGroupId = getPreferenceStore().getString(MavenConstants.PROJECT_GROUPID);
+        groupIdText.setText(oldGroupId);
+
+        oldAppendFolder = getPreferenceStore().getBoolean(MavenConstants.APPEND_FOLDER_TO_GROUPID);
+        appendFolderButton.setSelection(oldAppendFolder);
+
+        oldSkipGroupId = getPreferenceStore().getBoolean(MavenConstants.SKIP_BASE_GROUPID);
+        skipGroupIdButton.setSelection(oldSkipGroupId);
+
+        skipGroupIdButton.setEnabled(oldAppendFolder);
+        skipGroupIdLabel.setEnabled(oldAppendFolder);
+    }
+
+    private void addListeners() {
         groupIdText.addModifyListener(new ModifyListener() {
 
             @Override
             public void modifyText(ModifyEvent e) {
                 String text = groupIdText.getText();
                 if (!PomIdsHelper.isValidGroupId(text)) {
-                    setErrorMessage(Messages.getString("MavenGroupIdProjectSettingPage.groupIdErrMsg"));
+                    setErrorMessage(Messages.getString("MavenGroupIdProjectSettingPage.groupIdErrMsg")); //$NON-NLS-1$
                     setValid(false);
                 } else {
                     setErrorMessage(null);
@@ -95,19 +131,41 @@ public class MavenGroupIdProjectSettingPage extends AbstractProjectSettingPage {
             }
         });
 
-        oldAppendFolder = getPreferenceStore().getBoolean(MavenConstants.APPEND_FOLDER_TO_GROUPID);
-        appendFolderButton = new BooleanFieldEditor(MavenConstants.APPEND_FOLDER_TO_GROUPID,
-                Messages.getString("MavenGroupIdProjectSettingPage.appendFolderLabel"), parent); //$NON-NLS-1$
-        addField(appendFolderButton);
+        appendFolderButton.addSelectionListener(new SelectionAdapter() {
 
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                skipGroupIdButton.setEnabled(appendFolderButton.getSelection());
+                skipGroupIdLabel.setEnabled(appendFolderButton.getSelection());
+            }
+
+        });
     }
 
     @Override
     public boolean performOk() {
-        if (groupIdText != null && !groupIdText.isDisposed() && appendFolderButton != null) {
-            if (!oldGroupId.equals(groupIdText.getText()) || oldAppendFolder != appendFolderButton.getBooleanValue()) {
-                getPreferenceStore().setValue(MavenConstants.PROJECT_GROUPID, groupIdText.getText());
-                getPreferenceStore().setValue(MavenConstants.APPEND_FOLDER_TO_GROUPID, appendFolderButton.getBooleanValue());
+        if (groupIdText != null && !groupIdText.isDisposed()) {
+            String groupId = groupIdText.getText();
+            boolean appendFolder = appendFolderButton.getSelection();
+            boolean skipGroupId = skipGroupIdButton.getSelection();
+            boolean needSync = false;
+            if (!oldGroupId.equals(groupId)) {
+                getPreferenceStore().setValue(MavenConstants.PROJECT_GROUPID, groupId);
+                needSync = true;
+            }
+            if (oldAppendFolder != appendFolder) {
+                getPreferenceStore().setValue(MavenConstants.APPEND_FOLDER_TO_GROUPID, appendFolder);
+                needSync = true;
+            }
+            if (oldSkipGroupId != skipGroupId) {
+                if (!appendFolder) {
+                    getPreferenceStore().setValue(MavenConstants.SKIP_BASE_GROUPID, false);
+                } else {
+                    getPreferenceStore().setValue(MavenConstants.SKIP_BASE_GROUPID, skipGroupId);
+                    needSync = true;
+                }
+            }
+            if (needSync) {
                 try {
                     new AggregatorPomsHelper().syncAllPoms();
                 } catch (Exception e) {
