022cac129b75123f6ea1f547fc7a6353a0e0e73b
pyzhou-talend <pyzhou@talend.com>
2018-03-12 10:36:08 +0800

    fix(TDI-40187): Change Salesforce620Migration

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/Salesforce620Migration.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/Salesforce620Migration.java
@@ -12,17 +12,22 @@
 // ============================================================================
 package org.talend.designer.core.generic.model.migration;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Date;
 import java.util.GregorianCalendar;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.lang.ArrayUtils;
 import org.talend.commons.exception.ExceptionHandler;
 import org.talend.commons.exception.PersistenceException;
 import org.talend.components.api.properties.ComponentProperties;
+import org.talend.components.common.UserPasswordProperties;
 import org.talend.core.language.ECodeLanguage;
 import org.talend.core.model.components.ComponentUtilities;
 import org.talend.core.model.components.ModifyComponentsAction;
@@ -39,13 +44,15 @@ import org.talend.daikon.NamedThing;
 import org.talend.daikon.properties.Properties;
 import org.talend.daikon.properties.property.EnumProperty;
 import org.talend.daikon.properties.property.Property;
-import org.talend.daikon.properties.property.PropertyValueEvaluator;
-import org.talend.daikon.serialize.PostDeserializeSetup;
-import org.talend.daikon.serialize.SerializerDeserializer;
 import org.talend.designer.core.generic.utils.ComponentsUtils;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 /**
  * created by nrousseau on May 25, 2016 Detailled comment
@@ -85,47 +92,28 @@ public class Salesforce620Migration extends AbstractJobMigrationTask {
             public void transform(NodeType node) {
                 ElementParameterType elemParamType = ComponentUtilities.getNodeProperty(node, "PROPERTIES");
                 String propertiesString = elemParamType.getValue();
-                SerializerDeserializer.Deserialized<ComponentProperties> fromSerialized = Properties.Helper.fromSerializedPersistent(propertiesString,
-                        ComponentProperties.class, new PostDeserializeSetup() {
+                
+//				Map propertiesMap = new HashMap();
+				// extractProperties(propertiesMap,propertiesString);
+				ObjectMapper mapper = new ObjectMapper();
+				ComponentProperties newProperties = ComponentsUtils.getComponentProperties(node.getComponentName());
+				
+				
+				try {
+					JsonNode propertiesJson = mapper.readTree(propertiesString);
+					copyProperties(newProperties, propertiesJson);
+				} catch (JsonProcessingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 
-                            @Override
-                            public void setup(Object properties) {
-                                ((Properties)properties).setValueEvaluator(new PropertyValueEvaluator() {
 
-                                    @Override
-                                    public Object evaluate(Property property, Object storedValue) {
-                                        if (storedValue instanceof String) {
-                                            if (GenericTypeUtils.isEnumType(property)) {
-                                                ComponentProperties newProperties = ComponentsUtils.getComponentProperties(node
-                                                        .getComponentName());
-
-                                                Property newProperty = (Property) newProperties.getProperty(property.getName());
-                                                if (newProperty == null) {
-                                                    newProperty = (Property) newProperties.getProperty("connection.loginType");
-                                                }
-                                                if (newProperty != null) {
-                                                    List<?> propertyPossibleValues = ((Property<?>) newProperty)
-                                                            .getPossibleValues();
-                                                    if (propertyPossibleValues != null) {
-                                                        for (Object possibleValue : propertyPossibleValues) {
-                                                            if (possibleValue.toString().equals(storedValue)) {
-                                                                property.setStoredValue(possibleValue);
-                                                                return possibleValue;
-                                                            }
-                                                        }
-                                                    }
-                                                }
-                                            }
-                                        }
-                                        return storedValue;
-                                    }
-
-                                });
-                            }
-                        });
-                ComponentProperties newProperties = ComponentsUtils.getComponentProperties(node.getComponentName());
-                updateSubProperties(fromSerialized.object, newProperties);
-                newProperties.copyValuesFrom(fromSerialized.object, true, false);
+				System.out.println("###END");
+                
+                
                 NamedThing nt = newProperties.getProperty("module.moduleName");
                 if (nt != null && nt instanceof Property) {
                     Property moduleNameProperty = (Property) nt;
@@ -178,6 +166,116 @@ public class Salesforce620Migration extends AbstractJobMigrationTask {
         }
         return ExecutionResult.SUCCESS_WITH_ALERT;
 
+    }
+    
+	private Object extractProperties(String name, JsonNode propertiesJson) {
+		if (!propertiesJson.isMissingNode()) {
+			if (!propertiesJson.path("type").isMissingNode()) {
+				String type = propertiesJson.path("type").path("name").asText();
+
+
+				JsonNode storedValue = propertiesJson.path("storedValue");
+				//
+				System.out.println(type+": "+storedValue);
+				
+				switch (type) {
+				case "STRING":
+					return storedValue.asText();
+				case "BOOLEAN":
+					return storedValue.path("value").asBoolean(false);
+				case "INT":
+					return storedValue.asInt();
+				case "SCHEMA":
+					// todo
+					break;
+				default://ENUM type
+					return storedValue.asText();
+				}
+			}
+		}
+
+		return null;  
+    	
+    }
+	
+	private void deseralized(){
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);  
+		mapper.disable(DeserializationFeature.FAIL_ON_UNRESOLVED_OBJECT_IDS);  
+		mapper.disable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE);
+		mapper.disable(DeserializationFeature.FAIL_ON_READING_DUP_TREE_KEY);
+//		try {
+////			Class type = Class.forName(className)
+////			ComponentProperties cp = mapper.readValue(propertiesString, newProperties.getClass());
+//			
+////			System.out.println(cp.getName());
+//		} catch (IOException e1) {
+//			// TODO Auto-generated catch block
+//			e1.printStackTrace();
+//		}
+	}
+    
+    private void copyProperties(Properties newProperties,JsonNode json) throws JsonProcessingException, IOException{
+		if (newProperties == null || json.isMissingNode()) {
+			return;
+		}
+        List<NamedThing> Propertlist = newProperties.getProperties();
+        for (NamedThing property : Propertlist) {
+        	String propertyName = property.getName();
+        	System.out.println("propertyName: "+propertyName);
+
+        	if (property instanceof Property) {
+        		//
+
+                
+                JsonNode jsonNode = json.path(propertyName);
+                Object storeValue = extractProperties(property.getName(),jsonNode);
+
+                evaluate((Property)property,storeValue);
+                	
+        	}else if (property instanceof ComponentProperties) {
+        		copyProperties((Properties)property,json.path(propertyName));
+        		
+            }else if (property instanceof UserPasswordProperties){
+            	copyProperties((Properties)property,json.path(propertyName));
+            }
+		}
+    	
+    }
+    
+    
+    private void evaluate(Property property, Object storedValue) {
+    	if(storedValue==null){
+    		return ;
+    	}
+        if (storedValue instanceof String) {
+        	String parentName = property.getName();
+        	if("securityKey".equals(parentName)||"password".equals(parentName)){
+                String encrypt = (String)storedValue;
+                if (ContextParameterUtils.isContainContextParam(encrypt)) {
+                	encrypt = TalendQuoteUtils.removeQuotes(encrypt);
+                } else {
+                	encrypt = TalendQuoteUtils.addPairQuotesIfNotExist(encrypt);
+                }
+                property.setStoredValue(encrypt);
+                return;
+        		
+        	}
+            if (GenericTypeUtils.isEnumType(property)) {
+                    List<?> propertyPossibleValues = property.getPossibleValues();
+                    if (propertyPossibleValues != null) {
+                        for (Object possibleValue : propertyPossibleValues) {
+                            if (possibleValue.toString().equals(storedValue)) {
+                                property.setStoredValue(possibleValue);
+                                return ;
+                            }
+                        }
+                    }
+            
+            }
+        }
+        System.out.println("Value: "+storedValue);
+        property.setStoredValue(storedValue);
     }
 
     /**
