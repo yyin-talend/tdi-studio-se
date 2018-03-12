3ed6a8f9c309308be43ad25fbffa399ad50994b0
nrousseau <nrousseau@talend.com>
2018-03-12 10:30:13 +0800

    fix(TUP-19758) generate code even if the jars are missing (#2065)

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessorUtilities.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/java/JavaProcessorUtilities.java
@@ -466,7 +466,7 @@ public class JavaProcessorUtilities {
 
                     });
                 }
-            } else {
+            } else if (!Boolean.getBoolean("skip_missing_jars")) {
                 throw new ProcessorException(missingJars);
             }
 
