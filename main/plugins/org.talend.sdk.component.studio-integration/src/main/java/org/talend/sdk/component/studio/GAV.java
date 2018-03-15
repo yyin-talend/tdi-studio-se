5f68260ef354423f072fb3a985b84eded89b847d
Chao MENG <cmeng@talend.com>
2018-03-15 20:18:36 +0800

    upgrade TaCoKit version to 0.0.3 (#2088)
    * upgrade TaCoKit version to 0.0.2
    
    * upgrade TaCoKit version to 0.0.3

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/GAV.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/GAV.java
@@ -21,7 +21,7 @@ public interface GAV {
 
     String ARTIFACT_ID = "org.talend.sdk.component.studio-integration";
 
-    String COMPONENT_RUNTIME_VERSION = "0.0.1";
+    String COMPONENT_RUNTIME_VERSION = "0.0.3";
 
     String CLI_VERSION = "1.4";
 
