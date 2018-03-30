107027f20575d2d3a38111d9576081a1c615b9d3
Romain Manni-Bucau <rmannibucau@gmail.com>
2018-03-30 12:15:52 +0200

    ensure to use a unified regex syntax between the web and the studio for tacokit (#2163)

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/listener/PatternValidator.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/parameter/listener/PatternValidator.java
@@ -15,21 +15,30 @@
  */
 package org.talend.sdk.component.studio.model.parameter.listener;
 
-import java.util.regex.Pattern;
+import java.util.function.Predicate;
+import java.util.logging.Logger;
+
+import javax.script.Bindings;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 
 import org.talend.sdk.component.studio.i18n.Messages;
 import org.talend.sdk.component.studio.model.parameter.ValidationLabel;
 
 /**
- * Checks whether new value corresponds specified {@code regex}
+ * Checks whether new value corresponds specified {@code regex}.
+ *
+ * IMPORTANT: to ensure we respect our java/web constraint we run only js regex
+ * which are more limited than java ones.
  */
 public class PatternValidator extends PropertyValidator {
 
-    private final Pattern pattern;
+    private final JsRegex pattern;
 
     public PatternValidator(final ValidationLabel label, final String regex) {
         super(label, Messages.getString("validation.pattern.error", regex));
-        pattern = Pattern.compile(regex);
+        this.pattern = new JsRegex(regex);
     }
 
     @Override
@@ -37,8 +46,52 @@ public class PatternValidator extends PropertyValidator {
         if (newValue == null) {
             return false;
         }
-        String value = (String) newValue;
-        return pattern.matcher(value).matches();
+        return pattern.test(newValue.toString());
     }
 
+    @Override
+    public String toString() {
+        return pattern.regex;
+    }
+
+    private static class JsRegex implements Predicate<CharSequence> {
+        private static final ScriptEngine ENGINE;
+
+        static {
+            ENGINE = new ScriptEngineManager().getEngineByName("javascript");
+        }
+
+        private final String regex;
+        private final String indicators;
+
+        private JsRegex(final String regex) {
+            if (regex.startsWith("/") && regex.length() > 1) {
+                final int end = regex.lastIndexOf('/');
+                if (end < 0) {
+                    this.regex = regex;
+                    this.indicators = "";
+                } else {
+                    this.regex = regex.substring(1, end);
+                    this.indicators = regex.substring(end + 1);
+                }
+            } else {
+                this.regex = regex;
+                this.indicators = "";
+            }
+        }
+
+        @Override
+        public boolean test(final CharSequence string) {
+            final Bindings bindings = ENGINE.createBindings();
+            bindings.put("text", string);
+            bindings.put("regex", regex);
+            bindings.put("indicators", indicators);
+            try {
+                return Boolean.class.cast(ENGINE.eval("new RegExp(regex, indicators).test(text)", bindings));
+            } catch (final ScriptException e) {
+                Logger.getLogger(PatternValidator.class.getName()).warning(e.getMessage());
+                return false;
+            }
+        }
+    }
 }
---
+++ main/plugins/org.talend.sdk.component.studio-integration/src/test/java/org/talend/sdk/component/studio/model/parameter/listener/PatternValidatorTest.java
@@ -0,0 +1,76 @@
+/**
+ * Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.talend.sdk.component.studio.model.parameter.listener;
+
+import static java.util.Arrays.asList;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.Collection;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
+import org.talend.core.model.process.EParameterFieldType;
+import org.talend.sdk.component.studio.model.parameter.ValidationLabel;
+
+class PatternValidatorTest {
+
+    @Test
+    void simpleRegex() {
+        Stream.of(
+                new RegexCases("\\S+@\\S+\\.\\S+", asList("foo@bar.com", "test.thing@dummy.provider.com"),
+                        asList("wrong", "notgoodgmail.com")),
+                new RegexCases("^[a-zA-Z ]+$", asList("John Doe", "Some other Name"),
+                        asList("@bsolutely wrong", "Not a Name v3")))
+                .flatMap(base -> Stream.of(base, new RegexCases("/^" + base.regex + "$/", base.validTexts, base.invalidTexts)))
+                .forEach(Runnable::run);
+    }
+
+    @Test
+    void withFlags() {
+        new RegexCases("/[a-z]+/i", asList("foo", "FOO", "FoO"), asList("123", "@")).run();
+    }
+
+    private static class RegexCases implements Runnable {
+
+        private final PatternValidator regex;
+
+        private final Collection<String> validTexts;
+
+        private final Collection<String> invalidTexts;
+
+        private RegexCases(final String regex, final Collection<String> validTexts, final Collection<String> invalidTexts) {
+            this.regex = new PatternValidator(new ValidationLabel(null) {
+
+                @Override
+                public EParameterFieldType getFieldType() {
+                    return EParameterFieldType.LABEL;
+                }
+            }, regex);
+            this.validTexts = validTexts;
+            this.invalidTexts = invalidTexts;
+        }
+
+        @Override
+        public void run() {
+            validTexts.forEach(v -> validate(v, true));
+            invalidTexts.forEach(v -> validate(v, false));
+        }
+
+        private void validate(final String value, final boolean state) {
+            assertEquals(state, regex.validate(value), "Error in validating " + regex + " > " + value);
+        }
+    }
+}
