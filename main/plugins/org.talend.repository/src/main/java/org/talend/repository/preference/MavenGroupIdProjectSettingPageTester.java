6f309fc31f9b7e00cacf44d79f0a5f3d97a720a7
zwxue <zwxue@talend.com>
2018-04-09 10:48:12 +0800

    workitem(TUP-19959):Wrong content for CI project settings on TOS and (#2198)
    missing documentation for user

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/MavenGroupIdProjectSettingPageTester.java
@@ -0,0 +1,27 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2018 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.preference;
+
+import org.eclipse.core.runtime.IConfigurationElement;
+import org.eclipse.jface.preference.IPreferenceNode;
+import org.talend.core.PluginChecker;
+import org.talend.core.runtime.preference.IProjectSettingPageTester;
+
+public class MavenGroupIdProjectSettingPageTester implements IProjectSettingPageTester {
+
+    @Override
+    public boolean valid(IConfigurationElement element, IPreferenceNode node) {
+        return PluginChecker.isTIS();
+    }
+
+}
