e443b50e4e236372cea294e7757b8b2c0aeba579
Ivan Gonchar <gonchar-ivan@users.noreply.github.com>
2018-04-13 05:29:52 +0300

    Igonchar/tcomp 755 2 (#2226)
    * fix(TCOMP-755): remove unused TacokitConnector
    
    * fix(TCOMP-755): add IAdditionalInfo interface for TaCoKitNodeConnector
    
    * fix(TCOMP-755): set tacokit connector type in TaCoKitNodeConnector during creation

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/connector/TaCoKitNodeConnector.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/connector/TaCoKitNodeConnector.java
@@ -18,21 +18,29 @@ package org.talend.sdk.component.studio.model.connector;
 import static org.talend.core.model.process.EConnectionType.FLOW_MAIN;
 import static org.talend.core.model.process.EConnectionType.REJECT;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Objects;
 
 import org.talend.core.CorePlugin;
 import org.talend.core.model.process.EConnectionType;
 import org.talend.core.model.process.INode;
+import org.talend.core.runtime.IAdditionalInfo;
 import org.talend.designer.core.model.components.NodeConnector;
 import org.talend.sdk.component.studio.lang.Strings;
-
 
 /**
  * DOC cmeng class global comment. Detailled comment
  */
-public class TaCoKitNodeConnector extends NodeConnector {
+public class TaCoKitNodeConnector extends NodeConnector implements IAdditionalInfo {
     
     static final String DEFAULT = "__default__";
+    
+    private static final String TYPE = "CONNECTOR_TYPE";
+    
+    private static final String TACOKIT_TYPE = "tacokit";
+    
+    private final Map<String, Object> info = new HashMap<>();
 
     private boolean hasInput = false;
     
@@ -77,6 +85,7 @@ public class TaCoKitNodeConnector extends NodeConnector {
         setDefaultConnectionType(defaultConnectionType);
         addConnectionProperty(CorePlugin.getDefault() == null ? null : type, type.getRGB(),
                 type.getDefaultLineStyle());
+        putInfo(TYPE, TACOKIT_TYPE);
     }
     
     /**
@@ -144,4 +153,28 @@ public class TaCoKitNodeConnector extends NodeConnector {
     void setHasOutput(final boolean output) {
         hasOutput = output;
     }
+
+    @Override
+    public Object getInfo(String key) {
+        Objects.requireNonNull(key);
+        return info.get(key);
+    }
+
+    @Override
+    public void putInfo(String key, Object value) {
+        Objects.requireNonNull(key);
+        info.put(key, value);
+    }
+
+    @Override
+    public void onEvent(String event, Object... parameters) {
+        throw new UnsupportedOperationException();        
+    }
+
+    @Override
+    public void cloneAddionalInfoTo(IAdditionalInfo target) {
+        Objects.requireNonNull(target);
+        info.entrySet().stream().forEach(e -> target.putInfo(e.getKey(), e.getValue()));
+    }
+    
 }
