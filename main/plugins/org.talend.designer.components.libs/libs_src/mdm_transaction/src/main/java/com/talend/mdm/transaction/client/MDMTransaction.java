09b7364e8dd2c6420eab280f9e7b804d8667c1ef
Haiting Yin <htyin@talend.com>
2018-04-17 09:45:52 +0800

    TPS-2412 MDM transaction does not commit when multiple tMDMOutput used in same subjob (TMDM-11629) (#2229)

--- main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransaction.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransaction.java
@@ -1,6 +1,7 @@
 package com.talend.mdm.transaction.client;
 
 import java.io.IOException;
+import java.util.List;
 
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpException;
@@ -10,98 +11,96 @@ import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.DeleteMethod;
 import org.apache.commons.httpclient.methods.PostMethod;
 
+@SuppressWarnings("nls")
 public class MDMTransaction {
 
-    public static final String JVM_STICKY_SESSION = "sticky_session"; //$NON-NLS-1$
-    public static final String DEFAULT_STICKY_SESSION = "JSESSIONID"; //$NON-NLS-1$
+    private String url;
 
-	private String url;
-	private String id;
-	private String username;
-	private String password;
-	private String sessionId;
-	
-	public void commit() throws IOException {
-		HttpClient client = new HttpClient();
-		client.getState().setCredentials(AuthScope.ANY,
-				new UsernamePasswordCredentials(username, password));
+    private String id;
 
-		HttpMethod method = new PostMethod(url + "/" + id);
-		method.setDoAuthentication(true);
-		try {
-		    method.setRequestHeader("Cookie", getStickySession() + "=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
-			client.executeMethod(method);
-		} catch (HttpException e) {
-			throw e;
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			method.releaseConnection();
-		}
+    private String username;
 
-		int statuscode = method.getStatusCode();
-		if (statuscode >= 400) {
-		    throw new MDMTransactionException("Commit failed. The commit operation has returned the code " + statuscode + ".");
-		}
-	}
+    private String password;
 
-	public void rollback() throws IOException {
-		HttpClient client = new HttpClient();
-		client.getState().setCredentials(AuthScope.ANY,
-				new UsernamePasswordCredentials(username, password));
+    private List<String> cookies;
 
-		HttpMethod method = new DeleteMethod(url + "/" + id);
-		method.setDoAuthentication(true);
-		try {
-		    method.setRequestHeader("Cookie", getStickySession() + "=" + sessionId); //$NON-NLS-1$ //$NON-NLS-2$
-			client.executeMethod(method);
-		} catch (HttpException e) {
-			throw e;
-		} catch (IOException e) {
-			throw e;
-		} finally {
-			method.releaseConnection();
-		}
+    public void commit() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
 
-		int statuscode = method.getStatusCode();
-		if (statuscode >= 400) {
-		    throw new MDMTransactionException("Rollback failed. The rollback operation has returned the code " + statuscode + ".");
-		}
-	}
-	
-	public void setUrl(String url) {
-		this.url = url;
-	}
-
-	public void setId(String id) {
-		this.id = id;
-	}
-	
-	public String getId() {
-		return id;
-	}
-
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public String getSessionId() {
-        return sessionId;
-    }
-
-    public void setSessionId(String sessionId) {
-        this.sessionId = sessionId;
-    }
-
-	public static String getStickySession() {
-        String stickySession = System.getProperty(JVM_STICKY_SESSION);
-        if(stickySession == null) {
-            stickySession = DEFAULT_STICKY_SESSION;
+        HttpMethod method = new PostMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            for (String cookie : cookies) {
+                method.addRequestHeader("Cookie", cookie);
+            }
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
         }
-        return stickySession;
+
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException("Commit failed. The commit operation has returned the code " + statuscode + ".");
+        }
     }
+
+    public void rollback() throws IOException {
+        HttpClient client = new HttpClient();
+        client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        HttpMethod method = new DeleteMethod(url + "/" + id);
+        method.setDoAuthentication(true);
+        try {
+            for (String cookie : cookies) {
+                method.addRequestHeader("Cookie", cookie);
+            }
+            client.executeMethod(method);
+        } catch (HttpException e) {
+            throw e;
+        } catch (IOException e) {
+            throw e;
+        } finally {
+            method.releaseConnection();
+        }
+
+        int statuscode = method.getStatusCode();
+        if (statuscode >= 400) {
+            throw new MDMTransactionException(
+                    "Rollback failed. The rollback operation has returned the code " + statuscode + ".");
+        }
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<String> getCookies() {
+        return cookies;
+    }
+
+    public void setCookies(List<String> cookies) {
+        this.cookies = cookies;
+    }
+
 }
