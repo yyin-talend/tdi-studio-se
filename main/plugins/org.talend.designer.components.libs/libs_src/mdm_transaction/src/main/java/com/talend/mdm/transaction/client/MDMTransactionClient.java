cd705075ea57f38481071c1d842366a37243eb78
Haiting Yin <htyin@talend.com>
2018-04-20 10:55:29 +0800

    TPS-2412 MDM transaction does not commit when multiple tMDMOutput used in same subjob (TMDM-11629) (#2229) (#2238)

--- main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransactionClient.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/mdm_transaction/src/main/java/com/talend/mdm/transaction/client/MDMTransactionClient.java
@@ -1,6 +1,8 @@
 package com.talend.mdm.transaction.client;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
@@ -10,12 +12,9 @@ import org.apache.commons.httpclient.UsernamePasswordCredentials;
 import org.apache.commons.httpclient.auth.AuthScope;
 import org.apache.commons.httpclient.methods.GetMethod;
 import org.apache.commons.httpclient.methods.PutMethod;
-import org.apache.commons.logging.Log;
-import org.apache.commons.logging.LogFactory;
 
+@SuppressWarnings("nls")
 public class MDMTransactionClient {
-
-    private static final Log LOG = LogFactory.getLog(MDMTransactionClient.class);
 
     public static MDMTransaction newTransaction(String url, String username, String password) throws IOException {
         HttpClient client = new HttpClient();
@@ -25,11 +24,11 @@ public class MDMTransactionClient {
         PutMethod put = new PutMethod(url);
         put.setDoAuthentication(true);
         String tid;
-        String sessionID;
+        List<String> cookies;
         try {
             client.executeMethod(put);
             tid = put.getResponseBodyAsString();
-            sessionID = parseSessionID(put);
+            cookies = parseCookies(put);
         } catch (HttpException e) {
             throw e;
         } catch (IOException e) {
@@ -43,30 +42,30 @@ public class MDMTransactionClient {
         result.setId(tid);
         result.setUsername(username);
         result.setPassword(password);
-        result.setSessionId(sessionID);
+        result.setCookies(cookies);
 
         return result;
     }
 
     public static String getMDMTransactionURL(String url, boolean isNewServer) {
-        if(url == null || "".equals(url)) {
+        if (url == null || "".equals(url)) {
             return "";
         }
 
         int count = 3;
-        int i=0;
-        for(;i<url.length();i++) {
+        int i = 0;
+        for (; i < url.length(); i++) {
             char c = url.charAt(i);
-            if('/' == c) {
+            if ('/' == c) {
                 count--;
             }
-            if(count == 0) {
+            if (count == 0) {
                 break;
             }
         }
 
         String result = url.substring(0, i);
-        if(isNewServer){
+        if (isNewServer) {
             result += "/talendmdm/services/rest/transactions";
         } else {
             result += "/datamanager/services/transactions";
@@ -75,17 +74,17 @@ public class MDMTransactionClient {
         return result;
     }
 
-    public static String getSessionID(String url, String username, String password) throws IOException {
+    public static List<String> getCookies(String url, String username, String password) throws IOException {
         HttpClient client = new HttpClient();
         client.getState().setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
         client.getParams().setAuthenticationPreemptive(true);
 
         GetMethod get = new GetMethod(url);
         get.setDoAuthentication(true);
-        String sessionID;
+        List<String> cookies;
         try {
             client.executeMethod(get);
-            sessionID = parseSessionID(get);
+            cookies = parseCookies(get);
         } catch (HttpException e) {
             throw e;
         } catch (IOException e) {
@@ -93,44 +92,16 @@ public class MDMTransactionClient {
         } finally {
             get.releaseConnection();
         }
-        return sessionID;
+        return cookies;
     }
 
-    private static String parseSessionID(HttpMethod method) {
-        String sessionID = null;
-        String stickySession = MDMTransaction.getStickySession();
-        Header[] setCookie = method.getResponseHeaders("Set-Cookie"); //$NON-NLS-1$
-        for(Header header : setCookie) {
-            String headerValue = header.getValue();
-            if(headerValue.startsWith(stickySession + "=")) { //$NON-NLS-1$
-                int beginIndex = (stickySession + "=").length(); //$NON-NLS-1$
-                int endIndex = headerValue.indexOf(";", beginIndex); //$NON-NLS-1$
-                sessionID = headerValue.substring(beginIndex, endIndex);
-                break;
-            }
+    private static List<String> parseCookies(HttpMethod method) {
+        List<String> cookies = new ArrayList<String>();
+        Header[] setCookie = method.getResponseHeaders("Set-Cookie");
+        for (Header header : setCookie) {
+            cookies.add(header.getValue());
         }
-        if(sessionID == null) {
-            if(LOG.isDebugEnabled()) {
-                LOG.warn("Cookie for sticky session not found!"); //$NON-NLS-1$
-            }
-            sessionID = ""; //$NON-NLS-1$
-        }
-        return sessionID;
-    }
-
-    public static void main(String[] args) throws IOException {
-        String sessionID = MDMTransactionClient.getSessionID("http://localhost:8621/talendmdm/services/rest/transactions", "administrator", "administrator");
-        System.out.println(sessionID);
-
-        MDMTransaction mt = MDMTransactionClient.newTransaction("http://localhost:8180/talendmdm/services/rest/transactions", "administrator", "administrator");
-        mt.commit();
-
-        MDMTransaction mt1 = MDMTransactionClient.newTransaction("http://localhost:8180/talendmdm/services/rest/transactions", "administrator", "administrator");
-        mt1.rollback();
-
-        String url = "http://localhost:8180/talend/TalendPort";
-        String mdmurl = MDMTransactionClient.getMDMTransactionURL(url, true);
-        System.out.println(mdmurl);
+        return cookies;
     }
 
 }
