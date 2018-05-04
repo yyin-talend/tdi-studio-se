c4c510c0a650c5d85c13fc8fe2d260b0f985006a
Chao MENG <cmeng@talend.com>
2018-05-04 17:48:46 +0800

    fix(TUP-19961): tELTMSSqlInput component does not support the schema (#2285)
    with a DB-column name having Degree symbol in it
    https://jira.talendforge.org/browse/TUP-19961

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/process/DataProcess.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/process/DataProcess.java
@@ -3171,8 +3171,6 @@ public class DataProcess implements IGeneratingProcess {
         }
         buildGraphicalMap.put(graphicalNode, newGraphicalNode);
 
-        buildGraphicalNodeForInputConnections(process, graphicalNode, newGraphicalNode, new HashSet<INode>());
-
         for (IConnection connection : (List<IConnection>) graphicalNode.getOutgoingConnections()) {
             if (!connection.isActivate()) {
                 continue;
@@ -3284,6 +3282,13 @@ public class DataProcess implements IGeneratingProcess {
             if (node.isSubProcessStart() && node.isActivate()) {
                 buildNodeFromNode(node, duplicatedProcess);
             }
+        }
+        Set<INode> visitedNodes = new HashSet<INode>();
+        Set<Map.Entry> entrySet = buildGraphicalMap.entrySet();
+        for (Map.Entry entry : entrySet) {
+            INode node = (INode) entry.getKey();
+            INode newNode = (INode) entry.getValue();
+            buildGraphicalNodeForInputConnections(duplicatedProcess, node, newNode, visitedNodes);
         }
 
         // make sure the new tUnite incomingConnections order is the same as the old one. @see
