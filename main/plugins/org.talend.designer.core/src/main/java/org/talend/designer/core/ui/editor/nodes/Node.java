32058fae42fc4d3b05f598161b992894c3cabef2
Jane Ding <35018295+jding-tlnd@users.noreply.github.com>
2018-06-29 10:12:29 +0800

    fix(TUP-20333)When upgrading from Talend v5.6.1 to v6.4.1, the tmap has (#2459)
    lost many mappings due to name change
    https://jira.talendforge.org/browse/TUP-20333

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/nodes/Node.java
@@ -1882,6 +1882,7 @@ public class Node extends Element implements IGraphicalNode {
 
         if (id.equals(EParameterName.CONNECTION_FORMAT.getName())) {
             connectionToParse = (String) value;
+            setConnectionName(ElementParameterParser.parse(this, connectionToParse));
             // to check
             // String newValue = ElementParameterParser.parse(this, connectionToParse);
             // setConnectionName(newValue);
