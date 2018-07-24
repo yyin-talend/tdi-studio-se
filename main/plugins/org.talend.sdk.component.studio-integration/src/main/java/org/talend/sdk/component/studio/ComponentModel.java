18b6bb5d706beb9719553b502b5acc160f824f31
Ivan Gonchar <gonchar-ivan@users.noreply.github.com>
2018-07-24 16:09:46 +0300

    Igonchar/tcomp 934 (#2539)
    * fix(TCOMP-934): remove unused fields in ComponentModel
    
    * fix(TCOMP-934): ensure palette value uses only categories

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/ComponentModel.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/ComponentModel.java
@@ -71,13 +71,7 @@ import org.talend.sdk.component.studio.mvn.Mvn;
 import org.talend.sdk.component.studio.service.ComponentService;
 import org.talend.sdk.component.studio.util.TaCoKitUtil;
 
-// TODO: finish the impl
 public class ComponentModel extends AbstractBasicComponent implements IAdditionalInfo {
-
-    /**
-     * Separator between family and component name
-     */
-    private static final String COMPONENT_SEPARATOR = "";
 
     private final ComponentIndex index;
 
@@ -99,15 +93,13 @@ public class ComponentModel extends AbstractBasicComponent implements IAdditiona
      * "Business/Salesforce|Cloud/Salesforce", where "Business", "Cloud" are
      * categories, "Salesforce" - is familyName
      */
-    private final String familyName;
+    private final String paletteValue;
 
     private volatile Set<ModuleNeeded> modulesNeeded;
 
     private Map<String, Object> additionalInfoMap = new HashMap<>();
 
     private Boolean useLookup = null;
-
-    private boolean hasConditionalOutput = false;
 
     private ETaCoKitComponentType tacokitComponentType;
 
@@ -117,7 +109,7 @@ public class ComponentModel extends AbstractBasicComponent implements IAdditiona
         this.index = component;
         this.detail = detail;
         this.tacokitComponentType = ETaCoKitComponentType.valueOf(this.detail.getType().toLowerCase());
-        this.familyName = computeFamilyName();
+        this.paletteValue = computePaletteValue();
         this.codePartListX = createCodePartList();
         this.reportPath = reportPath;
         this.isCatcherAvailable = isCatcherAvailable;
@@ -132,7 +124,7 @@ public class ComponentModel extends AbstractBasicComponent implements IAdditiona
         this.index = component;
         this.detail = detail;
         this.tacokitComponentType = ETaCoKitComponentType.valueOf(this.detail.getType().toLowerCase());
-        this.familyName = computeFamilyName();
+        this.paletteValue = computePaletteValue();
         this.codePartListX = createCodePartList();
         this.image = null;
         this.image24 = null;
@@ -148,12 +140,16 @@ public class ComponentModel extends AbstractBasicComponent implements IAdditiona
     }
 
     /**
-     * TODO change to StringBuilder impl? Seems, here StringBuilder instance is
-     * created per category
+     * Computes palette value, which is used to define component location in Studio palette
+     * Palette value has following format: "Category1/Family|Category2/Family"
+     * Component may have several entries in palette (each entry is in different category)
+     * Entries in palette value are separated with "|"
+     * "/" separates categories, subcategories and family
+     * 
+     * @return palette value
      */
-    private String computeFamilyName() {
-        return index.getCategories().stream().map(category -> category + "/" + index.getId().getFamily()).collect(
-                Collectors.joining("|"));
+    private String computePaletteValue() {
+        return index.getCategories().stream().collect(Collectors.joining("|"));
     }
 
     /**
@@ -210,15 +206,16 @@ public class ComponentModel extends AbstractBasicComponent implements IAdditiona
     }
 
     /**
-     * Returns string which is concatenation of all component palette entries
-     * Component palette entry is computed as category + "/" + familyName E.g.
-     * "Business/Salesforce|Cloud/Salesforce"
+     * Returns string which is concatenation of all component palette entries.
+     * This string has following format: "Category1/Family|Category2/SubCategory2/Family"
+     * Different entries are separated with "|".
+     * "/" separates categories, subcategories and family inside one entry
      *
      * @return all palette entries for this component
      */
     @Override
     public String getOriginalFamilyName() {
-        return familyName;
+        return paletteValue;
     }
 
     /**
