c23955d51f636f84d5e17ce741b96e8f04e397b4
Emmanuel GALLOIS <egallois@talend.com>
2019-02-26 09:20:08 +0100

    fix(TDI-41795): migrate correctly connection and linked connection (#3251)
    * fix(TDI-41795): migrate correctly connection and linked connection
    * fix(TDI-41795): fix invalid char in properties files
    * chore(TDI-41795): do net set value at each property, just once
    * fix(TDI-41795): correct NPE for tGoogleDriveCopy/Put
    * fix(TDI-41795): do not corrupt tcompv0 components
      - fix migration version and breaks
      - do not execute migration for tcompv0 components

--- main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewGoogleDriveMigrationTask.java
+++ main/plugins/org.talend.designer.core.generic/src/main/java/org/talend/designer/core/generic/model/migration/NewGoogleDriveMigrationTask.java
@@ -6,11 +6,38 @@ import java.util.Date;
 import java.util.GregorianCalendar;
 import java.util.Properties;
 
+import org.talend.core.model.properties.Item;
+import org.talend.core.utils.TalendQuoteUtils;
 import org.talend.designer.core.generic.utils.ParameterUtilTool;
 import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
 import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
 
 public class NewGoogleDriveMigrationTask extends org.talend.designer.core.generic.model.migration.NewComponentFrameworkMigrationTask {
+
+    public static final String GOOGLE_DRIVE_PREFIX = "tGoogleDrive";
+
+    public static final String JAVAJET_VERSION = "0.102";
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
+        if (processType != null) {
+            for (Object obj : processType.getNode()) {
+                if (obj != null && obj instanceof NodeType) {
+                    String componentName = ((NodeType) obj).getComponentName();
+                    String version = ((NodeType) obj).getComponentVersion();
+                    if (componentName != null && componentName.startsWith(GOOGLE_DRIVE_PREFIX)) {
+                        if (!JAVAJET_VERSION.equals(version)) {
+                            // not a javajet component (tcompv0), we skip this migration for not corrupting comps.
+                            return ExecutionResult.NOTHING_TO_DO;
+                        }
+                    }
+                }
+            }
+        }
+        return super.execute(item);
+    }
 
     @Override
     public Date getOrder() {
@@ -33,44 +60,43 @@ public class NewGoogleDriveMigrationTask extends org.talend.designer.core.generi
 
     @Override
     protected ElementParameterType getParameterType(NodeType node, String paramName) {
+        // sanitize inexistent props
+        ElementParameterType oAuthMethod = ParameterUtilTool.findParameterType(node, "OAUTH_METHOD");
+        if (oAuthMethod == null) {
+            ParameterUtilTool.addParameterType(node, "TEXT", "OAUTH_METHOD", "AccessToken");
+        }
+        ElementParameterType dsPath = ParameterUtilTool.findParameterType(node, "DATASTORE_PATH");
+        if (dsPath == null) {
+            String datastorePath = (System.getProperty("user.home", ".") + "/.credentials/talend-googledrive")
+                    .replace("\\", "/");
+            ParameterUtilTool.addParameterType(node, "TEXT", "DATASTORE_PATH",
+                    TalendQuoteUtils.addQuotesIfNotExist(datastorePath));
+        }
         ElementParameterType paramType = ParameterUtilTool.findParameterType(node, paramName);
         if (node != null && paramType != null) {
-            Object value = ParameterUtilTool.convertParameterValue(paramType);
             String componentName = node.getComponentName();
-            //
             if ("tGoogleDriveCopy".equals(componentName)) {
-                if ("FILE_MODE".equals(paramName)) {
-                    if ("true".equals(String.valueOf(value))) {
-                        paramType.setValue("File");
-                    } else {
-                        paramType.setValue("Folder");
-                    }
-                }
-                if ("FILE_NAME".equals(paramName)) {
-                    ElementParameterType fileMode = ParameterUtilTool.findParameterType(node, "FILE_MODE");
-                    Object fileModeValue = ParameterUtilTool.convertParameterValue(fileMode);
-                    if ("true".equals(String.valueOf(fileModeValue))) {
-                        paramType.setValue(String.valueOf(value));
-                    } else {
-                        ElementParameterType folderName = ParameterUtilTool.findParameterType(node, "FOLDER_NAME");
-                        Object folderNameValue = ParameterUtilTool.convertParameterValue(folderName);
-                        paramType.setValue(String.valueOf(folderNameValue));
-                    }
+                Object copyModeParam = ParameterUtilTool.findParameterType(node, "COPY_MODE");
+                if (copyModeParam == null) {
+                    Object fileMode = ParameterUtilTool.getParameterValue(node, "FILE_MODE");
+                    Object fileName = ParameterUtilTool.getParameterValue(node, "FILE_NAME");
+                    Object folderName = ParameterUtilTool.getParameterValue(node, "FOLDER_NAME");
+                    String copyMode = "true".equals(fileMode) ? "File" : "Folder";
+                    String source = "File".equals(copyMode) ? String.valueOf(fileName) : String.valueOf(folderName);
+                    ParameterUtilTool.addParameterType(node, "TEXT", "COPY_MODE", copyMode);
+                    ParameterUtilTool.addParameterType(node, "TEXT", "SOURCE", source);
                 }
             }
             if ("tGoogleDrivePut".equals(componentName)) {
-                if ("READ_CONTENT_FROM_INPUT".equals(paramName)) {
-                    if ("true".equals(String.valueOf(value))) {
-                        paramType.setValue("READ_CONTENT_FROM_INPUT");
-                    } else {
-                        ElementParameterType upl = ParameterUtilTool.findParameterType(node, "UPLOAD_LOCAL_FILE");
-                        Object uplv = ParameterUtilTool.convertParameterValue(upl);
-                        if ("true".equals(String.valueOf(uplv))) {
-                            paramType.setValue("UPLOAD_LOCAL_FILE");
-                        } else {
-                            paramType.setValue("EXPOSE_OUTPUT_STREAM");
-                        }
+                Object uploadModeParam = ParameterUtilTool.findParameterType(node, "UPLOAD_MODE");
+                if (uploadModeParam == null) {
+                    Object isReadContentFromInput = ParameterUtilTool.getParameterValue(node, "READ_CONTENT_FROM_INPUT");
+                    Object uploadLocalFile = ParameterUtilTool.getParameterValue(node, "UPLOAD_LOCAL_FILE");
+                    String uploadMode = "READ_CONTENT_FROM_INPUT";
+                    if (!"true".equals(isReadContentFromInput)) {
+                        uploadMode = "true".equals(uploadLocalFile) ? "UPLOAD_LOCAL_FILE" : "EXPOSE_OUTPUT_STREAM";
                     }
+                    ParameterUtilTool.addParameterType(node, "TEXT", "UPLOAD_MODE", uploadMode);
                 }
             }
         }
