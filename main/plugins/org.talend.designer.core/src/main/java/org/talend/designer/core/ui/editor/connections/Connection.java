8cb2aa115263a7d48c0b79ba0895437fcc71ed67
apoltavtsev <apoltavtsev@gmail.com>
2019-03-13 10:22:14 +0300

    TESB-24784 Propagation of Connection extra properties is added (#3309)
    * TESB-24784 Propagation of Connection extra properties is added
    
    * TESB-24784 Propagation of Connection extra properties is added
    
    * Propagation of Connection extra properties is corrected

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/connections/Connection.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/connections/Connection.java
@@ -108,6 +108,8 @@ public class Connection extends Element implements IConnection, IPerformance {
     private boolean monitorConnection = false;
 
     public boolean setNullColumn = false;
+    
+    private Map<EParameterName, Object> paramValues = new HashMap<EParameterName, Object>();
 
     /**
      * Tells if this connection has a subjob source or not instead of a node.
@@ -127,6 +129,13 @@ public class Connection extends Element implements IConnection, IPerformance {
     public Connection(INode source, INode target, EConnectionType lineStyle, String connectorName, String metaName,
             String linkName, String uniqueName, final boolean monitorConnection) {
         this.uniqueName = uniqueName;
+        init(source, target, lineStyle, connectorName, metaName, linkName, monitorConnection);
+    }
+
+    public Connection(INode source, INode target, EConnectionType lineStyle, String connectorName, String metaName,
+            String linkName, String uniqueName, final boolean monitorConnection, final Map<EParameterName, Object> paramValues) {
+        this.uniqueName = uniqueName;
+        this.paramValues = paramValues;
         init(source, target, lineStyle, connectorName, metaName, linkName, monitorConnection);
     }
 
@@ -520,6 +529,22 @@ public class Connection extends Element implements IConnection, IPerformance {
                 }
             }
         }
+        
+        updateParametersValues();
+    }
+    
+    private void updateParametersValues() {
+    	
+    	if (paramValues == null) {
+    		return;
+    	}
+    	
+        for (Map.Entry<EParameterName, Object> param : paramValues.entrySet()) {
+        	if (param.getValue() != null) {
+        		setPropertyValue(param.getKey().getName(), param.getValue());
+        	}
+			
+		}        
     }
 
     private void createParallelizeParameters() {
