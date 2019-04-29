4203f4e155cb7095097140d3ca7757aa22ea17b4
hwang-talend <hwang@talend.com>
2019-04-29 16:24:22 +0800

    Hwang/tup 22649 (#3413) (#3439)
    bugfix(TUP-22649):tELTOracleMap : wrong Generated SQL Select query after
    patch TPS-2944

--- main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/utils/DataMapExpressionParser.java
+++ main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/utils/DataMapExpressionParser.java
@@ -62,9 +62,11 @@ public class DataMapExpressionParser {
 
     // ((String)globalMap.get("tableName")).columnName
     private final static String GLOBALMAP_EXPRESSION4 = "(" + GLOBALMAP_PATTERN + COLUMN_PATTERN + ")";
-
+    
+    private final static String GLOBALMAP_EXPRESSION5 = "\\s*(\\s*(\\w+)\\s*\\.\\\\\"\\s*(.+?)\\s*\\\\\")\\s*";
+    
     private final static String GLOBALMAP_EXPRESSION = GLOBALMAP_EXPRESSION1 + "|" + GLOBALMAP_EXPRESSION2 + "|"//$NON-NLS-1$//$NON-NLS-2$
-            + GLOBALMAP_EXPRESSION3 + "|" + GLOBALMAP_EXPRESSION4;//$NON-NLS-1$
+            + GLOBALMAP_EXPRESSION3 + "|" + GLOBALMAP_EXPRESSION4 + "|" + GLOBALMAP_EXPRESSION5;//$NON-NLS-1$
 
     private final static String EXPRESSION_PATTERN = "(\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*)" //$NON-NLS-1$
             + "|(\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*\\.\\s*(\\w+)\\s*)" //$NON-NLS-1$
@@ -143,6 +145,10 @@ public class DataMapExpressionParser {
                 } else if (matchResult.group(35) != null) {
                     // ((String)globalMap.get("tableName")).columnName
                     location = new TableEntryLocation(matchResult.group(36), matchResult.group(38));
+                }else if (matchResult.group(39) != null) {
+                 // table.\"column\"
+                	// Case NVL(Keys.\"ORG_SCD_ID\",-99) When -99 Then 'N' Else 'Y' End
+                    location = new TableEntryLocation(matchResult.group(40), matchResult.group(41));
                 }
                 if (location != null) {
                     resultList.add(location);
