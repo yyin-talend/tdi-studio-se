ac3a59f7dd30679444804cb6f59bcca66327f7d2
wwang-talend <wwang@talend.com>
2019-05-17 16:09:11 +0800

    add the meter log

--- main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
@@ -13,10 +13,10 @@ public interface JobAuditLogger extends EventAuditLogger {
 	@AuditEvent(category = "connector", message = "Component run : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, connector_type:{connectorType}, connector_id:{connectorId}, parameters:{parameters}", level = LogLevel.INFO)
     void runcomponent(Context context);
 
-    @AuditEvent(category = "connector", message = "Component {connectorType} {connectionType} {rows} rows in {time} and speed is {speed} with {connectionName} line", level = LogLevel.INFO)
+    @AuditEvent(category = "connection", message = "Component {connectorType} {connectionType} {rows} rows in {time} and speed is {speed} with {connectionName} line", level = LogLevel.INFO)
     void flowOutput(Context context);
     
-    @AuditEvent(category = "connector", message = "Component {connectorType} received {rows} rows in {time} and speed is {speed} with {connectionName} line", level = LogLevel.INFO)
+    @AuditEvent(category = "connection", message = "Component {connectorType} received {rows} rows in {time} and speed is {speed} with {connectionName} line", level = LogLevel.INFO)
     void flowInput(Context context);
 
 }
---
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobEventAuditLoggerFactory.java
@@ -0,0 +1,42 @@
+package org.talend.job.audit;
+
+import java.lang.reflect.Proxy;
+import java.util.Properties;
+
+import org.talend.logging.audit.AuditLoggerFactory;
+import org.talend.logging.audit.EventAuditLogger;
+import org.talend.logging.audit.impl.AbstractBackend;
+import org.talend.logging.audit.impl.AuditConfiguration;
+import org.talend.logging.audit.impl.AuditConfigurationMap;
+import org.talend.logging.audit.impl.AuditLoggerBase;
+import org.talend.logging.audit.impl.DefaultAuditLoggerBase;
+import org.talend.logging.audit.impl.ProxyEventAuditLogger;
+
+public class JobEventAuditLoggerFactory {
+
+	public static JobAuditLogger createJobAuditLogger(final Properties properties) {
+		final AuditConfigurationMap config = AuditConfiguration.loadFromProperties(properties);
+
+		AbstractBackend logger = null;
+		String loggerClass = "org.talend.logging.audit.logback.LogbackBackend";
+		try {
+            final Class<?> clz = Class.forName(loggerClass);
+            logger = (AbstractBackend) clz.getConstructor(AuditConfigurationMap.class).newInstance(config);
+        } catch (ReflectiveOperationException e) {
+            throw new RuntimeException("Unable to load backend " + loggerClass, e);
+        }
+
+
+		final DefaultAuditLoggerBase loggerBase = new DefaultAuditLoggerBase(logger, config);
+
+		JobAuditLogger result = getEventAuditLogger(JobAuditLogger.class, loggerBase);
+		return result;
+	}
+
+	// we can't access AuditLoggerFactory.getEventAuditLogger method now, should
+	// adjust the daikon jar, now only do it here soon for POC
+	private static <T extends EventAuditLogger> T getEventAuditLogger(Class<T> clz, AuditLoggerBase auditLoggerBase) {
+		return (T) Proxy.newProxyInstance(AuditLoggerFactory.class.getClassLoader(), new Class<?>[] { clz },
+				new ProxyEventAuditLogger(auditLoggerBase));
+	}
+}
