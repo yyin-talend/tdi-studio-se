84edd12af5c0015be02ed09464d26fde9b2dcb9c
Emmanuel GALLOIS <egallois@talend.com>
2019-08-01 16:06:49 +0200

    fix TalendExcel imports (#3788)

--- main/plugins/org.talend.designer.components.libs/libs_src/talendExcel/src/main/java/org/talend/ExcelTool.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talendExcel/src/main/java/org/talend/ExcelTool.java
@@ -3,6 +3,7 @@ package org.talend;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
@@ -25,7 +26,6 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.usermodel.WorkbookFactory;
 import org.apache.poi.xssf.streaming.SXSSFSheet;
 import org.apache.poi.xssf.streaming.SXSSFWorkbook;
-import org.apache.poi.ss.usermodel.FormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbookType;
 
