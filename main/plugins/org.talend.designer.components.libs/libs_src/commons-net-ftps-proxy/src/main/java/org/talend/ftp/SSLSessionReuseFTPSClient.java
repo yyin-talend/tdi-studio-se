7cff06e3ad01e054bd5e7419ec72d52a21cf1348
wang wei <wwang@talend.com>
2019-08-08 10:08:09 +0800

    fix(TDI-42737): [java 11] FTPS protocol for FTP components don`t work with linux and java 11 (#3764)

--- main/plugins/org.talend.designer.components.libs/libs_src/commons-net-ftps-proxy/src/main/java/org/talend/ftp/SSLSessionReuseFTPSClient.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/commons-net-ftps-proxy/src/main/java/org/talend/ftp/SSLSessionReuseFTPSClient.java
@@ -29,11 +29,8 @@ public class SSLSessionReuseFTPSClient extends FTPSClient {
                 final Object cache = sessionHostPortCache.get(context);
                 final Method putMethod = cache.getClass().getDeclaredMethod("put", Object.class, Object.class);
                 putMethod.setAccessible(true);
-                final Method getHostMethod = socket.getClass().getDeclaredMethod("getHost");
-                getHostMethod.setAccessible(true);
-                Object host = getHostMethod.invoke(socket);
                 final String key =
-                        String.format("%s:%s", host, String.valueOf(socket.getPort())).toLowerCase(Locale.ROOT);
+                        String.format("%s:%s", socket.getInetAddress().getHostName(), String.valueOf(socket.getPort())).toLowerCase(Locale.ROOT);
                 putMethod.invoke(cache, key, session);
             } catch (Exception e) {
                 e.printStackTrace();
