73a7c3d0f6d6973a61f16c47d1701d08ea6e9c79
Chao MENG <cmeng@talend.com>
2019-10-08 11:27:34 +0800

    fix(TUP-23229): IllegalArgumentException when press enter for tLibraryLoad->Advanced settings->Import (#3975)
    * fix(TUP-23229): IllegalArgumentException when press enter for
    tLibraryLoad->Advanced settings->Import
    https://jira.talendforge.org/browse/TUP-23229

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/viewer/ReconcilerStyledText.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/viewer/ReconcilerStyledText.java
@@ -15,12 +15,18 @@ package org.talend.designer.core.ui.viewer;
 import org.eclipse.jface.text.BadLocationException;
 import org.eclipse.jface.text.IRegion;
 import org.eclipse.swt.custom.StyledText;
+import org.eclipse.swt.graphics.Rectangle;
 import org.eclipse.swt.widgets.Composite;
+import org.talend.commons.CommonsPlugin;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.designer.core.i18n.Messages;
 
 /**
  * DOC nrousseau ReconcilerViewer class global comment. Detailled comment
  */
 public class ReconcilerStyledText extends StyledText {
+
+    private boolean isDebugMode = false;
 
     private ReconcilerViewer viewer;
 
@@ -34,6 +40,7 @@ public class ReconcilerStyledText extends StyledText {
     public ReconcilerStyledText(Composite parent, int style, ReconcilerViewer viewer) {
         super(parent, style);
         this.viewer = viewer;
+        isDebugMode = CommonsPlugin.isDebugMode();
     }
 
     /*
@@ -63,4 +70,52 @@ public class ReconcilerStyledText extends StyledText {
             viewer.getUndoManager().reset();
         }
     }
+
+    @Override
+    public Rectangle getTextBounds(int start, int end) {
+        /**
+         * There is issue in Eclipse4.10 CursorLinePainter#paint CursorLinePainter#updateHighlightLine, the update
+         * action is based on lines in the whole document rather than the visible document, so there will be issue with
+         * new line in VisibleRegion
+         */
+
+        int newStart = start;
+        int newEnd = end;
+        int charCount = getCharCount();
+        boolean illegalArgs = false;
+        if (charCount <= newStart) {
+            if (charCount == 0 && newStart == 0) {
+                // normal case
+            } else {
+                illegalArgs = true;
+                newStart = charCount - 1;
+            }
+        }
+        if (newStart < 0) {
+            illegalArgs = true;
+            newStart = 0;
+        }
+        if (charCount <= newEnd) {
+            if (charCount == 0 && newEnd == 0) {
+                // normal case
+            } else {
+                illegalArgs = true;
+                newEnd = charCount - 1;
+            }
+        }
+        if (newEnd < newStart) {
+            illegalArgs = true;
+            newEnd = newStart;
+        }
+        if (illegalArgs && isDebugMode) {
+            ExceptionHandler.process(new IllegalArgumentException(
+                    Messages.getString("ReconcilerStyledText.illegalArgs", charCount, start, end, newStart, newEnd)));
+        }
+        if (charCount <= 0 && newStart <= 0 && newEnd <= 0) {
+            return new Rectangle(0, 0, 0, 0);
+        } else {
+            return super.getTextBounds(newStart, newEnd);
+        }
+    }
+
 }
