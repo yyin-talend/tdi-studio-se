76aff58dc979a5ed91f8f6837cb502731916a067
Liu Xinquan <xqliu@talend.com>
2019-12-09 15:56:39 +0800

    TDQ-17742 change the dialog title to Enter a new license key (#4195)
    * TDQ-17742 change the dialog title to Enter a new license key
    
    * TDQ-17742 change the dialog title to Enter a new license key

---
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/properties/controllers/generator/LicenseKeyControllerGenerator.java
@@ -0,0 +1,45 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.designer.core.ui.editor.properties.controllers.generator;
+
+import org.talend.core.ui.properties.tab.IDynamicProperty;
+import org.talend.designer.core.ui.editor.properties.controllers.AbstractElementPropertySectionController;
+import org.talend.designer.core.ui.editor.properties.controllers.LicenseKeyController;
+
+public class LicenseKeyControllerGenerator extends PasswordControllerGenerator {
+
+    private IDynamicProperty dp;
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.talend.designer.core.ui.editor.properties.controllers.generator.IControllerGenerator#generate()
+     */
+    @Override
+    public AbstractElementPropertySectionController generate() {
+        return new LicenseKeyController(dp);
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see
+     * org.talend.designer.core.ui.editor.properties.controllers.generator.IControllerGenerator#setDynamicProperty(org
+     * .talend.designer.core.ui.editor.properties.controllers.generator.IDynamicProperty)
+     */
+    @Override
+    public void setDynamicProperty(IDynamicProperty dp) {
+        this.dp = dp;
+    }
+
+}
