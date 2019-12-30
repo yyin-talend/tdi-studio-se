617095c83badc8f2c0954277d7af7762723ee19c
Jane Ding <35018295+jding-tlnd@users.noreply.github.com>
2019-12-30 17:49:20 +0800

    feat(TUP-24057)There is no error message when studio can't connect to (#4290)
    customer artifact repository
    https://jira.talendforge.org/browse/TUP-24057

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginHelper.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginHelper.java
@@ -28,10 +28,12 @@ import org.eclipse.core.runtime.IProgressMonitor;
 import org.eclipse.core.runtime.OperationCanceledException;
 import org.eclipse.core.runtime.Path;
 import org.eclipse.core.runtime.Platform;
+import org.eclipse.jface.dialogs.IDialogConstants;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.dialogs.ProgressMonitorDialog;
 import org.eclipse.jface.operation.IRunnableWithProgress;
 import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.ui.PlatformUI;
@@ -57,6 +59,7 @@ import org.talend.core.model.general.ConnectionBean;
 import org.talend.core.model.general.Project;
 import org.talend.core.model.properties.PropertiesFactory;
 import org.talend.core.model.properties.User;
+import org.talend.core.nexus.TalendLibsServerManager;
 import org.talend.core.prefs.ITalendCorePrefConstants;
 import org.talend.core.prefs.PreferenceManipulator;
 import org.talend.core.repository.model.IRepositoryFactory;
@@ -530,6 +533,35 @@ public class LoginHelper {
             CommonExceptionHandler.process(e);
             MessageDialog.openError(getUsableShell(), getUsableShell().getText(), e.getMessage());
             return false;
+        }
+
+        // Check user library connection
+        try {
+            if (!factory.isLocalConnectionProvider() && !factory.getRepositoryContext().isOffline()) {
+                if (!TalendLibsServerManager.getInstance().canConnectUserLibrary()) {
+                    boolean[] flag = new boolean[] { false };
+                    Display.getDefault().syncExec(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            String[] dialogButtonLabels = new String[] { IDialogConstants.YES_LABEL, IDialogConstants.NO_LABEL };
+                            int open = MessageDialog.open(MessageDialog.WARNING, Display.getDefault().getActiveShell(),
+                                    Messages.getString("LoginHelper.connectUserLibraryTitle"),
+                                    Messages.getString("LoginHelper.connectUserLibraryFailureWarning"), SWT.NONE,
+                                    dialogButtonLabels);
+                            if (open == 0) {
+                                flag[0] = true;
+                            }
+                        }
+
+                    });
+                    if (!flag[0]) {
+                        return false;
+                    }
+                }
+            }
+        } catch (PersistenceException e1) {
+            CommonExceptionHandler.process(e1);
         }
 
         final Shell shell = getUsableShell();
