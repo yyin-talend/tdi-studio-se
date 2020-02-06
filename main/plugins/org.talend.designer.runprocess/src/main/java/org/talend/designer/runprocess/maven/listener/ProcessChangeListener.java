e3e9c9b4010cfa4f77ef82937cc256b1b20a4abb
hzhao-talendbj <49395568+hzhao-talendbj@users.noreply.github.com>
2020-02-06 09:34:56 +0800

    backpot72(TUP-25951)Syncing of project and pom files doesnt happen (#4393)
    * backpot72(TUP-25951)Syncing of project and pom files doesnt happen
    initally
    
    * backport72(TUP-25951)Syncing of project and pom files doesnt happen
    initally
    
    * remove synchronized

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/listener/ProcessChangeListener.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/listener/ProcessChangeListener.java
@@ -29,6 +29,8 @@ import org.eclipse.core.runtime.Path;
 import org.eclipse.ltk.core.refactoring.resource.MoveResourceChange;
 import org.eclipse.ltk.core.refactoring.resource.RenameResourceChange;
 import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.commons.utils.workbench.resources.ResourceUtils;
 import org.talend.core.GlobalServiceRegister;
 import org.talend.core.model.properties.Item;
 import org.talend.core.model.properties.Property;
@@ -146,7 +148,7 @@ public class ProcessChangeListener implements PropertyChangeListener {
                 IFolder targetFolder = processTypeFolder.getFolder(targetPath);
                 try {
                     if (!targetFolder.exists()) {
-                        targetFolder.create(true, true, null);
+                        ResourceUtils.createFolder(targetFolder);
                     }
                     List<IRepositoryViewObject> allVersions = ProxyRepositoryFactory.getInstance().getAllVersion(obj.getId());
                     for (IRepositoryViewObject objs : allVersions) {
@@ -300,16 +302,20 @@ public class ProcessChangeListener implements PropertyChangeListener {
             if (getAllProcessTypes().contains(processType)) {
                 IFolder processTypeFolder = getAggregatorPomsHelper().getProcessFolder(processType);
                 IFolder sourceFolder = processTypeFolder.getFolder(sourcePath);
+                if (!sourceFolder.exists()) {
+                    return;
+                }
                 try {
+                    sourceFolder.refreshLocal(IResource.DEPTH_INFINITE, new NullProgressMonitor());
                     removeFromParentSourceFolder(sourceFolder);
                     IFolder targetFolder = processTypeFolder.getFolder(targetPath);
                     if (!targetFolder.exists()) {
-                        targetFolder.create(true, true, null);
+                        ResourceUtils.createFolder(targetFolder);
                     }
                     MoveResourceChange change = new MoveResourceChange(sourceFolder, targetFolder);
                     change.perform(new NullProgressMonitor());
                     updatePomsInNewFolder(targetFolder.getFolder(sourceFolder.getName()));
-                } catch (OperationCanceledException | CoreException e) {
+                } catch (OperationCanceledException | CoreException | PersistenceException e) {
                     ExceptionHandler.process(e);
                 }
             }
