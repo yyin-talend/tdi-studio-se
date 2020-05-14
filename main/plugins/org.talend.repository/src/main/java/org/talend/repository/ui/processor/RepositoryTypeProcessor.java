ea13d1115d9d5c96589772419ea96d574b147a72
hcyi <hcyi@talend.com>
2020-05-14 10:06:25 +0800

    fix(TUP-27000):Talend Salesforce Einstein connector Repository (#4671)
    connection issue in Talend 7.3

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/processor/RepositoryTypeProcessor.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/processor/RepositoryTypeProcessor.java
@@ -61,7 +61,7 @@ public class RepositoryTypeProcessor extends SingleTypeProcessor {
 
     @Override
     protected ERepositoryObjectType getType() {
-        final String repositoryType = getRepositoryType();
+        String repositoryType = getRepositoryType();
 
         if (repositoryType == null) { // all
             return ERepositoryObjectType.METADATA;
@@ -95,7 +95,12 @@ public class RepositoryTypeProcessor extends SingleTypeProcessor {
             return ERepositoryObjectType.METADATA_WSDL_SCHEMA;
         }
         if (repositoryType.equals(ERepositoryCategoryType.SALESFORCE.getName())) {
-            return ERepositoryObjectType.METADATA_SALESFORCE_SCHEMA;
+            if (isGeneric) {
+                return ERepositoryObjectType.METADATA_SALESFORCE_SCHEMA;
+            } else {
+                // Javajet components: tSalesforceEinsteinBulkExec/tSalesforceEinsteinOutputBulkExec
+                repositoryType = repositoryType.toLowerCase();
+            }
         }
 
         if (repositoryType.startsWith(ERepositoryCategoryType.DATABASE.getName())) {
@@ -135,7 +140,7 @@ public class RepositoryTypeProcessor extends SingleTypeProcessor {
         // http://jira.talendforge.org/browse/TESB-5218 LiXiaopeng
         if (repositoryType.equals("SERVICES:OPERATION") || repositoryType.equals("WEBSERVICE")) { //$NON-NLS-1$
             if (GlobalServiceRegister.getDefault().isServiceRegistered(IESBService.class)) {
-                IESBService service = (IESBService) GlobalServiceRegister.getDefault().getService(IESBService.class);
+                IESBService service = GlobalServiceRegister.getDefault().getService(IESBService.class);
                 return service.getServicesType();
             }
         }
