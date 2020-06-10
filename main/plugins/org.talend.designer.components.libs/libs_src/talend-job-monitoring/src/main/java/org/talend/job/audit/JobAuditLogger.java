e4643a78cb3041cd76e1615291712846033ef20c
wang wei <wwang@talend.com>
2020-06-10 16:36:30 +0800

    fix(TDI-44172): Observability: Create new "component execution" event and emit it frequently (#4765)

--- main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobAuditLogger.java
@@ -6,20 +6,23 @@ import org.talend.logging.audit.EventAuditLogger;
 import org.talend.logging.audit.LogLevel;
 
 public interface JobAuditLogger extends EventAuditLogger {
-	
-	@AuditEvent(category = "job", message = "Job start : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}", level = LogLevel.INFO)
-    void jobstart(Context context);
-	
-	@AuditEvent(category = "job", message = "Job stop : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, timestamp:{timestamp}, status:{status}, duration:{duration}", level = LogLevel.INFO)
-    void jobstop(Context context);
-	
-	@AuditEvent(category = "connector", message = "Component run : job_name:{jobName}, job_version:{jobVersion}, job_id:{jobId}, connector_type:{connectorType}, connector_id:{connectorId}", level = LogLevel.INFO)
-    void runcomponent(Context context);
 
-    @AuditEvent(category = "connection", message = "Component {connectorType} {connectionType} {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
-    void flowOutput(Context context);
-    
-    @AuditEvent(category = "connection", message = "Component {connectorType} received {rows} rows in {duration} with {connectionName} line", level = LogLevel.INFO)
-    void flowInput(Context context);
+	@AuditEvent(category = "jobstart", message = "Job start : job_name:{job_name}, job_version:{job_version}, job_id:{job_id}, timestamp:{timestamp}", level = LogLevel.INFO)
+	void jobstart(Context context);
+
+	@AuditEvent(category = "jobstop", message = "Job stop : job_name:{job_name}, job_version:{job_version}, job_id:{job_id}, timestamp:{timestamp}, status:{status}, duration:{duration}", level = LogLevel.INFO)
+	void jobstop(Context context);
+
+	@AuditEvent(category = "runcomponent", message = "Component run : job_name:{job_name}, job_version:{job_version}, job_id:{job_id}, connector_type:{connector_type}, connector_id:{connector_id}, connector_label:{connector_label}", level = LogLevel.INFO)
+	void runcomponent(Context context);
+
+	@AuditEvent(category = "flowoutput", message = "Component {connector_type} {connector_label} {connection_type} {rows} rows in {duration} with {connection_name} line", level = LogLevel.INFO)
+	void flowOutput(Context context);
+
+	@AuditEvent(category = "flowinput", message = "Component {connector_type} {connector_label} received {rows} rows in {duration} with {connection_name} line", level = LogLevel.INFO)
+	void flowInput(Context context);
+
+	@AuditEvent(category = "flowexecution", message = "connection : {connection_name}, row : {rows}, cost : {duration}", level = LogLevel.INFO)
+	void flowExecution(Context context);
 
 }
--- main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobContextBuilder.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-job-monitoring/src/main/java/org/talend/job/audit/JobContextBuilder.java
@@ -5,86 +5,160 @@ import org.talend.logging.audit.ContextBuilder;
 
 public class JobContextBuilder {
 
-    private final ContextBuilder builder;
+	private final ContextBuilder builder;
 
-    public JobContextBuilder(ContextBuilder builder) {
-        this.builder = builder;
-    }
+	public JobContextBuilder(ContextBuilder builder) {
+		this.builder = builder;
+	}
 
-    public static JobContextBuilder create() {
-        return new JobContextBuilder(ContextBuilder.create());
-    }
+	public static JobContextBuilder create() {
+		return new JobContextBuilder(ContextBuilder.create());
+	}
 
-    public JobContextBuilder jobName(String jobName) {
-        builder.with("jobName", jobName);
-        return this;
-    }
-    
-    public JobContextBuilder jobVersion(String jobVersion) {
-        builder.with("jobVersion", jobVersion);
-        return this;
-    }
-    
-    public JobContextBuilder jobId(String jobId) {
-        builder.with("jobId", jobId);
-        return this;
-    }
-    
-    /**
-     * component type like "tXMLMap"
-     * @param connectorType
-     * @return self
-     */
-    public JobContextBuilder connectorType(String connectorType) {
-        builder.with("connectorType", connectorType);
-        return this;
-    }
-    
-    /**
-     * component type like "tXMLMap_1"
-     * @param connectorType
-     * @return self
-     */
-    public JobContextBuilder connectorId(String connectorId) {
-        builder.with("connectorId", connectorId);
-        return this;
-    }
-    
-    public JobContextBuilder rows(long rowCount) {
-        builder.with("rows", String.valueOf(rowCount));
-        return this;
-    }
-    
-    //output or reject
-    public JobContextBuilder connectionType(String connectionType) {
-        builder.with("connectionType", connectionType);
-        return this;
-    }
-    
-    //like "row1"
-    public JobContextBuilder connectionName(String connectionName) {
-        builder.with("connectionName", connectionName);
-        return this;
-    }
-    
-    //100s
-    public JobContextBuilder duration(String duration) {
-        builder.with("duration", duration);
-        return this;
-    }
-    
-    public JobContextBuilder timestamp(String timestamp) {
-        builder.with("timestamp", timestamp);
-        return this;
-    }
-    
-    public JobContextBuilder status(String status) {
-        builder.with("status", status);
-        return this;
-    }
+	public JobContextBuilder jobName(String job_name) {
+		builder.with("job_name", job_name);
+		return this;
+	}
 
-    public Context build() {
-        return builder.build();
-    }
+	public JobContextBuilder jobVersion(String job_version) {
+		builder.with("job_version", job_version);
+		return this;
+	}
+
+	public JobContextBuilder jobId(String job_id) {
+		builder.with("job_id", job_id);
+		return this;
+	}
+
+	/**
+	 * component label, default is unique name like "tXMLMap_1", but user can adjust
+	 * it in studio to any value
+	 * 
+	 * @param connectorType
+	 * @return self
+	 */
+	public JobContextBuilder connectorLabel(String connector_label) {
+		builder.with("connector_label", connector_label);
+		return this;
+	}
+
+	/**
+	 * component type like "tXMLMap"
+	 * 
+	 * @param connectorType
+	 * @return self
+	 */
+	public JobContextBuilder connectorType(String connector_type) {
+		builder.with("connector_type", connector_type);
+		return this;
+	}
+
+	/**
+	 * component unique name like "tXMLMap_1"
+	 * 
+	 * @param connectorType
+	 * @return self
+	 */
+	public JobContextBuilder connectorId(String connector_id) {
+		builder.with("connector_id", connector_id);
+		return this;
+	}
+
+	public JobContextBuilder rows(long rows) {
+		builder.with("rows", String.valueOf(rows));
+		return this;
+	}
+
+	// output or reject
+	public JobContextBuilder connectionType(String connection_type) {
+		builder.with("connection_type", connection_type);
+		return this;
+	}
+
+	// like "row1"
+	public JobContextBuilder connectionName(String connection_name) {
+		builder.with("connection_name", connection_name);
+		return this;
+	}
+
+	// 100s
+	public JobContextBuilder duration(String duration) {
+		builder.with("duration", duration);
+		return this;
+	}
+
+	public JobContextBuilder timestamp(String timestamp) {
+		builder.with("timestamp", timestamp);
+		return this;
+	}
+
+	public JobContextBuilder status(String status) {
+		builder.with("status", status);
+		return this;
+	}
+
+	public Context build() {
+		return builder.build();
+	}
+
+	/**
+	 * source connector id
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder sourceId(String source_id) {
+		builder.with("source_id", source_id);
+		return this;
+	}
+
+	/**
+	 * source connector label
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder sourceLabel(String source_label) {
+		builder.with("source_label", source_label);
+		return this;
+	}
+
+	/**
+	 * source connector name
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder sourceConnectorType(String source_connector_type) {
+		builder.with("source_connector_type", source_connector_type);
+		return this;
+	}
+
+	/**
+	 * target connector id
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder targetId(String target_id) {
+		builder.with("target_id", target_id);
+		return this;
+	}
+
+	/**
+	 * target connector label
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder targetLabel(String target_label) {
+		builder.with("target_label", target_label);
+		return this;
+	}
+
+	/**
+	 * target connector name
+	 * 
+	 * @return
+	 */
+	public JobContextBuilder targetConnectorType(String target_connector_type) {
+		builder.with("target_connector_type", target_connector_type);
+		return this;
+	}
 
 }
