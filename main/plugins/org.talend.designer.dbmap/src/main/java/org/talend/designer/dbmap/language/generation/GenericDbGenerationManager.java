604fb23a715a72a392fd01b66998e80b35822242
Chao MENG <cmeng@talend.com>
2020-07-23 18:56:58 +0800

    patch(TPS-4193): remove localprovider, make it compatible with javajet modifications (#4984)
    * patch(TPS-4193): remove localprovider, make it compatible with javajet
    modifications
    
    * patch(TPS-4193): remove localprovider, make it compatible with javajet
    modifications

--- main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/GenericDbGenerationManager.java
+++ main/plugins/org.talend.designer.dbmap/src/main/java/org/talend/designer/dbmap/language/generation/GenericDbGenerationManager.java
@@ -12,8 +12,12 @@
 // ============================================================================
 package org.talend.designer.dbmap.language.generation;
 
+import java.util.Optional;
+
+import org.talend.commons.exception.ExceptionHandler;
 import org.talend.designer.dbmap.DbMapComponent;
 import org.talend.designer.dbmap.language.GenericDbLanguage;
+import org.talend.designer.dbmap.language.mssql.MssqlGenerationManager;
 
 /**
  * wzhang class global comment. Detailled comment
@@ -26,6 +30,17 @@ public class GenericDbGenerationManager extends DbGenerationManager {
 
     @Override
     public String buildSqlSelect(DbMapComponent component, String outputTableName) {
+        try {
+            if (Optional.ofNullable(component).map(c -> c.getComponent()).map(c -> "tELTMSSqlMap".equals(c.getName()))
+                    .orElse(false)) {
+                MssqlGenerationManager genManager = new MssqlGenerationManager();
+                String result = genManager.buildSqlSelect(component, outputTableName);
+                this.queryColumnsName = genManager.getQueryColumnsName();
+                return result;
+            }
+        } catch (Throwable e) {
+            ExceptionHandler.process(e);
+        }
         return super.buildSqlSelect(component, outputTableName);
     }
 }
