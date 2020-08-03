e7cd94f8791e4047bc98cf58e2cb4f86da6aac2d
Mike Yan <yanbasic@gmail.com>
2020-08-03 16:56:23 +0800

    fix(TESB-29210): Build type not updated for consumer job (#5014)

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DefaultRunProcessService.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/DefaultRunProcessService.java
@@ -295,7 +295,7 @@ public class DefaultRunProcessService implements IRunProcessService {
             return new MavenJavaProcessor(process, property, filenameFromLabel);
         } else {
             if (property != null) {
-                if (!ProcessorUtilities.isGeneratePomOnly()) {
+                // if (!ProcessorUtilities.isGeneratePomOnly()) {
                     // for esb type only
                     boolean servicePart = false;
                     List<Relation> relations = RelationshipItemBuilder.getInstance().getItemsRelatedTo(property.getId(),
@@ -337,7 +337,7 @@ public class DefaultRunProcessService implements IRunProcessService {
                             return soapService.createOSGIJavaProcessor(process, property, filenameFromLabel);
                         }
                     }
-                }
+                // }
                 return new MavenJavaProcessor(process, property, filenameFromLabel);
             } else {
                 return new MavenJavaProcessor(process, property, filenameFromLabel);
