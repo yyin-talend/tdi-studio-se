3ee0dd2559b3ee745afa92a95bfcbf3b955dbe07
SunChaoqun <csun@talend.com>
2020-08-11 13:56:33 +0800

    TESB-29133 Update dom4j to 2.1.3 (#5000) (#5053)
    Co-authored-by: Maksym Sheverda <maksym.sheverda@synapse.com>
    
    Co-authored-by: msheverda-talend <53863455+msheverda-talend@users.noreply.github.com>
    Co-authored-by: Maksym Sheverda <maksym.sheverda@synapse.com>

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/scriptsmanager/esb/JobJavaScriptOSGIForESBManager.java
@@ -927,7 +927,8 @@ public class JobJavaScriptOSGIForESBManager extends JobJavaScriptsManager {
                 if (url.getPath().matches("(.*)camel-(.*)-alldep-(.*)$") 
                         || url.getPath().matches("(.*)activemq-all-[\\d\\.]*.jar$")
                         || url.getPath().matches("(.*)jms[\\d\\.-]*.jar$")
-                        || url.getPath().matches("(.*)tdm-lib-di-[\\d\\.-]*.jar$")) {
+                        || url.getPath().matches("(.*)tdm-lib-di-[\\d\\.-]*.jar$")
+                        || url.getPath().matches("(.*)dom4j-[\\d\\.-]*.jar$")) {
                     continue;
                 }
                 File dependencyFile = new File(FilesUtils.getFileRealPath(url.getPath()));
