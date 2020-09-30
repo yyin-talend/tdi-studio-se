eedcc4501dccc7667bb8db66d81708451df7428a
mbasiuk-talend <mbasiuk@talend.com>
2020-09-30 16:45:55 +0300

    chore: TDI-32442 revert (#5265)
    * Revert "fix(TDI-32442): add missing else block (#4545)"
    
    This reverts commit 9c82017e726f63880ce8c116785ee551f02b56b4.
    
    * Revert "Mbasiuk/tdi 32442 dynamic excel dateformat (#4377)"
    
    This reverts commit a94989277c4c4feb15403a3cc31312a6e161b391.

--- main/plugins/org.talend.designer.components.libs/libs_src/simpleexcel/src/main/java/com/talend/excel/xssf/event/DefaultTalendSheetContentsHandler.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/simpleexcel/src/main/java/com/talend/excel/xssf/event/DefaultTalendSheetContentsHandler.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -12,12 +12,8 @@
 // ============================================================================
 package com.talend.excel.xssf.event;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import org.apache.poi.xssf.usermodel.XSSFComment;
 
@@ -37,33 +33,8 @@ public class DefaultTalendSheetContentsHandler implements TalendXSSFSheetXMLHand
 
     private int lastColumnIndex = -1;
 
-    private int endColumnIndex = 0;
-
-    private boolean isCellDateType = false;
-
-    //Cache Formatters
-    private Map<Integer, SimpleDateFormat> formatters = new HashMap<Integer, SimpleDateFormat>();
-
-    private SimpleDateFormat currentExcelFormatter;
-
-    private SimpleDateFormat dynamicDateFormatter;
-
-    private final String dynamicDatePattern;
-
-    private final int startDynamicIndex;
-
-    private final int endDynamicIndex;
-
     public DefaultTalendSheetContentsHandler(DataBufferCache cache) {
-        this(cache,null,0,0);
-    }
-
-    public DefaultTalendSheetContentsHandler(DataBufferCache cache, String dynamicDatePattern, int startDynamicIndex,
-            int endDynamicIndex) {
         this.cache = cache;
-        this.dynamicDatePattern = dynamicDatePattern;
-        this.startDynamicIndex = startDynamicIndex;
-        this.endDynamicIndex = endDynamicIndex;
     }
 
     @Override
@@ -75,11 +46,6 @@ public class DefaultTalendSheetContentsHandler implements TalendXSSFSheetXMLHand
     public void endRow(int rowNum) {
         cache.writeData(row);
         row = null;
-        //Store count of columns to determine date pattern in case of dynamic value.
-        //It can be applied since in XLSX file has to be a header, so we can count total number of columns
-        if (dynamicDatePattern != null && endColumnIndex == 0) {
-            endColumnIndex = lastColumnIndex;
-        }
         // when each row end ,reset lastColumnIndex
         lastColumnIndex = -1;
         if (stop) {
@@ -88,28 +54,9 @@ public class DefaultTalendSheetContentsHandler implements TalendXSSFSheetXMLHand
     }
 
     @Override
-    public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+    public void cell(String cellReference, String formattedValue,XSSFComment comment) {
         checkHasNullValue(cellReference);
-        if (isCellDateType) {
-            try {
-                if (formattedValue != null) {
-                    if (dynamicDatePattern != null && (currentColumnIndex >= startDynamicIndex
-                            && currentColumnIndex <= endColumnIndex - endDynamicIndex)) {
-                        if (dynamicDateFormatter == null) {
-                            dynamicDateFormatter = new SimpleDateFormat(dynamicDatePattern);
-                        }
-                        row.add(dynamicDateFormatter.format(currentExcelFormatter.parse(formattedValue)));
-                    } else {
-                        row.add(String.valueOf(currentExcelFormatter.parse(formattedValue).getTime()));
-                    }
-                }
-            } catch (ParseException e) {
-                row.add(null);
-            }
-            isCellDateType = false;
-        } else {
-            row.add(formattedValue);
-        }
+        row.add(formattedValue);
     }
 
     @Override
@@ -118,7 +65,7 @@ public class DefaultTalendSheetContentsHandler implements TalendXSSFSheetXMLHand
 
     @Override
     public void endSheet() {
-        formatters.clear();
+
     }
 
     public void stop() {
@@ -161,18 +108,4 @@ public class DefaultTalendSheetContentsHandler implements TalendXSSFSheetXMLHand
         }
         return columnIndex;
     }
-
-    public void setExcelDateFormat(String excelDateFormat) {
-        SimpleDateFormat temp;
-        if ((temp = formatters.get(currentColumnIndex)) == null) {
-            temp = new SimpleDateFormat(excelDateFormat);
-            formatters.put(currentColumnIndex, temp);
-        }
-        currentExcelFormatter = temp;
-    }
-
-    public void setCellDateType(boolean isCellDateType) {
-        this.isCellDateType = isCellDateType;
-    }
-
 }
--- main/plugins/org.talend.designer.components.libs/libs_src/simpleexcel/src/main/java/com/talend/excel/xssf/event/ExcelReader.java
+++ main/plugins/org.talend.designer.components.libs/libs_src/simpleexcel/src/main/java/com/talend/excel/xssf/event/ExcelReader.java
@@ -1,6 +1,6 @@
 // ============================================================================
 //
-// Copyright (C) 2006-2020 Talend Inc. - www.talend.com
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
 //
 // This source code is available under agreement available at
 // %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
@@ -63,12 +63,6 @@ public class ExcelReader implements Callable {
 
     private boolean includePhoneticRuns;
 
-    private String dynamicDatePattern;
-
-    private int startDynamicIndex = Integer.MIN_VALUE;
-
-    private int endDynamicIndex = Integer.MAX_VALUE;
-
     public ExcelReader() {
         cache = DataBufferCache.getInstance();
         futureTask = new FutureTask(this);
@@ -111,18 +105,6 @@ public class ExcelReader implements Callable {
 
     public void stopRead() {
         sheetContentsHandler.stop();
-    }
-
-    public void setDynamicDatePattern(String dynamicDatePattern) {
-        this.dynamicDatePattern = dynamicDatePattern;
-    }
-
-    public void setStartDynamicIndex(int startDynamicIndex) {
-        this.startDynamicIndex = startDynamicIndex;
-    }
-
-    public void setEndDynamicIndex(int endDynamicIndex) {
-        this.endDynamicIndex = endDynamicIndex;
     }
 
     public boolean isIncludePhoneticRuns() {
@@ -169,8 +151,7 @@ public class ExcelReader implements Callable {
 
             StylesTable styles = r.getStylesTable();
             ReadOnlySharedStringsTable strings = new ReadOnlySharedStringsTable(pkg, includePhoneticRuns);
-            sheetContentsHandler = new DefaultTalendSheetContentsHandler(cache, dynamicDatePattern, startDynamicIndex,
-                    endDynamicIndex);
+            sheetContentsHandler = new DefaultTalendSheetContentsHandler(cache);
             DataFormatter formatter = new DataFormatter();
             boolean formulasNotResults = false;
 
