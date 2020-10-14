5eb4100d9869314ac8032ae3583739eac48e564e
Emmanuel GALLOIS <egallois@talend.com>
2020-10-14 16:53:38 +0200

    feat(TCOMP-1757): support context in design time actions (#5220)
    * feat(TCOMP-1757): support context in design time actions
    * feat(TCOMP-1757): add context selection when multiple contexts

--- main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/action/Action.java
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/model/action/Action.java
@@ -20,11 +20,26 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Optional;
 
+import org.eclipse.jface.window.Window;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.IWorkbenchWindow;
+import org.eclipse.ui.PlatformUI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.talend.commons.CommonsPlugin;
+import org.talend.core.model.process.IContext;
+import org.talend.core.model.process.IContextManager;
+import org.talend.core.model.process.IContextParameter;
+import org.talend.designer.core.ui.editor.properties.controllers.uidialog.OpenContextChooseComboDialog;
 import org.talend.sdk.component.studio.Lookups;
 import org.talend.sdk.component.studio.websocket.WebSocketClient.V1Action;
 
 public class Action<T> {
+
+    private final static Logger LOGGER = LoggerFactory.getLogger(Action.class.getName());
 
     public static final String STATUS = "status";
 
@@ -46,6 +61,8 @@ public class Action<T> {
      * Action parameters map. Key is an ElementParameter path. Value is a list of action parameters associated with the ElementParameter
      */
     private final Map<String, List<IActionParameter>> parameters = new HashMap<>();
+
+    private IContextManager contextManager;
 
     public Action(final String actionName, final String family, final Type type) {
         this.actionName = actionName;
@@ -89,13 +106,70 @@ public class Action<T> {
 
     protected final Map<String, String> payload() {
         final Map<String, String> payload = new HashMap<>();
+        IContext context = selectContext();
         parameters.values().stream()
                 .flatMap(List::stream)
                 .flatMap(actionParam -> actionParam.parameters().stream())
                 .forEach(param -> {
-                    payload.put(param.getFirst(), param.getSecond());
-                 });
+                    final String initialValue = param.getSecond();
+                    String value = Optional.ofNullable(context)
+                            .map(cx -> Optional.ofNullable(
+                                    cx.getContextParameter(initialValue.replace("context.", "")))
+                                    .map(IContextParameter::getValue)
+                                    .orElse(initialValue)
+                            ).orElse(initialValue);
+                    payload.put(param.getFirst(), value);
+                });
         return payload;
+    }
+
+    public IContextManager getContextManager() {
+        return contextManager;
+    }
+
+    public void setContextManager(final IContextManager contextManager) {
+        this.contextManager = contextManager;
+    }
+
+    private IContext selectContext() {
+        if (getContextManager() == null) {
+            return null;
+        }
+        final List<IContext> allContexts = getContextManager().getListContext();
+        if (allContexts.size() == 1) {
+            return getContextManager().getDefaultContext();
+        }
+        final IContext[] selectedContext = { getContextManager().getDefaultContext() };
+        try {
+            if (!CommonsPlugin.isHeadless() && PlatformUI.isWorkbenchRunning()) {
+                final Display display = Display.getDefault();
+                if (display != null) {
+                    display.syncExec(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            Shell shell = null;
+                            final IWorkbenchWindow activeWindow = PlatformUI.getWorkbench().getActiveWorkbenchWindow();
+                            if (activeWindow != null) {
+                                shell = activeWindow.getShell();
+                            } else {
+                                shell = display.getActiveShell();
+                            }
+                            OpenContextChooseComboDialog dialog = new OpenContextChooseComboDialog(shell, allContexts);
+                            dialog.create();
+                            dialog.getShell().setMinimumSize(200, 200);
+                            if (Window.OK == dialog.open()) {
+                                selectedContext[0] = dialog.getSelectedContext();
+                            }
+                        }
+                    });
+                }
+            }
+        } catch (RuntimeException e) {
+            LOGGER.error("Error while selecting context: " + e.getMessage());
+        }
+
+        return selectedContext[0];
     }
 
     public enum Type {
