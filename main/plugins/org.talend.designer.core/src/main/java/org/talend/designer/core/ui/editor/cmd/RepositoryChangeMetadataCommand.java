2a8a6c074abd9b4504595cf4e08db0f983158518
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2020-11-02 15:11:42 +0800

    bugfix(TUP-29131) Replace jar name by maven uri for spark jobs (#5444)
    * fix(TUP-29131): Replace jar name by uri for spark job
    
    * fix(TUP-29131): Replace jar name by maven uri for spark jobs

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/RepositoryChangeMetadataCommand.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/ui/editor/cmd/RepositoryChangeMetadataCommand.java
@@ -368,11 +368,7 @@ public class RepositoryChangeMetadataCommand extends ChangeMetadataCommand {
                             if (value instanceof Map) {
                                 Map map = new HashMap();
                                 String driver = String.valueOf(((Map) value).get("drivers"));
-                                MavenArtifact artifact = MavenUrlHelper.parseMvnUrl(TalendTextUtils.removeQuotes(driver));
-                                if (artifact != null) {
-                                    driver = artifact.getFileName();
-                                }
-                                map.put("JAR_NAME", driver);
+                                map.put("JAR_NAME", TalendTextUtils.removeQuotes(driver));
                                 newValue.add(map);
                             }
                         }
