ffe5323d2f050cf2e42a2925a97627282de8049b
Chao MENG <cmeng@talend.com>
2021-01-28 12:08:13 +0800

    feat(TUP-30264): Force regeneration of signatures in the studio https://jira.talendforge.org/browse/TUP-30264

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/SecurityProjectSettingPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/preference/SecurityProjectSettingPage.java
@@ -12,23 +12,60 @@
 // ============================================================================
 package org.talend.repository.preference;
 
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.commons.lang3.StringUtils;
+import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRunnable;
+import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.IStatus;
+import org.eclipse.core.runtime.Status;
+import org.eclipse.core.runtime.jobs.ISchedulingRule;
+import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.events.SelectionAdapter;
+import org.eclipse.swt.events.SelectionEvent;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormLayout;
 import org.eclipse.swt.widgets.Button;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.ui.internal.progress.ProgressMonitorFocusJobDialog;
+import org.talend.commons.exception.LoginException;
+import org.talend.commons.exception.PersistenceException;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.CorePlugin;
+import org.talend.core.PluginChecker;
+import org.talend.core.model.general.Project;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
+import org.talend.repository.ProjectManager;
+import org.talend.repository.RepositoryWorkUnit;
 import org.talend.repository.i18n.Messages;
 import org.talend.repository.model.IProxyRepositoryFactory;
+
 
 /**
  * DOC wzhang class global comment. Detailled comment
  */
 public class SecurityProjectSettingPage extends ProjectSettingPage {
 
+    private static final String MIGRATION_TOKEN_KEY = "force_import_unsupported_job";
+
     private Button button;
+
+    private Button forceRegenSignatureBtn;
+
+    private Boolean hasMigrationToken;
 
     /*
      * (non-Javadoc)
@@ -37,19 +74,173 @@ public class SecurityProjectSettingPage extends ProjectSettingPage {
      */
     @Override
     protected Control createContents(Composite parent) {
-        Composite composite = new Composite(parent, SWT.NULL);
-        GridLayout layout = new GridLayout(2, false);
+        Composite composite = new Composite(parent, SWT.NONE);
+        FormLayout layout = new FormLayout();
         composite.setLayout(layout);
 
         button = new Button(composite, SWT.CHECK | SWT.TOP);
         button.setText(Messages.getString("SecurityProjectSettingPage.hidePass")); //$NON-NLS-1$
+        FormData layoutData = new FormData();
+        layoutData.top = new FormAttachment(0);
+        layoutData.left = new FormAttachment(0);
+        button.setLayoutData(layoutData);
 
         button.setSelection(pro.getEmfProject().isHidePassword());
         IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
         if (factory.isUserReadOnlyOnCurrentProject()) {
             composite.setEnabled(false);
         }
+
+        if (PluginChecker.isTIS() && hasMigrationToken()) {
+            layoutForceRegenSignature(composite);
+        }
         return composite;
+    }
+
+    private boolean hasMigrationToken() {
+        if (hasMigrationToken != null) {
+            return hasMigrationToken;
+        }
+        try {
+            hasMigrationToken = StringUtils.isNotBlank(System.getProperty(MIGRATION_TOKEN_KEY));
+        } catch (Exception e) {
+            hasMigrationToken = false;
+            ExceptionHandler.process(e);
+        }
+        return hasMigrationToken;
+    }
+
+    private void layoutForceRegenSignature(Composite parent) {
+        FormData layoutData = new FormData();
+        forceRegenSignatureBtn = new Button(parent, SWT.PUSH);
+        forceRegenSignatureBtn.setText(Messages.getString("SecurityProjectSettingPage.forceRegenSignature")); //$NON-NLS-1$
+        layoutData = new FormData();
+        layoutData.top = new FormAttachment(button, 15, SWT.BOTTOM);
+        layoutData.left = new FormAttachment(button, 0, SWT.LEFT);
+        forceRegenSignatureBtn.setLayoutData(layoutData);
+        forceRegenSignatureBtn.addSelectionListener(new SelectionAdapter() {
+
+            @Override
+            public void widgetSelected(SelectionEvent e) {
+                onForceRegenSignatureBtnSelected(e);
+            }
+
+        });
+
+        Label note = new Label(parent, SWT.NONE);
+        note.setText(Messages.getString("SecurityProjectSettingPage.forceRegenSignature.warn")); //$NON-NLS-1$
+        note.setForeground(Display.getDefault().getSystemColor(SWT.COLOR_RED));
+        layoutData = new FormData();
+        layoutData.top = new FormAttachment(forceRegenSignatureBtn, 5, SWT.BOTTOM);
+        layoutData.left = new FormAttachment(forceRegenSignatureBtn, 0, SWT.LEFT);
+        note.setLayoutData(layoutData);
+    }
+
+    private void onForceRegenSignatureBtnSelected(SelectionEvent event) {
+        Job forceRegenSignatureJob = new Job(Messages.getString("SecurityProjectSettingPage.forceRegenSignature")) { //$NON-NLS-1$
+
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+                regenerateSignature(monitor);
+                return Status.OK_STATUS;
+            }
+
+            @Override
+            protected void canceling() {
+                Thread thread = getThread();
+                if (thread != null) {
+                    thread.interrupt();
+                }
+                super.canceling();
+            }
+
+        };
+        forceRegenSignatureJob.schedule();
+
+        Shell shell = Display.getDefault().getActiveShell();
+        ProgressMonitorFocusJobDialog dialog = new ProgressMonitorFocusJobDialog(shell);
+        dialog.show(forceRegenSignatureJob, shell);
+    }
+
+    private void regenerateSignature(IProgressMonitor monitor) {
+        IProxyRepositoryFactory factory = ProxyRepositoryFactory.getInstance();
+        Project currentProject = ProjectManager.getInstance().getCurrentProject();
+        RepositoryWorkUnit rwu = new RepositoryWorkUnit(currentProject,
+                Messages.getString("SecurityProjectSettingPage.forceRegenSignature")) {
+
+            @Override
+            protected void run() throws LoginException, PersistenceException {
+
+                final IWorkspaceRunnable op = new IWorkspaceRunnable() {
+
+                    public void run(IProgressMonitor monitor) throws CoreException {
+                        Thread currentThread = Thread.currentThread();
+                        List<ERepositoryObjectType> repoObjTypes = Arrays
+                                .asList((ERepositoryObjectType[]) ERepositoryObjectType.values());
+                        int repoObjTypeSize = repoObjTypes.size();
+                        monitor.beginTask(Messages.getString("SecurityProjectSettingPage.forceRegenSignature.progress.begin"),
+                                repoObjTypeSize);
+                        for (ERepositoryObjectType repoObjType : repoObjTypes) {
+                            if (monitor.isCanceled() || currentThread.isInterrupted()) {
+                                ExceptionHandler.log(
+                                        Messages.getString("SecurityProjectSettingPage.forceRegenSignature.progress.cancel"));
+                                return;
+                            }
+                            monitor.setTaskName(
+                                    Messages.getString("SecurityProjectSettingPage.forceRegenSignature.progress.curRepoType",
+                                            repoObjType.getLabel()));
+                            try {
+                                List<IRepositoryViewObject> repoObjs = factory.getAll(currentProject, repoObjType, isValid(),
+                                        isControlCreated());
+                                if (monitor.isCanceled() || currentThread.isInterrupted()) {
+                                    ExceptionHandler.log(
+                                            Messages.getString("SecurityProjectSettingPage.forceRegenSignature.progress.cancel"));
+                                    return;
+                                }
+                                if (repoObjs != null) {
+                                    int repoObjSize = repoObjs.size();
+                                    Iterator<IRepositoryViewObject> iterator = repoObjs.iterator();
+                                    for (int i = 0; iterator.hasNext(); i++) {
+                                        if (monitor.isCanceled() || currentThread.isInterrupted()) {
+                                            ExceptionHandler.log(Messages
+                                                    .getString("SecurityProjectSettingPage.forceRegenSignature.progress.cancel"));
+                                            return;
+                                        }
+                                        IRepositoryViewObject next = iterator.next();
+                                        if (next == null) {
+                                            continue;
+                                        }
+                                        monitor.subTask(
+                                                Messages.getString("SecurityProjectSettingPage.forceRegenSignature.progress.save",
+                                                        repoObjType, i + "/" + repoObjSize, next.getLabel()));
+                                        try {
+                                            factory.save(next.getProperty().getItem());
+                                        } catch (Exception e) {
+                                            ExceptionHandler.process(e);
+                                        }
+                                    }
+                                }
+                            } catch (PersistenceException e) {
+                                ExceptionHandler.process(e);
+                            } finally {
+                                monitor.worked(1);
+                            }
+                        }
+                    }
+                };
+
+                IWorkspace workspace = ResourcesPlugin.getWorkspace();
+                ISchedulingRule refreshRule = workspace.getRuleFactory()
+                        .refreshRule(ProjectManager.getInstance().getResourceProject(currentProject.getEmfProject()));
+                try {
+                    workspace.run(op, refreshRule, IWorkspace.AVOID_UPDATE, monitor);
+                } catch (CoreException e) {
+                    throw new PersistenceException(e);
+                }
+
+            }
+        };
+        factory.executeRepositoryWorkUnit(rwu);
     }
 
     /*
