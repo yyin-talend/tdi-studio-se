cf55315820131b4fab9c9d3ed94d759488754e29
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2021-02-03 18:02:38 +0800

    fix(TUP-29764): encrypt all PASSWORD fields of job in new format (#5760)
    * fix(TUP-29764): migrate all PASSWORD fields of job
    
    * fix(TUP-29764): Resolve comments

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/UpgradePasswordEncryptionAlg4ItemMigrationTask.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/UpgradePasswordEncryptionAlg4ItemMigrationTask.java
@@ -278,7 +278,7 @@ public class UpgradePasswordEncryptionAlg4ItemMigrationTask extends UnifyPasswor
                 if (p instanceof ElementParameterType) {
                     ElementParameterType param = (ElementParameterType) p;
                     // variable name used for Stat&Logs
-                    if ("PASS".equals(param.getName())) { //$NON-NLS-1$
+                    if ("PASS".equals(param.getName()) || EParameterFieldType.PASSWORD.getName().equals(param.getField())) { //$NON-NLS-1$
                         if (reencryptValueIfNeeded(param)) {
                             modified = true;
                         }
