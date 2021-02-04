c5fdda74a155fa7b8513a221a7d675bc687f06c5
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2021-02-04 10:47:48 +0800

    fix(TUP-29764): encrypt all PASSWORD fields of job in new format (#5760) (#5813)
    * fix(TUP-29764): migrate all PASSWORD fields of job
    
    * fix(TUP-29764): Resolve comments

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/FixUpgradePasswordEncryptionAlg4ItemMigrationTask.java
@@ -0,0 +1,134 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2019 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.repository.model.migration;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.runtime.model.emf.EmfHelper;
+import org.talend.core.model.process.EParameterFieldType;
+import org.talend.core.model.properties.Item;
+import org.talend.core.model.properties.JobletProcessItem;
+import org.talend.core.model.properties.ProcessItem;
+import org.talend.core.model.repository.ERepositoryObjectType;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.ParametersType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+import org.talend.utils.security.PasswordMigrationUtil;
+import org.talend.utils.security.StudioEncryption;
+
+public class FixUpgradePasswordEncryptionAlg4ItemMigrationTask extends UnifyPasswordEncryption4ItemMigrationTask {
+
+    @Override
+    public List<ERepositoryObjectType> getTypes() {
+        List<ERepositoryObjectType> toReturn = new ArrayList<ERepositoryObjectType>();
+        toReturn.addAll(ERepositoryObjectType.getAllTypesOfProcess());
+        toReturn.addAll(ERepositoryObjectType.getAllTypesOfProcess2());
+        toReturn.addAll(ERepositoryObjectType.getAllTypesOfTestContainer());
+        return toReturn;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.talend.core.model.migration.AbstractItemMigrationTask#execute(org .talend.core.model.properties.Item)
+     */
+    @Override
+    public ExecutionResult execute(Item item) {
+        boolean modified = false;
+        try {
+            if (item instanceof ProcessItem) {
+                ProcessItem processItem = (ProcessItem) item;
+                modified = updateProcessItem(item, processItem.getProcess());
+            } else if (item instanceof JobletProcessItem) {
+                JobletProcessItem jobletItem = (JobletProcessItem) item;
+                modified = updateProcessItem(item, jobletItem.getJobletProcess());
+            }
+        } catch (Exception ex) {
+            ExceptionHandler.process(ex);
+            return ExecutionResult.FAILURE;
+        }
+
+        if (modified) {
+            try {
+                factory.save(item, true);
+                return ExecutionResult.SUCCESS_NO_ALERT;
+            } catch (Exception ex) {
+                ExceptionHandler.process(ex);
+                return ExecutionResult.FAILURE;
+            }
+        }
+        return ExecutionResult.NOTHING_TO_DO;
+    }
+
+
+    private boolean updateProcessItem(Item item, ProcessType processType) throws Exception {
+        EmfHelper.visitChilds(processType);
+
+        boolean modified = false;
+
+        // job settings
+        if (checkJobsettintsParameter(item, processType)) {
+            modified = true;
+        }
+
+        return modified;
+    }
+
+    protected boolean checkJobsettintsParameter(Item item, ProcessType processType) throws Exception {
+        boolean modified = false;
+
+        ParametersType parameters = processType.getParameters();
+        if (parameters != null) {
+            for (Object p : parameters.getElementParameter()) {
+                if (p instanceof ElementParameterType) {
+                    ElementParameterType param = (ElementParameterType) p;
+                    // variable name used for Stat&Logs
+                    if (EParameterFieldType.PASSWORD.getName().equals(param.getField())) { // $NON-NLS-1$
+                        if (reencryptValueIfNeeded(param)) {
+                            modified = true;
+                        }
+                    }
+                }
+            }
+        }
+        return modified;
+    }
+
+
+    protected static boolean reencryptValueIfNeeded(ElementParameterType param) throws Exception {
+        String value = param.getValue();
+        if (value != null && !StudioEncryption.hasEncryptionSymbol(value)) {
+            String decryptValue = PasswordMigrationUtil.decryptPassword(value);
+            if (decryptValue != null) {
+                param.setRawValue(decryptValue);
+            }
+            return true;
+        }
+        return false;
+    }
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.talend.core.model.migration.IProjectMigrationTask#getOrder()
+     */
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2021, 01, 25, 12, 0, 0);
+        return gc.getTime();
+    }
+}
