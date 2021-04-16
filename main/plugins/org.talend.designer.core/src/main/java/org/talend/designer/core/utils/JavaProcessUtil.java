ac5cc1ee1d2fc34076b3a960b231ad71b030508d
Jane Ding <jding@talend.com>
2021-04-16 19:19:19 +0800

    fix(TUP-30992):CVE: junit-4.11,4.13 (#6110)
    https://jira.talendforge.org/browse/TUP-30992
    
    Signed-off-by: jding-tlnd <jding@talend.com>

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
@@ -473,7 +473,7 @@ public class JavaProcessUtil {
 
     public static void addJunitNeededModules(Collection<ModuleNeeded> modulesNeeded) {
         ModuleNeeded junitModule = new ModuleNeeded("junit", "junit.jar", null, true); //$NON-NLS-1$ //$NON-NLS-2$
-        junitModule.setMavenUri("mvn:junit/junit/4.13/jar");
+        junitModule.setMavenUri("mvn:junit/junit/4.13.2/jar");
         modulesNeeded.add(junitModule);
         //junit4 required dependencies
         ModuleNeeded requiredMoule = new ModuleNeeded("hamcrest-core", "hamcrest-core-1.3.jar", null, true);
