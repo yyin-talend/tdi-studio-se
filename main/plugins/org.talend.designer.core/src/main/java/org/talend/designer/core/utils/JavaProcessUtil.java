78875aa07f8693d279aa5d46977ff6a3e52a79f0
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2021-04-19 14:18:14 +0800

    chore(TUP-30992): fix junits (#6113)

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/utils/JavaProcessUtil.java
@@ -472,8 +472,8 @@ public class JavaProcessUtil {
     }
 
     public static void addJunitNeededModules(Collection<ModuleNeeded> modulesNeeded) {
-        ModuleNeeded junitModule = new ModuleNeeded("junit", "junit.jar", null, true); //$NON-NLS-1$ //$NON-NLS-2$
-        junitModule.setMavenUri("mvn:junit/junit/4.13/jar");
+        ModuleNeeded junitModule = new ModuleNeeded("junit-4.13.2", "junit-4.13.2.jar", null, true); //$NON-NLS-1$ //$NON-NLS-2$
+        junitModule.setMavenUri("mvn:junit/junit/4.13.2/jar");
         modulesNeeded.add(junitModule);
         //junit4 required dependencies
         ModuleNeeded requiredMoule = new ModuleNeeded("hamcrest-core", "hamcrest-core-1.3.jar", null, true);
