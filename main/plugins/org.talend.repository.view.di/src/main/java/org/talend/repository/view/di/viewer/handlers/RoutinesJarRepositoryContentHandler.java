703814356061898fe89a46c376acaae70af1c18d
Zhiwei Xue <zwxue@talend.com>
2021-04-21 18:17:09 +0800

    fix(TUP-31246):Inner routine node disappeared after refresh repository (#6131)
    view

--- main/plugins/org.talend.repository.view.di/src/main/java/org/talend/repository/view/di/viewer/handlers/RoutinesJarRepositoryContentHandler.java
+++ main/plugins/org.talend.repository.view.di/src/main/java/org/talend/repository/view/di/viewer/handlers/RoutinesJarRepositoryContentHandler.java
@@ -30,7 +30,6 @@ import org.talend.core.model.repository.IRepositoryViewObject;
 import org.talend.core.model.repository.RepositoryViewObject;
 import org.talend.core.model.routines.CodesJarInfo;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
-import org.talend.core.utils.CodesJarResourceCache;
 import org.talend.repository.model.IRepositoryNode.ENodeType;
 import org.talend.repository.model.IRepositoryNode.EProperties;
 import org.talend.repository.model.RepositoryNode;
@@ -76,15 +75,17 @@ public class RoutinesJarRepositoryContentHandler extends AbstractRepositoryConte
     @Override
     public void addNode(ERepositoryObjectType type, RepositoryNode recBinNode, IRepositoryViewObject repositoryObject,
             RepositoryNode node) {
-        if (type != ERepositoryObjectType.ROUTINESJAR) {
+        if (type != ERepositoryObjectType.ROUTINESJAR
+                || repositoryObject.getRepositoryObjectType() != ERepositoryObjectType.ROUTINESJAR) {
             return;
         }
-        CodesJarInfo info = CodesJarResourceCache.getCodesJarById(node.getId());
-        if (info == null) {
+        Property property = repositoryObject.getProperty();
+        if (property == null) {
             return;
         }
         try {
-            List<IRepositoryViewObject> innerRoutinesObj = ProxyRepositoryFactory.getInstance().getAllInnerCodes(info);
+            List<IRepositoryViewObject> innerRoutinesObj = ProxyRepositoryFactory.getInstance()
+                    .getAllInnerCodes(CodesJarInfo.create(property));
             for (IRepositoryViewObject innerRoutineObj : innerRoutinesObj) {
                 Property innerRoutineProperty = innerRoutineObj.getProperty();
                 RepositoryNode innerRoutineNode = new RepositoryNode(new RepositoryViewObject(innerRoutineProperty),
