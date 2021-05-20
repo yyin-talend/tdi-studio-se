1199c9e202cce2a28e3aa27adc5ef45e5cb87c02
apoltavtsev <apoltavtsev@gmail.com>
2021-05-20 08:55:48 +0200

    fix(APPINT-32953) Routelets are built before Route
    * fix(APPINT-32953) Routelets are built before Route
    * NPE is corrected
    * chore(APPINT-32953) fix the NPE when using DI license

--- main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/MavenJavaProcessor.java
+++ main/plugins/org.talend.designer.runprocess/src/main/java/org/talend/designer/runprocess/maven/MavenJavaProcessor.java
@@ -388,7 +388,9 @@ public class MavenJavaProcessor extends JavaProcessor {
         boolean isGoalPackage = TalendMavenConstants.GOAL_PACKAGE.equals(goal);
         boolean isGoalInstall = TalendMavenConstants.GOAL_INSTALL.equals(goal);
         boolean isMainJob = LastGenerationInfo.getInstance().isCurrentMainJob();
-        if (!isMainJob && isGoalInstall) {
+        boolean isRoutelet =
+            ERepositoryObjectType.PROCESS_ROUTELET != null && getProperty() != null && ERepositoryObjectType.PROCESS_ROUTELET.equals(ERepositoryObjectType.getType(getProperty()));
+        if ((!isMainJob && isGoalInstall) || isRoutelet) {
             if (!buildCacheManager.isJobBuild(getProperty())) {
                 deleteExistedJobJarFile(talendJavaProject);
                 String buildType = getBuildType(getProperty());
