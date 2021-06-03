ba36adadb07f2b503165d2cc8bdf939b8a25dbec
bhe-talendbj <52311292+bhe-talendbj@users.noreply.github.com>
2021-06-03 16:14:29 +0800

    chore(TUP-29803): init ERepositoryObjectType in background (#6246)
    * chore(TUP-29803): init ERepositoryObjectType in background
    
    * chore(): init ERepositoryObjectType

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginProjectPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/login/LoginProjectPage.java
@@ -96,6 +96,7 @@ import org.talend.core.model.general.ConnectionBean;
 import org.talend.core.model.general.Project;
 import org.talend.core.model.properties.ProjectReference;
 import org.talend.core.model.properties.User;
+import org.talend.core.model.repository.ERepositoryObjectType;
 import org.talend.core.model.repository.SVNConstant;
 import org.talend.core.repository.model.IRepositoryFactory;
 import org.talend.core.repository.model.ProxyRepositoryFactory;
@@ -251,6 +252,7 @@ public class LoginProjectPage extends AbstractLoginActionPage {
         finishButtonAction = FINISH_ACTION_OPEN_PROJECT;
         loginHelper = LoginHelper.getInstance();
         loginFetchLicenseHelper = LoginFetchLicenseHelper.getInstance();
+        initRepositoryNodeProviderRegistryReader();
     }
 
     private void scheduleCheckSandboxJob() {
@@ -2773,5 +2775,18 @@ public class LoginProjectPage extends AbstractLoginActionPage {
             }
             return hasError || super.hasError();
         }
+    }
+
+    private void initRepositoryNodeProviderRegistryReader() {
+        new Job("Init RepositoryNodeProviderRegistryReader") {
+
+            @Override
+            protected IStatus run(IProgressMonitor monitor) {
+                // just invoke this to initialize
+                ERepositoryObjectType t = ERepositoryObjectType.PROCESS;
+                return Status.OK_STATUS;
+            }
+
+        }.schedule();
     }
 }
