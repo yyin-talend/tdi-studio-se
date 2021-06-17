d090841fb0ec081477da8d0c94ea3ea6cbfdc79a
jzhao-talend <jzhao@talend.com>
2021-06-17 22:12:39 +0800

    fix(TDI-46297):FCB 'EnableRegionalDisco' is disabled error when using tMicrosoftCrmInput

---
+++ main/plugins/org.talend.designer.components.libs/libs_src/talend-mscrm/src/main/java/org/talend/ms/crm/sdk/Instance.java
@@ -0,0 +1,85 @@
+package org.talend.ms.crm.sdk;
+
+public class Instance {
+
+	public String id;
+	
+	public String uniqueName;
+	
+	public String urlName;
+	
+	public String friendlyName;
+	
+	public String state;
+	
+	public String version;
+	
+	public String url;
+	
+	public String apiUrl;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getUniqueName() {
+		return uniqueName;
+	}
+
+	public void setUniqueName(String uniqueName) {
+		this.uniqueName = uniqueName;
+	}
+
+	public String getUrlName() {
+		return urlName;
+	}
+
+	public void setUrlName(String urlName) {
+		this.urlName = urlName;
+	}
+
+	public String getFriendlyName() {
+		return friendlyName;
+	}
+
+	public void setFriendlyName(String friendlyName) {
+		this.friendlyName = friendlyName;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
+	public String getUrl() {
+		return url;
+	}
+
+	public void setUrl(String url) {
+		this.url = url;
+	}
+
+	public String getApiUrl() {
+		return apiUrl;
+	}
+
+	public void setApiUrl(String apiUrl) {
+		this.apiUrl = apiUrl;
+	}
+
+}
