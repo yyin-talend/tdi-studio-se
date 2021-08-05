0f9548705ea89988f38a5530c6afcc45cc8b8bbd
qiongli <qiongli@LT-6TNJ593.talend.com>
2021-08-05 17:59:24 +0800

    fix(TDQ-19637):Judge if it includes 'tdqReportRun' in main/sub-job when generate code.

--- main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
+++ main/plugins/org.talend.designer.publish.core/src/org/talend/designer/publish/core/actions/AbstractPublishJobAction.java
@@ -117,7 +117,6 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
             exportChoiceMap.put(ExportChoice.needContext, true);
 
             ProcessItem processItem = (ProcessItem) node.getObject().getProperty().getItem();
-            exportItemForDQComponent(processItem);
 
             String contextName = (String) exportChoiceMap.get(ExportChoice.contextName);
             if (contextName == null) {
@@ -148,17 +147,6 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
         }
     }
 
-    /**
-     * TDQ-15391: when have tDqReportRun, must always export items.
-     *
-     * @param processItem
-     */
-    private void exportItemForDQComponent(ProcessItem processItem) {
-        if (EmfModelUtils.getComponentByName(processItem, "tDqReportRun") != null) { //$NON-NLS-1$
-            exportChoiceMap.put(ExportChoice.needJobItem, true);
-        }
-    }
-
     private void exportJobForPOJO(IProgressMonitor monitor) throws InvocationTargetException, InterruptedException {
         File tmpJob = null;
         try {
@@ -178,7 +166,6 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
             exportChoiceMap.put(ExportChoice.addStatistics, true);
 
             ProcessItem processItem = (ProcessItem) node.getObject().getProperty().getItem();
-            exportItemForDQComponent(processItem);
 
             String contextName = (String) exportChoiceMap.get(ExportChoice.contextName);
             if (contextName == null) {
@@ -219,7 +206,6 @@ public abstract class AbstractPublishJobAction implements IRunnableWithProgress 
             exportChoiceMap.put(ExportChoice.addStatistics, true);
 
             ProcessItem processItem = (ProcessItem) node.getObject().getProperty().getItem();
-            exportItemForDQComponent(processItem);
 
             String contextName = (String) exportChoiceMap.get(ExportChoice.contextName);
             if (contextName == null) {
--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JavaJobScriptsExportWSWizardPage.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/ui/wizards/exportjob/JavaJobScriptsExportWSWizardPage.java
@@ -1166,13 +1166,7 @@ public class JavaJobScriptsExportWSWizardPage extends JavaJobScriptsExportWizard
         exportChoiceMap.put(ExportChoice.needTalendLibraries, Boolean.TRUE);
         exportChoiceMap.put(ExportChoice.needMetaInfo, true);
         exportChoiceMap.put(ExportChoice.binaries, true);
-        // TDQ-15391: when have tDqReportRun, must always export items.
-        if (EmfModelUtils.getComponentByName(getProcessItem(), "tDqReportRun") != null) { //$NON-NLS-1$
-            exportChoiceMap.put(ExportChoice.needJobItem, Boolean.TRUE);
-        } else {
-            exportChoiceMap.put(ExportChoice.needJobItem, Boolean.FALSE);
-        }
-        // TDQ-15391~
+        exportChoiceMap.put(ExportChoice.needJobItem, Boolean.FALSE);
         exportChoiceMap.put(ExportChoice.needSourceCode, Boolean.FALSE);
         exportChoiceMap.put(ExportChoice.needDependencies, Boolean.TRUE);
         exportChoiceMap.put(ExportChoice.needJobScript, Boolean.FALSE);
@@ -1206,13 +1200,7 @@ public class JavaJobScriptsExportWSWizardPage extends JavaJobScriptsExportWizard
         exportChoiceMap.put(ExportChoice.needSystemRoutine, Boolean.TRUE);
         exportChoiceMap.put(ExportChoice.needUserRoutine, Boolean.TRUE);
         exportChoiceMap.put(ExportChoice.needTalendLibraries, Boolean.TRUE);
-        // TDQ-15391: when have tDqReportRun, must always export items.
-        if (EmfModelUtils.getComponentByName(getProcessItem(), "tDqReportRun") != null) { //$NON-NLS-1$
-            exportChoiceMap.put(ExportChoice.needJobItem, Boolean.TRUE);
-        } else {
-            exportChoiceMap.put(ExportChoice.needJobItem, Boolean.FALSE);
-        }
-        // TDQ-15391~
+        exportChoiceMap.put(ExportChoice.needJobItem, Boolean.FALSE);
         exportChoiceMap.put(ExportChoice.needSourceCode, Boolean.FALSE);
         exportChoiceMap.put(ExportChoice.needDependencies, Boolean.TRUE);
         exportChoiceMap.put(ExportChoice.needJobScript, Boolean.FALSE);
