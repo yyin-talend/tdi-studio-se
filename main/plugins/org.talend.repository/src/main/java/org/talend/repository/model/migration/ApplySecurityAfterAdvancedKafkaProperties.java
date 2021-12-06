07c7e9659e179653fe25cc7a06a359bee036ed6a
olhava <ovarnavina@talend.com>
2021-12-06 14:14:49 +0200

    feat(TDI-46430): Kafka Input Output Avro support
    * add migration task for security settings setup order
    
    # Conflicts:
    #	main/plugins/org.talend.repository/plugin.xml

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/ApplySecurityAfterAdvancedKafkaProperties.java
@@ -0,0 +1,117 @@
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.eclipse.emf.common.util.EList;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ElementParameterType;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class ApplySecurityAfterAdvancedKafkaProperties extends AbstractJobMigrationTask {
+
+    // Components
+    private static final String KAFKA_INPUT = "tKafkaInput";
+    private static final String KAFKA_OUTPUT = "tKafkaOutput";
+    private static final String KAFKA_CONNECTION = "tKafkaConnection";
+
+    // Properties
+    private static final String USE_EXISTING_CONNECTION = "USE_EXISTING_CONNECTION";
+    private static final String CONNECTION = "CONNECTION";
+    private static final String USE_HTTPS = "USE_HTTPS";
+    private static final String USE_KRB = "USE_KRB";
+    private static final String APPLY_SECURITY_PROPERTIES_AFTER_ADVANCED = "APPLY_SECURITY_PROPERTIES_AFTER_ADVANCED";
+
+    @Override
+    public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2021, 12, 2, 13, 0, 0);
+        return gc.getTime();
+    }
+
+    @Override
+    public ExecutionResult execute(Item item) {
+        final ProcessType processType = getProcessType(item);
+
+        try {
+            IComponentFilter kafkaInputFilter = new NameComponentFilter(KAFKA_INPUT);
+            ModifyComponentsAction.searchAndModify(item, processType, kafkaInputFilter,
+                    Arrays.<IComponentConversion> asList(new KafkaComponentConversion(processType)));
+
+            IComponentFilter kafkaOutputFilter = new NameComponentFilter(KAFKA_OUTPUT);
+            ModifyComponentsAction.searchAndModify(item, processType, kafkaOutputFilter,
+                    Arrays.<IComponentConversion> asList(new KafkaComponentConversion(processType)));
+
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+    }
+
+    private NodeType searchNodeTypeByUniqueName(ProcessType processType, String componentName, String uniqueName) {
+        if (processType == null || componentName == null || (uniqueName == null || "".equals(uniqueName.trim()))) {
+            return null;
+        }
+        NodeType searchNode = null;
+        EList<NodeType> nodes = processType.getNode();
+        for (NodeType node : nodes) {
+            if (node != null && componentName.equals(node.getComponentName())) {
+                ElementParameterType uniqueNameParam = ComponentUtilities.getNodeProperty(node, "UNIQUE_NAME");
+                if (uniqueNameParam != null && uniqueNameParam.getValue().equals(uniqueName)) {
+                    searchNode = node;
+                    break;
+                }
+            }
+        }
+
+        return searchNode;
+    }
+
+    private class KafkaComponentConversion implements IComponentConversion {
+
+        private ProcessType processType;
+
+        public KafkaComponentConversion(ProcessType processType) {
+            this.processType = processType;
+        }
+
+        @Override
+        public void transform(NodeType node) {
+            NodeType connectionPropertiesNode = node;
+
+            ElementParameterType useExistingConnection =
+                    ComponentUtilities.getNodeProperty(node, USE_EXISTING_CONNECTION);
+            if (useExistingConnection != null && "true".equalsIgnoreCase(useExistingConnection.getValue())) {
+                ElementParameterType connection = ComponentUtilities.getNodeProperty(node, CONNECTION);
+                if (connection != null) {
+                    NodeType connectionNode =
+                            searchNodeTypeByUniqueName(processType, KAFKA_CONNECTION, connection.getValue());
+                    if (connectionNode != null) {
+                        connectionPropertiesNode = connectionNode;
+                    }
+                }
+            }
+
+            ElementParameterType useHttps = ComponentUtilities.getNodeProperty(connectionPropertiesNode, USE_HTTPS);
+            ElementParameterType useKrb = ComponentUtilities.getNodeProperty(connectionPropertiesNode, USE_KRB);
+
+            if ((useHttps != null && "true".equalsIgnoreCase(useHttps.getValue()))
+                    || (useKrb != null && "true".equalsIgnoreCase(useKrb.getValue()))) {
+
+                if (ComponentUtilities.getNodeProperty(node, APPLY_SECURITY_PROPERTIES_AFTER_ADVANCED) == null) {
+                    ComponentUtilities.addNodeProperty(node, APPLY_SECURITY_PROPERTIES_AFTER_ADVANCED, "CHECK");
+                    ComponentUtilities.getNodeProperty(node, APPLY_SECURITY_PROPERTIES_AFTER_ADVANCED).setValue("true");
+                }
+            }
+        }
+    }
+}
