33809a44b2b7b94deeb36de3a500b651bb08b1e8
Tetiana Meronyk <44087449+Tanya5091@users.noreply.github.com>
2022-04-14 12:43:37 +0300

    feat(TDI-47263): Upgrade Redshift driver to v2.x (#7056)
    * feat(TDI-47263): Upgrade Redshift driver to v2.x
    
    * Add dependency aws-java-sdk-redshift-internal-1.12.x.jar
    
    * UI change
    
    * Connection javajet
    
    * Connection javajet change
    
    * Row generate code
    
    * Migration task
    
    * input compile error
    
    * input compile error
    
    * add back jar version
    
    * add back jar version
    
    * class version
    
    * migration add tCreate table
    
    * feat(TUP-35069):Upgrade Redshift driver to v2.x (#7411)
    
    https://jira.talendforge.org/browse/TUP-35069
    
    Co-authored-by: pyzhou <pyzhou@talend.com>
    Co-authored-by: Jane Ding <jding@talend.com>

---
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/RedshiftVersion2MigrationTask.java
@@ -0,0 +1,86 @@
+package org.talend.repository.model.migration;
+
+import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.commons.exception.PersistenceException;
+import org.talend.core.language.ECodeLanguage;
+import org.talend.core.model.components.ComponentUtilities;
+import org.talend.core.model.components.ModifyComponentsAction;
+import org.talend.core.model.components.conversions.IComponentConversion;
+import org.talend.core.model.components.filters.IComponentFilter;
+import org.talend.core.model.components.filters.NameComponentFilter;
+import org.talend.core.model.migration.AbstractJobMigrationTask;
+import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+
+public class RedshiftVersion2MigrationTask extends AbstractJobMigrationTask {
+
+    @Override public ExecutionResult execute(final Item item) {
+        final ProcessType processType = getProcessType(item);
+        if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
+            return ExecutionResult.NOTHING_TO_DO;
+        }
+        String[] compNames = { "tRedshiftBulkExec","tRedshiftConnection","tRedshiftInput","tRedshiftOutput","tRedshiftOutputBulkExec","tRedshiftRow","tRedshiftUnload" }; //$NON-NLS-1$
+
+        IComponentConversion action = node -> {
+            if (node == null) {
+                return;
+            }
+            if (ComponentUtilities.getNodeProperty(node, "DRIVER_VERSION") == null ) {
+                ComponentUtilities.addNodeProperty(node, "DRIVER_VERSION", "CLOSED_LIST");//$NON-NLS-1$ //$NON-NLS-2$
+                ComponentUtilities.getNodeProperty(node, "DRIVER_VERSION")
+                        .setValue("DRIVER_V1");//$NON-NLS-1$ //$NON-NLS-2$
+
+            }
+
+        };
+
+        IComponentConversion createTableAction = node -> {
+            if (node == null) {
+                return;
+            }
+            if (ComponentUtilities.getNodeProperty(node, "REDSHIFT_DRIVER_VERSION") == null ) {
+                ComponentUtilities.addNodeProperty(node, "REDSHIFT_DRIVER_VERSION", "CLOSED_LIST");//$NON-NLS-1$ //$NON-NLS-2$
+                ComponentUtilities.getNodeProperty(node, "REDSHIFT_DRIVER_VERSION")
+                        .setValue("DRIVER_V1");//$NON-NLS-1$ //$NON-NLS-2$
+
+            }
+
+        };
+
+        boolean modified = false;
+
+        IComponentFilter createTableFilter = new NameComponentFilter("tCreateTable");
+
+        try {
+            modified |= ModifyComponentsAction.searchAndModify(item, processType, createTableFilter, Arrays.asList(createTableAction));
+        } catch (PersistenceException e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
+        }
+
+
+
+        for (String name : compNames) {
+            IComponentFilter filter = new NameComponentFilter(name);
+
+            try {
+                modified |= ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays.asList(action));
+            } catch (PersistenceException e) {
+                ExceptionHandler.process(e);
+                return ExecutionResult.FAILURE;
+            }
+        }
+
+        return modified ? ExecutionResult.SUCCESS_NO_ALERT : ExecutionResult.NOTHING_TO_DO;
+    }
+
+    @Override public Date getOrder() {
+        GregorianCalendar gc = new GregorianCalendar(2022, Calendar.MARCH, 23, 0, 0, 0);
+        return gc.getTime();
+    }
+}
