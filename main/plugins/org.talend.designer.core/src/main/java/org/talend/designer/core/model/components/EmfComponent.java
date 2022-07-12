c8e9707bb912b96c650dc137a6a2df430a884b9f
jiezhang-tlnd <40561780+jiezhang-tlnd@users.noreply.github.com>
2022-07-12 16:58:14 +0800

    Revert "feat(TUP-35808)Generate module name from maven url (#7751)" (#7759)
    This reverts commit 7365a191eae0dec43847a8dabb12aff4ea26b422.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EmfComponent.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/components/EmfComponent.java
@@ -160,7 +160,6 @@ import org.talend.hadoop.distribution.model.DistributionVersionModule;
 import org.talend.hadoop.distribution.model.DynamicDistributionVersion;
 import org.talend.hadoop.distribution.utils.ComponentConditionUtil;
 import org.talend.librariesmanager.model.ModulesNeededProvider;
-import org.talend.librariesmanager.model.service.LocalLibraryManager;
 import org.talend.librariesmanager.prefs.LibrariesManagerUtils;
 
 /**
@@ -3330,7 +3329,6 @@ public class EmfComponent extends AbstractBasicComponent {
                 ExceptionHandler.process(e);
             }
         }
-        boolean isExtComponentProvider = LocalLibraryManager.isExtComponentProvider(provider.getId());
         for (DistributionVersionModule versionModule : dv.getVersionModules()) {
             IMPORTType importType = ComponentFactory.eINSTANCE.createIMPORTType();
             importType.setMODULEGROUP(versionModule.moduleGroup.getModuleName());
@@ -3338,8 +3336,7 @@ public class EmfComponent extends AbstractBasicComponent {
             importType.setREQUIREDIF(versionModule.getModuleRequiredIf().getConditionString());
             ModulesNeededProvider
             .collectModuleNeeded(compName, // $NON-NLS-1$
-                            importType, cachedModuleNeededList, versionModule.distributionVersion.getVersion(),
-                            isExtComponentProvider);
+                    importType, cachedModuleNeededList, versionModule.distributionVersion.getVersion());
         }
         distributionModuleNeededMap.put(dv, cachedModuleNeededList);
         moduleNeededList.addAll(cachedModuleNeededList);
@@ -3410,7 +3407,6 @@ public class EmfComponent extends AbstractBasicComponent {
         Set<DistributionModuleGroup> nodeModuleGroups = hc.getModuleGroups(hadoopLibParamName, compName);
         if (nodeModuleGroups != null) {
             Iterator<DistributionModuleGroup> moduleGroups = nodeModuleGroups.iterator();
-            boolean isExtComponentProvider = LocalLibraryManager.isExtComponentProvider(provider.getId());
             while (moduleGroups.hasNext()) {
                 DistributionModuleGroup group = moduleGroups.next();
                 IMPORTType importType = ComponentFactory.eINSTANCE.createIMPORTType();
@@ -3421,8 +3417,7 @@ public class EmfComponent extends AbstractBasicComponent {
                     importType.setREQUIREDIF(new NestedComponentCondition(condition).getConditionString());
                 }
                 importType.setMRREQUIRED(group.isMrRequired());
-                ModulesNeededProvider.collectModuleNeeded(compName, importType, cachedModuleNeededList, hc.getVersion(),
-                        isExtComponentProvider);
+                ModulesNeededProvider.collectModuleNeeded(compName, importType, cachedModuleNeededList, hc.getVersion());
             }
         }
         hadoopLibModuleNeededMap.put(hc, cachedModuleNeededList);
@@ -3464,10 +3459,8 @@ public class EmfComponent extends AbstractBasicComponent {
 
         Set<String> moduleNames = new HashSet<>();
         List<IMPORTType> importTypes = info.getImportType();
-        boolean isExtComponentProvider = LocalLibraryManager.isExtComponentProvider(provider.getId());
         for (IMPORTType importType : importTypes) {
-            ModulesNeededProvider.collectModuleNeeded(this.getName(), importType, componentImportNeedsList,
-                    isExtComponentProvider);
+            ModulesNeededProvider.collectModuleNeeded(this.getName(), importType, componentImportNeedsList);
         }
         for (String name : info.getComponentNames()) {
             IComponent component = null;
