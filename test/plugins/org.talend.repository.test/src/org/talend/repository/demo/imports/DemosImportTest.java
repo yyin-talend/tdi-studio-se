b0d427b93608c2bf5baf6f692f0e32b15a965291
zyuan-talend <72058919+zyuan-talend@users.noreply.github.com>
2022-08-09 11:25:55 +0800

    fix(TUP-35909):update junit test. (#7854)

--- test/plugins/org.talend.repository.test/src/org/talend/repository/demo/imports/DemosImportTest.java
+++ test/plugins/org.talend.repository.test/src/org/talend/repository/demo/imports/DemosImportTest.java
@@ -326,7 +326,7 @@ public abstract class DemosImportTest {
 	protected void collectDemoData(String rootPath) throws PersistenceException {
 		Map<String, String> map = getCollectFolderMap(rootPath);
 		for (String path : map.keySet()) {
-			Assert.assertTrue(new File(path).exists());
+            Assert.assertTrue("Path:" + path + " does not exist!", new File(path).exists());
 			File tempFolder = new File(path);
 			List<File> itemFiles = DemoImportTestUtil.collectProjectFilesFromDirectory(tempFolder, map.get(path), true);
 			demoDataMap.put(path, itemFiles);
