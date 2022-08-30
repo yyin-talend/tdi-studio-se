d5df42537dba9241a8bfbcf28c50aceef41f13cd
pyzhou <pyzhou@talend.com>
2022-08-31 02:17:45 +0800

    fix(TDI-48443):backport TDI-47447 (#7917)
    * fix(TDI-48443):backport TDI-47447
    
    * correct name

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/TSendMailOAuthMigrationTask.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/TSendMailOAuthMigrationTask.java
@@ -15,46 +15,48 @@ import org.talend.core.model.components.filters.IComponentFilter;
 import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.Item;
+import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
 import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
+import org.talend.migration.IMigrationTask.ExecutionResult;
 
 public class TSendMailOAuthMigrationTask extends AbstractJobMigrationTask {
-
-    @Override public ExecutionResult execute(final Item item) {
-        final ProcessType processType = getProcessType(item);
+    
+    @Override
+    public ExecutionResult execute(Item item) {
+        ProcessType processType = getProcessType(item);
         if (getProject().getLanguage() != ECodeLanguage.JAVA || processType == null) {
             return ExecutionResult.NOTHING_TO_DO;
         }
-        String[] compNames = { "tSendMail"}; //$NON-NLS-1$
+        String[] componentsName = new String[] { "tSendMail" };
 
-        IComponentConversion action = node -> {
-            if (node == null) {
-                return;
+        try {
+            for (int i = 0; i < componentsName.length; i++) {
+                IComponentFilter filter = new NameComponentFilter(componentsName[i]);
+                ModifyComponentsAction.searchAndModify(item, processType, filter,
+                        Arrays.<IComponentConversion> asList(new IComponentConversion() {
+
+                            public void transform(NodeType node) {
+                                if (node == null) {
+                                    return;
+                                }
+                                if (ComponentUtilities.getNodeProperty(node, "AUTH_MODE") == null ) {
+                                        ComponentUtilities.addNodeProperty(node, "AUTH_MODE", "CLOSED_LIST");//$NON-NLS-1$ //$NON-NLS-2$
+                                    boolean needAuth = "true".equals(ComponentUtilities.getNodeProperty(node,
+                                            "NEED_AUTH").getValue());
+                                        ComponentUtilities.getNodeProperty(node, "AUTH_MODE")
+                                                .setValue(needAuth?"BASIC":"NO_AUTH");//$NON-NLS-1$ //$NON-NLS-2$
+
+                                }
+                            }
+
+                        }));
             }
-            if (ComponentUtilities.getNodeProperty(node, "AUTH_MODE") == null ) {
-                    ComponentUtilities.addNodeProperty(node, "AUTH_MODE", "CLOSED_LIST");//$NON-NLS-1$ //$NON-NLS-2$
-                boolean needAuth = "true".equals(ComponentUtilities.getNodeProperty(node,
-                        "NEED_AUTH").getValue());
-                    ComponentUtilities.getNodeProperty(node, "AUTH_MODE")
-                            .setValue(needAuth?"BASIC":"NO_AUTH");//$NON-NLS-1$ //$NON-NLS-2$
 
-            }
-
-        };
-
-
-        boolean modified = false;
-        for (String name : compNames) {
-            IComponentFilter filter = new NameComponentFilter(name);
-
-            try {
-                modified |= ModifyComponentsAction.searchAndModify(item, processType, filter, Arrays.asList(action));
-            } catch (PersistenceException e) {
-                ExceptionHandler.process(e);
-                return ExecutionResult.FAILURE;
-            }
+            return ExecutionResult.SUCCESS_NO_ALERT;
+        } catch (Exception e) {
+            ExceptionHandler.process(e);
+            return ExecutionResult.FAILURE;
         }
-
-        return modified ? ExecutionResult.SUCCESS_NO_ALERT : ExecutionResult.NOTHING_TO_DO;
     }
 
     @Override public Date getOrder() {
