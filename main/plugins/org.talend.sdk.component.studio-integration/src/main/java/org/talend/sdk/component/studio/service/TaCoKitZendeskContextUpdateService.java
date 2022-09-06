df2a63a0d3bc58e2d13f4d1f2729e52aaf7abc65
jiezhang-tlnd <jie.zhang@talend.com>
2022-09-06 11:13:25 +0800

    add zendesk context

---
+++ main/plugins/org.talend.sdk.component.studio-integration/src/main/java/org/talend/sdk/component/studio/service/TaCoKitZendeskContextUpdateService.java
@@ -0,0 +1,68 @@
+// ============================================================================
+//
+// Copyright (C) 2006-2021 Talend Inc. - www.talend.com
+//
+// This source code is available under agreement available at
+// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
+//
+// You should have received a copy of the agreement
+// along with this program; if not, write to Talend SA
+// 9 rue Pages 92150 Suresnes, France
+//
+// ============================================================================
+package org.talend.sdk.component.studio.service;
+
+import org.apache.commons.lang3.StringUtils;
+import org.talend.commons.exception.ExceptionHandler;
+import org.talend.core.AbstractRepositoryContextUpdateService;
+import org.talend.core.model.metadata.builder.connection.Connection;
+import org.talend.sdk.component.server.front.model.ConfigTypeNode;
+import org.talend.sdk.component.studio.metadata.model.TaCoKitConfigurationModel;
+import org.talend.sdk.component.studio.metadata.model.TaCoKitConfigurationModel.ValueModel;
+
+public class TaCoKitZendeskContextUpdateService extends AbstractRepositoryContextUpdateService {
+
+	@Override
+	public boolean updateContextParameter(Connection conn, String oldValue, String newValue) {
+		boolean isModified = false;
+		if (conn.isContextMode()) {
+			TaCoKitConfigurationModel taCoKitConfigurationModel = new TaCoKitConfigurationModel(conn);
+			ValueModel authenticationLoginValue;
+			ValueModel serverUrlValue;
+			ValueModel apiTokenValue;
+			try {
+				authenticationLoginValue = taCoKitConfigurationModel.getValue("configuration.authenticationLogin");
+				serverUrlValue = taCoKitConfigurationModel.getValue("configuration.serverUrl");
+				apiTokenValue = taCoKitConfigurationModel.getValue("configuration.apiToken");
+				if (authenticationLoginValue != null
+						&& StringUtils.equals(oldValue, authenticationLoginValue.getValue())) {
+					taCoKitConfigurationModel.setValue("configuration.authenticationLogin",
+							authenticationLoginValue.getValue());
+					isModified = true;
+				} else if (serverUrlValue != null && StringUtils.equals(oldValue, serverUrlValue.getValue())) {
+					taCoKitConfigurationModel.setValue("configuration.serverUrl", serverUrlValue.getValue());
+					isModified = true;
+				} else if (apiTokenValue != null && StringUtils.equals(oldValue, apiTokenValue.getValue())) {
+					taCoKitConfigurationModel.setValue("configuration.apiToken", apiTokenValue.getValue());
+					isModified = true;
+				}
+			} catch (Exception e) {
+				ExceptionHandler.process(e);
+			}
+
+		}
+		return isModified;
+	}
+
+	@Override
+	public boolean accept(Connection connection) {
+		String name = null;
+		boolean isTacokit = TaCoKitConfigurationModel.isTacokit(connection);
+		if (isTacokit) {
+			TaCoKitConfigurationModel taCoKitConfigurationModel = new TaCoKitConfigurationModel(connection);
+			ConfigTypeNode configTypeNode = taCoKitConfigurationModel.getConfigTypeNode();
+			name = configTypeNode.getDisplayName();
+		}
+		return StringUtils.equals(name, "Zendesk");
+	}
+}
