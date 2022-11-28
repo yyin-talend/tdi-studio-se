8efda3a5d7aa7a12dd37639920239e022611602f
vyu-talend <vyu@talend.com>
2022-11-28 21:56:49 +0800

    fix(TPS-5390):fix codes.

--- main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddPubkeyAcceptedKeyTypesToFTPTPS5390.java
+++ main/plugins/org.talend.repository/src/main/java/org/talend/repository/model/migration/AddPubkeyAcceptedKeyTypesToFTPTPS5390.java
@@ -1,24 +1,8 @@
-// ============================================================================
-//
-// Copyright (C) 2006-2021 Talend Inc. - www.talend.com
-//
-// This source code is available under agreement available at
-// %InstallDIR%\features\org.talend.rcp.branding.%PRODUCTNAME%\%PRODUCTNAME%license.txt
-//
-// You should have received a copy of the agreement
-// along with this program; if not, write to Talend SA
-// 9 rue Pages 92150 Suresnes, France
-//
-// ============================================================================
 package org.talend.repository.model.migration;
 
-import java.util.Arrays;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.GregorianCalendar;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
+import org.eclipse.emf.common.util.EList;
 import org.talend.commons.ui.runtime.exception.ExceptionHandler;
 import org.talend.core.language.ECodeLanguage;
 import org.talend.core.model.components.ComponentUtilities;
@@ -28,20 +12,17 @@ import org.talend.core.model.components.filters.IComponentFilter;
 import org.talend.core.model.components.filters.NameComponentFilter;
 import org.talend.core.model.migration.AbstractJobMigrationTask;
 import org.talend.core.model.properties.Item;
-import org.talend.designer.core.model.utils.emf.talendfile.ElementValueType;
-import org.talend.designer.core.model.utils.emf.talendfile.NodeType;
-import org.talend.designer.core.model.utils.emf.talendfile.ProcessType;
-import org.talend.designer.core.model.utils.emf.talendfile.TalendFileFactory;
+import org.talend.designer.core.model.utils.emf.talendfile.*;
 
 
 public class AddPubkeyAcceptedKeyTypesToFTPTPS5390 extends AbstractJobMigrationTask {
-	
-	/*
-	 * (non-Javadoc)
-	 *
-	 * @see org.talend.migration.IMigrationTask#getOrder()
-	 */
-	public Date getOrder() {
+
+    /*
+     * (non-Javadoc)
+     *
+     * @see org.talend.migration.IMigrationTask#getOrder()
+     */
+    public Date getOrder() {
         GregorianCalendar gc = new GregorianCalendar(2022, 10, 24, 11, 0, 0);
         return gc.getTime();
     }
@@ -59,7 +40,7 @@ public class AddPubkeyAcceptedKeyTypesToFTPTPS5390 extends AbstractJobMigrationT
             return ExecutionResult.NOTHING_TO_DO;
         }
         String[] componentsName = new String[] { "tFTPConnection","tFTPDelete","tFTPFileExist","tFTPFileList",
-        		"tFTPFileProperties","tFTPGet","tFTPPut","tFTPRename","tFTPTruncate" };
+                "tFTPFileProperties","tFTPGet","tFTPPut","tFTPRename","tFTPTruncate" };
 
         try {
             boolean modified = false;
@@ -69,26 +50,37 @@ public class AddPubkeyAcceptedKeyTypesToFTPTPS5390 extends AbstractJobMigrationT
                         Arrays.<IComponentConversion> asList(new IComponentConversion() {
 
                             public void transform(NodeType node) {
-                            	String useExistConnection = ComponentUtilities.getNodePropertyValue(node, "USE_EXISTING_CONNECTION");
-                            	boolean ifConfigClient = "true".equals(ComponentUtilities.getNodePropertyValue(node, "CONFIG_CLIENT"));
-                            	boolean useSFTP = "true".equals(ComponentUtilities.getNodePropertyValue(node, "SFTP"));
-                            	boolean ifHasKey = (ComponentUtilities.getNodePropertyValue(node, "__CLIENT_PARAMETERS__")).contains("PubkeyAcceptedKeyTypes");
-                                if (useSFTP && (useExistConnection == null || "false". equals(useExistConnection)) 
-                                		&& ifConfigClient && !ifHasKey) {
-                                	
-                                	List<ElementValueType> values = new ArrayList<ElementValueType>();
+                                String useExistConnection = ComponentUtilities.getNodePropertyValue(node, "USE_EXISTING_CONNECTION");
+                                boolean ifConfigClient = "true".equals(ComponentUtilities.getNodePropertyValue(node, "CONFIG_CLIENT"));
+                                boolean useSFTP = "true".equals(ComponentUtilities.getNodePropertyValue(node, "SFTP"));
+
+                                final ElementParameterType client_parameters = ComponentUtilities.getNodeProperty(node, "CLIENT_PARAMETERS");
+                                boolean ifHasKey = false;
+                                final EList configurations = client_parameters.getElementValue();
+                                final Iterator iterator = configurations.iterator();
+                                while (iterator.hasNext()){
+                                    final ElementValueType next = (ElementValueType)iterator.next();
+                                    if(next.getValue().trim().equalsIgnoreCase("PubkeyAcceptedKeyTypes")){
+                                        ifHasKey = true;
+                                    }
+                                }
+                                
+                                if (useSFTP && (useExistConnection == null || "false". equals(useExistConnection))
+                                        && ifConfigClient && !ifHasKey) {
+
                                     TalendFileFactory fileFact = TalendFileFactory.eINSTANCE;
-                                	
-                                	ElementValueType elementKey = fileFact.createElementValueType();
+
+                                    ElementValueType elementKey = fileFact.createElementValueType();
                                     elementKey.setElementRef("PARAMETER"); //$NON-NLS-1$
                                     elementKey.setValue("\"PubkeyAcceptedKeyTypes\"");
-                                    values.add(elementKey);
-                                    
+                                    configurations.add(elementKey);
+
                                     ElementValueType elementValue = fileFact.createElementValueType();
                                     elementValue.setElementRef("VALUE"); //$NON-NLS-1$
                                     elementValue.setValue("\"ssh-rsa\"");
-                                    values.add(elementValue);
-                                    ComponentUtilities.setNodeProperty(node, "CLIENT_PARAMETERS", values);
+                                    configurations.add(elementValue);
+
+                                    ComponentUtilities.setNodeProperty(node, "CLIENT_PARAMETERS", configurations);
                                 }
                             }
 
