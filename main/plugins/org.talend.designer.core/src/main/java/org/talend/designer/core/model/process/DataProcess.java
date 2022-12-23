5fbd8cb46a390afa1a236eb65d8bd9ff9a2ffa37
Jane Ding <jding@talend.com>
2022-12-23 11:42:56 +0800

    Revert "fix(TUP-37227):Issue building when items names too long (#8225)" (#8339)
    This reverts commit 5e02e74cc4e238ffd4a5d280f0d4e22a3cf0df63.

--- main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/process/DataProcess.java
+++ main/plugins/org.talend.designer.core/src/main/java/org/talend/designer/core/model/process/DataProcess.java
@@ -3701,7 +3701,6 @@ public class DataProcess implements IGeneratingProcess {
      * @return
      */
     public void replaceNodeFromProviders(List<INode> graphicalNodeList) {
-        clearNodeReplaceProviderCache();
         List<INode> orginalList = new ArrayList<INode>(graphicalNodeList);
         IJobletProviderService jobletService = null;
         if (GlobalServiceRegister.getDefault().isServiceRegistered(IJobletProviderService.class)) {
@@ -3726,13 +3725,6 @@ public class DataProcess implements IGeneratingProcess {
         // such as ReplaceParallelization for Partition row in Joblet
         if (needReplaceForJoblet) {
             replaceNodeFromProviders(graphicalNodeList);
-        }
-        clearNodeReplaceProviderCache();
-    }
-
-    public void clearNodeReplaceProviderCache() {
-        if (IJobletProviderService.get() != null) {
-            IJobletProviderService.get().clearJobletProcessProviderCache();
         }
     }
 
